8b77f01f11fe09c98ecde7da65c60705fa8e5cc6
Varun Upadhyay <varun.u28@gmail.com>
2017-10-18 09:26:25 -0700

    Solved a binary tree problem

---
+++ Easy/Second Minimum Node in a binary tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.left == null && root.right == null) {
+            return -1;
+        }
+
+        int left = root.left.val;
+        int right = root.right.val;
+
+        // if value same as root value, need to find the next candidate
+        if (root.left.val == root.val) {
+            left = findSecondMinimumValue(root.left);
+        }
+        if (root.right.val == root.val) {
+            right = findSecondMinimumValue(root.right);
+        }
+
+        if (left != -1 && right != -1) {
+            return Math.min(left, right);
+        } else if (left != -1) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+}
