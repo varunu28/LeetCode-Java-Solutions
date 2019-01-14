b56fc45e0fa00c7a3275b50ce627acd2dc59d33f
varunu28 <varun.u28@gmail.com>
2019-01-14 04:24:42 -0800

    Added a solution

---
+++ Medium/Subarray Sums Divisible by K.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        for (int i = 0; i < A.length; i++) {
+            sum = (sum + A[i]) % K;
+            if (sum < 0) {
+                sum += K;
+            }
+            
+            count += map.getOrDefault(sum, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
+    }
+}
