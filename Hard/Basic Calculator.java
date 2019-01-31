a922150e7a1267b95a043e6adde87a3da3e166f4
varunu28 <varun.u28@gmail.com>
2019-01-31 07:01:41 -0800

    Added 2 solutions

---
+++ Hard/Basic Calculator.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        int num = 0;
+        int sign = 1;
+
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = num * 10 + (int) (c - '0');
+            }
+            else if (c == '+') {
+                res += sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if (c == '-') {
+                res += sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                sign = 1;
+                res = 0;
+            }
+            else if (c == ')') {
+                res += sign * num;
+                num = 0;
+                res *= stack.pop();
+                res += stack.pop();
+            }
+        }
+
+        if (num != 0) {
+            res += sign * num;
+        }
+
+        return res;
+    }
+}
