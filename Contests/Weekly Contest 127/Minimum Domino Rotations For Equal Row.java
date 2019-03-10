e78f49e2258b68816a5c4e4926ecdf37773132fa
varunu28 <varun.u28@gmail.com>
2019-03-10 14:20:31 -0700

    Updated solution for weekly contest 127

--- Contests/Weekly Contest 127/Minimum Domino Rotations For Equal Row.java
+++ Contests/Weekly Contest 127/Minimum Domino Rotations For Equal Row.java
@@ -1,35 +1,25 @@
 class Solution {
     public int minDominoRotations(int[] A, int[] B) {
-        Map<Integer, Set<Integer>> map1 = new HashMap<>();
-
+        int[] aCount = new int[7];
+        int[] bCount = new int[7];
+        int[] sameCount = new int[7];
+        
         for (int i = 0; i < A.length; i++) {
-            map1.computeIfAbsent(A[i], k -> new HashSet<>()).add(i);
-            map1.computeIfAbsent(B[i], k -> new HashSet<>()).add(i);
-        }
-
-        int min = Integer.MAX_VALUE;
-
-        for (int key : map1.keySet()) {
-            if (map1.get(key).size() >= A.length) {
-                int cnt1 = findCount(A, B, key);
-                int cnt2 = findCount(B, A, key);
-
-                min = Math.min(cnt1, min);
-                min = Math.min(cnt2, min);
+            if (A[i] == B[i]) {
+                sameCount[A[i]]++;
+            }
+            else {
+                aCount[A[i]]++;
+                bCount[B[i]]++;
             }
         }
-
-        return min == Integer.MAX_VALUE ? -1 : min;
-    }
-
-    private int findCount(int[] a, int[] b, int key) {
-        int cnt = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != key && b[i] == key) {
-                cnt++;
+        
+        for (int i = 1; i <= 6; i++) {
+            if (aCount[i] + bCount[i] + sameCount[i] == A.length) {
+                return sameCount[i] == A.length ? 0 : Math.min(aCount[i], bCount[i]);
             }
         }
-
-        return cnt;
+        
+        return -1;
     }
 }
