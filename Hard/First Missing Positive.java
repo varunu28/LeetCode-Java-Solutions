0b68c5e5d3c2ebcc62d4355345db1e4d2fe29f33
varunu28 <varun.u28@gmail.com>
2019-05-15 13:33:01 -0700

    Refactored 2 solutions

--- Hard/First Missing Positive.java
+++ Hard/First Missing Positive.java
@@ -1,29 +1,19 @@
 class Solution {
-    public int firstMissingPositive(int[] arr) {
-        if (arr.length == 0) {
-            return 1;
-        }
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
+    public int firstMissingPositive(int[] nums) {
         Set<Integer> set = new HashSet<>();
+        int max = 0;
 
-        for (int num : arr) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
+        for (int num : nums) {
             set.add(num);
+            max = Math.max(max, num);
         }
 
-        int temp = 1;
-
-        while (temp <= max) {
-            if (set.contains(temp) == false && temp > 0) {
-                return temp;
+        for (int i = 1; i <= max; i++) {
+            if (!set.contains(i)) {
+                return i;
             }
-
-            temp++;
         }
 
-        return max + 1 == 0 ? 1 : max+1;
+        return max + 1;
     }
 }
--- Medium/Course Schedule.java
+++ Medium/Course Schedule.java
@@ -1,33 +1,29 @@
 class Solution {
-    Set<Integer> dfs;
+    Set<Integer> set = new HashSet<>();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, Set<Integer>> map = new HashMap<>();
-        Set<Integer> numOfCoursesTaken = new HashSet<>();
-        
-        for (int[] prerequisite : prerequisites) {
-            map.computeIfAbsent(prerequisite[0], e -> new HashSet<>()).add(prerequisite[1]);
+        for (int[] prereq : prerequisites) {
+            map.computeIfAbsent(prereq[0], k -> new HashSet<>()).add(prereq[1]);
         }
         
-        for (int i=0; i<numCourses; i++) {
-            dfs = new HashSet<>();
+        for (int i = 0; i < numCourses; i++) {
+            set.clear();
             dfsHelper(map, i);
             
-            if (dfs.contains(i)) {
+            if (set.contains(i)) {
                 return false;
             }
-            
-            numOfCoursesTaken.add(i);
         }
         
-        return numOfCoursesTaken.size() == numCourses;
+        return true;
     }
     
     private void dfsHelper(Map<Integer, Set<Integer>> map, int courseNum) {
         Iterator<Integer> iterator = map.getOrDefault(courseNum, new HashSet<>()).iterator();
         while (iterator.hasNext()) {
             int course = iterator.next();
-            if (!dfs.contains(course)) {
-                dfs.add(course);
+            if (!set.contains(course)) {
+                set.add(course);
                 dfsHelper(map, course);
             }
         }
