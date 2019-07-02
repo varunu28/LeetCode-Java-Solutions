83f568b7597e6891de02f06b416099781899b8a0
varunu28 <varun.u28@gmail.com>
2019-07-01 17:20:34 -0700

    Added 1 solution & updated 1 solution

--- Medium/Simplify Path.java
+++ Medium/Simplify Path.java
@@ -1,32 +1,46 @@
 class Solution {
     public String simplifyPath(String path) {
-        String[] splitPaths = path.split("\\/");
-        Stack<String> paths = new Stack<>();
-        for (String p : splitPaths) {
-            if (p.length() == 0 || p.equals("/") || p.equals(" ") || p.equals(".")) {
-                continue;
-            }
-
-            if (p.equals("..")) {
-                if (!paths.isEmpty()) {
-                    paths.pop();
-                }
-            }
-            else {
-                paths.push(p.trim());
-            }
-        }
-
-        Stack<String> revPath = new Stack<>();
-        while (!paths.isEmpty()) {
-            revPath.push(paths.pop());
-        }
-
+        Stack<String> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
-        while (!revPath.isEmpty()) {
-            sb.append("/").append(revPath.pop());
+        int idx = 0;
+        int n = path.length();
+
+        while (idx < n) {
+            if (path.charAt(idx) != '/') {
+                sb.append(path.charAt(idx));
+                while (idx + 1 < n && path.charAt(idx + 1) != '/') {
+                    sb.append(path.charAt(idx + 1));
+                    idx++;
+                }
+
+                if (sb.toString().equals("..")) {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                }
+                else if (!sb.toString().equals(".")) {
+                    stack.push(sb.toString());
+                }
+
+                sb.setLength(0);
+            }
+
+            idx++;
         }
 
-        return sb.length() == 0 ? "/" : sb.toString();
+        List<String> list = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+
+        sb.append("/");
+        for (int i = list.size() - 1; i >= 0; i--) {
+            sb.append(list.get(i));
+            if (i != 0) {
+                sb.append("/");
+            }
+        }
+
+        return sb.toString();
     }
 }
