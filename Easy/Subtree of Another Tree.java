7e9c8e44d360978371a6ac12aaa348f0d01e7cf9
varunu28 <varun.u28@gmail.com>
2019-09-15 08:20:07 -0700

    Added 2 solutions & updated 1 solution

--- Easy/Subtree of Another Tree.java
+++ Easy/Subtree of Another Tree.java
@@ -9,24 +9,25 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        return traverseAndString(s).contains(traverseAndString(t));
-    }
-    
-    public String traverseAndString(TreeNode root) {
-        StringBuilder res = new StringBuilder();
-        traverse(root, res);
-        return res.toString();
-    }
-    
-    public void traverse(TreeNode root, StringBuilder res) {
-        
-        if (root == null) {
-            res.append("-");
-            return;
+        if (s == null) {
+            return false;
         }
-        
-        traverse(root.left, res);
-        traverse(root.right, res);
-        res.append(String.valueOf(root.val) + ",");
+        if (helper(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean helper(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if ((s == null && t != null) || (s != null && t == null)) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return helper(s.left, t.left) && helper(s.right, t.right);
     }
 }
