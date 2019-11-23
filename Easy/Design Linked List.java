d9b3f3866bc566c423cce1d8e776eadb85cdd077
Varun Upadhyay <varun.u28@gmail.com>
2019-11-22 21:09:42 -0800

    Update Design Linked List.java

--- Easy/Design Linked List.java
+++ Easy/Design Linked List.java
@@ -1,48 +1,105 @@
 class MyLinkedList {
 
     /** Initialize your data structure here. */
-    List<Integer> list;
+    Node head;
+    Node tail;
+    int count;
     public MyLinkedList() {
-        list = new ArrayList<>();
+        head = new Node(-1);
+        tail = new Node(-1);
+        head.next = tail;
+        tail.prev = head;
+        count = 0;
     }
     
     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
     public int get(int index) {
-        if (index >= list.size() || index < 0) {
+        if (index < 0 || index >= count) {
             return -1;
         }
-        
-        return list.get(index);
+        Node curr = head.next;
+        int idx = 0;
+        while (idx < index) {
+            idx++;
+            curr = curr.next;
+        }
+        return curr.val;
     }
     
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
     public void addAtHead(int val) {
-        list.add(0, val);
+        Node newNode = new Node(val);
+        newNode.next = head.next;
+        newNode.prev = head;
+        head.next.prev = newNode;
+        head.next = newNode;
+        count++;
     }
     
     /** Append a node of value val to the last element of the linked list. */
     public void addAtTail(int val) {
-        list.add(val);
+        Node newNode = new Node(val);
+        newNode.prev = tail.prev;
+        newNode.next = tail;
+        tail.prev.next = newNode;
+        tail.prev = newNode;
+        count++;
     }
     
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
     public void addAtIndex(int index, int val) {
-        if (index > list.size()) {
+        if (index < 0 || index > count) {
             return;
         }
-        if (index == list.size()) {
+        if (index == 0) {
+            addAtHead(val);
+        }
+        else if (index == count) {
             addAtTail(val);
         }
         else {
-            list.add(index, val);
+            int idx = 0;
+            Node curr = head;
+            while (idx < index) {
+                idx++;
+                curr = curr.next;
+            }
+            Node newNode = new Node(val);
+            newNode.prev = curr;
+            newNode.next = curr.next;
+            curr.next.prev = newNode;
+            curr.next = newNode;
+            count++;
         }
     }
     
     /** Delete the index-th node in the linked list, if the index is valid. */
     public void deleteAtIndex(int index) {
-        if (index >= 0 && index < list.size()) {
-            list.remove(index);
+        if (index < 0 || index >= count) {
+            return;
         }
+        int idx = 0;
+        Node curr = head;
+        while (idx < index) {
+            idx++;
+            curr = curr.next;
+        }
+        Node next = curr.next.next;
+        next.prev = curr;
+        curr.next = next;
+        count--;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node prev;
+    
+    public Node(int val) {
+        this.val = val;
+        next = null;
+        prev = null;
     }
 }
 
