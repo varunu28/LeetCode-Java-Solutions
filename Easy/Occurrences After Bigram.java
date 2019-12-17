18db5e4ff05fafac6d91b10287ef7bfd5ab10b8d
varunu28 <varun.u28@gmail.com>
2019-12-17 06:33:13 +0530

    Added Print Immutable Linked List in Reverse.java

--- Easy/Occurrences After Bigram.java
+++ Easy/Occurrences After Bigram.java
@@ -1,19 +1,12 @@
 class Solution {
-    public String[] findOcurrences(String text, String first, String second) {
-        String[] words = text.split("\\s+");
-        List<Integer> indexes = new ArrayList<>();
-        
-        for (int i = 0; i < words.length - 2; i++) {
-            if (words[i].equals(first) && words[i + 1].equals(second)) {
-                indexes.add(i + 2);
-            }
-        }
-        
-        String[] ans = new String[indexes.size()];
-        for (int i = 0; i < indexes.size(); i++) {
-            ans[i] = words[indexes.get(i)];
-        }
-        
-        return ans;
+  public String[] findOcurrences(String text, String first, String second) {
+    String[] words = text.split("\\s+"); 
+    List<String> list = new ArrayList<>();
+    for (int i = 0; i < words.length - 2; i++) {
+      if (words[i].equals(first) && words[i + 1].equals(second)) {
+        list.add(words[i + 2]);
+      }
     }
+    return list.toArray(new String[list.size()]);
+  }
 }
