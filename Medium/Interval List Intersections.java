e529f20d3ef0111bc1353de62c1dbdc6e90a57c0
varunu28 <varun.u28@gmail.com>
2020-01-01 14:07:35 -0800

    Added 1 solution & modified 2 solutions

--- Medium/Interval List Intersections.java
+++ Medium/Interval List Intersections.java
@@ -1,39 +1,22 @@
 class Solution {
-    public int[][] intervalIntersection(int[][] A, int[][] B) {
-        List<int[]> intersections = new ArrayList<>();
-        int idx1 = 0;
-        int idx2 = 0;
-    
-        while (idx1 < A.length && idx2 < B.length) {
-            if (A[idx1][0] > B[idx2][1]) {
-                idx2++;
-                continue;
-            }
-            
-            if (A[idx1][1] < B[idx2][0]) {
-                idx1++;
-                continue;
-            }
-            
-            int maxStart = Math.max(A[idx1][0], B[idx2][0]);
-            int minEnd = Math.min(A[idx1][1], B[idx2][1]);
-            
-            int[] interval = {maxStart, minEnd};
-            intersections.add(interval);
-            
-            if (A[idx1][1] > B[idx2][1]) {
-                idx2++;
-            }
-            else {
-                idx1++;
-            }
-        }
-        
-        int[][] ans = new int[intersections.size()][2];
-        for (int i = 0; i < intersections.size(); i++) {
-            ans[i] = intersections.get(i);
-        }
-        
-        return ans;
+  public int[][] intervalIntersection(int[][] A, int[][] B) {
+    List<int[]> list = new ArrayList<>();
+    int startA = 0;
+    int startB = 0;
+    while (startA < A.length && startB < B.length) {
+      int intervalStart = Math.max(A[startA][0], B[startB][0]);
+      int intervalEnd = Math.min(A[startA][1], B[startB][1]);
+      if (intervalStart <= intervalEnd) {
+        list.add(new int[]{intervalStart, intervalEnd});
+      }
+      if (A[startA][1] < B[startB][1]) {
+        startA++;
+      }
+      else {
+        startB++;
+      }
     }
+    int[][] ans = new int[list.size()][2];
+    return list.toArray(ans);
+  }
 }
