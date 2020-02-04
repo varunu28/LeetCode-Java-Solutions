dcbae7612b84f7d422cf8be9abd9f277e4b25dce
varunu28 <varun.u28@gmail.com>
2020-02-04 07:11:52 -0800

    Modified 2 solutions

--- Easy/Unique Morse Code Words.java
+++ Easy/Unique Morse Code Words.java
@@ -1,18 +1,17 @@
 class Solution {
-    public static int uniqueMorseRepresentations(String[] words) {
-        String[] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        Set<String> set = new HashSet<>();
-
-        for (String word : words) {
-            StringBuilder sb = new StringBuilder();
-
-            for (char c : word.toCharArray()) {
-                sb.append(codes[c-'a']);
-            }
-
-            set.add(sb.toString());
-        }
-
-        return set.size();
+  public int uniqueMorseRepresentations(String[] words) {
+    String[] morseCodes = {
+      ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",
+      ".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."
+    };
+    Set<String> set = new HashSet<>();
+    for (String word : words) {
+      StringBuilder sb = new StringBuilder();
+      for (char c : word.toCharArray()) {
+        sb.append(morseCodes[c - 'a']);
+      }
+      set.add(sb.toString());
     }
+    return set.size();
+  }
 }
