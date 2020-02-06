743bba84e9abddd849d0fde309a4532eea5b51fe
varunu28 <varun.u28@gmail.com>
2020-02-05 19:03:14 -0800

    Modified 3 solutions

--- Easy/Add Strings.java
+++ Easy/Add Strings.java
@@ -1,31 +1,27 @@
 class Solution {
-    public String addStrings(String num1, String num2) {
-        StringBuilder sb = new StringBuilder();
-        int idx1 = num1.length() - 1;
-        int idx2 = num2.length() - 1;
-        int carry = 0;
-        
-        while (idx1 >= 0 || idx2 >= 0) {
-            int temp = carry;
-            if (idx1 >= 0 && idx2 >= 0) {
-                temp += (num1.charAt(idx1) - '0') + (num2.charAt(idx2) - '0');
-            }
-            else if (idx1 >= 0 && idx2 < 0) {
-                temp += (num1.charAt(idx1) - '0');
-            }
-            else {
-                temp += (num2.charAt(idx2) - '0');
-            }
-            
-            carry = temp > 9 ? temp / 10 : 0;
-            temp = temp > 9 ? temp % 10 : temp;
-            sb.append(temp); 
-            idx1--;
-            idx2--;
-        }
-        
-        sb.append(carry > 0 ? carry : "");
-        
-        return sb.reverse().toString();
+  public String addStrings(String num1, String num2) {
+    StringBuilder sb = new StringBuilder();
+    int idx1 = num1.length() - 1;
+    int idx2 = num2.length() - 1;
+    int carry = 0;
+    while (idx1 >= 0 || idx2 >= 0) {
+      int temp = carry;
+      if (idx1 >= 0 && idx2 >= 0) {
+        temp += Character.getNumericValue(num1.charAt(idx1--)) + Character.getNumericValue(num2.charAt(idx2--));
+      }
+      else if (idx1 >= 0 && idx2 < 0) {
+        temp += Character.getNumericValue(num1.charAt(idx1--));
+      }
+      else {
+        temp += Character.getNumericValue(num2.charAt(idx2--));
+      }
+      carry = temp > 9 ? 1 : 0;
+      temp = temp > 9 ? temp % 10 : temp;
+      sb.append(temp);
     }
+    if (carry > 0) {
+      sb.append(carry);
+    }
+    return sb.reverse().toString();
+  }
 }
--- Easy/Palindrome Number.java
+++ Easy/Palindrome Number.java
@@ -1,15 +1,17 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        return x < 0 ? false : x == getReverseDigit(x);
+  public boolean isPalindrome(int x) {
+    if (x < 0) {
+      return false;
     }
-    
-    private int getReverseDigit(int x) {
-        int num = 0;
-        while (x > 0) {
-            num = num * 10 + x % 10;
-            x /= 10;
-        }
-        
-        return num;
+    return x - reverse(x) == 0;
+  }
+  
+  private int reverse(int n) {
+    int newNum = 0;
+    while (n > 0) {
+      newNum = newNum * 10 + n % 10;
+      n /= 10;
     }
+    return newNum;
+  }
 }
