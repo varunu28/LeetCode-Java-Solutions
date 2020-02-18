1032199b8a49b6d8626b900abb3918ba7f9bc124
varunu28 <varun.u28@gmail.com>
2020-02-18 04:44:54 -0800

    Added 2 solutions & modified 1 solution

--- Medium/Maximum Average Subtree.java
+++ Medium/Maximum Average Subtree.java
@@ -8,55 +8,24 @@
  * }
  */
 class Solution {
-    Map<TreeNode, Integer> sumMap = new HashMap<>();
-    Map<TreeNode, Integer> numNodesMap = new HashMap<>();
-    double maxAvg = -1.0;
-    public double maximumAverageSubtree(TreeNode root) {
-        dfs(root);
-        return maxAvg;
+  double maxAvg;
+  public double maximumAverageSubtree(TreeNode root) {
+    maxAvg = 0.0;
+    helper(root);
+    return maxAvg;
+  }
+  
+  private int[] helper(TreeNode root) {
+    int[] data = new int[2];
+    if (root == null) {
+      return data;
     }
-    
-    private void dfs(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        
-        int sum = getSum(root);
-        int nodes = getNumOfNodes(root);
-        
-        maxAvg = Math.max(maxAvg, ((double) (sum) / nodes));
-        
-        dfs(root.left);
-        dfs(root.right);
-    }
-    
-    private int getSum(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        
-        if (sumMap.containsKey(root)) {
-            return sumMap.get(root);
-        }
-        
-        int sum = root.val + getSum(root.left) + getSum(root.right);
-        sumMap.put(root, sum);
-        
-        return sum;
-    }
-    
-    private int getNumOfNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        
-        if (numNodesMap.containsKey(root)) {
-            return numNodesMap.get(root);
-        }
-        
-        int numNodes = 1 + getNumOfNodes(root.left) + getNumOfNodes(root.right);
-        numNodesMap.put(root, numNodes);
-        
-        return numNodes;
-    }
+    int[] dataLeft = helper(root.left);
+    int[] dataRight = helper(root.right);
+    data[0] += root.val + dataLeft[0] + dataRight[0];
+    data[1] += 1 + dataLeft[1] + dataRight[1];
+    double avg = ((double) data[0]) / data[1];
+    maxAvg = Math.max(maxAvg, avg);
+    return data;
+  }
 }
