edf31b0613f65259a179dfef4b6ea3acb84d1d6c
varunu28 <varun.u28@gmail.com>
2020-02-18 05:55:58 -0800

    Added 1 solution & modified 2 solutions

--- Easy/N-Repeated Element in Size 2N Array.java
+++ Easy/N-Repeated Element in Size 2N Array.java
@@ -1,15 +1,13 @@
 class Solution {
-    public int repeatedNTimes(int[] A) {
-        int maxCounter = A.length / 2;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : A) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            
-            if (map.get(num) == maxCounter) {
-                return num;
-            }
-        }
-        
-        return -1;
+  public int repeatedNTimes(int[] A) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int n = A.length / 2;
+    for (int num : A) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+      if (map.get(num) == n) {
+        return num;
+      }
     }
+    return -1;
+  }
 }
--- Easy/Subdomain Visit Count.java
+++ Easy/Subdomain Visit Count.java
@@ -1,31 +1,22 @@
 class Solution {
-    public List<String> subdomainVisits(String[] cpdomains) {
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (String s : cpdomains) {
-            String[] strs = s.split("\\s+");
-            int count = Integer.parseInt(strs[0]);
-            String[] domains = strs[1].split("\\.");
-            
-            String temp = domains[domains.length-1];
-            int domainLen = domains.length-1;
-            
-            while(domainLen >= 0) {
-                map.put(temp, map.getOrDefault(temp, 0) + count);
-                if (domainLen > 0) {
-                	temp = domains[domainLen-1] + "." + temp;
-                }
-                domainLen--;
-            }
-            
+  public List<String> subdomainVisits(String[] cpdomains) {
+    Map<String, Integer> map = new HashMap<>();
+    for(String domain : cpdomains) {
+      int count = Integer.parseInt(domain.split("\\s+")[0]);
+      String[] subDomains = domain.split("\\s+")[1].split("\\.");
+      StringBuilder sb = new StringBuilder();
+      for (int i = subDomains.length - 1; i >= 0; i--) {
+        if (sb.length() != 0) {
+          sb.insert(0, ".");
         }
-        
-        List<String> ans = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            String str = String.valueOf(entry.getValue()) + " " + entry.getKey();
-            ans.add(str);
-        }
-        
-        return ans;
+        sb.insert(0, subDomains[i]);
+        map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + count);
+      }
     }
+    List<String> list = new ArrayList<>();
+    for (String key : map.keySet()) {
+      list.add(map.get(key) + " " + key);
+    }
+    return list;
+  }
 }
