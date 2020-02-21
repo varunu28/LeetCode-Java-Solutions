69f20382ff256ef7b8390523c5c4c5af68dedad2
varunu28 <varun.u28@gmail.com>
2020-02-21 07:33:03 -0800

    Modified 4 solutions

--- Easy/Largest Unique Number.java
+++ Easy/Largest Unique Number.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int largestUniqueNumber(int[] A) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : A) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        int maxVal = Integer.MIN_VALUE;
-        for (Integer key : map.keySet()) {
-            if (map.get(key) == 1) {
-                maxVal = Math.max(maxVal, key);
-            }
-        }
-
-        return maxVal == Integer.MIN_VALUE ? -1 : maxVal;
+  public int largestUniqueNumber(int[] A) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : A) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
     }
+    int max = -1;
+    for (Integer key : map.keySet()) {
+      if (map.get(key) == 1) {
+        max = Math.max(key, max);
+      }
+    }
+    return max;
+  }
 }
