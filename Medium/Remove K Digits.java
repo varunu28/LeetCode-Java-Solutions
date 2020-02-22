cadc923106964f58d9d7a7892c23e5ec1d8ff96c
varunu28 <varun.u28@gmail.com>
2020-02-22 15:48:19 -0800

    Added 2 solutions & modified 2 solutions

--- Medium/Remove K Digits.java
+++ Medium/Remove K Digits.java
@@ -1,35 +1,25 @@
 class Solution {
-    public String removeKdigits(String num, int k) {
-        Stack<Integer> stack = new Stack<>();
-        int idx = 0;
-        int n = num.length();
-        while (idx < n) {
-            while (k > 0 && !stack.isEmpty()) {
-                if (stack.peek() <= Character.getNumericValue(num.charAt(idx))) {
-                    break;
-                }
-                stack.pop();
-                k--;
-            }
-            
-            stack.push(Character.getNumericValue(num.charAt(idx++)));
-        }
-        
-        while (k-- > 0) {
-            stack.pop();
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        
-        String ans = sb.reverse().toString();
-        idx = -1;
-        while (idx + 1 < ans.length() && ans.charAt(idx + 1) == '0') {
-            idx++;
-        }
-        
-        return ans.substring(idx + 1).length() == 0 ? "0" : ans.substring(idx + 1);
+  public String removeKdigits(String num, int k) {
+    LinkedList<Character> stack = new LinkedList<>();
+    for (char c : num.toCharArray()) {
+      while (!stack.isEmpty() && k > 0 && stack.peekLast() > c) {
+        stack.removeLast();
+        k--;
+      } 
+      stack.addLast(c);
     }
+    while (k-- > 0) {
+      stack.removeLast();
+    }
+    StringBuilder sb = new StringBuilder();
+    boolean currZero = true;
+    for (char c : stack) {
+      if (currZero && c == '0') {
+        continue;
+      }
+      currZero = false;
+      sb.append(c);
+    }
+    return sb.length() == 0 ? "0" : sb.toString();
+  }
 }
--- Medium/Sort Colors.java
+++ Medium/Sort Colors.java
@@ -1,23 +1,25 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int idx1 = 0;
-        int idx2 = nums.length - 1;
-        int idx = 0;
-        
-        while (idx <= idx2) {
-            if (nums[idx] == 0) {
-                nums[idx] = nums[idx1];
-                nums[idx1] = 0;
-                idx1++;
-            }
-            if (nums[idx] == 2) {
-                nums[idx] = nums[idx2];
-                nums[idx2] = 2;
-                idx2--;
-                idx--;
-            }
-            
-            idx++;
-        }
+  public void sortColors(int[] nums) {
+    int zeroIdx = 0;
+    int curr = 0;
+    int n = nums.length - 1;
+    int twoIdx = n;
+    while (curr <= twoIdx) {
+      if (nums[curr] == 0) {
+        swap(nums, curr++, zeroIdx++);
+      }
+      else if (nums[curr] == 2) {
+        swap(nums, curr, twoIdx--);
+      }
+      else {
+        curr++;
+      }
     }
+  }
+  
+  private void swap(int[] nums, int idx1, int idx2) {
+    int temp = nums[idx1];
+    nums[idx1] = nums[idx2];
+    nums[idx2] = temp;
+  }
 }
