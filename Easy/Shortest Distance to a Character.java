ab92d04b38051820076f0d16e0ba76a5b032b51c
varunu28 <varun.u28@gmail.com>
2020-02-28 09:49:54 -0800

    Modified 2 solutions

--- Easy/Shortest Distance to a Character.java
+++ Easy/Shortest Distance to a Character.java
@@ -1,36 +1,20 @@
 class Solution {
-    public static int[] shortestToChar(String S, char C) {
-    	int[] ans = new int[S.length()];
-    	
-    	int count = (int) S.chars().filter(ch -> ch == C).count();
-    	
-    	int[] Cindexes = new int[count];
-    	int[] indexes = new int[S.length()];
-    	
-    	int i = 0;
-    	
-    	for (int j=0; j<S.length(); j++) {
-    		if (S.charAt(j) == C) {
-    			Cindexes[i++] = j; 
-    		}
-    		
-    		indexes[j] = j;
-    	}
-    	
-    	for (int j=0; j<S.length(); j++) {
-    		ans[j] = findMin(Cindexes, indexes[j]);
-    	}
-    	
-    	return ans;
+  public int[] shortestToChar(String S, char C) {
+    int[] ans = new int[S.length()];
+    int prev = Integer.MIN_VALUE / 2;
+    for (int i = 0; i < S.length(); i++) {
+      if (S.charAt(i) == C) {
+        prev = i;
+      }
+      ans[i] = i - prev;
     }
-
-	private static int findMin(int[] cindexes, int i) {
-		int minDist = Integer.MAX_VALUE;
-		
-		for (int index : cindexes) {
-			minDist = Math.min(minDist, Math.abs(index - i));
-		}
-		
-		return minDist;
-	}
+    prev = Integer.MAX_VALUE / 2;
+    for (int i = S.length() - 1; i >= 0; i--) {
+      if (S.charAt(i) == C) {
+        prev = i;
+      }
+      ans[i] = Math.min(ans[i], prev - i);
+    }
+    return ans;
+  }
 }
--- Medium/Subarray Sum Equals K.java
+++ Medium/Subarray Sum Equals K.java
@@ -1,16 +1,14 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        int count = 0;
-        int currentSum = 0;
-        
-        for (int num : nums) {
-            currentSum += num;
-            count += map.getOrDefault(currentSum - k, 0);
-            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
-        }
-        
-        return count;
+  public int subarraySum(int[] nums, int k) {
+    int sum = 0;
+    int count = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      count += map.getOrDefault(sum - k, 0);
+      map.put(sum, map.getOrDefault(sum, 0) + 1);
     }
+    return count;
+  }
 }
