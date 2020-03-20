520ce8f3075d5ba88f1fc07fd091aa6ce3155335
varunu28 <varun.u28@gmail.com>
2020-03-19 19:19:12 -0700

    Modified Ransom Note.java

--- Easy/Ransom Note.java
+++ Easy/Ransom Note.java
@@ -1,35 +1,17 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> noteMap = new HashMap<>();
-        Map<Character, Integer> magMap = new HashMap<>();
-        
-        for(char c: ransomNote.toCharArray()) {
-            if (noteMap.containsKey(c)) {
-                noteMap.put(c, noteMap.get(c) + 1);
-            }
-            else {
-                noteMap.put(c, 1);
-            }
-        }
-        
-        for(char c: magazine.toCharArray()) {
-            if (magMap.containsKey(c)) {
-                magMap.put(c, magMap.get(c) + 1);
-            }
-            else {
-                magMap.put(c, 1);
-            }
-        }
-        
-        for (Map.Entry<Character, Integer> entry: noteMap.entrySet()) {
-            if (magMap.containsKey(entry.getKey()) && magMap.get(entry.getKey()) >= entry.getValue()) {
-                continue;
-            }
-            else {
-                return false;
-            }
-        }
-        
-        return true;
+  public boolean canConstruct(String ransomNote, String magazine) {
+    int[] counter = new int[26];
+    for (char c : ransomNote.toCharArray()) {
+      counter[c - 'a']++;
     }
+    for (char c : magazine.toCharArray()) {
+      counter[c - 'a']--;
+    }
+    for (int i = 0; i < 26; i++) {
+      if (counter[i] > 0) {
+        return false;
+      }
+    }
+    return true;
+  } 
 }
