dda4aced4899411fae07322783100cc8731dc8a3
varunu28 <varun.u28@gmail.com>
2020-03-21 15:56:03 -0700

    Added 3 solutions & modified 2 solutions

--- Medium/Word Break.java
+++ Medium/Word Break.java
@@ -1,19 +1,23 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordDictSet=new HashSet(wordDict);
-
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
-                }
+  public boolean wordBreak(String s, List<String> wordDict) {
+    Set<String> set = new HashSet<>(wordDict);
+    Queue<Integer> queue = new LinkedList<>();
+    int[] visited = new int[s.length()];
+    queue.add(0);
+    while (!queue.isEmpty()) {
+      int start = queue.remove();
+      if (visited[start] == 0) {
+        for (int end = start + 1; end <= s.length(); end++) {
+          if (set.contains(s.substring(start, end))) {
+            queue.add(end);
+            if (end == s.length()) {
+              return true;
             }
+          }
         }
-
-        return dp[s.length()];
+        visited[start] = 1;
+      }
     }
+    return false;
+  }   
 }
