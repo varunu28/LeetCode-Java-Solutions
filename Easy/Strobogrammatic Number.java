4eec0631c99e7cfdeaaa27ba1df06c71e1b68c3e
varunu28 <varun.u28@gmail.com>
2020-03-25 20:08:49 -0700

    Modified 2 solutions

--- Easy/Strobogrammatic Number.java
+++ Easy/Strobogrammatic Number.java
@@ -1,18 +1,17 @@
 class Solution {
-    public boolean isStrobogrammatic(String num) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : num.toCharArray()) {
-            if (c == '0' || c == '1' || c == '8') {
-                sb.append(c);
-            }
-            else if (c == '6' || c == '9') {
-                sb.append(c == '6' ? '9' : '6');
-            }
-            else {
-                return false;
-            }
-        }
-        
-        return sb.reverse().toString().equals(num);
+  public boolean isStrobogrammatic(String num) {
+    StringBuilder sb = new StringBuilder();
+    for (char c : num.toCharArray()) {
+      if (c == '0' || c == '1' || c == '8') {
+        sb.append(c);
+      }
+      else if (c == '6' || c == '9') {
+        sb.append(c == '6' ? '9' : '6');
+      }
+      else {
+        return false;
+      }
     }
+    return sb.reverse().toString().equals(num);
+  }
 }
--- Medium/Remove Duplicates from Sorted List II.java
+++ Medium/Remove Duplicates from Sorted List II.java
@@ -8,29 +8,26 @@
  */
 class Solution {
   public ListNode deleteDuplicates(ListNode head) {
-    ListNode dummy = new ListNode(0);
-    dummy.next = head;
-    ListNode prev = dummy;
     ListNode curr = head;
+    ListNode prev = null;
     while (curr != null) {
-      int val = curr.val;
-      int count = 0;
-      ListNode temp = curr;
-      // Track the number of occurrences of value of current node
-      while (temp != null && temp.val == val) {
-        temp = temp.next;
-        count++;
-      }
-      // Update prev.next to the temp which can be a null or node with different value
-      if (count > 1) {
-        prev.next = temp;
-        curr = temp;
+      if (curr.next != null && curr.val == curr.next.val) {
+        int currVal = curr.val;
+        while (curr != null && curr.val == currVal) {
+          curr = curr.next;
+        }
+        if (prev == null) {
+          head = curr;
+        }
+        else {
+          prev.next = curr;
+        }
       }
       else {
         prev = curr;
         curr = curr.next;
       }
     }
-    return dummy.next;
+    return head;
   }
 }
