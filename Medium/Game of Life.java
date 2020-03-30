d825ceb19a0dba5eb82a36ae5494af7b2116255b
varunu28 <varun.u28@gmail.com>
2020-03-30 14:52:56 -0700

    Added 1 solution & modified 1 solution

--- Medium/Game of Life.java
+++ Medium/Game of Life.java
@@ -1,43 +1,39 @@
 class Solution {
-    public void gameOfLife(int[][] board) {
-        for (int i=0; i<board.length; i++) {
-            for (int j=0; j<board[i].length; j++) {
-                int aliveNeighbours = getLiveCount(board, i, j);
-                boolean selfAlive = isAlive(board, i, j);
-                if (selfAlive) {
-                	if (aliveNeighbours < 2 || aliveNeighbours > 3) {
-                		board[i][j] = selfAlive ? 2 : 0;
-                	}
-                }
-                else {
-            		if (aliveNeighbours == 3) {
-            			board[i][j] = selfAlive ? 1 : 3;
-            		}
-            	}
-            }
+  int[][] dirs = {{1, 0}, {0, 1}, {1, 1}, {-1, -1}, {-1, 0}, {0, -1}, {1, -1}, {-1, 1}};
+  public void gameOfLife(int[][] board) {
+    int rows = board.length;
+    int cols = board[0].length;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        int count = getLiveNeighbourCount(board, i, j);
+        if (board[i][j] == 1) {
+          if (count < 2 || count > 3) {
+            board[i][j] = -1;
+          }
         }
-        
-        for (int i=0; i<board.length; i++) {
-            for (int j=0; j<board[i].length; j++) {
-                board[i][j] = board[i][j] == 1 || board[i][j] == 3 ? 1 : 0;
-            }
+        else {
+          if (count == 3) {
+            board[i][j] = 2;
+          }
         }
+      }
     }
-
-    private boolean isAlive(int[][] board, int row, int col) {
-		return row >= 0 && row < board.length && col >= 0 && col < board[0].length && (board[row][col] == 1 || board[row][col] == 2);
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        board[i][j] = board[i][j] > 0 ? 1 : 0;
+      }
     }
-
-    private int getLiveCount(int[][] board, int i, int j) {
-        int liveCount = 0;
-
-        int[][] possibleCoordinates = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
-        for (int[] coordinate : possibleCoordinates) {
-        	if (isAlive(board, i + coordinate[0], j + coordinate[1])) {
-        		liveCount++;
-        	}
-        }
-
-        return liveCount;
+  }
+  
+  private int getLiveNeighbourCount(int[][] board, int x, int y) {
+    int count = 0;
+    for (int[] dir : dirs) {
+      int newX = x + dir[0];
+      int newY = y + dir[1];
+      if (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length && Math.abs(board[newX][newY]) == 1) {
+        count++;
+      }
     }
+    return count;
+  }
 }
