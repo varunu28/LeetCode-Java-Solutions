76cda3a9f2ba76f5a8366e7c435dd55e5ca4fc74
varunu28 <varun.u28@gmail.com>
2020-04-01 18:04:48 -0700

    Modified 2 solutions

--- Easy/Sentence Similarity.java
+++ Easy/Sentence Similarity.java
@@ -1,23 +1,21 @@
 class Solution {
-    public boolean areSentencesSimilar(String[] words1, String[] words2, String[][] pairs) {
-        if (words1.length != words2.length) return false;
-        
-        Map<String, HashSet<String>> map = new HashMap<>();
-        
-        for (String[] p: pairs) {
-            if (!map.containsKey(p[0])) {
-                map.put(p[0], new HashSet<>());
-            }
-            map.get(p[0]).add(p[1]);
-        }
-        
-        for (int i=0;i<words1.length;i++) {
-            if (!((words1[i].equals(words2[i])) || (map.containsKey(words1[i]) && map.get(words1[i]).contains(words2[i])) ||
-                 (map.containsKey(words2[i]) && map.get(words2[i]).contains(words1[i])))) {
-                return false;
-            }
-        }
-        
-        return true;
+  public boolean areSentencesSimilar(String[] words1, String[] words2, List<List<String>> pairs) {
+    if (words1.length != words2.length) {
+      return false;
     }
+    Set<String> set = new HashSet<>();
+    for (List<String> pair : pairs) {
+      String word1 = pair.get(0);
+      String word2 = pair.get(1);
+      set.add(word1 + "|" + word2);
+    }
+    for (int i = 0; i < words1.length; i++) {
+      String word1 = words1[i];
+      String word2 = words2[i];
+      if (!(word1.equals(word2) || set.contains(word1 + "|" + word2) || set.contains(word2 + "|" + word1))) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
--- Easy/Single-Row Keyboard.java
+++ Easy/Single-Row Keyboard.java
@@ -1,17 +1,16 @@
 class Solution {
-    public int calculateTime(String keyboard, String word) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < keyboard.length(); i++) {
-            map.put(keyboard.charAt(i), i);
-        }
-        
-        int currPos = 0;
-        int totalTime = 0;
-        for (char c : word.toCharArray()) {
-            totalTime += Math.abs(currPos - map.get(c));
-            currPos = map.get(c);
-        }
-        
-        return totalTime;
+  public int calculateTime(String keyboard, String word) {
+    Map<Character, Integer> map = new HashMap<>();
+    int pos = 0;
+    for (char c : keyboard.toCharArray()) {
+      map.put(c, pos++);
     }
+    int total = 0;
+    int currPos = 0;
+    for (char c : word.toCharArray()) {
+      total += Math.abs(currPos - map.get(c));
+      currPos = map.get(c);
+    }
+    return total;
+  }
 }
