3959ebe2ff7df731e53605b327ca8c8ecde5d172
varunu28 <varun.u28@gmail.com>
2020-04-06 13:20:15 -0700

    Modified 3 solutions

--- Medium/Coin Change.java
+++ Medium/Coin Change.java
@@ -1,18 +1,29 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
-        int[] cache = new int[amount + 1];
-        Arrays.fill(cache, max);
-        cache[0] = 0;
-
-        for (int i=1; i<=amount; i++) {
-            for (int j=0; j<coins.length; j++) {
-                if (coins[j] <= i) {
-                    cache[i] = Math.min(cache[i], cache[i - coins[j]] + 1);
-                }
-            }
-        }
-
-        return cache[amount] > amount ? -1 : cache[amount];
+  public int coinChange(int[] coins, int amount) {
+    if (amount < 1) {
+      return 0;
     }
+    return coinChange(coins, amount, new int[amount]);
+  }
+  
+  private int coinChange(int[] coins, int amount, int[] memo) {
+    if (amount < 0) {
+      return -1;
+    }
+    if (amount == 0) {
+      return 0;
+    }
+    if (memo[amount - 1] != 0) {
+      return memo[amount - 1];
+    }
+    int min = Integer.MAX_VALUE;
+    for (int coin : coins) {
+      int res = coinChange(coins, amount - coin, memo);
+      if (res >= 0 && res < min) {
+        min = 1 + res;
+      }
+    }
+    memo[amount - 1] = min == Integer.MAX_VALUE ? -1 : min;
+    return memo[amount - 1];
+  }
 }
--- Medium/Rectangle Area.java
+++ Medium/Rectangle Area.java
@@ -1,19 +1,15 @@
 class Solution {
-    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int area = ((D - B) * (C - A)) + ((H - F) * (G- E));
-        area -= overlap(A, C, E, G) * overlap(B, D, F, H);
-        return area;
+  public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    int areaOfSqrA = (C - A) * (D - B);
+    int areaOfSqrB = (G - E) * (H - F);
+    int left = Math.max(A, E);
+    int right = Math.min(G, C);
+    int bottom = Math.max(F, B);
+    int top = Math.min(D, H);
+    int overlap = 0;
+    if(right > left && top > bottom) {
+      overlap = (right - left) * (top - bottom);
     }
-    
-    private int overlap(int x1, int x2, int x3, int x4) {
-        if (x2 <= x3) {
-            return 0;
-        }
-        
-        if (x1 >= x4) {
-            return 0;
-        }
-        
-        return Math.min(x2, x4) - Math.max(x1, x3);
-    }
+    return areaOfSqrA + areaOfSqrB - overlap;
+  }
 }
