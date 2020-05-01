a11895f48f152c5d31219569df87282fda6373dd
varunu28 <varun.u28@gmail.com>
2020-05-01 10:51:57 -0700

    Modified First Bad Version.java

--- Easy/First Bad Version.java
+++ Easy/First Bad Version.java
@@ -2,18 +2,21 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        int start = 1;
-        int end = n;
-        while (start < end) {
-            int mid = start + (end - start)/2;
-            if (!isBadVersion(mid)) {
-                start = mid + 1;
-            }
-            else {
-                end = mid;
-            }
+  public int firstBadVersion(int n) {
+    int start = 1;
+    int end = n;
+    while (start < end) {
+      int mid = start + (end - start) / 2;
+      if (isBadVersion(mid)) {
+        if (mid == 1 || isBadVersion(mid - 1) == false) {
+          return mid;
         }
-        return start;
+        end = mid - 1;
+      }
+      else {
+        start = mid + 1;
+      }
     }
+    return start;
+  } 
 }
