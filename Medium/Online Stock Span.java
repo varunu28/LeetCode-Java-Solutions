811d67652c023396600225b3c4d2506d110b488a
varunu28 <varun.u28@gmail.com>
2020-05-22 08:57:27 -0500

    Modified Online Stock Span.java

--- Medium/Online Stock Span.java
+++ Medium/Online Stock Span.java
@@ -1,24 +1,21 @@
 class StockSpanner {
-    Stack<Integer> prices;
-    Stack<Integer> weights;
+  Stack<Integer> stack;
+  Stack<Integer> occurences;
+  public StockSpanner() {
+    stack = new Stack<>();
+    occurences = new Stack<>();
+  }
 
-    public StockSpanner() {
-        prices = new Stack();
-        weights = new Stack();
+  public int next(int price) {
+    int count = 1;
+    while (!stack.isEmpty() && stack.peek() <= price) {
+      stack.pop();
+      count += occurences.pop();
     }
-
-    public int next(int price) {
-        int w = 1;
-        while (!prices.isEmpty() && prices.peek() <= price) {
-            prices.pop();
-            w += weights.pop();
-        }
-
-        prices.push(price);
-        weights.push(w);
-        
-        return w;
-    }
+    stack.push(price);
+    occurences.push(count);
+    return count;
+  }
 }
 
 /**
