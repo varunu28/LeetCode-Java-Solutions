3be318840c5289681ae6abaa874f9c4ca1cb1c36
varunu28 <varun.u28@gmail.com>
2020-05-26 18:19:03 -0500

    Modified Two City Scheduling.java

--- Easy/Two City Scheduling.java
+++ Easy/Two City Scheduling.java
@@ -1,22 +1,15 @@
 class Solution {
-    public int twoCitySchedCost(int[][] costs) {
-        Arrays.sort(costs, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o1[1] - (o2[0] - o2[1]);
-            }
-        });
-
-        int total = 0;
-        for (int i = 0; i < costs.length; i++) {
-            if (i < costs.length / 2) {
-                total += costs[i][0];
-            }
-            else {
-                total += costs[i][1];
-            }
-        }
-        
-        return total;
+  public int twoCitySchedCost(int[][] costs) {
+    Arrays.sort(costs, new Comparator<int[]>(){
+      public int compare(int[] o1, int[] o2) {
+       return o1[0] - o1[1] - (o2[0] - o2[1]); 
+      }
+    });
+    int total = 0;
+    int n = costs.length / 2;
+    for (int i = 0; i < n; i++) {
+      total += costs[i][0] + costs[i + n][1];
     }
+    return total;
+  }
 }
