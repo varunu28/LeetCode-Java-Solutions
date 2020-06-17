70c17a7b5d79ee3ccfb28624cbbf16de6c7e7344
varunu28 <varun.u28@gmail.com>
2020-06-16 20:08:27 -0500

    Modified Validate IP Address.java

--- Medium/Validate IP Address.java
+++ Medium/Validate IP Address.java
@@ -1,74 +1,97 @@
 class Solution {
-    Set<Character> hexCharSet = new HashSet<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F'));
-    
-    public String validIPAddress(String IP) {
-        if (IP.indexOf('.') != -1) {
-            return isValidIPv4(IP) ? "IPv4" : "Neither";
-        }
-        else if (IP.indexOf(':') != -1) {
-            return isValidIPv6(IP) ? "IPv6" : "Neither";
-        }
-        
-        return "Neither";
+  public String validIPAddress(String IP) {
+    if (IP.indexOf(':') != -1) {
+      return validateIPv6(IP);
     }
-    
-    private boolean isValidIPv4(String ip) {
-        if (ip.startsWith(".") || ip.endsWith(".")) {
-            return false;
-        }
-        String[] components = ip.split("\\.");
-        if (components.length != 4) {
-            return false;
-        }
-        
-        for (String component : components) {
-            try {
-                int value = Integer.parseInt(component);
-                if (value < 0 || value > 255) {
-                    return false;
-                }
-                
-                if (value > 0 && component.startsWith("0")) {
-                    return false;
-                }
-                
-                if (value == 0 && component.length() > 1) {
-                    return false;
-                }
-            } catch(Exception e) {
-                return false;
-            }
-        }
-        
-        return true;
+    else if (IP.indexOf('.') != -1) {
+      return validateIPv4(IP);
     }
-    
-    private boolean isValidIPv6(String ip) {
-        if (ip.startsWith(":") || ip.endsWith(":")) {
-            return false;
-        }
-        
-        String[] components = ip.split(":");
-        if (components.length != 8) {
-            return false;
-        }
-        
-        for (String component : components) {
-            if (component.length() > 4 || component.length() == 0) {
-                return false;
-            }
-            for (char c : component.toCharArray()) {
-                if (Character.isLetter(c) && hexCharSet.contains(c)) {
-                    continue;
-                }
-                else if (Character.isDigit(c)) {
-                    continue;
-                }
-                else {
-                    return false;
-                }
-            }
-        }
-        return true;
+    else {
+      return "Neither";
     }
+  }
+  
+  private String validateIPv6(String ip) {
+    int count = 0;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < ip.length(); i++) {
+      if (ip.charAt(i) == ':') {
+        if (!isValidIPv6(sb.toString())) {
+          return "Neither";
+        }
+        sb = new StringBuilder();
+        count++;
+      }
+      else {
+        sb.append(ip.charAt(i));
+      }
+      if (i == ip.length() - 1) {
+        if (!isValidIPv6(sb.toString())) {
+          return "Neither";
+        }
+      }
+    }
+    return count == 7 ? "IPv6" : "Neither";
+  }
+  
+  private boolean isValidIPv6(String s) {
+    String lower = "abcdef";
+    String upper = "ABCDEF";
+    if (s.length() == 0 || s.length() > 4) {
+      return false;
+    }
+    for (char c : s.toCharArray()) {
+      if (!Character.isDigit(c)) {
+        if (lower.indexOf(c) == -1 && upper.indexOf(c) == -1) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  private String validateIPv4(String ip) {
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+    for (int i = 0; i < ip.length(); i++) {
+      if (ip.charAt(i) == '.') {
+        if (!isValidIPv4(sb.toString())) {
+          return "Neither";
+        }
+        sb = new StringBuilder();
+        count++;
+      }
+      else {
+        if (!Character.isDigit(ip.charAt(i))) {
+          return "Neither";
+        }
+        sb.append(ip.charAt(i));
+      }
+      if (i == ip.length() - 1) {
+        if (!isValidIPv4(sb.toString())) {
+          return "Neither";
+        }
+      }
+    }
+    return count == 3 ? "IPv4" : "Neither";
+  }
+  
+  private boolean isValidIPv4(String s) {
+    if (s.length() == 0) {
+      return false;
+    }
+    try {
+      int val = Integer.parseInt(s);
+      if (val < 0 || val > 255) {
+        return false;
+      }
+      if ((s.startsWith("0") && val != 0) || (val == 0 && s.length() > 1)) {
+        return false;
+      }
+    }
+    catch (NumberFormatException e) {
+      return false;
+    }
+    return true;
+  }
 }
