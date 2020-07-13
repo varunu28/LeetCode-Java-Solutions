704b98bb443bc9b0f2942ecbe5e7023329697146
varunu28 <varun.u28@gmail.com>
2020-07-13 10:51:30 -0500

    Modified 2 solutions

--- Easy/Same Tree.java
+++ Easy/Same Tree.java
@@ -4,19 +4,26 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        
-        if (p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        }
-        return false;   
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
     }
+    if (p == null || q == null) {
+      return false;
+    }
+    if (p.val != q.val) {
+      return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
 }
