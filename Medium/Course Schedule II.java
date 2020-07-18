62c43f5671306bf2a24dc66479a5f6c6e9e35bdb
varunu28 <varun.u28@gmail.com>
2020-07-18 17:19:27 -0500

    Modified 3 solutions

--- Medium/Course Schedule II.java
+++ Medium/Course Schedule II.java
@@ -1,47 +1,29 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int[] prerequisite : prerequisites) {
-            map.computeIfAbsent(prerequisite[0], k -> new ArrayList<>()).add(prerequisite[1]);
-        }
-
-        boolean[] registered = new boolean[numCourses];
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (!isDeadlock(i, map, stack, registered, new boolean[numCourses])) {
-                return new int[]{};
-            }
-        }
-
-        int[] ans = new int[numCourses];
-        int idx = numCourses - 1;
-        while (!stack.isEmpty()) {
-            ans[idx--] = stack.pop();
-        }
-
-        return ans;
+  public int[] findOrder(int numCourses, int[][] prerequisites) {
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    int[] prereqCount = new int[numCourses];
+    for (int[] prerequisite : prerequisites) {
+      prereqCount[prerequisite[0]]++;
+      map.computeIfAbsent(prerequisite[1], k -> new ArrayList<>()).add(prerequisite[0]);
     }
-
-    private boolean isDeadlock(int c1, Map<Integer, List<Integer>> map, Stack<Integer> stack, boolean[] registered, boolean[] visited) {
-        if (registered[c1]) {
-            return true;
-        }
-
-        if (visited[c1]) {
-            return false;
-        }
-
-        visited[c1] = true;
-        for (Integer prereq : map.getOrDefault(c1, new ArrayList<>())) {
-            if (!isDeadlock(prereq, map, stack, registered, visited)) {
-                return false;
-            }
-        }
-
-        registered[c1] = true;
-        stack.push(c1);
-
-        return true;
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 0; i < numCourses; i++) {
+      if (prereqCount[i] == 0) {
+        queue.add(i);
+      }
     }
-
+    int[] ans = new int[numCourses];
+    int idx = 0;
+    while (!queue.isEmpty()) {
+      int removed = queue.remove();
+      for (Integer dependentCourse : map.getOrDefault(removed, new ArrayList<>())) {
+        prereqCount[dependentCourse]--;
+        if (prereqCount[dependentCourse] == 0) {
+          queue.add(dependentCourse);
+        }
+      }
+      ans[idx++] = removed;
+    }
+    return idx == numCourses ? ans : new int[]{};
+  }
 }
