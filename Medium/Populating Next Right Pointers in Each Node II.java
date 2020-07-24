9ff12b20b9f8c961e95c6ed7907a1ddd4f869a9c
varunu28 <varun.u28@gmail.com>
2020-07-24 08:47:29 -0500

    Added 1 solution & modified 3 solutions

--- Medium/Populating Next Right Pointers in Each Node II.java
+++ Medium/Populating Next Right Pointers in Each Node II.java
@@ -7,8 +7,12 @@ class Node {
     public Node next;
 
     public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
 
-    public Node(int _val,Node _left,Node _right,Node _next) {
+    public Node(int _val, Node _left, Node _right, Node _next) {
         val = _val;
         left = _left;
         right = _right;
@@ -16,33 +20,30 @@ class Node {
     }
 };
 */
+
 class Solution {
-    public Node connect(Node root) {
-        Queue<Node> queue = new LinkedList<>();
-        
-        queue.add(root);
-        queue.add(null);
-        
-        while (queue.peek() != null) {
-            int size = queue.size() - 1;
-            
-            while (size-- > 0) {
-                Node removed = queue.remove();
-                removed.next = queue.peek();
-                
-                if (removed.left != null) {
-                    queue.add(removed.left);
-                }
-                
-                if (removed.right != null) {
-                    queue.add(removed.right);
-                }
-            }
-            
-            queue.remove();
-            queue.add(null);
-        }
-        
-        return root;
+  public Node connect(Node root) {
+    if (root == null) {
+      return null;
     }
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+    queue.add(null);
+    while (!queue.isEmpty() && queue.peek() != null) {
+      int size = queue.size() - 1;
+      while (size-- > 0) {
+        Node removed = queue.remove();
+        removed.next = queue.peek();
+        if (removed.left != null) {
+          queue.add(removed.left);
+        }
+        if (removed.right != null) {
+          queue.add(removed.right);
+        }
+      }
+      queue.remove();
+      queue.add(null);
+    }
+    return root;
+  }
 }
