a8a390d7cd4ede8e9db91931e6501bc7553a7c06
varunu28 <varun.u28@gmail.com>
2020-07-25 14:32:06 -0500

    Added 1 solution & modified 1 solution

--- Hard/Find Minimum in Rotated Sorted Array II.java
+++ Hard/Find Minimum in Rotated Sorted Array II.java
@@ -1,27 +1,19 @@
 class Solution {
-    public int findMin(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
-        int minVal = Integer.MAX_VALUE;
-
-        while (start <= end) {
-            int mid = (start + end)/2;
-            if (((mid > 0 ? nums[mid] < nums[mid-1] : true) && (mid < nums.length-1 ? nums[mid] < nums[mid+1] : true))) {
-                return nums[mid];
-            }
-            else if (nums[mid] < nums[end]) {
-                end = mid;
-            }
-            else if (nums[mid] > nums[end]){
-                start = mid+1;
-            }
-            else {
-                end--;
-            }
-
-            minVal = Math.min(nums[mid], minVal);
-        }
-
-        return minVal;
+  public int findMin(int[] nums) {
+    int start = 0;
+    int end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] < nums[end]) {
+        end = mid;
+      }
+      else if (nums[mid] > nums[end]) {
+        start = mid + 1;
+      }
+      else {
+        end--;
+      }
     }
+    return nums[start];
+  }
 }
