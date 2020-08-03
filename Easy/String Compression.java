d05fd27d5e5a912c964a11d63ade006c3742b702
varunu28 <varun.u28@gmail.com>
2020-08-02 19:47:42 -0500

    Modified 2 solutions

--- Easy/String Compression.java
+++ Easy/String Compression.java
@@ -1,27 +1,23 @@
 class Solution {
-    public int compress(char[] chars) {
-        
-        int ans = 0;
-        int ind = 0;
-        
-        while (ind < chars.length) {
-            char currChar = chars[ind];
-            int count = 0;
-            
-            while(ind < chars.length && chars[ind] == currChar) {
-                ind++;
-                count++;
-            }
-            
-            chars[ans++] = currChar;
-            
-            if (count != 1) {
-                for (char c : Integer.toString(count).toCharArray()) {
-                    chars[ans++] = c;
-                }
-            }
+  public int compress(char[] chars) {
+    int start = 0;
+    int end = 0;
+    int n = chars.length;
+    while (end < n) {
+      char c = chars[end];
+      int count = 0;
+      while (end < n && chars[end] == c) {
+        end++;
+        count++;
+      }
+      chars[start++] = c;
+      if (count > 1) {
+        String countStr = String.valueOf(count);
+        for (char countC : countStr.toCharArray()) {
+          chars[start++] = countC;
         }
-        
-        return ans;
+      }
     }
+    return start;
+  }
 }
