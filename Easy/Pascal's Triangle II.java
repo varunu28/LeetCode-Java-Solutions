96abc5a037f922392a8b627d060fd85540bfdfb1
varunu28 <varun.u28@gmail.com>
2020-08-12 11:06:58 -0500

    Added 1 solution & modified 1 solution

--- Easy/Pascal's Triangle II.java
+++ Easy/Pascal's Triangle II.java
@@ -1,14 +1,21 @@
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        List<Integer> res = new ArrayList<Integer>();
-        
-        for(int i = 0;i<rowIndex+1;i++) {
-            res.add(1);
-            for(int j=i-1;j>0;j--) {
-                res.set(j, res.get(j-1) + res.get(j));
-            }
+  public List<Integer> getRow(int rowIndex) {
+    int count = 0;
+    List<Integer> list = new ArrayList<>();
+    List<Integer> prev = new ArrayList<>();
+    for (int i = 0; i <= rowIndex; i++) {
+      prev = list;
+      List<Integer> temp = new ArrayList<>();
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          temp.add(1);
         }
-        
-        return res;
+        else {
+          temp.add(prev.get(j - 1) + prev.get(j));
+        }
+      }
+      list = temp;
     }
+    return list;
+  }   
 }
