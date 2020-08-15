ce9e131b4fa0b27cf6d55bc6546ea7f72ed0eb10
varunu28 <varun.u28@gmail.com>
2020-08-15 15:48:34 -0500

    Added 1 solution & modified 1 solution

--- Medium/Non Overlapping Intervals.java
+++ Medium/Non Overlapping Intervals.java
@@ -1,35 +1,17 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 class Solution {
-    public static int eraseOverlapIntervals(Interval[] intervals) {
-        if (intervals.length == 0) {
-            return 0;
-        }
-        
-        Arrays.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval o1, Interval o2) {
-                return o1.end - o2.end;
-            }
-        });
-
-        int count = 1;
-        int end = intervals[0].end;
-
-        for (int i=1; i<intervals.length; i++) {
-            if (intervals[i].start >= end) {
-                end = intervals[i].end;
-                count++;
-            }
-        }
-
-        return intervals.length - count;
+  public int eraseOverlapIntervals(int[][] intervals) {
+    if (intervals.length == 0) {
+      return 0;
     }
+    Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+    int notRemoved = 1;
+    int end = intervals[0][1];
+    for (int i = 1; i < intervals.length; i++) {
+      if (intervals[i][0] >= end) {
+        notRemoved++;
+        end = intervals[i][1];
+      }
+    }
+    return intervals.length - notRemoved;
+  }
 }
