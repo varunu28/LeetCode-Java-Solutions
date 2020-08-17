826d5cbd61d1a0c0834343f69a6abce850590f05
varunu28 <varun.u28@gmail.com>
2020-08-17 18:53:28 -0500

    Modified 2 solutions

--- Easy/Number of Steps to Reduce a Number to Zero.java
+++ Easy/Number of Steps to Reduce a Number to Zero.java
@@ -2,13 +2,8 @@ class Solution {
   public int numberOfSteps (int num) {
     int count = 0;
     while (num > 0) {
-      if (num % 2 == 0) {
-        num /= 2;
-      }
-      else {
-        num--;
-      }
       count++;
+      num = num % 2 == 0 ? num / 2 : num - 1;
     }
     return count;
   }
--- Medium/Print Immutable Linked List in Reverse.java
+++ Medium/Print Immutable Linked List in Reverse.java
@@ -8,43 +8,11 @@
  */
 
 class Solution {
-  Stack<ImmutableListNode> stack;
-  ImmutableListNode lastPopped;
   public void printLinkedListInReverse(ImmutableListNode head) {
-    stack = new Stack<>();
-    lastPopped = null;
-    int lengthSquareRoot = (int) Math.log(getLength(head));
-    ImmutableListNode curr = head;
-    int count = 0;
-    while (curr != null) {
-      if (count % lengthSquareRoot == 0) {
-        stack.push(curr);
-      }
-      curr = curr.getNext();
-      count++;
-    }
-    while (!stack.isEmpty()) {
-      ImmutableListNode popped = stack.pop();
-      printNode(popped);
-      lastPopped = popped;
-    }
-  }
-  
-  private void printNode(ImmutableListNode node) {
-    if (node == lastPopped) {
+    if (head == null) {
       return;
     }
-    printNode(node.getNext());
-    node.printValue();
-  }
-  
-  private int getLength(ImmutableListNode node) {
-    int count = 0;
-    while (node != null) {
-      node = node.getNext();
-      count++;
-    }
-    return count;
+    printLinkedListInReverse(head.getNext());
+    head.printValue();
   }
 }
-
