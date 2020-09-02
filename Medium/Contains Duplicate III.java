e68a3dfc0422c23670b924e2cf3bfab1662c8dc6
varunu28 <varun.u28@gmail.com>
2020-09-02 09:49:02 -0500

    Modified Contains Duplicate III.java

--- Medium/Contains Duplicate III.java
+++ Medium/Contains Duplicate III.java
@@ -1,23 +1,18 @@
 class Solution {
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        TreeSet<Long> treeSet = new TreeSet<>();
-        for (int i=0; i<nums.length; i++) {
-            long num = nums[i];
-            Long floor = treeSet.floor(num + t);
-            Long ceil = treeSet.ceiling(num - t);
-
-            if (floor != null && floor >= nums[i] ||
-                ceil != null && ceil <= nums[i]) {
-                return true;
-            }
-
-            treeSet.add(num);
-
-            if (i >= k) {
-                treeSet.remove((long) nums[i - k]);
-            }
-        }
-
-        return false;
+  public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+    TreeSet<Long> set = new TreeSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      long num = nums[i];
+      Long floor = set.floor(num + t);
+      Long ceil = set.ceiling(num - t);
+      if ((floor != null && floor >= num) || (ceil != null && ceil <= num)) {
+        return true;
+      }
+      set.add(num);
+      if (i >= k) {
+        set.remove((long) nums[i - k]);
+      }
     }
+    return false;
+  }
 }
