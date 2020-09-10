b618289969c59a7954a7b087de7b7ad2bd2ba92e
varunu28 <varun.u28@gmail.com>
2020-09-10 07:51:37 -0500

    Modified 2 solutions

--- Easy/Bulls and Cows.java
+++ Easy/Bulls and Cows.java
@@ -1,25 +1,20 @@
 class Solution {
   public String getHint(String secret, String guess) {
     int[] counter = new int[10];
-    int cows = 0;
+    for (Character c : secret.toCharArray()) {
+      counter[Character.getNumericValue(c)]++;
+    }
     int bulls = 0;
+    int cows = 0;
     for (int i = 0; i < guess.length(); i++) {
-      int s = Character.getNumericValue(secret.charAt(i));
-      int g = Character.getNumericValue(guess.charAt(i));
-      if (s == g) {
+      if (guess.charAt(i) == secret.charAt(i)) {
         bulls++;
       }
-      else {
-        if (counter[s] < 0) {
-          cows++;
-        }
-        if (counter[g] > 0) {
-          cows++;
-        }
-        counter[s]++;
-        counter[g]--;
+      if (counter[Character.getNumericValue(guess.charAt(i))] > 0) {
+        counter[Character.getNumericValue(guess.charAt(i))]--;
+        cows++;
       }
     }
-    return new StringBuilder().append(bulls).append("A").append(cows).append("B").toString();
+    return bulls + "A" + (cows - bulls) + "B";
   }
 }
