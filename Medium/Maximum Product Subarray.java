21f0fdb0e6f32248f212b2bc2b616ccf0dac7c4e
varunu28 <varun.u28@gmail.com>
2020-09-11 09:18:52 -0500

    Modified Maximum Product Subarray.java

--- Medium/Maximum Product Subarray.java
+++ Medium/Maximum Product Subarray.java
@@ -1,17 +1,14 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int max = nums[0];
-        int min = nums[0];
-        int maxGlobal = max;
-        
-        for (int i = 1; i < nums.length; i++) {
-            int temp = max;
-            max = Math.max(Math.max(nums[i] * temp, nums[i] * min), nums[i]);
-            min = Math.min(Math.min(nums[i] * temp, nums[i] * min), nums[i]);
-            
-            maxGlobal = Math.max(max, maxGlobal);
-        }
-        
-        return maxGlobal;
+  public int maxProduct(int[] nums) {
+    int currMax = nums[0];
+    int currMin = nums[0];
+    int maxProd = currMax;
+    for (int i = 1; i < nums.length; i++) {
+      int temp = currMax;
+      currMax = Math.max(Math.max(temp * nums[i], currMin * nums[i]), nums[i]);
+      currMin = Math.min(Math.min(temp * nums[i], currMin * nums[i]), nums[i]);
+      maxProd = Math.max(maxProd, currMax);
     }
+    return maxProd;
+  }
 }
