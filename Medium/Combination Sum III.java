581d81428a695719b96920975e9ff18b72d1d859
varunu28 <varun.u28@gmail.com>
2020-09-12 08:04:45 -0500

    Modified Combination Sum III.java

--- Medium/Combination Sum III.java
+++ Medium/Combination Sum III.java
@@ -1,30 +1,21 @@
 class Solution {
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-
-        helper(list, temp, k, n, 0, 1);
-
-        return list;
+  public List<List<Integer>> combinationSum3(int k, int n) {
+    List<List<Integer>> lists = new ArrayList<>();
+    helper(n, lists, new ArrayList<>(), 0, k, 1);
+    return lists;
+  }
+  
+  private void helper(int n, List<List<Integer>> lists, List<Integer> list, int currSum, int k, int currNum) {
+    if (list.size() == k) {
+      if (currSum == n) {
+        lists.add(new ArrayList<>(list));
+      }
+      return;
     }
-
-    private void helper(List<List<Integer>> list, List<Integer> temp, int k, int n, int sum, int start) {
-        if (sum == n && temp.size() == k) {
-            list.add(new ArrayList<>(temp));
-            return;
-        }
-
-        for (int i=start; i<=9; i++) {
-            // Choose
-            temp.add(i);
-            sum += i;
-
-            // Explore
-            helper(list, temp, k, n, sum, i+1);
-
-            // Un-choose
-            sum -= temp.get(temp.size() - 1);
-            temp.remove(temp.size() - 1);
-        }
+    for (int i = currNum; i <= 9; i++) {
+      list.add(i);
+      helper(n, lists, list, currSum + i, k, i + 1);
+      list.remove(list.size() - 1);
     }
+  }
 }
