1b450779f4d6a98e586683525b15310578547eb2
varunu28 <varun.u28@gmail.com>
2020-09-20 15:15:14 -0500

    Modified Unique Paths III.java

--- Hard/Unique Paths III.java
+++ Hard/Unique Paths III.java
@@ -1,66 +1,48 @@
 class Solution {
-    int numOfPaths;
-
-    public int uniquePathsIII(int[][] grid) {
-        numOfPaths = 0;
-        if (grid.length == 0 || grid[0].length == 0) {
-            return numOfPaths;
-        }
-
-        int startX = -1;
-        int startY = -1;
-        int emptyObstacleCount = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) {
-                    startX = i;
-                    startY = j;
-                } else if (grid[i][j] == 0) {
-                    emptyObstacleCount++;
-                }
-            }
-        }
-
-        dfsHelper(grid, startX, startY, emptyObstacleCount, 0, new boolean[grid.length][grid[0].length]);
-
-        return numOfPaths;
+  int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  public int uniquePathsIII(int[][] grid) {
+    if (grid.length == 0 || grid[0].length == 0) {
+      return 0;
     }
-
-    private void dfsHelper(int[][] grid,
-                           int startX,
-                           int startY,
-                           int emptyObstacleCount,
-                           int currObstacleCount,
-                           boolean[][] visited) {
-        if (startX < 0 ||
-                startX >= grid.length ||
-                startY < 0 ||
-                startY >= grid[0].length ||
-                visited[startX][startY] ||
-                grid[startX][startY] == -1) {
-            return;
+    int rows = grid.length;
+    int cols = grid[0].length;
+    int startX = 0;
+    int startY = 0;
+    int numOfEmptySquare = 0;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (grid[i][j] == 1) {
+          startX = i;
+          startY = j;
         }
-
-        if (grid[startX][startY] == 2) {
-            if (currObstacleCount == emptyObstacleCount) {
-                numOfPaths++;
-            }
-
-            return;
+        if (grid[i][j] == 0) {
+          numOfEmptySquare++;
         }
-
-        visited[startX][startY] = true;
-
-        dfsHelper(grid, startX + 1, startY, emptyObstacleCount,
-                grid[startX][startY] == 0 ? currObstacleCount + 1 : currObstacleCount, visited);
-        dfsHelper(grid, startX - 1, startY, emptyObstacleCount,
-                grid[startX][startY] == 0 ? currObstacleCount + 1 : currObstacleCount, visited);
-        dfsHelper(grid, startX, startY + 1, emptyObstacleCount,
-                grid[startX][startY] == 0 ? currObstacleCount + 1 : currObstacleCount, visited);
-        dfsHelper(grid, startX, startY - 1, emptyObstacleCount,
-                grid[startX][startY] == 0 ? currObstacleCount + 1 : currObstacleCount, visited);
-
-        visited[startX][startY] = false;
+      }
     }
+    int[] count = {0};
+    helper(grid, startX, startY, numOfEmptySquare, 0, new boolean[rows][cols], count);
+    return count[0];
+  }
+  
+  private void helper(
+    int[][] grid, int startX, int startY, int numOfEmptySquare,
+    int currEmptySquare, boolean[][] visited, int[] count
+  ) {
+    if (startX < 0 || startX >= grid.length || startY < 0 || startY >= grid[0].length || grid[startX][startY] == -1 || visited[startX][startY]) {
+      return;
+    }
+    if (grid[startX][startY] == 2) {
+      if (currEmptySquare == numOfEmptySquare) {
+        count[0]++;
+      }
+      return;
+    }
+    visited[startX][startY] = true;
+    for (int[] dir : dirs) {
+      helper(grid, startX + dir[0], startY + dir[1], numOfEmptySquare, (grid[startX][startY] == 0 ? currEmptySquare + 1 : currEmptySquare), visited, count);
+    }
+    visited[startX][startY] = false;
+  }
 }
+
