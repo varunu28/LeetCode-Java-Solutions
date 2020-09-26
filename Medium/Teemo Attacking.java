d47b95b20c1891c636e26bdfa87229501b54f854
varunu28 <varun.u28@gmail.com>
2020-09-26 07:11:42 -0500

    Modified Teemo Attacking.java

--- Medium/Teemo Attacking.java
+++ Medium/Teemo Attacking.java
@@ -1,16 +1,22 @@
 class Solution {
-    public int findPoisonedDuration(int[] timeSeries, int duration) {
-        if (duration == 0 || timeSeries.length == 0) {
-            return 0;
-        }
-
-        int totalTime = 0;
-        int beginTime = timeSeries[0];
-        for (int time : timeSeries) {
-            totalTime += (time < beginTime + duration ? time - beginTime : duration);
-            beginTime = time;
-        }
-        
-        return totalTime + duration;
+  public int findPoisonedDuration(int[] timeSeries, int duration) {
+    if (timeSeries.length == 0 || duration <= 0) {
+      return 0;
     }
+    int totalTimePoisened = 0;
+    int currPoisenedTime = 0;
+    int n = timeSeries.length;
+    for (int i = 0; i < n; i++) {
+      if (currPoisenedTime > 0) {
+        if (currPoisenedTime <= timeSeries[i]) {
+          totalTimePoisened += duration;
+        }
+        else {
+          totalTimePoisened += timeSeries[i] - timeSeries[i - 1];
+        }
+      }
+      currPoisenedTime = timeSeries[i] + duration;
+    }
+    return totalTimePoisened + (currPoisenedTime - timeSeries[n - 1]);
+  }
 }
