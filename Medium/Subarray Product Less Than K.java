9c70403ece790ffee037edfa6b071dc96abaad9a
varunu28 <varun.u28@gmail.com>
2020-09-28 06:28:45 -0700

    Modified Subarray Product Less Than K.java

--- Medium/Subarray Product Less Than K.java
+++ Medium/Subarray Product Less Than K.java
@@ -1,15 +1,18 @@
 class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k<2) return 0;
-        int result = 0;
-        int product = 1;
-        for (int i = 0, right = 0; right < nums.length; right++) {
-            product *= nums[right];
-            while (i < nums.length && product >= k) {
-                product /= nums[i++];
-            }
-            result += right - i + 1;
-        }
-        return result;
+  public int numSubarrayProductLessThanK(int[] nums, int k) {
+    if (k <= 1) {
+      return 0;
     }
+    int prod = 1;
+    int ans = 0;
+    int left = 0;
+    for (int right = 0; right < nums.length; right++) {
+      prod *= nums[right];
+      while (prod >= k) {
+        prod /= nums[left++];
+      }
+      ans += right - left + 1;
+    }
+    return ans;
+  }
 }
