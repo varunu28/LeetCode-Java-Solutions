7d00750e424d8201fcfe3695ec877b889b44079b
varunu28 <varun.u28@gmail.com>
2020-10-02 12:42:48 -0700

    Modified Combination Sum.java

--- Medium/Combination Sum.java
+++ Medium/Combination Sum.java
@@ -1,24 +1,24 @@
 class Solution {
-  List<List<Integer>> list;
   public List<List<Integer>> combinationSum(int[] candidates, int target) {
-    list = new ArrayList<>();
-    helper(candidates, 0, target, 0, new ArrayList<>());
-    return list;
+    List<List<Integer>> ans = new ArrayList<>();
+    helper(candidates, 0, ans, new ArrayList<>(), target);
+    return ans;
   }
   
-  private void helper(int[] candidates, int idx, int target, int currSum, List<Integer> temp) {
-    if (currSum == target) {
-      list.add(new ArrayList<>(temp));
-    }
-    else if (idx == candidates.length || currSum > target) {
+  private void helper(
+    int[] candidates, int idx, List<List<Integer>> ans, List<Integer> list, int target
+  ) {
+    if (target == 0) {
+      ans.add(new ArrayList<>(list));
       return;
     }
-    else {
-      for (int i = idx; i < candidates.length; i++) {
-        temp.add(candidates[i]);
-        helper(candidates, i, target, currSum + candidates[i], temp);
-        temp.remove(temp.size() - 1);
-      }
+    if (target < 0 || idx == candidates.length) {
+      return;
+    }
+    for (int i = idx; i < candidates.length; i++) {
+      list.add(candidates[i]);
+      helper(candidates, i, ans, list, target - candidates[i]);
+      list.remove(list.size() - 1);
     }
   }
 }
