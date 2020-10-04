e73e86c4799d8eb7ba3f4a5e4a242a486de40df4
varunu28 <varun.u28@gmail.com>
2020-10-04 07:30:34 -0700

    Modified Remove Covered Intervals.java

--- Medium/Remove Covered Intervals.java
+++ Medium/Remove Covered Intervals.java
@@ -1,25 +1,24 @@
 class Solution {
   public int removeCoveredIntervals(int[][] intervals) {
-    Arrays.sort(intervals, new Comparator<int[]>(){
-      public int compare(int[] i1, int[] i2) {
-        int c = i2[1] - i1[1];
-        if (c != 0) {
-          return c;
-        }
-        return i1[0] - i2[0];
+    Arrays.sort(intervals, (o1, o2) -> {
+      int c = o1[0] - o2[0];
+      if (c != 0) {
+        return c;
       }
+      return o2[1] - o1[1];
     });
-    int idx = 0;
     int count = 0;
-    while (idx < intervals.length) {
-      int[] currInterval = intervals[idx];
+    int idx = 0;
+    int n = intervals.length;
+    while (idx < n) {
+      int start = intervals[idx][0];
+      int end = intervals[idx][1];
       idx++;
-      while (idx < intervals.length && intervals[idx][0] >= currInterval[0] && intervals[idx][1] <= currInterval[1]
-      ) {
-        count++;
+      while (idx < n && intervals[idx][1] <= end) {
         idx++;
       }
+      count++;
     }
-    return intervals.length - count;
+    return count;
   }
 }
