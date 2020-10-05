9a592faf4758b4dcf60d5c44224235850624e50f
varunu28 <varun.u28@gmail.com>
2020-10-05 05:51:12 -0700

    Modified Complement of Base 10 Integer.java

--- Easy/Complement of Base 10 Integer.java
+++ Easy/Complement of Base 10 Integer.java
@@ -1,32 +1,15 @@
 class Solution {
-    public int bitwiseComplement(int N) {
-        String binaryString = getInvertedBinaryString(N);
-        return getDecimalVal(binaryString);
+  public int bitwiseComplement(int N) {
+    if (N == 0) {
+      return 1;
     }
-    
-    private String getInvertedBinaryString(int n) {
-        if (n == 0) {
-            return "1";
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        while (n > 0) {
-            sb.append(n % 2 == 0 ? 1 : 0);
-            n /= 2;
-        }
-        
-        return sb.reverse().toString();
+    int todo = N;
+    int bit = 1;
+    while (todo != 0) {
+      N = N ^ bit;
+      bit = bit << 1;
+      todo = todo >> 1;
     }
-    
-    private int getDecimalVal(String s) {
-        int pow = 0;
-        int sum = 0;
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            sum += Math.pow(2, pow) * Character.getNumericValue(s.charAt(i));
-            pow++;
-        }
-        
-        return sum;
-    }
+    return N;
+  }
 }
