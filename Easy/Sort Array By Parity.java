e46541fab57858f6db366f33403c04f2fd09dde2
varunu28 <varun.u28@gmail.com>
2020-10-06 13:56:41 -0700

    Modified Sort Array By Parity.java

--- Easy/Sort Array By Parity.java
+++ Easy/Sort Array By Parity.java
@@ -1,18 +1,15 @@
 class Solution {
   public int[] sortArrayByParity(int[] A) {
-    int start = 0;
-    int end = A.length - 1;
-    while (start < end) {
-      if (A[start] % 2 > A[end] % 2) {
-        int temp = A[start];
-        A[start] = A[end];
-        A[end] = temp;
-      }
-      if (A[start] % 2 == 0) {
-        start++;
-      }
-      if (A[end] % 2 != 0) {
-        end--;
+    int evenIdx = 0;
+    int oddIdx = A.length - 1;
+    while (evenIdx < oddIdx) {
+      if (A[evenIdx] % 2 == 0) {
+        evenIdx++;
+      } 
+      else {
+        int temp = A[evenIdx];
+        A[evenIdx] = A[oddIdx];
+        A[oddIdx--] = temp;
       }
     }
     return A;
