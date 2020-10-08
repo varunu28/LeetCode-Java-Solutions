1ea1960d9a9c6286de8f77d8993c192e53b17774
varunu28 <varun.u28@gmail.com>
2020-10-08 06:09:13 -0700

    Modified 2 solutions

--- Easy/Binary Search.java
+++ Easy/Binary Search.java
@@ -1,19 +1,17 @@
 class Solution {
   public int search(int[] nums, int target) {
-    return binarySearchImpl(nums, 0, nums.length - 1, target);
-  }
-  
-  private int binarySearchImpl(int[] nums, int start, int end, int target) {
-    while (start <= end) {
-      int mid = (start + end) / 2;
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
       if (nums[mid] == target) {
         return mid;
       }
       else if (nums[mid] > target) {
-        end = mid - 1;
+        right = mid - 1;
       }
       else {
-        start = mid + 1;
+        left = mid + 1;
       }
     }
     return -1;
--- Easy/Two Sum III - Data Structure Design.java
+++ Easy/Two Sum III - Data Structure Design.java
@@ -1,39 +1,27 @@
 class TwoSum {
 
-    /** Initialize your data structure here. */
-    List<Integer> list;
-    Map<Integer, Integer> map;
-    public TwoSum() {
-        list = new ArrayList<>();
-        map = new HashMap<>();
-    }
-    
-    /** Add the number to an internal data structure.. */
-    public void add(int number) {
-        if (map.containsKey(number)) {
-            map.put(number, map.get(number) + 1);
+  /** Initialize your data structure here. */
+  Map<Integer, Integer> map;
+  public TwoSum() {
+    map = new HashMap<>();
+  }
+
+  /** Add the number to an internal data structure.. */
+  public void add(int number) {
+    map.put(number, map.getOrDefault(number, 0) + 1);
+  }
+
+  /** Find if there exists any pair of numbers which sum is equal to the value. */
+  public boolean find(int value) {
+    for (Integer key : map.keySet()) {
+      if (map.containsKey(value - key)) {
+        if (value - key != key || (value - key == key && map.get(key) > 1)) {
+          return true;
         }
-        else {
-            map.put(number, 1);
-            list.add(number);
-        }
+      }
     }
-    
-    /** Find if there exists any pair of numbers which sum is equal to the value. */
-    public boolean find(int value) {
-        for (int num : list) {
-            int diff = value - num;
-            if (diff != num && map.containsKey(diff)) {
-                return true;
-            }
-            
-            if (diff == num && map.get(diff) > 1) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
+    return false;
+  }
 }
 
 /**
