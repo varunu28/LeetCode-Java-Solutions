886c9a1c655ea262aaca2a116910569c9ed06acc
varunu28 <varun.u28@gmail.com>
2020-10-13 05:17:36 -0700

    Modified Sort List.java

--- Medium/Sort List.java
+++ Medium/Sort List.java
@@ -3,26 +3,48 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode sortList(ListNode head) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode curr = head;
-        while (curr != null) {
-            arr.add(curr.val);
-            curr = curr.next;
-        }
-        
-        Collections.sort(arr);
-        
-        curr = head;
-        for (int i=0;i<arr.size();i++) {
-            curr.val = arr.get(i);
-            curr = curr.next;
-        }
-        
-        return head;
+  public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    ListNode mid = getMid(head);
+    ListNode left = sortList(head);
+    ListNode right = sortList(mid);
+    return merge(left, right);
+  }
+  
+  private ListNode getMid(ListNode head) {
+    ListNode prev = null;
+    while (head != null && head.next != null) {
+      prev = prev == null ? head : prev.next;
+      head = head.next.next;
+    }
+    ListNode mid = prev.next;
+    prev.next = null;
+    return mid;
+  }
+  
+  private ListNode merge(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode();
+    ListNode tail = dummyHead;
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        tail.next = l1;
+        l1 = l1.next;
+      }
+      else {
+        tail.next = l2;
+        l2 = l2.next;
+      }
+      tail = tail.next;
+    }
+    tail.next = l1 != null ? l1 : l2;
+    return dummyHead.next;
+  }
 }
