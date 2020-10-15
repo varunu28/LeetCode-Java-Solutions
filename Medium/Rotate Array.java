9c1ed47ef5026e2eda0bbc4714fd73b1804ba91e
varunu28 <varun.u28@gmail.com>
2020-10-15 06:10:01 -0700

    Added Rotate Array.java

---
+++ Medium/Rotate Array.java
@@ -0,0 +1,18 @@
+class Solution {
+  public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+  
+  private void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      int temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+}
