c48c663d702e79efb66b6d0fa707a1185376d462
varunu28 <varun.u28@gmail.com>
2020-10-19 11:47:21 -0500

    Refactored Repeated DNA Sequences.java

--- Medium/Repeated DNA Sequences.java
+++ Medium/Repeated DNA Sequences.java
@@ -1,19 +1,13 @@
 class Solution {
-    public List<String> findRepeatedDnaSequences(String s) {
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (int i=0; i<=s.length() - 10; i++) {
-            map.put(s.substring(i, i+10), map.getOrDefault(s.substring(i, i+10), 0) + 1);
-        }
-        
-        List<String> ans = new ArrayList<>();
-        
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                ans.add(entry.getKey());
-            }
-        }
-        
-        return ans;
+  public List<String> findRepeatedDnaSequences(String s) {
+    Set<String> seenSequences = new HashSet<>();
+    Set<String> duplicateSequences = new HashSet<>();
+    for (int idx = 0; idx <= s.length() - 10; idx++) {
+      String currentSequence = s.substring(idx, idx + 10);
+      if (!seenSequences.add(currentSequence)) {
+        duplicateSequences.add(currentSequence);
+      }
     }
+    return new ArrayList<>(duplicateSequences);
+  }
 }
