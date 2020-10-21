56791afef96a6c2ab8baa5e6c2de4541ded96536
varunu28 <varun.u28@gmail.com>
2020-10-21 05:37:29 -0700

    Refactored Relative Ranks.java

--- Easy/Relative ranks.java
+++ Easy/Relative ranks.java
@@ -1,34 +1,20 @@
-import java.util.Arrays;
-
-public class Solution {
-    public String[] findRelativeRanks(int[] nums) {
-        int[] nums_copy = nums.clone();
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length / 2; i++) {
-            int temp = nums[i];
-            nums[i] = nums[nums.length - 1 - i];
-            nums[nums.length - 1 - i] = temp;
-        }
-
-        String[] rank = {"Gold Medal", "Silver Medal", "Bronze Medal"};
-
-        String[] ans = new String[nums.length];
-        for (int i=0;i<nums.length;i++) {
-            if (findIndex(nums,nums_copy[i]) == 0 || findIndex(nums,nums_copy[i]) == 1 || findIndex(nums,nums_copy[i]) == 2) {
-                ans[i] = rank[findIndex(nums,nums_copy[i])];
-            }
-            else {
-                ans[i] = String.valueOf(findIndex(nums,nums_copy[i]) + 1);
-            }
-        }
-        return ans;
+class Solution {
+  public String[] findRelativeRanks(int[] nums) {
+    PriorityQueue<Integer> orderedRanking = new PriorityQueue<>((o1, o2) -> nums[o2] - nums[o1]);
+    orderedRanking.addAll(IntStream.range(0, nums.length).boxed().collect(Collectors.toList()));
+    String[] relativeRanks = new String[nums.length];
+    int currentRank = 1;
+    while (!orderedRanking.isEmpty()) {
+      int removed = orderedRanking.poll();
+      if (currentRank > 3) {
+        relativeRanks[removed] = String.valueOf(currentRank++);
+      } else {
+        relativeRanks[removed] = (
+            currentRank == 1 ? "Gold Medal" : (currentRank == 2 ? "Silver Medal" : "Bronze Medal")
+        );
+        currentRank++;
+      }
     }
-    public int findIndex(int[] nums, int elem) {
-        for (int i=0;i<nums.length;i++) {
-            if (nums[i] == elem) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    return relativeRanks;
+  }
 }
