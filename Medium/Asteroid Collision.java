953c60aa49dfe3c45269783e813dcb731e029de8
varunu28 <varun.u28@gmail.com>
2020-10-21 05:18:21 -0700

    Refactored Asteroid Collision.java

--- Medium/Asteroid Collision.java
+++ Medium/Asteroid Collision.java
@@ -2,28 +2,27 @@ class Solution {
   public int[] asteroidCollision(int[] asteroids) {
     Stack<Integer> stack = new Stack<>();
     for (int asteroid : asteroids) {
-      boolean toAdd = true;
       if (asteroid < 0) {
-        while (!stack.isEmpty() && stack.peek() > 0) {
-          if (Math.abs(asteroid) > stack.peek()) {
+        while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid)) {
+          stack.pop();
+        }
+        if (!stack.isEmpty() && stack.peek() > 0) {
+          if (stack.peek() == Math.abs(asteroid)) {
             stack.pop();
-            continue;
           }
-          else if (Math.abs(asteroid) == stack.peek()) {
-            stack.pop();
-          }
-          toAdd = false;
-          break;
+        }
+        else {
+          stack.push(asteroid);
         }
       }
-      if (toAdd) {
+      else {
         stack.push(asteroid);
       }
     }
-    int[] ans = new int[stack.size()];
-    for (int i = ans.length - 1; i >= 0; i--) {
-      ans[i] = stack.pop();
+    int[] state = new int[stack.size()];
+    for (int i = state.length - 1; i >= 0; i--) {
+      state[i] = stack.pop();
     }
-    return ans;
+    return state;
   }
 }
