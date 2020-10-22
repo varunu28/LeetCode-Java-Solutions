6e0b577c6ea5445d188111e4b623074605ce2b57
varunu28 <varun.u28@gmail.com>
2020-10-22 12:32:49 -0700

    Refactored Count Binary Substrings.java

--- Easy/Count Binary Substrings.java
+++ Easy/Count Binary Substrings.java
@@ -1,17 +1,19 @@
 class Solution {
-    public int countBinarySubstrings(String s) {
-        int prev = 0;
-        int curr = 1; 
-        int res = 0;
-        
-        for (int i=1;i<s.length();i++) {
-            if (s.charAt(i) == s.charAt(i-1)) curr++;
-            else {
-                prev = curr;
-                curr = 1;
-            }
-            if (prev >= curr) res++;
-        }
-        return res;
+  public int countBinarySubstrings(String s) {
+    int currValueCount = 1;
+    int prevValueCount = 0;
+    int count = 0;
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) == s.charAt(i - 1)) {
+        currValueCount++;
+      } else {
+        prevValueCount = currValueCount;
+        currValueCount = 1;
+      }
+      if (prevValueCount >= currValueCount) {
+        count++;
+      }
     }
+    return count;
+  }
 }
