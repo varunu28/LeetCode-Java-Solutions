5efc356a131ee25c195c95a6282898ec7ee95b33
varunu28 <varun.u28@gmail.com>
2020-10-27 08:07:26 -0700

    Refactored Linked List Cycle II.java

--- Medium/Linked List Cycle II.java
+++ Medium/Linked List Cycle II.java
@@ -11,18 +11,16 @@
  */
 public class Solution {
   public ListNode detectCycle(ListNode head) {
-    ListNode slow = head;
     ListNode fast = head;
-    boolean cycle = false;
+    ListNode slow = head;
     while (fast != null && fast.next != null) {
       slow = slow.next;
       fast = fast.next.next;
       if (slow == fast) {
-        cycle = true;
         break;
       }
     }
-    if (!cycle) {
+    if (fast == null || fast.next == null) {
       return null;
     }
     slow = head;
