16f2be32e04785e73dd4489a611e688b6fd09490
varunu28 <varun.u28@gmail.com>
2020-11-10 07:30:52 -0800

    Refactored Smallest String Starting From Leaf.java

--- Medium/Smallest String Starting From Leaf.java
+++ Medium/Smallest String Starting From Leaf.java
@@ -4,36 +4,33 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public String smallestFromLeaf(TreeNode root) {
-        return helper(root);
+  public String smallestFromLeaf(TreeNode root) {
+    return dfs(root, "");
+  }
+  
+  private String dfs(TreeNode root, String curr) {
+    if (root == null) {
+      return curr;
     }
-    
-    private String helper(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        
-        char ch = (char) (97 + root.val);
-        String leftVal = helper(root.left);
-        String rightVal = helper(root.right);
-        
-        if (leftVal == null && rightVal == null) {
-            return "" + ch;
-        }
-        
-        if (root.left == null || root.right == null) {
-            return rightVal == null ? leftVal + ch : rightVal + ch;
-        }
-        
-        if (rightVal.compareTo(leftVal) > 0) {
-            return leftVal + ch;
-        }
-        else {
-            return rightVal + ch;
-        }
+    curr = "" + (char)(97 + root.val) + curr;
+    if (root.left == null && root.right == null) {
+      return curr;
+    } else if (root.left == null || root.right == null) {
+      return root.left == null ? dfs(root.right, curr) : dfs(root.left, curr);
+    } else {
+      String left = dfs(root.left, curr);
+      String right = dfs(root.right, curr);
+      return left.compareTo(right) <= 0 ? left : right;
     }
+  }
 }
