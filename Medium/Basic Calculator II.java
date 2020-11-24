f1d1256821bd06475e6159c9a3ddb0dd92e121bb
varunu28 <varun.u28@gmail.com>
2020-11-24 07:02:33 -0800

    Refactored Basic Calculator II.java

--- Medium/Basic Calculator II.java
+++ Medium/Basic Calculator II.java
@@ -1,34 +1,31 @@
 class Solution {
   public int calculate(String s) {
-    Stack<Integer> stack = new Stack<>();
     int num = 0;
     char sign = '+';
+    Stack<Integer> stack = new Stack<>();
     for (int i = 0; i < s.length(); i++) {
       char c = s.charAt(i);
       if (Character.isDigit(c)) {
         num = num * 10 + Character.getNumericValue(c);
-      }
+      } 
       if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1) {
-        if (sign == '-') {
-          stack.push(-num);
-        }
-        else if (sign == '+') {
+        if (sign == '+') {
           stack.push(num);
-        }
-        else if (sign == '*') {
+        } else if (sign == '-') {
+          stack.push(-num);
+        } else if (sign == '*') {
           stack.push(stack.pop() * num);
-        }
-        else {
+        } else {
           stack.push(stack.pop() / num);
         }
         sign = c;
         num = 0;
       }
     }
-    int res = 0;
+    int result = 0;
     while (!stack.isEmpty()) {
-      res += stack.pop();
+      result += stack.pop();
     }
-    return res;
+    return result;
   }
 }
