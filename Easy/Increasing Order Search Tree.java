b1800cf48834a9666f951357fa92fc40e623a2b0
varunu28 <varun.u28@gmail.com>
2020-12-03 07:37:14 -0800

    Refactored Increasing Order Search Tree.java

--- Easy/Increasing Order Search Tree.java
+++ Easy/Increasing Order Search Tree.java
@@ -4,7 +4,13 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
