39f5eb9204aa8f2f0c6ff5574ac305c0e2ec0da3
varunu28 <varun.u28@gmail.com>
2020-12-22 15:41:47 -0800

    Refactored Balanced Binary Tree.java

--- Easy/Balanced Binary Tree.java
+++ Easy/Balanced Binary Tree.java
@@ -4,23 +4,38 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        
-        int lh = height(root.left);
-        int rh = height(root.right);
-        
-        return Math.abs(lh-rh) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+  public boolean isBalanced(TreeNode root) {
+    if (root == null) {
+      return true;
     }
-    
-    public int height(TreeNode root) {
-        if(root == null) return 0;
-        
-        return 1 + Math.max(height(root.left), height(root.right));
+    Map<TreeNode, Integer> map = new HashMap<>();
+    int leftHeight = getHeight(root.left, map);
+    int rightHeight = getHeight(root.right, map);
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return false;
     }
-    
+    return isBalanced(root.left) && isBalanced(root.right);
+  }
+  
+  private int getHeight(TreeNode root, Map<TreeNode, Integer> map) {
+    if (root == null) {
+      return 0;
+    }
+    if (map.containsKey(root)) {
+      return map.get(root);
+    }
+    int height = 1 + Math.max(getHeight(root.left, map), getHeight(root.right, map));
+    map.put(root, height);
+    return height;
+  }
 }
