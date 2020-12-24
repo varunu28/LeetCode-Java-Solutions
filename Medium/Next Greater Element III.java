5f511d1405b5c5a58803b9b035d6e35edc83a79d
varunu28 <varun.u28@gmail.com>
2020-12-23 16:41:57 -0800

    Refactored Next Greater Element III.java

--- Medium/Next Greater Element III.java
+++ Medium/Next Greater Element III.java
@@ -1,53 +1,33 @@
 class Solution {
-    public int nextGreaterElement(int n) {
-        int[] arr = getArr(n);
-        int i;
-        for (i=arr.length-1; i>0; i--) {
-            if (arr[i-1] < arr[i]) {
-                break;
-            }
+  public int nextGreaterElement(int n) {
+    char[] digits = String.valueOf(n).toCharArray();
+    int idx = digits.length - 2;
+    while (idx >= 0) {
+      if (Character.getNumericValue(digits[idx]) < Character.getNumericValue(digits[idx + 1])) {
+        int secondIdx = digits.length - 1;
+        while (secondIdx >= idx && digits[secondIdx] <= digits[idx]) {
+          secondIdx--;
+        } 
+        swap(digits, idx, secondIdx);
+        int start = idx + 1;
+        int end = digits.length - 1;
+        while (start < end) {
+          swap(digits, start++, end--);
         }
-
-        if (i == 0) {
-            return -1;
+        try {
+          return Integer.parseInt(new String(digits));
+        } catch (Exception e) {
+          return -1;
         }
-
-        int num = arr[i-1];
-        int small = i;
-        for (int j=i+1; j<arr.length; j++) {
-            if (arr[j] > num && arr[j] <= arr[small]) {
-                small = j;
-            }
-        }
-
-        int temp = arr[i-1];
-        arr[i-1] = arr[small];
-        arr[small] = temp;
-
-        Arrays.sort(arr, i, arr.length);
-        long ans = getNumber(arr);
-        
-        return ans <= Integer.MAX_VALUE ? (int) ans : -1;
+      }
+      idx--;
     }
-
-    private long getNumber(int[] arr) {
-        long num = 0;
-        for (int i=0; i<arr.length; i++) {
-            num = num * 10 + arr[i];
-        }
-
-        return num;
-    }
-
-    private int[] getArr(int n) {
-        int[] arr = new int[String.valueOf(n).length()];
-        int i = arr.length - 1;
-        while (i >= 0) {
-            arr[i] = n % 10;
-            n /= 10;
-            i--;
-        }
-
-        return arr;
-    }
+    return -1;
+  }
+  
+  private void swap(char[] digits, int idxOne, int idxTwo) {
+    char temp = digits[idxOne];
+    digits[idxOne] = digits[idxTwo];
+    digits[idxTwo] = temp;
+  }
 }
