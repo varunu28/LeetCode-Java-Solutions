fea3167af3d9faf2973c0575d0caf0d3f6009b9a
Varun Upadhyay <varun.u28@gmail.com>
2021-01-07 18:52:12 -0800

    Refactored Solve the Equation.java

--- Medium/Solve the Equation.java
+++ Medium/Solve the Equation.java
@@ -1,93 +1,46 @@
 class Solution {
-    public String solveEquation(String equation) {
-        int leftXCount = 0;
-        int rightXCount = 0;
-        int leftNumCount = 0;
-        int rightNumCount = 0;
-        boolean switchDone = false;
-        int sign = 1;
-        StringBuilder sb = new StringBuilder();
-
-        for (char c : equation.toCharArray()) {
-            if (c == '=') {
-                if (sb.length() > 0) {
-                    leftNumCount += sign * Integer.parseInt(sb.toString());
-                    sb.setLength(0);
-                    sign = 1;
-                }
-
-                switchDone = true;
-                continue;
-            }
-
-            if (c == 'x') {
-                if (switchDone) {
-                    rightXCount += sign * (sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1);
-                }
-                else {
-                    leftXCount += sign * (sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1);
-                }
-
-                sign = 1;
-                sb.setLength(0);
-            }
-            else if (Character.isDigit(c)) {
-                sb.append(c);
-            }
-            else if (c == '+' || c == '-') {
-                if (sb.length() > 0) {
-                    if (switchDone) {
-                        rightNumCount += sign * (Integer.parseInt(sb.toString()));
-                    }
-                    else {
-                        leftNumCount += sign * (Integer.parseInt(sb.toString()));
-                    }
-
-                    sb.setLength(0);
-                }
-
-                sign = c == '+' ? 1 : -1;
-            }
-        }
-
-        if (sb.length() > 0) {
-            rightNumCount += sign * Integer.parseInt(sb.toString());
-        }
-
-        if (leftXCount == rightXCount && leftNumCount != rightNumCount) {
-            return "No solution";
-        }
-        else if (leftXCount == rightXCount && leftNumCount == rightNumCount) {
-            return "Infinite solutions";
-        }
-        else {
-            int totalXCount = leftXCount - rightXCount;
-            int totalValueCount = rightNumCount - leftNumCount;
-
-            int gcd = getGcd(totalXCount, totalValueCount);
-            if (gcd != 0) {
-                totalXCount /= gcd;
-                totalValueCount /= gcd;
-            }
-
-            if (totalXCount < 0) {
-                totalXCount *= -1;
-                totalValueCount *= -1;
-            }
-
-            return new StringBuilder()
-                            .append(totalXCount > 1 ? totalXCount : "")
-                            .append("x=")
-                            .append(totalValueCount)
-                            .toString();
-        }
+  public String solveEquation(String equation) {
+    int[] equationFirst = parseEquation(equation.split("=")[0]);
+    int[] equationSecond = parseEquation(equation.split("=")[1]);
+    int constantPart = equationSecond[1] - equationFirst[1];
+    int variablePart = equationFirst[0] - equationSecond[0];
+    if (variablePart == 0) {
+      return constantPart == 0 ? "Infinite solutions" : "No solution";
     }
-
-    private int getGcd(int totalXCount, int totalValueCount) {
-        if (totalXCount == 0) {
-            return totalValueCount;
+    int sign = (variablePart < 0 && constantPart < 0) || (variablePart > 0 && constantPart > 0) ? 1 : -1;
+    if (Math.abs(constantPart) % Math.abs(variablePart) == 0) {
+      constantPart = Math.abs(constantPart) / Math.abs(variablePart);
+      variablePart = 1;
+    } 
+    return (variablePart > 1 ? variablePart : "") + "x=" + sign * constantPart;
+  }
+  
+  private int[] parseEquation(String s) {
+    int variablePart = 0;
+    int constantPart = 0;
+    int sign = 1;
+    int n = s.length();
+    for (int idx = 0; idx < n;) {
+      if (s.charAt(idx) == 'x') {
+        variablePart += sign;
+        idx++;
+        sign = 1;
+      } else if (Character.isDigit(s.charAt(idx))) {
+        int num = 0;
+        while (idx < n && Character.isDigit(s.charAt(idx))) {
+          num = num * 10 + Character.getNumericValue(s.charAt(idx++));
         }
-
-        return getGcd(totalValueCount % totalXCount, totalXCount);
+        if (idx != n && s.charAt(idx) == 'x') {
+          variablePart += sign * num;
+          idx++;
+        } else {
+          constantPart += sign * num;
+        }
+        sign = 1;
+      } else {
+        sign = s.charAt(idx++) == '+' ? 1 : -1;
+      }
     }
+    return new int[]{variablePart, constantPart};
+  }
 }
