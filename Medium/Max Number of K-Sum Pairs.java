905a5062773b124934e435344ea6612264220ce1
Varun Upadhyay <varun.u28@gmail.com>
2021-01-18 10:17:02 -0800

    Refactored Max Number of K-Sum Pairs.java

--- Medium/Max Number of K-Sum Pairs.java
+++ Medium/Max Number of K-Sum Pairs.java
@@ -1,22 +1,15 @@
 class Solution {
   public int maxOperations(int[] nums, int k) {
-    Map<Integer, Integer> counter = new HashMap<>();
+    Map<Integer, Integer> map = new HashMap<>();
+    int count = 0;
     for (int num : nums) {
-      counter.put(num, counter.getOrDefault(num, 0) + 1);
-    }
-    Set<Integer> keys = counter.keySet();
-    int numOfPairs = 0;
-    for (Integer key : keys) {
-      if (counter.containsKey(k - key) && (k - key != key)) {
-        int min = Math.min(counter.get(key), counter.get(k - key));
-        counter.put(key, counter.get(key) - min);
-        counter.put(k - key, counter.get(k - key) - min);
-        numOfPairs += min;
-      } else if (counter.containsKey(k - key) && (k - key == key)) {
-        numOfPairs += counter.get(key) / 2;
-        counter.put(key, counter.get(key) / 2);
+      if (map.getOrDefault(k - num, 0) > 0) {
+        count++;
+        map.put(k - num, map.getOrDefault(k - num, 0) - 1);
+      } else {
+        map.put(num, map.getOrDefault(num, 0) + 1);
       }
     }
-    return numOfPairs;
+    return count;
   }
 }
