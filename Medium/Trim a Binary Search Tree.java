c1c792dcfed2ddfa9c8e69e757b55d3621bc1292
Varun Upadhyay <varun.u28@gmail.com>
2021-02-02 08:38:43 -0800

    Refactored Trim a Binary Search Tree.java

---
+++ Medium/Trim a Binary Search Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public TreeNode trimBST(TreeNode root, int low, int high) {
+    if (root == null) {
+      return null;
+    }
+    if (root.val < low) {
+      return trimBST(root.right, low, high);
+    } else if (root.val > high) {
+      return trimBST(root.left, low, high);
+    }
+    root.left = trimBST(root.left, low, high);
+    root.right = trimBST(root.right, low, high);
+    return root;
+  }
+}
