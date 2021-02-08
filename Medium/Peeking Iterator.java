b64b386b6ff464a6129b72b6f0865f8237d260b9
Varun Upadhyay <varun.u28@gmail.com>
2021-02-08 05:49:40 -0800

    Refactored Peeking Iterator.java

--- Medium/Peeking Iterator.java
+++ Medium/Peeking Iterator.java
@@ -1,47 +1,35 @@
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
-import java.util.NoSuchElementException;
 
 class PeekingIterator implements Iterator<Integer> {
-
-    private Integer next;
-    private Iterator<Integer> iter;
-    private boolean noNextElement = false;
-    
-    public PeekingIterator(Iterator<Integer> iterator) {
-        // initialize any member here.
-        iter = iterator;
-        moveIterator();
-    }
-
-    // Returns the next element in the iteration without advancing the iterator.
-    public Integer peek() {
-        return next;
-    }
-
-    // hasNext() and next() should behave the same as in the Iterator interface.
-    // Override them if needed.
-    @Override
-    public Integer next() {
-        if (noNextElement) {
-            throw new NoSuchElementException();
-        }
-        Integer val = next;
-        moveIterator();
-        return val;   
-    }
-
-    @Override
-    public boolean hasNext() {
-        return !noNextElement;
-    }
-    
-    public void moveIterator() {
-        if (iter.hasNext()) {
-            next = iter.next();
-        }
-        else {
-            noNextElement = true;
-        }
-    }
+  Integer nextElement;
+  Iterator<Integer> iterator;
+	public PeekingIterator(Iterator<Integer> iterator) {
+    // initialize any member here.
+    this.iterator = iterator;
+    updateNext();
+	}
+  
+  private void updateNext() {
+    nextElement = iterator.hasNext() ? iterator.next() : null;
+  }
+	
+  // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+    return nextElement;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+    Integer returnValue = nextElement;
+    updateNext();
+    return returnValue;
+	}
+	
+	@Override
+	public boolean hasNext() {
+	  return nextElement != null;
+	}
 }
