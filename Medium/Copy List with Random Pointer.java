121ca508d5a8c7b04fa84354e177d8d615e7a9a8
Varun Upadhyay <varun.u28@gmail.com>
2021-02-10 10:57:01 -0800

    Refactored Copy List with Random Pointer.java

--- Medium/Copy List with Random Pointer.java
+++ Medium/Copy List with Random Pointer.java
@@ -1,28 +1,33 @@
-/**
- * Definition for singly-linked list with a random pointer.
- * class RandomListNode {
- *     int label;
- *     RandomListNode next, random;
- *     RandomListNode(int x) { this.label = x; }
- * };
- */
-public class Solution {
-    public RandomListNode copyRandomList(RandomListNode head) {
-        if (head == null) {
-            return null;
-        }
-        
-        RandomListNode ans = new RandomListNode(head.label);
-        RandomListNode curr = ans;
-        
-        while (head != null) {
-            curr.next = head.next == null ? null : new RandomListNode(head.next.label);
-            curr.random = head.random == null ? null : new RandomListNode(head.random.label);
-            
-            curr = curr.next;
-            head = head.next;
-        }
-        
-        return ans;
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
+}
+*/
+
+class Solution {
+  public Node copyRandomList(Node head) {
+    Map<Node, Node> map = new HashMap<>();
+    Node curr = head;
+    while (curr != null) {
+      map.put(curr, new Node(curr.val));
+      curr = curr.next;
+    }
+    curr = head;
+    while (curr != null) {
+      Node temp = map.get(curr);
+      temp.next = curr.next == null ? null : map.get(curr.next);
+      temp.random = curr.random == null ? null : map.get(curr.random);
+      curr = curr.next;
+    }
+    return map.get(head);
+  }
 }
