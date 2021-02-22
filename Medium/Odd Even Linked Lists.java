37f01c696cefa881d7e5544f69d3360bcd32c828
Varun Upadhyay <varun.u28@gmail.com>
2021-02-21 16:09:30 -0800

    Refactored Odd Even Linked Lists.java

--- Medium/Odd Even Linked Lists.java
+++ Medium/Odd Even Linked Lists.java
@@ -3,23 +3,26 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode oddEvenList(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evenHead = even;
-        while(odd.next != null && even.next != null){
-            odd.next = even.next;
-            odd = odd.next;
-            even.next = odd.next;
-            even = even.next;
-        }
-        odd.next = evenHead;
-        return head;
+  public ListNode oddEvenList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    ListNode oddIdxNode = head;
+    ListNode evenIdxNodeStart = head.next;
+    ListNode evenIdxNode = evenIdxNodeStart;
+    while (evenIdxNode != null && evenIdxNode.next != null) {
+      oddIdxNode.next = evenIdxNode.next;
+      oddIdxNode = oddIdxNode.next;
+      evenIdxNode.next = oddIdxNode.next;
+      evenIdxNode = evenIdxNode.next;
+    }
+    oddIdxNode.next = evenIdxNodeStart;
+    return head;
+  }
 }
