00ab1ba76cc4165b23797de169e980e6dc51a901
Varun Upadhyay <varun.u28@gmail.com>
2021-02-24 09:11:39 -0800

    Refactored Score of Parentheses.java

--- Medium/Score of Parentheses.java
+++ Medium/Score of Parentheses.java
@@ -1,25 +1,15 @@
 class Solution {
-    public static int scoreOfParentheses(String S) {
-        Stack<String> st = new Stack<>();
-        for (int i=0; i<S.length(); i++) {
-            if (S.charAt(i) == '(') {
-                st.push(String.valueOf(S.charAt(i)));
-            }
-            else {
-                int num = 0;
-                while (!st.peek().equals("(")) {
-                    num += Integer.parseInt(st.pop());
-                }
-                st.pop();
-                st.push(num == 0 ? String.valueOf("1") : String.valueOf(2*num));
-            }
-        }
-
-        int ans = 0;
-        while (!st.empty()) {
-            ans += Integer.parseInt(st.pop());
-        }
-
-        return ans;
+  public int scoreOfParentheses(String S) {
+    Stack<Integer> stack = new Stack<>();
+    int currMultiplier = 0;
+    for (char c : S.toCharArray()) {
+      if (c == '(') {
+        stack.push(currMultiplier);
+        currMultiplier = 0;
+      } else {
+        currMultiplier = stack.pop() + Math.max(2 * currMultiplier, 1);
+      }
     }
+    return currMultiplier;
+  }
 }
