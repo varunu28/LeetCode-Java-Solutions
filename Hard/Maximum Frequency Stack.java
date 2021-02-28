363aadca08ce27f8e4cca4da866fef4f864efabb
Varun Upadhyay <varun.u28@gmail.com>
2021-02-28 10:42:59 -0800

    Refactored Maximum Frequency Stack.java

--- Hard/Maximum Frequency Stack.java
+++ Hard/Maximum Frequency Stack.java
@@ -1,26 +1,32 @@
 class FreqStack {
-    Map<Integer, Integer> map;
-    Map<Integer, Stack<Integer>> freqMap;
-    int maxFreq;
-    public FreqStack() {
-        freqMap = new HashMap<>();
-        map = new HashMap<>();
-        maxFreq = Integer.MIN_VALUE;
-    }
+  Map<Integer, Integer> valueToCurrentFrequency;
+  Map<Integer, Stack<Integer>> frequencyToValues;
+  int maxFrequency;
+  public FreqStack() {
+    valueToCurrentFrequency = new HashMap<>();
+    frequencyToValues = new HashMap<>();
+    maxFrequency = 1;
+  }
 
-    public void push(int x) {
-        map.put(x, map.getOrDefault(x, 0) + 1);
-        maxFreq = Math.max(maxFreq, map.get(x));
-        freqMap.computeIfAbsent(map.get(x), k -> new Stack<>()).push(x);
-    }
+  public void push(int x) {
+    valueToCurrentFrequency.put(x, valueToCurrentFrequency.getOrDefault(x, 0) + 1);
+    frequencyToValues.computeIfAbsent(valueToCurrentFrequency.get(x), k -> new Stack<>()).add(x);
+    maxFrequency = Math.max(maxFrequency, valueToCurrentFrequency.get(x));
+  }
 
-    public int pop() {
-        int popped = freqMap.get(maxFreq).pop();
-        map.put(popped, map.get(popped) - 1);
-        if (freqMap.get(maxFreq).size() == 0) {
-            maxFreq--;
-        }
-
-        return popped;
+  public int pop() {
+    int val = frequencyToValues.get(maxFrequency).pop();
+    valueToCurrentFrequency.put(val, valueToCurrentFrequency.getOrDefault(val, 0) - 1);
+    if (frequencyToValues.get(maxFrequency).isEmpty()) {
+      maxFrequency--;
     }
+    return val;
+  }
 }
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ */
