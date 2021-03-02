a61b451fd2f9cb89ae3aa2602e94d67a256ee682
Varun Upadhyay <varun.u28@gmail.com>
2021-03-02 08:15:47 -0800

    Refactored Set Mismatch.java to functional style

--- Easy/Set Mismatch.java
+++ Easy/Set Mismatch.java
@@ -1,17 +1,14 @@
+import java.util.Map.Entry;
+
 class Solution {
   public int[] findErrorNums(int[] nums) {
-    Map<Integer, Integer> frequency = new HashMap<>();
-    for (int num : nums) {
-      frequency.put(num, frequency.getOrDefault(num, 0) + 1);
-    }
-    int[] ans = new int[2];
-    for (int idx = 1; idx <= nums.length; idx++) {
-      if (!frequency.containsKey(idx)) {
-        ans[1] = idx;
-      } else if (frequency.get(idx) > 1) {
-        ans[0] = idx;
-      }
-    }
-    return ans;
+    HashMap<Integer, Long> frequencyMap = Arrays.stream(nums).boxed()
+        .collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
+    return new int[]{
+        frequencyMap.entrySet().stream().filter(entry -> entry.getValue().equals(2L))
+            .map(Entry::getKey).findFirst().orElse(-1),
+        IntStream.range(1, nums.length + 1).boxed().filter(key -> !frequencyMap.containsKey(key))
+            .findFirst().orElse(-1)
+    };
   }
 }
