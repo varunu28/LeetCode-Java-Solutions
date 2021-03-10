c8433f6979eae00122ce5c09753747abe27b047a
Varun Upadhyay <varun.u28@gmail.com>
2021-03-10 06:25:07 -0800

    Refactored Integer To Roman.java

--- Medium/Integer To Roman.java
+++ Medium/Integer To Roman.java
@@ -1,17 +1,15 @@
 class Solution {
-    public String intToRoman(int num) {
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-    
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                sb.append(strs[i]);
-            }
-        }
-        
-        return sb.toString();
+  public String intToRoman(int num) {
+    int[] divisor = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < divisor.length; i++) {
+      int fact = num / divisor[i];
+      while (fact-- > 0) {
+        sb.append(numerals[i]);
+      }
+      num = num % divisor[i];
     }
+    return sb.toString();
+  }
 }
