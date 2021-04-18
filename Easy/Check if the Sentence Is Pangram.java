ac3f601f9268b2523d9489c4d4e51eec88631519
Varun Upadhyay <varun.u28@gmail.com>
2021-04-18 06:43:42 -0700

    Update Check if the Sentence Is Pangram.java

--- Easy/Check if the Sentence Is Pangram.java
+++ Easy/Check if the Sentence Is Pangram.java
@@ -1,7 +1,5 @@
 class Solution {
   public boolean checkIfPangram(String sentence) {
-    Set<Character> allUnique = sentence.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
-    return "abcdefghijklmnopqrstuvwxyz".chars().mapToObj(c -> (char) c)
-        .allMatch(allUnique::contains);
+    return sentence.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()).size() == 26;
   }
 }
