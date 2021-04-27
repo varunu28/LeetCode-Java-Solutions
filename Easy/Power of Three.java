35a2f55e70a6e6cf84fa73aac0bec663d2ae1019
Varun Upadhyay <varun.u28@gmail.com>
2021-04-27 07:36:52 -0700

    Update and rename Power of three.java to Power of Three.java

---
+++ Easy/Power of Three.java
@@ -0,0 +1,14 @@
+class Solution {
+  public boolean isPowerOfThree(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    while (n > 1) {
+      if (n % 3 != 0) {
+        return false;
+      }
+      n /= 3;
+    }
+    return true;
+  }
+}
--- Easy/Power of three.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public boolean isPowerOfThree(int n) {
-        return n>0 &&  1162261467%n==0;
-    }
-}
