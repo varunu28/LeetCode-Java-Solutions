656c1a05cb1a4e64d89cfd7ecfaeb047e43ce7fd
Varun Upadhyay <varun.u28@gmail.com>
2021-05-03 09:49:24 -0700

    Refactored Running Sum of 1d Array.java

--- Easy/Running Sum of 1d Array.java
+++ Easy/Running Sum of 1d Array.java
@@ -1,10 +1,8 @@
 class Solution {
   public int[] runningSum(int[] nums) {
     int[] ans = new int[nums.length];
-    int sum = 0;
     for (int i = 0; i < nums.length; i++) {
-      sum += nums[i];
-      ans[i] = sum;
+      ans[i] = nums[i] + (i == 0 ? 0 : ans[i - 1]);
     }
     return ans;
   }
