4908c18a93f0251d939e4adeb55866e6698fa205
Varun Upadhyay <varun.u28@gmail.com>
2021-05-18 09:12:00 -0700

    Update Find Duplicate File in System.java

--- Medium/Find Duplicate File in System.java
+++ Medium/Find Duplicate File in System.java
@@ -1,22 +1,17 @@
 class Solution {
   public List<List<String>> findDuplicate(String[] paths) {
-    Map<String, List<String>> map = new HashMap<>();
+    Map<String, Set<String>> map = new HashMap<>();
     for (String path : paths) {
-      String[] strs = path.split("\\s+");
-      String filePath = strs[0];
-      for (int i = 1; i < strs.length; i++) {
-        int startIdx = strs[i].indexOf('(');
-        String fileName = strs[i].substring(0, startIdx);
-        String content = strs[i].substring(startIdx, strs[i].length());
-        map.computeIfAbsent(content, k -> new ArrayList<>()).add(filePath + "/" + fileName);
+      String[] splitPath = path.split("\\s+");
+      String directory = splitPath[0];
+      for (int i = 1; i < splitPath.length; i++) {
+        String fileName = splitPath[i].substring(0, splitPath[i].indexOf('('));
+        String fileContent = splitPath[i]
+            .substring(splitPath[i].indexOf('(') + 1, splitPath[i].indexOf(')'));
+        map.computeIfAbsent(fileContent, k -> new HashSet<>()).add(directory + "/" + fileName);
       }
     }
-    List<List<String>> duplicateFiles = new ArrayList<>();
-    for (String key : map.keySet()) {
-      if (map.get(key).size() > 1) {
-        duplicateFiles.add(map.get(key));
-      } 
-    }
-    return duplicateFiles;
+    return map.values().stream().filter(entry -> entry.size() > 1).map(ArrayList::new)
+        .collect(Collectors.toList());
   }
 }
