fd43b15d6dfee78fd5e08cbac4bd13f6b98cf916
Varun Upadhyay <varun.u28@gmail.com>
2021-05-21 08:25:34 -0700

    Update Find and Replace Pattern.java

--- Medium/Find and Replace Pattern.java
+++ Medium/Find and Replace Pattern.java
@@ -1,21 +1,15 @@
 class Solution {
   public List<String> findAndReplacePattern(String[] words, String pattern) {
-    String patternCode = getCode(pattern);
-    List<String> list = new ArrayList<>();
-    for (String word : words) {
-      String wordCode = getCode(word);
-      if (wordCode.equals(patternCode)) {
-        list.add(word);
-      }
-    }
-    return list;
+    String patternString = getPattern(pattern);
+    return Arrays.stream(words).filter(e -> getPattern(e).equals(patternString))
+      .collect(Collectors.toList());
   }
   
-  private String getCode(String word) {
-    Map<Character, Integer> map = new HashMap<>();
+  private String getPattern(String s) {
     StringBuilder sb = new StringBuilder();
-    int count = 0;
-    for (char c : word.toCharArray()) {
+    Map<Character, Integer> map = new HashMap<>();
+    int count = 1;
+    for (char c : s.toCharArray()) {
       if (!map.containsKey(c)) {
         map.put(c, count++);
       }
