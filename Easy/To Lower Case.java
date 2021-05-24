e0a28b2c259c0dfe2e4ae4a2a283bea92a0d05d6
Varun Upadhyay <varun.u28@gmail.com>
2021-05-24 08:26:14 -0700

    Update To Lower Case.java

--- Easy/To Lower Case.java
+++ Easy/To Lower Case.java
@@ -1,15 +1,6 @@
 class Solution {
-  public String toLowerCase(String str) {
-    StringBuilder sb = new StringBuilder();
-    for (char c : str.toCharArray()) {
-      int ascii = (int) c;
-      if (c >= 65 && c <= 90) {
-        sb.append((char) (c - 65 + 97));
-      }
-      else {
-        sb.append(c);
-      }
-    }
-    return sb.toString();
+  public String toLowerCase(String s) {
+    return s.chars().mapToObj(c -> (char) c).map(Character::toLowerCase).map(String::valueOf)
+        .collect(Collectors.joining());
   }
 }
