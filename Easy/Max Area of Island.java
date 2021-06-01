8f4401671767d1314cd265a71e21500210424c3f
Varun Upadhyay <varun.u28@gmail.com>
2021-06-01 07:42:11 -0700

    Update Max Area of Island.java

--- Easy/Max Area of Island.java
+++ Easy/Max Area of Island.java
@@ -1,27 +1,45 @@
 class Solution {
-  int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  
+  private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  
   public int maxAreaOfIsland(int[][] grid) {
-    int m = grid.length;
-    int n = grid[0].length;
+    int rows = grid.length;
+    int cols = grid[0].length;
     int maxArea = 0;
-    boolean[][] visited = new boolean[m][n];
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        maxArea = Math.max(maxArea, helper(grid, i, j, m, n, visited));
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (grid[i][j] != 0) {
+          grid[i][j] = 0;
+          int currentArea = getArea(grid, i, j);
+          maxArea = Math.max(maxArea, currentArea);
+        }
       }
     }
     return maxArea;
   }
   
-  private int helper(int[][] grid, int i, int j, int m, int n, boolean[][] visited) {
-    if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || grid[i][j] == 0) {
-      return 0;
+  private int getArea(int[][] grid, int i, int j) {
+    int currentArea = 0;
+    Queue<int[]> queue = new LinkedList<>();
+    queue.add(new int[]{i, j});
+    while (!queue.isEmpty()) {
+      int[] removed = queue.remove();
+      int currX = removed[0];
+      int currY = removed[1];
+      currentArea++;
+      for (int[] dir : DIRS) {
+        int newX = dir[0] + currX;
+        int newY = dir[1] + currY;
+        if (isValidCoordinate(grid, newX, newY) && grid[newX][newY] == 1) {
+          queue.add(new int[]{newX, newY});
+          grid[newX][newY] = 0;
+        }
+      }
     }
-    int curr = 1;
-    visited[i][j] = true;
-    for (int[] dir : dirs) {
-      curr += helper(grid, i + dir[0], j + dir[1], m, n, visited);
-    }
-    return curr;
+    return currentArea;
+  }
+  
+  private boolean isValidCoordinate(int[][] grid, int i, int j) {
+    return i >= 0 && j >= 0 && i < grid.length && j < grid[0].length;
   }
 }
