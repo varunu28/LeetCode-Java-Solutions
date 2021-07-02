f0769c9fb2c1da2fb60e1b588a6499f72e6c94b9
Varun Upadhyay <varun.u28@gmail.com>
2021-07-02 08:08:13 -0700

    Update Find K Closest Elements.java

--- Medium/Find K Closest Elements.java
+++ Medium/Find K Closest Elements.java
@@ -1,41 +1,11 @@
 class Solution {
   public List<Integer> findClosestElements(int[] arr, int k, int x) {
-    if (k == 0 || arr.length == 0) {
-      return new ArrayList<>();
-    }
-    int[] diffArr = new int[arr.length];
-    int minDiff = Integer.MAX_VALUE;
-    int minDiffIdx = -1;
-    for (int i = 0; i < arr.length; i++) {
-      diffArr[i] = Math.abs(arr[i] - x);
-      if (minDiff > diffArr[i]) {
-        minDiff = diffArr[i];
-        minDiffIdx = i;
-      }
-    }
-    int left = minDiffIdx - 1;
-    int right = minDiffIdx + 1;
-    List<Integer> list = new ArrayList<>();
-    list.add(arr[minDiffIdx]);
-    k--;
-    while (k > 0) {
-      if (left >= 0 && right < arr.length) {
-        if (diffArr[left] <= diffArr[right]) {
-          list.add(arr[left--]);
-        }
-        else {
-          list.add(arr[right++]);
-        }
-      }
-      else if (left >= 0 && right == arr.length) {
-        list.add(arr[left--]);
-      }
-      else {
-        list.add(arr[right++]);
-      }
-      k--;
-    }
-    Collections.sort(list);
-    return list;
+     return Arrays.stream(arr)
+        .mapToObj(e -> new int[]{e, Math.abs(e - x)})
+        .sorted((o1, o2) -> (o1[1] - o2[1] != 0) ? (o1[1] - o2[1]) : o1[0] - o2[0])
+        .map(e -> e[0])
+        .limit(k)
+        .sorted()
+        .collect(Collectors.toList());
   }
 }
