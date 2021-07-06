73f28c6625723f0df6fefe91fc7ab9087ba589c4
Varun Upadhyay <varun.u28@gmail.com>
2021-07-06 08:15:15 -0700

    Update Reduce Array Size to The Half.java

--- Medium/Reduce Array Size to The Half.java
+++ Medium/Reduce Array Size to The Half.java
@@ -1,24 +1,16 @@
 class Solution {
   public int minSetSize(int[] arr) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int num : arr) {
-      map.put(num, map.getOrDefault(num, 0) + 1);
+    Map<Integer, Long> frequencyMap = Arrays.stream(arr).boxed()
+        .collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
+    int halfSize = arr.length / 2;
+    PriorityQueue<Integer> pq = new PriorityQueue<>(
+        (o1, o2) -> (int) (frequencyMap.get(o2) - frequencyMap.get(o1)));
+    pq.addAll(frequencyMap.keySet());
+    int counter = 0;
+    while (!pq.isEmpty() && halfSize > 0) {
+      halfSize -= frequencyMap.get(pq.poll());
+      counter++;
     }
-    PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
-      public int compare(Integer o1, Integer o2) {
-        return map.get(o2) - map.get(o1);
-      }
-    });
-    for (Integer key : map.keySet()) {
-      pq.add(key);
-    }
-    int n = arr.length / 2;
-    int currSize = 0;
-    int count = 0;
-    while (currSize < n) {
-      currSize += map.get(pq.poll());
-      count++;
-    }
-    return count;
+    return counter;
   }
 }
