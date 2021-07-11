921a7e880840a4856869bdbdba127d380eca84eb
Varun Upadhyay <varun.u28@gmail.com>
2021-07-11 07:33:41 -0700

    Create Concatenation of Array.java

---
+++ Easy/Concatenation of Array.java
@@ -0,0 +1,9 @@
+class Solution {
+  public int[] getConcatenation(int[] nums) {
+    int[] ans = new int[nums.length * 2];
+    for (int i = 0; i < nums.length; i++) {
+      ans[i + nums.length] = ans[i] = nums[i];
+    }
+    return ans;
+  }
+}
