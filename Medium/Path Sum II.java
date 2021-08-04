f2b9fe1ab4d2a1df8a3ea8179ca5aa3f4d17f530
Varun Upadhyay <varun.u28@gmail.com>
2021-08-04 06:16:27 -0700

    Update Path Sum II.java

--- Medium/Path Sum II.java
+++ Medium/Path Sum II.java
@@ -4,36 +4,36 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
+  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
     List<List<Integer>> list = new ArrayList<>();
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        if (root == null) return list;
-        
-        Stack<Integer> path = new Stack<>();
-        
-        pathSumImpl(root, sum, path);
-        return list;
+    helper(root, targetSum, new ArrayList<>(), list);
+    return list;
+  }
+  
+  private void helper(TreeNode root, int targetSum, List<Integer> curr, List<List<Integer>> list) {
+    if (root == null) {
+      return;
     }
-    
-    private void pathSumImpl(TreeNode root, int sum, Stack<Integer> path) {
-        path.push(root.val);
-        if (root.left == null && root.right == null) {
-            if (sum == root.val) {
-                list.add(new ArrayList<Integer>(path));
-            }
-        }
-        
-        if (root.left != null) {
-            pathSumImpl(root.left, sum-root.val, path);
-        }
-        
-        if (root.right != null) {
-            pathSumImpl(root.right, sum-root.val, path);
-        }
-        
-        path.pop();
+    curr.add(root.val);
+    if (isLeaf(root) && (targetSum - root.val) == 0) {
+      list.add(new ArrayList<>(curr));
     }
+    helper(root.left, targetSum - root.val, curr, list);
+    helper(root.right, targetSum - root.val, curr, list);
+    curr.remove(curr.size() - 1);
+  }
+  
+  private boolean isLeaf(TreeNode root) {
+    return root.left == null && root.right == null;
+  }
 }
