0f9be0005f4ae3cf91a8dca78c0f745d3fba5ddf
Varun Upadhyay <varun.u28@gmail.com>
2021-08-09 07:55:03 -0700

    Update Add Strings.java

--- Easy/Add Strings.java
+++ Easy/Add Strings.java
@@ -1,26 +1,21 @@
 class Solution {
   public String addStrings(String num1, String num2) {
     StringBuilder sb = new StringBuilder();
-    int idx1 = num1.length() - 1;
-    int idx2 = num2.length() - 1;
+    int idxOne = num1.length() - 1;
+    int idxTwo = num2.length() - 1;
     int carry = 0;
-    while (idx1 >= 0 || idx2 >= 0) {
+    while (idxOne >= 0 || idxTwo >= 0 || carry > 0) {
       int temp = carry;
-      if (idx1 >= 0 && idx2 >= 0) {
-        temp += Character.getNumericValue(num1.charAt(idx1--)) + Character.getNumericValue(num2.charAt(idx2--));
-      }
-      else if (idx1 >= 0 && idx2 < 0) {
-        temp += Character.getNumericValue(num1.charAt(idx1--));
-      }
-      else {
-        temp += Character.getNumericValue(num2.charAt(idx2--));
+      if (idxOne >= 0 && idxTwo >= 0) {
+        temp += Character.getNumericValue(num1.charAt(idxOne--)) + Character.getNumericValue(num2.charAt(idxTwo--));
+      } else if (idxOne >= 0 && idxTwo < 0) {
+        temp += Character.getNumericValue(num1.charAt(idxOne--));
+      } else if (idxOne < 0 && idxTwo >= 0) {
+        temp += Character.getNumericValue(num2.charAt(idxTwo--));
       }
       carry = temp > 9 ? 1 : 0;
       temp = temp > 9 ? temp % 10 : temp;
       sb.append(temp);
-    }
-    if (carry > 0) {
-      sb.append(carry);
     }
     return sb.reverse().toString();
   }
