95c5bf345d83ad1640e4f4a3175c6946eb1ba2a5
Varun Upadhyay <varun.u28@gmail.com>
2021-08-16 16:10:48 -0700

    Update Minimum Window Substring.java

--- Hard/Minimum Window Substring.java
+++ Hard/Minimum Window Substring.java
@@ -1,43 +1,38 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            map.put(c, (map.getOrDefault(c, 0) + 1));
-        }
-
-        int start = 0;
-        int end = 0;
-        int count = map.size();
-        int minLen = Integer.MAX_VALUE;
-        String ans = "";
-
-        while (end < s.length()) {
-            if (map.containsKey(s.charAt(end))) {
-                map.put(s.charAt(end), map.get(s.charAt(end)) - 1);
-                if (map.get(s.charAt(end)) == 0) {
-                    count--;
-                }
-            }
-
-            end++;
-
-            while (count == 0) {
-                if (end - start < minLen) {
-                    minLen = end - start;
-                    ans = s.substring(start, end);
-                }
-
-                if (map.containsKey(s.charAt(start))) {
-                    map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
-                    if (map.get(s.charAt(start)) > 0) {
-                        count++;
-                    }
-                }
-
-                start++;
-            }
-        }
-
-        return ans;
+  public String minWindow(String s, String t) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : t.toCharArray()) {
+      map.put(c, map.getOrDefault(c, 0) + 1);
     }
+    int count = map.size();
+    int start = 0;
+    int end = 0;
+    int minWindowLength = Integer.MAX_VALUE;
+    int minWindowStart = 0;
+    int minWindowEnd = 0;
+    while (end < s.length()) {
+      char c = s.charAt(end++);
+      if (map.containsKey(c)) {
+        map.put(c, map.get(c) - 1);
+        if (map.get(c) == 0) {
+          count--;
+        }
+      }
+      while (count == 0 && start < end) {
+        if (end - start < minWindowLength) {
+          minWindowLength = end - start;
+          minWindowStart = start;
+          minWindowEnd = end;
+        }
+        char temp = s.charAt(start++);
+        if (map.containsKey(temp)) {
+          map.put(temp, map.get(temp) + 1);
+          if (map.get(temp) == 1) {
+            count++;
+          }
+        }
+      }
+    }
+    return s.substring(minWindowStart, minWindowEnd);
+  }
 }
