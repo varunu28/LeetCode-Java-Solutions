5397a2e48952f50cd0022f54465783dcfd924426
Varun Upadhyay <varun.u28@gmail.com>
2021-08-24 07:21:21 -0700

    Update Complex Number Multiplication.java

--- Medium/Complex Number Multiplication.java
+++ Medium/Complex Number Multiplication.java
@@ -3,7 +3,7 @@ class Solution {
     int realPartOne = Integer.parseInt(num1.split("\\+")[0]);
     int imaginaryPartOne = Integer.parseInt(num1.split("\\+")[1].substring(0, num1.split("\\+")[1].length() - 1));
     int realPartTwo = Integer.parseInt(num2.split("\\+")[0]);
-        int imaginaryPartTwo = Integer.parseInt(num2.split("\\+")[1].substring(0, num2.split("\\+")[1].length() - 1));
+    int imaginaryPartTwo = Integer.parseInt(num2.split("\\+")[1].substring(0, num2.split("\\+")[1].length() - 1));
     return (realPartOne * realPartTwo - imaginaryPartOne * imaginaryPartTwo) + "+" + (realPartOne * imaginaryPartTwo + realPartTwo * imaginaryPartOne) + "i";
   }
 }
