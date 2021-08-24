c685fc70d3af1b0c40038ad46e6e2b107d230fee
Varun Upadhyay <varun.u28@gmail.com>
2021-08-24 07:21:11 -0700

    Update Complex Number Multiplication.java

--- Medium/Complex Number Multiplication.java
+++ Medium/Complex Number Multiplication.java
@@ -1,19 +1,9 @@
 class Solution {
-  public String complexNumberMultiply(String a, String b) {
-    int realPart = 0;
-    int complexPart = 0;
-    int[] splitA = helper(a);
-    int[] splitB = helper(b);
-    realPart = splitA[0] * splitB[0] - splitA[1] * splitB[1];
-    complexPart = splitA[0] * splitB[1] + splitA[1] * splitB[0];
-    return realPart + "+" + complexPart + "i";
-  }
-  
-  private int[] helper(String s) {
-    String[] strs = s.split("\\+");
-    int[] arr = new int[2];
-    arr[0] = Integer.parseInt(strs[0]);
-    arr[1] = Integer.parseInt(strs[1].substring(0, strs[1].indexOf('i')));
-    return arr;
+  public String complexNumberMultiply(String num1, String num2) {
+    int realPartOne = Integer.parseInt(num1.split("\\+")[0]);
+    int imaginaryPartOne = Integer.parseInt(num1.split("\\+")[1].substring(0, num1.split("\\+")[1].length() - 1));
+    int realPartTwo = Integer.parseInt(num2.split("\\+")[0]);
+        int imaginaryPartTwo = Integer.parseInt(num2.split("\\+")[1].substring(0, num2.split("\\+")[1].length() - 1));
+    return (realPartOne * realPartTwo - imaginaryPartOne * imaginaryPartTwo) + "+" + (realPartOne * imaginaryPartTwo + realPartTwo * imaginaryPartOne) + "i";
   }
 }
