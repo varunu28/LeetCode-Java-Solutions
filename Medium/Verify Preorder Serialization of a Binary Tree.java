8bd75fd857f3514686f93a413e1eadda32089472
Varun Upadhyay <varun.u28@gmail.com>
2021-08-26 07:38:21 -0700

    Update Verify Preorder Serialization of a Binary Tree.java

--- Medium/Verify Preorder Serialization of a Binary Tree.java
+++ Medium/Verify Preorder Serialization of a Binary Tree.java
@@ -1,24 +1,15 @@
 class Solution {
-    public boolean isValidSerialization(String preorder) {
-        Stack<String> stack = new Stack<>();
-        String[] array = preorder.split(",");
-        
-        for (String node : array) {
-            if (node.equals("#")) {
-                while (!stack.isEmpty() && stack.peek().equals("#")) {
-                    stack.pop();
-                    if (stack.isEmpty()) {
-                        return false;
-                    }
-                    stack.pop();
-                }
-                stack.push(node);
-            }
-            else {
-                stack.push(node);
-            }
-        }
-        
-        return stack.size() == 1 && stack.peek().equals("#");
+  public boolean isValidSerialization(String preorder) {
+    String[] nodes = preorder.split(",");
+    int diff = 1;
+    for (String node : nodes) {
+      if (--diff < 0) {
+        return false;
+      }
+      if (!node.equals("#")) {
+        diff += 2;
+      }
     }
+    return diff == 0;
+  }
 }
