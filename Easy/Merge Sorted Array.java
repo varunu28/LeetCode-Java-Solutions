3630d1c72b7eede61cfccc85faab5d64dadacb8b
Varun Upadhyay <varun.u28@gmail.com>
2021-09-21 10:35:28 -0700

    Update Merge Sorted Array.java

--- Easy/Merge Sorted Array.java
+++ Easy/Merge Sorted Array.java
@@ -1,22 +1,14 @@
 class Solution {
   public void merge(int[] nums1, int m, int[] nums2, int n) {
     int idx = m + n - 1;
-    int numsOneIdx = m - 1;
-    int numsTwoIdx = n - 1;
-    while (numsOneIdx >= 0 || numsTwoIdx >= 0) {
-      int val = -1;
-      if (numsOneIdx >= 0 && numsTwoIdx >= 0) {
-        if (nums1[numsOneIdx] > nums2[numsTwoIdx]) {
-          val = nums1[numsOneIdx--];
-        } else {
-          val = nums2[numsTwoIdx--];
-        }
-      } else if (numsOneIdx >= 0 && numsTwoIdx < 0) {
-        val = nums1[numsOneIdx--];
+    while (m > 0 || n > 0) {
+      if (m > 0 && n > 0) {
+        nums1[idx--] = nums1[m - 1] > nums2[n - 1] ? nums1[m-- - 1] : nums2[n-- - 1];
+      } else if (m > 0 && n == 0) {
+        nums1[idx--] = nums1[m-- - 1];
       } else {
-        val = nums2[numsTwoIdx--];
+        nums1[idx--] = nums2[n-- - 1];
       }
-      nums1[idx--] = val;
     }
   }
 }
