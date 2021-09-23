da0b398556c54cb219aaed7560e913ebc9c5675f
Varun Upadhyay <varun.u28@gmail.com>
2021-09-23 09:28:10 -0500

    Update Break a Palindrome.java

--- Medium/Break a Palindrome.java
+++ Medium/Break a Palindrome.java
@@ -1,24 +1,13 @@
 class Solution {
   public String breakPalindrome(String palindrome) {
-    // If empty or of length 1 => return empty
-    if (palindrome.length() <= 1) {
-      return "";
-    }
     char[] chars = palindrome.toCharArray();
-    for (int i = 0; i < chars.length; i++) {
-      if (chars[i] != 'a') {
-        // Skip if the string is of odd length and it is the middle index
-        if (palindrome.length() % 2 != 0 && i == palindrome.length() / 2) {
-          continue;
-        }
+    for (int i = 0; i < palindrome.length() / 2; i++) {
+      if (chars[i] > 'a') {
         chars[i] = 'a';
-        break;
-      }
-      // If all chars all 'a' update last char to 'b'
-      if (i == chars.length - 1) {
-        chars[i] = 'b';
+        return String.valueOf(chars);
       }
     }
-    return String.valueOf(chars);
+    chars[chars.length - 1] = 'b';
+    return chars.length <= 1 ? "" : String.valueOf(chars);
   }
 }
