3a076706521d94c7e6ea521ed1122227b5af28ed
Varun Upadhyay <varun.u28@gmail.com>
2021-09-24 11:57:43 -0500

    Update N-th Tribonacci Number.java

--- Easy/N-th Tribonacci Number.java
+++ Easy/N-th Tribonacci Number.java
@@ -1,17 +1,10 @@
 class Solution {
   public int tribonacci(int n) {
-    int[] dp = new int[Math.max(n + 1, 3)];
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 1;
-    return helper(n, dp);
-  }
-  
-  private int helper(int n, int[] dp) {
-    if (n == 0 || dp[n] != 0) {
-      return dp[n];
+    int[] arr = new int[38];
+    arr[1] = arr[2] = 1;
+    for (int i = 3; i < 38; i++) {
+      arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
     }
-    dp[n] = helper(n - 1, dp) + helper(n - 2, dp) + helper(n - 3, dp);
-    return dp[n];
+    return arr[n];
   }
 }
