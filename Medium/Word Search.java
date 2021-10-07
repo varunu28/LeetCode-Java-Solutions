1b814352898a1578346ae39c0d3dc27ca883870c
Varun Upadhyay <varun.u28@gmail.com>
2021-10-07 08:24:31 -0500

    Update Word Search.java

--- Medium/Word Search.java
+++ Medium/Word Search.java
@@ -1,33 +1,33 @@
 class Solution {
-  public int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  public static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  
   public boolean exist(char[][] board, String word) {
-    if (board.length == 0 || board[0].length == 0) {
-      return false;
-    }
+    int rows = board.length;
+    int cols = board[0].length;
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[0].length; j++) {
-        if (exists(board, i, j, word, 0, new boolean[board.length][board[0].length])) {
+        if (dfs(board, word, 0, new boolean[rows][cols], i, j)) {
           return true;
         }
       }
     }
     return false;
-  } 
-  
-  private boolean exists(char[][] board, int i, int j, String word, int idx, boolean[][] visited) {
+  }
+
+  private boolean dfs(char[][] board, String word, int idx, boolean[][] visited, int i, int j) {
     if (idx == word.length()) {
       return true;
     }
-    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || word.charAt(idx) != board[i][j]) {
+    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]
+        || board[i][j] != word.charAt(idx)) {
       return false;
     }
     visited[i][j] = true;
-    for (int[] dir : dirs) {
-      if (exists(board, i + dir[0], j + dir[1], word, idx + 1, visited)) {
-        return true;
-      }
+    boolean ans = false;
+    for (int[] dir : DIRS) {
+      ans = ans || dfs(board, word, idx + 1, visited, i + dir[0], j + dir[1]);
     }
     visited[i][j] = false;
-    return false;
+    return ans;
   }
 }
