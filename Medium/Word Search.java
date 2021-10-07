99ca85102e366908d53d6190b5b47a1a8873db91
Varun Upadhyay <varun.u28@gmail.com>
2021-10-07 08:35:21 -0500

    Update Word Search.java

--- Medium/Word Search.java
+++ Medium/Word Search.java
@@ -2,11 +2,10 @@ class Solution {
   public static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
   
   public boolean exist(char[][] board, String word) {
-    int rows = board.length;
-    int cols = board[0].length;
+    char[] letters = word.toCharArray();
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[0].length; j++) {
-        if (dfs(board, word, 0, new boolean[rows][cols], i, j)) {
+        if (dfs(board, 0, letters, i, j)) {
           return true;
         }
       }
@@ -14,20 +13,22 @@ class Solution {
     return false;
   }
 
-  private boolean dfs(char[][] board, String word, int idx, boolean[][] visited, int i, int j) {
-    if (idx == word.length()) {
+  private boolean dfs(char[][] board, int idx, char[] letters, int i, int j) {
+    if (idx == letters.length) {
       return true;
     }
-    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]
-        || board[i][j] != word.charAt(idx)) {
+    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
       return false;
     }
-    visited[i][j] = true;
-    boolean ans = false;
-    for (int[] dir : DIRS) {
-      ans = ans || dfs(board, word, idx + 1, visited, i + dir[0], j + dir[1]);
+    if (board[i][j] != letters[idx]) {
+      return false;
     }
-    visited[i][j] = false;
-    return ans;
+    board[i][j] ^= 256;
+    boolean exists = false;
+    for (int[] dir : DIRS) {
+      exists = exists || dfs(board, idx + 1, letters, i + dir[0], j + dir[1]);
+    }
+    board[i][j] ^= 256;
+    return exists;
   }
 }
