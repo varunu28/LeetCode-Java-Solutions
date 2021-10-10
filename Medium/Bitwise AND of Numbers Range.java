9e00e5dafd33d9b6a50fd0caea586cc6ae7e2929
Varun Upadhyay <varun.u28@gmail.com>
2021-10-09 19:55:25 -0500

    Update Bitwise AND of Numbers Range.java

--- Medium/Bitwise AND of Numbers Range.java
+++ Medium/Bitwise AND of Numbers Range.java
@@ -1,34 +1,8 @@
 class Solution {
-    public int rangeBitwiseAnd(int m, int n) {
-        int temp1 = m;
-        int temp2 = n;
-        
-        int cnt1 = 0;
-        int cnt2 = 0;
-        
-        while (temp1 > 1 || temp2 > 1) {
-            if (temp1 > 1) {
-                temp1 /= 2;
-                cnt1++;
-            }     
-            
-            if (temp2 > 1) {
-                temp2 /= 2;
-                cnt2++;
-            } 
-        }
-        
-        if (cnt1 != cnt2) {
-            return 0;
-        }
-        
-        int res = ~0;
-        for (int i=m;i<n;i++) {
-            res = res&i;
-        }
-        
-        res = res&n;
-        
-        return res;
+  public int rangeBitwiseAnd(int left, int right) {
+    while (right > left) {
+      right = right & (right - 1);
     }
+    return left & right;
+  }
 }
