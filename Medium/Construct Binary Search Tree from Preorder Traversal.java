4645f741c32640681af46d58f666463e42c365aa
Varun Upadhyay <varun.u28@gmail.com>
2021-10-13 08:43:17 -0500

    Update Construct Binary Search Tree from Preorder Traversal.java

--- Medium/Construct Binary Search Tree from Preorder Traversal.java
+++ Medium/Construct Binary Search Tree from Preorder Traversal.java
@@ -4,25 +4,29 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  int idx;
   public TreeNode bstFromPreorder(int[] preorder) {
-    idx = 0;
-    return helper(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    int[] idx = {0};
+    return formBST(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE, idx);
   }
   
-  private TreeNode helper(int[] preorder, int lower, int upper) {
-    if (idx == preorder.length || preorder[idx] < lower || preorder[idx] > upper) {
+  private TreeNode formBST(int[] preorder, int min, int max, int[] idx) {
+    if (idx[0] == preorder.length || preorder[idx[0]] < min || preorder[idx[0]] > max) {
       return null;
     }
-    int val = preorder[idx];
-    idx++;
-    TreeNode root = new TreeNode(val);
-    root.left = helper(preorder, lower, val);
-    root.right = helper(preorder, val, upper);
-    return root;
+    int val = preorder[idx[0]++];
+    TreeNode node = new TreeNode(val);
+    node.left = formBST(preorder, min, val, idx);
+    node.right = formBST(preorder, val, max, idx);
+    return node;
   }
 }
