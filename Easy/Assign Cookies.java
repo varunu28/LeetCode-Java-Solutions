04b7792ad3745eae16ed14a6666a37d242d5df52
Varun Upadhyay <varun.u28@gmail.com>
2021-11-03 12:48:30 -0700

    Update Assign Cookies.java

--- Easy/Assign Cookies.java
+++ Easy/Assign Cookies.java
@@ -1,17 +1,14 @@
 class Solution {
-    public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
-        int count = 0;
-        for (int i=0;i<g.length;i++) {
-            for (int j=0;j<s.length;j++) {
-                if (g[i] <= s[j]) {
-                    s[j] = 0;
-                    count++;
-                    break;
-                }
-            }
-        }
-        return count;
+  public int findContentChildren(int[] g, int[] s) {
+    Arrays.sort(g);
+    Arrays.sort(s);
+    int contentChildrenCount = 0;
+    for (int i = 0, j = 0; i < s.length && j < g.length; i++) {
+      if (s[i] >= g[j]) {
+        contentChildrenCount++;
+        j++;
+      }
     }
+    return contentChildrenCount;
+  }
 }
