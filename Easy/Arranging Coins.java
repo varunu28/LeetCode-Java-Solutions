4f0cefe3a1697ed0a7aa6ad234e4ed08e7e6491f
Varun Upadhyay <varun.u28@gmail.com>
2021-11-04 17:40:02 -0700

    Update Arranging Coins.java

--- Easy/Arranging Coins.java
+++ Easy/Arranging Coins.java
@@ -1,14 +1,18 @@
 class Solution {
   public int arrangeCoins(int n) {
-    int copy = n;
-    int count = 0;
-    for (int i = 1; i <= copy && n > 0; i++) {
-      n -= i;
-      if (n < 0) {
-        break;
+    long left = 1;
+    long right = n;
+    while (left <= right) {
+      long mid = (left + right) / 2;
+      long sum = mid * (mid + 1) / 2;
+      if (sum == n) {
+        return (int) mid;
+      } else if (sum > n) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
       }
-      count++;
     }
-    return count;
+    return (int) right;
   }
 }
