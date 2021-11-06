d180b8001c61bf3b3f914c9beb18a0524e05f7ce
Varun Upadhyay <varun.u28@gmail.com>
2021-11-05 17:16:40 -0700

    Update Single Number III.java

--- Medium/Single Number III.java
+++ Medium/Single Number III.java
@@ -1,15 +1,17 @@
 class Solution {
   public int[] singleNumber(int[] nums) {
-    Set<Integer> set = new HashSet<>();
+    int xorValue = Integer.highestOneBit(Arrays.stream(nums)
+        .boxed()
+        .reduce((a, b) -> a ^ b)
+        .orElse(0));
+    int[] result = new int[2];
     for (int num : nums) {
-      if (set.contains(num)) {
-        set.remove(num);
-      }
-      else {
-        set.add(num);
+      if ((xorValue & num) == 0) {
+        result[0] ^= num;
+      } else {
+        result[1] ^= num;
       }
     }
-    Iterator<Integer> iter = set.iterator();
-    return new int[]{iter.next(), iter.next()};
+    return result;
   }
 }
