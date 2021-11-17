9064ac3787089012993b8117d0aae7857b95b8a3
Varun Upadhyay <varun.u28@gmail.com>
2021-11-17 05:45:47 -0800

    Update Unique Paths.java

--- Medium/Unique Paths.java
+++ Medium/Unique Paths.java
@@ -1,21 +1,20 @@
 class Solution {
-  Integer[][] dp;
   public int uniquePaths(int m, int n) {
-    dp = new Integer[m][n];
-    return dfs(m, n, 0, 0);
+    Integer[][] dp = new Integer[m][n];
+    return helper(0, 0, m, n, dp);
   }
   
-  private int dfs(int m, int n, int x, int y) {
-    if (x >= m || y >= n) {
-      return 0;
-    }
-    if (dp[x][y] != null) {
-      return dp[x][y];
-    }
-    if (x == m - 1 && y == n - 1) {
+  private int helper(int currX, int currY, int m, int n, Integer[][] dp) {
+    if (currX == m - 1 && currY == n - 1) {
       return 1;
     }
-    dp[x][y] = dfs(m, n, x + 1, y) + dfs(m, n, x, y + 1);
-    return dp[x][y];
+    if (currX >= m || currY >= n) {
+      return 0;
+    }
+    if (dp[currX][currY] != null) {
+      return dp[currX][currY];
+    }
+    dp[currX][currY] = helper(currX + 1, currY, m, n, dp) + helper(currX, currY + 1, m, n, dp);
+    return dp[currX][currY];
   }
 }
