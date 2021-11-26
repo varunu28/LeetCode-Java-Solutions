665141b21ddba9d615f22f8065740312002ac955
Varun Upadhyay <varun.u28@gmail.com>
2021-11-25 16:28:19 -0800

    Update Search Insert Position.java

--- Easy/Search Insert Position.java
+++ Easy/Search Insert Position.java
@@ -1,19 +1,17 @@
 class Solution {
   public int searchInsert(int[] nums, int target) {
-    int low = 0;
-    int high = nums.length - 1;
-    while (low <= high) {
-      int mid = (low + high) / 2;
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
       if (nums[mid] == target) {
         return mid;
-      }
-      else if (nums[mid] > target) {
-        high = mid - 1;
-      }
-      else {
-        low = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     }
-    return low;
+    return left;
   }
 }
