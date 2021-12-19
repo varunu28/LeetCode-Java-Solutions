7f75191ab7dc9e30485b5783d555d6f9bffb8038
Varun Upadhyay <varun.u28@gmail.com>
2021-12-19 07:43:12 -0800

    Update Decode String.java

--- Medium/Decode String.java
+++ Medium/Decode String.java
@@ -1,39 +1,34 @@
 class Solution {
-    public static String decodeString(String s) {
-        Stack<Integer> count = new Stack<>();
-        Stack<String> str = new Stack<>();
-        int i = 0;
-        str.push("");
-        while(i < s.length()) {
-            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-                int start = i;
-                while (s.charAt(i+1) >= '0' && s.charAt(i+1) <= '9') {
-                    i++;
-                }
-                count.push(Integer.parseInt(s.substring(start, i + 1)));
-            }
-            else if (s.charAt(i) == '[') {
-                str.push("");
-            }
-            else if (s.charAt(i) == ']') {
-                String st = str.pop();
-                StringBuilder sb = new StringBuilder();
-                int n = count.pop();
-
-                for (int j = 0; j < n; j++) {
-                    sb.append(st);
-                }
-
-                str.push(str.pop() + sb.toString());
-            }
-            else {
-                str.push(str.pop() + s.charAt(i));
-            }
-
-            i++;
+  public String decodeString(String s) {
+    Stack<Integer> countStack = new Stack<>();
+    Stack<String> wordStack = new Stack<>();
+    StringBuilder sb = new StringBuilder();
+    int idx = 0;
+    while (idx < s.length()) {
+      if (Character.isDigit(s.charAt(idx))) {
+        int count = 0;
+        while (idx < s.length() && Character.isDigit(s.charAt(idx))) {
+          count = count * 10 + Character.getNumericValue(s.charAt(idx++));
         }
-
-        return str.pop();
+        countStack.push(count);
+      } else if (Character.isLetter(s.charAt(idx))) {
+        sb.append(s.charAt(idx++));
+      } else if (s.charAt(idx) == '[') {
+        wordStack.push(sb.toString());
+        sb.setLength(0);
+        idx++;
+      } else {
+        StringBuilder temp = new StringBuilder(wordStack.pop());
+        String currentString = sb.toString();
+        sb.setLength(0);
+        int count = countStack.pop();
+        while (count-- > 0) {
+          temp.append(currentString);
+        }
+        sb.append(temp);
+        idx++;
+      }
     }
+    return sb.toString();
+  }
 }
-
