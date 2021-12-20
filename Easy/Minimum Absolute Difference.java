4b0f3def90a5ff95bde1edda45f39e111301c4c7
Varun Upadhyay <varun.u28@gmail.com>
2021-12-20 08:17:03 -0800

    Update Minimum Absolute Difference.java

--- Easy/Minimum Absolute Difference.java
+++ Easy/Minimum Absolute Difference.java
@@ -1,19 +1,16 @@
 class Solution {
   public List<List<Integer>> minimumAbsDifference(int[] arr) {
-    List<List<Integer>> list = new ArrayList<>();
     int minDiff = Integer.MAX_VALUE;
     Arrays.sort(arr);
     for (int i = 0; i < arr.length - 1; i++) {
-      int diff = arr[i + 1] - arr[i];
-      if (diff < minDiff) {
-        list.clear();
-        list.add(Arrays.asList(arr[i], arr[i + 1]));
-        minDiff = diff;
-      }
-      else if (diff == minDiff) {
-        list.add(Arrays.asList(arr[i], arr[i + 1]));
+      minDiff = Math.min(minDiff, arr[i + 1] - arr[i]);
+    }
+    List<List<Integer>> result = new ArrayList<>();
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i + 1] - arr[i] == minDiff) {
+        result.add(Arrays.asList(arr[i], arr[i + 1]));        
       }
     }
-    return list;
+    return result;
   }
 }
