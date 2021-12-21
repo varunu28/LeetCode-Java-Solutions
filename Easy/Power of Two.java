f7e70d06329435012b541be8d7365b6a468fc536
Varun Upadhyay <varun.u28@gmail.com>
2021-12-20 16:28:14 -0800

    Update Power of Two.java

--- Easy/Power of Two.java
+++ Easy/Power of Two.java
@@ -1,21 +1,19 @@
 class Solution {
   public boolean isPowerOfTwo(int n) {
-    if (n % 2 != 0 && n != 1) {
+    if (n > 1 && n % 2 != 0) {
       return false;
     }
     int start = 0;
     int end = n / 2;
     while (start <= end) {
       int mid = (start + end) / 2;
-      int pow = (int) (Math.pow(2, mid));
-      if (n == pow) {
+      int pow = (int) Math.pow(2, mid);
+      if (pow == n) {
         return true;
-      }
-      else if (n > pow) {
-        start = mid + 1;
-      }
-      else {
+      } else if (pow > n) {
         end = mid - 1;
+      } else {
+        start = mid + 1;
       }
     }
     return false;
