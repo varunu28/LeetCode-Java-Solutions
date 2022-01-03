c45c003f78d2a78a823fa796d05bc5f870698889
Varun Upadhyay <varun.u28@gmail.com>
2022-01-02 17:33:39 -0800

    Update Find the Town Judge.java

--- Easy/Find the Town Judge.java
+++ Easy/Find the Town Judge.java
@@ -1,18 +1,15 @@
 class Solution {
-    public int findJudge(int N, int[][] trust) {
-        int[] count = new int[N + 1];
-        
-        for (int[] item : trust) {
-            count[item[0]]--;
-            count[item[1]]++;
-        }
-        
-        for (int i = 1; i <= N; i++) {
-            if (count[i] == N - 1) {
-                return i;
-            }
-        }
-        
-        return -1;
+  public int findJudge(int n, int[][] trust) {
+    int[] trustScore = new int[n + 1];
+    for (int[] trustPair : trust) {
+      trustScore[trustPair[1]]++;
+      trustScore[trustPair[0]]--;
     }
+    for (int i = 1; i <= n; i++) {
+      if (trustScore[i] == n - 1) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
