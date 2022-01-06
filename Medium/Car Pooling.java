560963671bdf1049533a2e64c40d14ed51f8e446
Varun Upadhyay <varun.u28@gmail.com>
2022-01-05 17:14:33 -0800

    Update Car Pooling.java

--- Medium/Car Pooling.java
+++ Medium/Car Pooling.java
@@ -1,20 +1,17 @@
 class Solution {
   public boolean carPooling(int[][] trips, int capacity) {
-    Arrays.sort(trips, Comparator.comparingInt(o -> o[1]));
-    int start = 0;
+    Arrays.sort(trips, Comparator.comparingInt((int[] o) -> o[1]).thenComparingInt(o -> o[2]));
+    PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
     int idx = 0;
-    int n = trips.length;
-    int currCapacity = 0;
-    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-    while (idx < n) {
-      while (!pq.isEmpty() && pq.peek()[2] <= trips[idx][1]) {
-        currCapacity -= pq.poll()[0];
+    while (idx < trips.length) {
+      while (!priorityQueue.isEmpty() && priorityQueue.peek()[2] <= trips[idx][1]) {
+        capacity += priorityQueue.poll()[0];
       }
-      if (currCapacity + trips[idx][0] > capacity) {
+      if (capacity < trips[idx][0]) {
         return false;
       }
-      currCapacity += trips[idx][0];
-      pq.add(trips[idx++]);
+      priorityQueue.add(trips[idx]);
+      capacity -= trips[idx++][0];
     }
     return true;
   }
