a774b9af9b0ff11da1051edded7cde6a36123654
Varun Upadhyay <varun.u28@gmail.com>
2022-01-11 17:16:54 -0800

    Update Insert into a Binary Search Tree.java

--- Medium/Insert into a Binary Search Tree.java
+++ Medium/Insert into a Binary Search Tree.java
@@ -12,16 +12,14 @@
  *         this.right = right;
  *     }
  * }
- */
+*/
 class Solution {
   public TreeNode insertIntoBST(TreeNode root, int val) {
     if (root == null) {
       return new TreeNode(val);
-    }
-    if (root.val < val) {
+    } else if (root.val < val) {
       root.right = insertIntoBST(root.right, val);
-    }
-    else {
+    } else {
       root.left = insertIntoBST(root.left, val);
     }
     return root;
