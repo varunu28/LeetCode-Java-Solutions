0481fdb4f2cb75b6af08a231b19a47f63156681f
Varun Upadhyay <varun.u28@gmail.com>
2022-01-12 20:11:59 -0800

    Update Minimum Number of Arrows to Burst Balloons.java

--- Medium/Minimum Number of Arrows to Burst Balloons.java
+++ Medium/Minimum Number of Arrows to Burst Balloons.java
@@ -1,26 +1,14 @@
 class Solution {
   public int findMinArrowShots(int[][] points) {
-    if (points.length == 0) {
-      return 0;
-    }
-    Arrays.sort(points, new Comparator<int[]>(){
-      public int compare(int[] p1, int[] p2) {
-        int c = p1[0] - p2[0];
-        if (c != 0) {
-          return c;
-        }
-        return p1[1] - p2[1];
-      }
-    });
-    int end = points[0][1];
-    int count = 1;
-    for (int i = 1; i < points.length; i++) {
-      if (points[i][0] <= end) {
-        end = Math.min(end, points[i][1]);
-        continue;
+    Arrays.sort(points, Comparator.comparingInt((int[] o) -> o[1]));
+    int count = 0;
+    int idx = 0;
+    while (idx < points.length) {
+      int currEnd = points[idx][1];
+      while (idx < points.length && points[idx][0] <= currEnd) {
+        idx++;
       }
       count++;
-      end = points[i][1];
     }
     return count;
   }
