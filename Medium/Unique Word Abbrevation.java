4ae85605eee3e991be83303658a0b5dd997e72d1
Varun Upadhyay <varun.u28@gmail.com>
2022-01-19 13:43:50 -0800

    Update Unique Word Abbrevation.java

--- Medium/Unique Word Abbrevation.java
+++ Medium/Unique Word Abbrevation.java
@@ -1,41 +1,33 @@
 class ValidWordAbbr {
 
-    Map<String, String> map;
-    public ValidWordAbbr(String[] dictionary) {
-        map = new HashMap<>();
+  private final Map<String, String> abbreviationToWordMap;
 
-        for (String word : dictionary) {
-            String abbr = getAbbr(word);
-            if (map.containsKey(abbr)) {
-                if (!map.get(abbr).equals(word)) {
-                    map.put(abbr, "");
-                }
-            }
-            else {
-                map.put(abbr, word);
-            }
-        }
+  public ValidWordAbbr(String[] dictionary) {
+    this.abbreviationToWordMap = new HashMap<>();
+    for (String word : dictionary) {
+      String abbreviation = buildAbbreviation(word);
+      if (this.abbreviationToWordMap.containsKey(abbreviation) && 
+          !this.abbreviationToWordMap.get(abbreviation).equals(word)) {
+        this.abbreviationToWordMap.put(abbreviation, "");
+      } else {
+        this.abbreviationToWordMap.put(abbreviation, word);
+      }
     }
+  }
 
-    public boolean isUnique(String word) {
-        String abbr = getAbbr(word);
-        return !map.containsKey(abbr) || map.get(abbr).equals(word);
+  public boolean isUnique(String word) {
+    String abbreviation = buildAbbreviation(word);
+    return !this.abbreviationToWordMap.containsKey(abbreviation) || this.abbreviationToWordMap.get(
+        abbreviation).equals(word);
+  }
+
+  private String buildAbbreviation(String s) {
+    if (s.length() <= 2) {
+      return s;
     }
-
-    private String getAbbr(String word) {
-        if (word.length() <= 2) {
-            return word;
-        }
-
-        int num = word.length() - 2;
-        StringBuilder sb = new StringBuilder();
-        sb.append(word.charAt(0)).append(String.valueOf(num)).append(word.charAt(word.length()-1));
-
-        return sb.toString();
-    }
+    return s.charAt(0) + String.valueOf(s.length() - 2) + s.charAt(s.length() - 1);
+  }
 }
-
-
 /**
  * Your ValidWordAbbr object will be instantiated and called as such:
  * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
