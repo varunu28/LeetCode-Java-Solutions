9e9fd57ab6ca84155aa5c98a0829ea249dbcb4b7
Varun Upadhyay <varun.u28@gmail.com>
2022-01-20 13:37:41 -0800

    Update Design Search Autocomplete System.java

--- Hard/Design Search Autocomplete System.java
+++ Hard/Design Search Autocomplete System.java
@@ -1,84 +1,76 @@
 class AutocompleteSystem {
-  Map<String, Integer> map;
-  Node root;
-  Node currNode;
+
+  private final TrieNode root;
+  private TrieNode queryNode;
+  private final Map<String, Integer> popularityMap;
   StringBuilder sb;
   boolean invalidQuery;
+
   public AutocompleteSystem(String[] sentences, int[] times) {
-    map = new HashMap<>();
-    root = new Node('-');
+    root = new TrieNode();
+    this.popularityMap = new HashMap<>();
     for (int i = 0; i < sentences.length; i++) {
-      addSentence(sentences[i], 0, root);
-      map.put(sentences[i], map.getOrDefault(sentences[i], 0) + times[i]);
+      indexSentence(sentences[i], root);
+      this.popularityMap.put(
+          sentences[i], this.popularityMap.getOrDefault(sentences[i], 0) + times[i]);
     }
-    currNode = root;
-    invalidQuery = false;
-    sb = new StringBuilder();
+    this.queryNode = root;
+    this.sb = new StringBuilder();
+    this.invalidQuery = false;
   }
-  
-  private void addSentence(String s, int idx, Node root) {
-    if (idx == s.length()) {
-      return;
+
+  private void indexSentence(String sentence, TrieNode root) {
+    for (char c : sentence.toCharArray()) {
+      if (!root.children.containsKey(c)) {
+        root.children.put(c, new TrieNode());
+      }
+      root = root.children.get(c);
+      root.sentences.add(sentence);
     }
-    char c = s.charAt(idx);
-    if (!root.children.containsKey(c)) {
-      root.children.put(c, new Node(c));
-    }
-    Node node = root.children.get(c);
-    node.possibleSentences.add(s);
-    addSentence(s, idx + 1, node);
   }
 
   public List<String> input(char c) {
     if (c == '#') {
-      String s = sb.toString();
-      map.put(s, map.getOrDefault(s, 0) + 1);
-      addSentence(s, 0, root);
-      sb.setLength(0);
-      currNode = root;
-      invalidQuery = false;
+      cleanUp();
       return new ArrayList<>();
     }
-    sb.append(c);
-    if (!currNode.children.containsKey(c) || invalidQuery) {
-      invalidQuery = true;
+    this.sb.append(c);
+    if (!this.queryNode.children.containsKey(c) || invalidQuery) {
+      this.invalidQuery = true;
       return new ArrayList<>();
     }
-    else {
-      List<String> possibleSentences = new ArrayList<>();
-      currNode = currNode.children.get(c);
-      possibleSentences.addAll(currNode.possibleSentences);
-      return getTopThree(possibleSentences);
-    }
+    this.queryNode = this.queryNode.children.get(c);
+    return this.queryNode.sentences.stream()
+        .sorted(
+            (o1, o2) -> {
+              int c1 = popularityMap.get(o2).compareTo(popularityMap.get(o1));
+              if (c1 != 0) {
+                return c1;
+              }
+              return o1.compareTo(o2);
+            })
+        .limit(3)
+        .collect(Collectors.toList());
   }
-  
-  private List<String> getTopThree(List<String> list) {
-    Collections.sort(list, new Comparator<String>(){
-      public int compare(String s1, String s2) {
-        int c = map.get(s2) - map.get(s1);
-        if (c != 0) {
-          return c;
-        }
-        return s1.compareTo(s2);
-      }
-    });
-    List<String> ans = new ArrayList<>();
-    for (int i = 0; i < list.size() && ans.size() < 3; i++) {
-      ans.add(list.get(i));
-    }
-    return ans;
-  }
-}
 
-class Node {
-  char c;
-  Map<Character, Node> children;
-  Set<String> possibleSentences;
-  
-  public Node(char c) {
-    this.c = c;
-    children = new HashMap<>();
-    possibleSentences = new HashSet<>();
+  private void cleanUp() {
+    this.queryNode = root;
+    String inputString = this.sb.toString();
+    this.sb.setLength(0);
+    this.popularityMap.put(inputString, this.popularityMap.getOrDefault(inputString, 0) + 1);
+    this.invalidQuery = false;
+    indexSentence(inputString, root);
+  }
+
+  private static class TrieNode {
+
+    private final Map<Character, TrieNode> children;
+    private final Set<String> sentences;
+
+    public TrieNode() {
+      this.children = new HashMap<>();
+      this.sentences = new HashSet<>();
+    }
   }
 }
 
