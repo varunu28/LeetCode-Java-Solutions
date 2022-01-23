aa25b4447f8a9fd4c06bff0e2391405bd9a8f54e
Varun Upadhyay <varun.u28@gmail.com>
2022-01-23 11:07:18 -0800

    Update Sequential Digits.java

--- Medium/Sequential Digits.java
+++ Medium/Sequential Digits.java
@@ -2,17 +2,17 @@ class Solution {
   public List<Integer> sequentialDigits(int low, int high) {
     String bucket = "123456789";
     int n = 10;
-    List<Integer> list = new ArrayList<>();
-    int lowLength = String.valueOf(low).length();
-    int highLength = String.valueOf(high).length();
-    for (int length = lowLength; length < highLength + 1; length++) {
-      for (int start = 0; start < n - length; start++) {
-        int num = Integer.parseInt(bucket.substring(start, start + length));
+    List<Integer> result = new ArrayList<>();
+    int minLength = String.valueOf(low).length();
+    int maxLength = String.valueOf(high).length();
+    for (int currLength = minLength; currLength <= maxLength; currLength++) {
+      for (int idx = 0; idx < n - currLength; idx++) {
+        int num = Integer.parseInt(bucket.substring(idx, idx + currLength));
         if (num >= low && num <= high) {
-          list.add(num);
+          result.add(num);
         }
       }
     }
-    return list;
+    return result;
   }
 }
