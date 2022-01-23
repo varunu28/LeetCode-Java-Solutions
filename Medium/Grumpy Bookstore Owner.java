bff7d99d9b2538f0d3bfd67ad4dcaff825481f5b
Varun Upadhyay <varun.u28@gmail.com>
2022-01-23 14:55:39 -0800

    Update Grumpy Bookstore Owner.java

--- Medium/Grumpy Bookstore Owner.java
+++ Medium/Grumpy Bookstore Owner.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int maxSatisfied(int[] customers, int[] grumpy, int X) {
-        int totalSum = 0;
-        int maxDiff = 0;
-        int grumpySum = 0;
-        
-        for (int i = 0; i < customers.length; i++) {
-            totalSum += grumpy[i] == 1 ? 0 : customers[i];
-            grumpySum += grumpy[i] == 1 ? customers[i] : 0;
-            if (i >= X) {
-                grumpySum -= grumpy[i - X] == 1 ? customers[i - X] : 0;
-            }
-            
-            maxDiff = Math.max(maxDiff, grumpySum);
-        }
-
-        return totalSum + maxDiff;
+  public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+    int totalSum = 0;
+    int maxDiff = 0;
+    int grumpySum = 0;
+    for (int i = 0; i < customers.length; i++) {
+      totalSum += grumpy[i] == 1 ? 0 : customers[i];
+      grumpySum += grumpy[i] == 1 ? customers[i] : 0;
+      if (i >= minutes) {
+        grumpySum -= grumpy[i - minutes] == 1 ? customers[i - minutes] : 0;
+      }
+      maxDiff = Math.max(maxDiff, grumpySum);
     }
+    return totalSum + maxDiff;
+  }
 }
