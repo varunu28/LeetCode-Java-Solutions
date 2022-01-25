360d353dddb5be4d449ed2f5c358ae03f409cce9
Varun Upadhyay <varun.u28@gmail.com>
2022-01-25 14:05:21 -0800

    Update Shortest Word Distance.java

--- Easy/Shortest Word Distance.java
+++ Easy/Shortest Word Distance.java
@@ -1,17 +1,16 @@
 class Solution {
-  public int shortestDistance(String[] words, String word1, String word2) {
-    int idx1 = -1;
-    int idx2 = -1;
-    int minDistance = words.length;
-    for (int i = 0; i < words.length; i++) {
-      if (words[i].equals(word1)) {
-        idx1 = i;
+  public int shortestDistance(String[] wordsDict, String word1, String word2) {
+    int idxOne = -1;
+    int idxTwo = -1;
+    int minDistance = Integer.MAX_VALUE;
+    for (int i = 0; i < wordsDict.length; i++) {
+      if (wordsDict[i].equals(word1)) {
+        idxOne = i;
+      } else if (wordsDict[i].equals(word2)) {
+        idxTwo = i;
       }
-      else if (words[i].equals(word2)) {
-        idx2 = i;
-      }
-      if (idx1 != -1 && idx2 != -1) {
-        minDistance = Math.min(minDistance, Math.abs(idx1 - idx2));
+      if (idxOne != -1 && idxTwo != -1) {
+        minDistance = Math.min(minDistance, Math.abs(idxOne - idxTwo));
       }
     }
     return minDistance;
