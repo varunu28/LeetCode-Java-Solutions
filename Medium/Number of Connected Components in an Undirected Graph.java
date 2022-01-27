bb137e4bc12263405b21d1c3457cacca666d5869
Varun Upadhyay <varun.u28@gmail.com>
2022-01-26 16:38:29 -0800

    Update Number of Connected Components in an Undirected Graph.java

--- Medium/Number of Connected Components in an Undirected Graph.java
+++ Medium/Number of Connected Components in an Undirected Graph.java
@@ -1,41 +1,51 @@
 class Solution {
-    public int countComponents(int n, int[][] edges) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        
-        for (int[] edge : edges) {
-            map.computeIfAbsent(edge[0], k -> new ArrayList<Integer>()).add(edge[1]);
-            map.computeIfAbsent(edge[1], k -> new ArrayList<Integer>()).add(edge[0]);
-        }
-        
-        Set<Integer> set = new HashSet<>();
-        int count = 0;
-        
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
-            if (set.contains(entry.getKey())) {
-                continue;
-            }
-            
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(entry.getKey());
-            set.add(entry.getKey());
-            
-            while (!queue.isEmpty()) {
-                int popped = queue.remove();
-                List<Integer> connections = map.getOrDefault(popped, new ArrayList<Integer>());
-                
-                for (int connection : connections) {
-                    if (set.contains(connection)) {
-                        continue;
-                    }
-                    
-                    set.add(connection);
-                    queue.add(connection);
-                }
-            }
-            
-            count++;
-        }
-        
-        return count + (n - set.size());
+  public int countComponents(int n, int[][] edges) {
+    DisjointSet disjointSet = new DisjointSet(n);
+    for (int[] edge : edges) {
+      disjointSet.union(edge[0], edge[1]);
     }
+    Set<Integer> rootSet = new HashSet<>();
+    for (int i = 0; i < n; i++) {
+      rootSet.add(disjointSet.find(i));
+    }
+    return rootSet.size();
+  }
+
+  private static class DisjointSet {
+
+    private final int[] root;
+    private final int[] rank;
+
+    public DisjointSet(int size) {
+      this.root = new int[size];
+      this.rank = new int[size];
+      for (int i = 0; i < size; i++) {
+        this.root[i] = i;
+        this.rank[i] = 1;
+      }
+    }
+
+    public void union(int nodeOne, int nodeTwo) {
+      int rootOne = find(nodeOne);
+      int rootTwo = find(nodeTwo);
+      if (rootOne != rootTwo) {
+        if (this.rank[rootOne] > this.rank[rootTwo]) {
+          this.root[rootTwo] = rootOne;
+        } else if (this.rank[rootOne] < this.rank[rootTwo]) {
+          this.root[rootOne] = rootTwo;
+        } else {
+          this.root[rootTwo] = rootOne;
+          this.rank[rootOne]++;
+        }
+      }
+    }
+
+
+    public int find(int node) {
+      if (node == root[node]) {
+        return node;
+      }
+      return root[node] = find(root[node]);
+    }
+  }
 }
