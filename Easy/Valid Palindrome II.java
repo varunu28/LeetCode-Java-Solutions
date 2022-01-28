5e1c7d748db6725a44783018312c3eccb02d1b8b
Varun Upadhyay <varun.u28@gmail.com>
2022-01-28 09:21:15 -0800

    Update Valid Palindrome II.java

--- Easy/Valid Palindrome II.java
+++ Easy/Valid Palindrome II.java
@@ -1,27 +1,21 @@
 class Solution {
   public boolean validPalindrome(String s) {
-    int start = 0;
-    int end = s.length() - 1;
-    while (start < end && s.charAt(start) == s.charAt(end)) {
-      start++;
-      end--;
-    }
-    if (start >= end) {
+    int[] firstPalindromeCheck = isPalindromHelper(s);
+    if (firstPalindromeCheck[0] == -1) {
       return true;
     }
-    if (isPalindrome(s.substring(start + 1, end + 1)) || isPalindrome(s.substring(start, end))) {
-      return true;
-    }
-    return false;
+    int[] skipLeft = isPalindromHelper(s.substring(firstPalindromeCheck[0] + 1, firstPalindromeCheck[1] + 1));
+    int[] skipRight = isPalindromHelper(s.substring(firstPalindromeCheck[0], check[1]));
+    return skipLeft[0] == -1 || skipRight[0] == -1;
   }
-  
-  private boolean isPalindrome(String s) {
-    int start = 0;
-    int end = s.length() - 1;
-    while (start < end && s.charAt(start) == s.charAt(end)) {
-      start++;
-      end--;
+
+  private int[] isPalindromHelper(String s) {
+    int startIdx = 0;
+    int endIdx = s.length() - 1;
+    while (startIdx < endIdx && s.charAt(startIdx) == s.charAt(endIdx)) {
+      startIdx++;
+      endIdx--;
     }
-    return start >= end;
+    return startIdx >= endIdx ? new int[]{-1} : new int[]{startIdx, endIdx};
   }
 }
