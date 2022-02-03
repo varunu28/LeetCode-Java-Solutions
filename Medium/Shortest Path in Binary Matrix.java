fd74e1f50150fdb43efd9cc4c3e9c3bbc6eb1b2c
Varun Upadhyay <varun.u28@gmail.com>
2022-02-03 12:21:57 -0800

    Update Shortest Path in Binary Matrix.java

--- Medium/Shortest Path in Binary Matrix.java
+++ Medium/Shortest Path in Binary Matrix.java
@@ -1,34 +1,40 @@
 class Solution {
-  int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
   public int shortestPathBinaryMatrix(int[][] grid) {
-    int rows = grid.length;
-    int cols = grid[0].length;
-    if (grid[0][0] == 1 || grid[rows - 1][cols - 1] == 1) {
+    int n = grid.length;
+    if (n == 0 || grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
       return -1;
     }
-    boolean[][] visited = new boolean[rows][cols];
     Queue<int[]> queue = new LinkedList<>();
-    queue.add(new int[]{0, 0});
+    int numberOfSteps = 0;
+    boolean[][] visited = new boolean[n][n];
+    queue.add(new int[] {0, 0});
     visited[0][0] = true;
-    int steps = 0;
     while (!queue.isEmpty()) {
       int size = queue.size();
       while (size-- > 0) {
         int[] removed = queue.remove();
-        if (removed[0] == (rows - 1) && removed[1] == (cols - 1)) {
-          return steps + 1;
+        if (removed[0] == n - 1 && removed[1] == n - 1) {
+          return numberOfSteps + 1;
         }
-        for (int i = 0; i < dirs.length; i++) {
-          int newX = removed[0] + dirs[i][0];
-          int newY = removed[1] + dirs[i][1];
-          if (newX >= 0 && newX < rows && newY >= 0 && newY < cols && !visited[newX][newY] && grid[newX][newY] == 0) {
-            queue.add(new int[]{newX, newY});
+        for (int[] dir : EIGHT_DIRS) {
+          int newX = removed[0] + dir[0];
+          int newY = removed[1] + dir[1];
+          if (newX >= 0
+              && newX < n
+              && newY >= 0
+              && newY < n
+              && !visited[newX][newY]
+              && grid[newX][newY] == 0) {
+            queue.add(new int[] {newX, newY});
             visited[newX][newY] = true;
           }
         }
       }
-      steps++;
+      numberOfSteps++;
     }
     return -1;
   }
+
+  private static final int[][] EIGHT_DIRS = {
+    {0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
 }
