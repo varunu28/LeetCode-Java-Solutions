9e11021284707981b324fadd6e3d45d3e3223372
Varun Upadhyay <varun.u28@gmail.com>
2022-02-04 07:52:52 -0800

    Update Contiguous Array.java

--- Medium/Contiguous Array.java
+++ Medium/Contiguous Array.java
@@ -2,17 +2,16 @@ class Solution {
   public int findMaxLength(int[] nums) {
     Map<Integer, Integer> map = new HashMap<>();
     map.put(0, -1);
-    int maxLen = 0;
+    int maxLength = 0;
     int count = 0;
     for (int i = 0; i < nums.length; i++) {
       count += nums[i] == 0 ? -1 : 1;
       if (map.containsKey(count)) {
-        maxLen = Math.max(maxLen, i - map.get(count));
-      }
-      else {
+        maxLength = Math.max(maxLength, i - map.get(count));
+      } else {
         map.put(count, i);
       }
     }
-    return maxLen;
+    return maxLength;
   }
 }
