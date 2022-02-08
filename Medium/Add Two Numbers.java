a2082ac9bedec6356904f23f94c4489a2846ab49
Varun Upadhyay <varun.u28@gmail.com>
2022-02-07 16:45:32 -0800

    Update Add Two Numbers.java

--- Medium/Add Two Numbers.java
+++ Medium/Add Two Numbers.java
@@ -3,34 +3,34 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode ans = new ListNode(-1);
-    ListNode curr = ans;
+    ListNode root = new ListNode(-1);
+    ListNode curr = root;
     int carry = 0;
-    while (l1 != null || l2 != null || carry > 0) {
-      int temp = carry;
+    while (l1 != null || l2 != null || carry != 0) {
+      int currSum = carry;
       if (l1 != null && l2 != null) {
-        temp += l1.val + l2.val;
+        currSum += l1.val + l2.val;
         l1 = l1.next;
         l2 = l2.next;
-      }
-      else if (l1 != null && l2 == null) {
-        temp += l1.val;
+      } else if (l1 != null && l2 == null) {
+        currSum += l1.val;
         l1 = l1.next;
-      }
-      else if (l1 == null && l2 != null) {
-        temp += l2.val;
+      } else if (l1 == null && l2 != null) {
+        currSum += l2.val;
         l2 = l2.next;
       }
-      carry = temp > 9 ? temp / 10 : 0;
-      temp = temp % 10;
-      curr.next = new ListNode(temp);
+      carry = currSum > 9 ? 1 : 0;
+      currSum %= 10;
+      curr.next = new ListNode(currSum);
       curr = curr.next;
     }
-    return ans.next;
+    return root.next;
   }
 }
