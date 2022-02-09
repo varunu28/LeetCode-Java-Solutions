c51290124e9e6ad6f1cc941bf7ad0f1c3afa85b7
Varun Upadhyay <varun.u28@gmail.com>
2022-02-08 17:03:45 -0800

    Update K-diff Pairs in an Array.java

--- Easy/K-diff Pairs in an Array.java
+++ Easy/K-diff Pairs in an Array.java
@@ -1,21 +1,18 @@
 class Solution {
   public int findPairs(int[] nums, int k) {
-    Arrays.sort(nums);
-    int left = 0;
-    int right = 1;
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
     int count = 0;
-    while (left < nums.length && right < nums.length) {
-      if (left == right || nums[right] - nums[left] < k) {
-        right++;
-      }
-      else if (nums[right] - nums[left] > k) {
-        left++;
-      }
-      else {
-        left++;
-        count++;
-        while (left < nums.length && nums[left] == nums[left - 1]) {
-          left++;
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      if (k == 0) {
+        if (entry.getValue() >= 2) {
+          count++;
+        }
+      } else {
+        if (map.containsKey(entry.getKey() + k)) {
+          count++;
         }
       }
     }
