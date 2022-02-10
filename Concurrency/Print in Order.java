9d3ace1200799f39f5f933cab6af61a9ac6350d3
Varun Upadhyay <varun.u28@gmail.com>
2022-02-10 15:09:24 -0800

    Update Print in Order.java

--- Concurrency/Print in Order.java
+++ Concurrency/Print in Order.java
@@ -1,53 +1,44 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 class Foo {
-    
-    private int counter = 1;
-    private String mutex = "";
-    public Foo() {
-        
-    }
 
-    public void first(Runnable printFirst) throws InterruptedException {
-        
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        boolean flag = true;
-        while (flag) {
-            synchronized(mutex) {
-                if (counter == 1) {
-                    printFirst.run();
-                    counter++;
-                    flag = false;
-                }
-            }
-        }
-    }
+  private Object lock;
+  private AtomicInteger counter;
 
-    public void second(Runnable printSecond) throws InterruptedException {
-        
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        boolean flag = true;
-        while (flag) {
-            synchronized(mutex) {
-                if (counter == 2) {
-                    printSecond.run();
-                    counter++;
-                    flag = false;
-                }
-            }
-        }
-    }
+  public Foo() {
+    this.lock = new Object();
+    this.counter = new AtomicInteger(0);
+  }
 
-    public void third(Runnable printThird) throws InterruptedException {
-        
-        // printThird.run() outputs "third". Do not change or remove this line.
-        boolean flag = true;
-        while (flag) {
-            synchronized(mutex) {
-                if (counter == 3) {
-                    printThird.run();
-                    counter++;
-                    flag = false;
-                }
-            }
-        }
+  public void first(Runnable printFirst) throws InterruptedException {
+
+    // printFirst.run() outputs "first". Do not change or remove this line.
+    synchronized (lock) {
+      printFirst.run();
+      this.counter.incrementAndGet();
+      this.lock.notifyAll();
     }
+  }
+
+  public void second(Runnable printSecond) throws InterruptedException {
+    // printSecond.run() outputs "second". Do not change or remove this line.
+    synchronized (lock) {
+      while (this.counter.get() != 1) {
+        this.lock.wait();
+      }
+      printSecond.run();
+      this.counter.incrementAndGet();
+      this.lock.notifyAll();
+    }
+  }
+
+  public void third(Runnable printThird) throws InterruptedException {
+    // printThird.run() outputs "third". Do not change or remove this line.
+    synchronized (lock) {
+      while (this.counter.get() != 2) {
+        this.lock.wait();
+      }
+      printThird.run();
+    }
+  }
 }
