43446fd0e060c811bc505022e658b66d1c1d0983
Varun Upadhyay <varun.u28@gmail.com>
2022-02-10 16:18:23 -0800

    Update Meeting Scheduler.java

--- Medium/Meeting Scheduler.java
+++ Medium/Meeting Scheduler.java
@@ -1,34 +1,21 @@
 class Solution {
   public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
-    int idx1 = 0;
-    int idx2 = 0;
-    Arrays.sort(slots1, new sortByStartAndEndTime());
-    Arrays.sort(slots2, new sortByStartAndEndTime());
-    while (idx1 < slots1.length && idx2 < slots2.length) {
-      int maxStart = Math.max(slots1[idx1][0], slots2[idx2][0]);
-      int minEnd = Math.min(slots1[idx1][1], slots2[idx2][1]);
+    Arrays.sort(slots1, Comparator.comparingInt(o -> o[0]));
+    Arrays.sort(slots2, Comparator.comparingInt(o -> o[0]));
+    int idxOne = 0;
+    int idxTwo = 0;
+    while (idxOne < slots1.length && idxTwo < slots2.length) {
+      int maxStart = Math.max(slots1[idxOne][0], slots2[idxTwo][0]);
+      int minEnd = Math.min(slots1[idxOne][1], slots2[idxTwo][1]);
       if (minEnd - maxStart >= duration) {
         return Arrays.asList(maxStart, maxStart + duration);
       }
-      if (slots1[idx1][1] < slots2[idx2][1]) {
-        idx1++;
-      }
-      else {
-        idx2++;
+      if (slots1[idxOne][1] > slots2[idxTwo][1]) {
+        idxTwo++;
+      } else {
+        idxOne++;
       }
     }
     return new ArrayList<>();
-  }
-
-  class sortByStartAndEndTime implements Comparator<int[]> {
-
-    @Override
-    public int compare(int[] o1, int[] o2) {
-      int c = o1[0] - o2[0];
-      if (c != 0) {
-        return c;
-      }
-      return o1[1] - o2[1];
-    }
   }
 }
