a512165edf5c95be3c44f83e52f434112201e0f7
Varun Upadhyay <varun.u28@gmail.com>
2022-02-11 14:24:53 -0800

    Update Coin Change.java

--- Medium/Coin Change.java
+++ Medium/Coin Change.java
@@ -1,7 +1,17 @@
 class Solution {
   public int coinChange(int[] coins, int amount) {
-    Integer[] memo = new Integer[amount + 1];
-    return coinChangeMemoization(coins, amount, memo);
+    Integer[] dp = new Integer[amount + 1];
+    Arrays.fill(dp, amount + 1);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; i++) {
+      for (int coin : coins) {
+        if (i - coin < 0) {
+          continue;
+        }
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
+    }
+    return dp[amount] == (amount + 1) ? -1 : dp[amount];
   }
 
   private int coinChangeMemoization(int[] coins, int amount, Integer[] memo) {
@@ -24,5 +34,24 @@ class Solution {
     }
     memo[amount] = minCount == Integer.MAX_VALUE ? -1 : minCount;
     return memo[amount];
+  }
+  
+  // This approach times out due to overlapping subproblems
+  private int coinChangeRecursive(int[] coins, int amount) {
+    if (amount < 0) {
+      return -1;
+    }
+    if (amount == 0) {
+      return 0;
+    }
+    int minCount = Integer.MAX_VALUE;
+    for (int coin : coins) {
+      int count = coinChangeRecursive(coins, amount - coin);
+      if (count == -1) {
+        continue;
+      }
+      minCount = Math.min(minCount, count);
+    }
+    return minCount == Integer.MAX_VALUE ? -1 : minCount;
   }
 }
