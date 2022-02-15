0d7374d54e90497ed06969e1097bb9ae8273b5f8
Varun Upadhyay <varun.u28@gmail.com>
2022-02-14 16:06:00 -0800

    Update Single Number.java

--- Easy/Single Number.java
+++ Easy/Single Number.java
@@ -1,24 +1,9 @@
-public class Solution {
-    public int singleNumber(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        Arrays.sort(nums);
-        if (nums[0] != nums[1]) {
-            return nums[0];
-        }
-        boolean twice = true;
-        for (int i=0;i<nums.length-1;i++) {
-            if (nums[i] != nums[i+1]) {
-                if (twice) {
-                    twice = false;
-                }
-                else {
-                    return nums[i];
-                }
-            }
-            else {
-                twice = true;
-            }
-        }
-        return nums[nums.length-1];
+class Solution {
+  public int singleNumber(int[] nums) {
+    int xor = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      xor ^= nums[i];
     }
+    return xor;
+  }
 }
