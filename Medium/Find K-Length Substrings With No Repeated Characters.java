29fbe98b5124eddda7fba6cc19cf46ae731330b2
Varun Upadhyay <varun.u28@gmail.com>
2022-02-15 07:59:37 -0800

    Update Find K-Length Substrings With No Repeated Characters.java

--- Medium/Find K-Length Substrings With No Repeated Characters.java
+++ Medium/Find K-Length Substrings With No Repeated Characters.java
@@ -1,26 +1,24 @@
 class Solution {
-    public int numKLenSubstrNoRepeats(String S, int K) {
-        int count = 0;
-        int start = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < K - 1 && i < S.length(); i++) {
-            map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0) + 1);
-        }
-
-        for (int i = K - 1; i < S.length(); i++) {
-            map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0) + 1);
-            if (map.size() == K) {
-                count++;
-            }
-
-            map.put(S.charAt(start), map.get(S.charAt(start)) - 1);
-            if (map.get(S.charAt(start)) == 0) {
-                map.remove(S.charAt(start));
-            }
-
-            start++;
-        }
-
-        return count;
+  public int numKLenSubstrNoRepeats(String s, int k) {
+    if (s.length() < k) {
+      return 0;
     }
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < k; i++) {
+      map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+    }
+    int resultCount = 0;
+    for (int i = k; i < s.length(); i++) {
+      if (map.size() == k) {
+        resultCount++;
+      }
+      int startIdx = i - k;
+      map.put(s.charAt(startIdx), map.getOrDefault(s.charAt(startIdx), 0) - 1);
+      if (map.get(s.charAt(startIdx)) == 0) {
+        map.remove(s.charAt(startIdx));
+      }
+      map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+    }
+    return resultCount + (map.size() == k ? 1 : 0);
+  }
 }
