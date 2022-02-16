c67702c98339a448594be7d60c16b48db7dcca22
Varun Upadhyay <varun.u28@gmail.com>
2022-02-15 16:03:49 -0800

    Update Kth Largest Element in an Array.java

--- Medium/Kth Largest Element in an Array.java
+++ Medium/Kth Largest Element in an Array.java
@@ -1,10 +1,12 @@
 class Solution {
   public int findKthLargest(int[] nums, int k) {
-    PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
-    pq.addAll(Arrays.stream(nums).boxed().collect(Collectors.toList()));
-    while (k-- > 1) {
-      pq.poll();
+    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    for (int num : nums) {
+      priorityQueue.add(num);
+      if (priorityQueue.size() > k) {
+        priorityQueue.poll();
+      }
     }
-    return pq.poll();
+    return priorityQueue.peek();
   }
 }
