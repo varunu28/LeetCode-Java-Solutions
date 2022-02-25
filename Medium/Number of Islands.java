e024e06c84abfa054e9f942f5572d0da5d096127
Varun Upadhyay <varun.u28@gmail.com>
2022-02-25 14:42:26 -0800

    Update Number of Islands.java

--- Medium/Number of Islands.java
+++ Medium/Number of Islands.java
@@ -1,35 +1,38 @@
 class Solution {
+  
+  private static final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+  
   public int numIslands(char[][] grid) {
-    if (grid.length == 0 || grid[0].length == 0) {
-      return 0;
-    }
-    int rows = grid.length;
-    int cols = grid[0].length;
-    int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    boolean[][] visited = new boolean[rows][cols];
-    int count = 0;
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        if (grid[i][j] == '1' && !visited[i][j]) {
+    int countOfIslands = 0;
+    int numOfRows = grid.length;
+    int numOfCols = grid[0].length;
+    boolean[][] visited = new boolean[numOfRows][numOfCols];
+    for (int i = 0; i < numOfRows; i++) {
+      for (int j = 0; j < numOfCols; j++) {
+        if (!visited[i][j] && grid[i][j] == '1') {
           Queue<int[]> queue = new LinkedList<>();
           queue.add(new int[]{i, j});
-          visited[i][j] = true;
           while (!queue.isEmpty()) {
-            int[] removed = queue.remove();
-            for (int[] dir : dirs) {
-              int newX = removed[0] + dir[0];
-              int newY = removed[1] + dir[1];
-              if (newX < 0 || newX >= rows || newY < 0 || newY >= cols || visited[newX][newY] || grid[newX][newY] == '0') {
-                continue;
+            int size = queue.size();
+            while (size-- > 0) {
+              int[] removed = queue.remove();
+              int currX = removed[0];
+              int currY = removed[1];
+              visited[currX][currY] = true;
+              for (int[] dir : DIRS) {
+                int newX = currX + dir[0];
+                int newY = currY + dir[1];
+                if (newX >= 0 && newY >= 0 && newX < numOfRows && newY < numOfCols && !visited[newX][newY] && grid[newX][newY] == '1') {
+                  queue.add(new int[]{newX, newY});
+                  visited[newX][newY] = true;
+                }
               }
-              visited[newX][newY] = true;
-              queue.add(new int[]{newX, newY});
             }
           }
-          count++;
+          countOfIslands++;
         }
       }
     }
-    return count;
+    return countOfIslands;
   }
 }
