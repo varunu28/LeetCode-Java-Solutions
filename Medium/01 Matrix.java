66a9eeb5912acc1c24fc287e3021c1fb2a612cd9
Varun Upadhyay <varun.u28@gmail.com>
2022-03-02 14:20:35 -0800

    Update 01 Matrix.java

--- Medium/01 Matrix.java
+++ Medium/01 Matrix.java
@@ -1,40 +1,33 @@
-public class Solution {
-    public int[][] updateMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
-                }
-                else {
-                    matrix[i][j] = Integer.MAX_VALUE;
-                }
-            }
+class Solution {
+  private static final int[][] DIRS = {{1, 0},  {0, 1}, {-1, 0}, {0, -1}};
+
+  public int[][] updateMatrix(int[][] mat) {
+    int numRows = mat.length;
+    int numCols = mat[0].length;
+    int[][] distances = new int[numRows][numCols];
+    Queue<int[]> queue = new LinkedList<>();
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+        if (mat[i][j] == 0) {
+          queue.add(new int[]{i, j});
+        } else {
+          distances[i][j] = Integer.MAX_VALUE;
         }
-        
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] d : dirs) {
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-                if (r < 0 || 
-                    r >= m || 
-                    c < 0 || 
-                    c >= n || 
-                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) {
-                    continue;
-                }
-                
-                queue.add(new int[] {r, c});
-                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
-            }
-        }
-        
-        return matrix;
+      }
     }
+    while (!queue.isEmpty()) {
+      int[] removed = queue.remove();
+      int currX = removed[0];
+      int currY = removed[1];
+      for (int[] dir : DIRS) {
+        int newX = currX + dir[0];
+        int newY = currY + dir[1];
+        if (newX >= 0 && newY >= 0 && newX < numRows && newY < numCols && distances[newX][newY] > distances[currX][currY] + 1) {
+          queue.add(new int[]{newX, newY});
+          distances[newX][newY] = distances[currX][currY] + 1;
+        }
+      }
+    }
+    return distances;
+  }
 }
