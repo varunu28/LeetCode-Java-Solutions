5cb844ae2f522e7cbead60fc02c1446116293607
Varun Upadhyay <varun.u28@gmail.com>
2022-03-02 10:19:04 -0800

    Update Reverse Words in a String II.java

--- Medium/Reverse Words in a String II.java
+++ Medium/Reverse Words in a String II.java
@@ -1,27 +1,22 @@
 class Solution {
-    public static void reverseWords(char[] str) {
-        reverse(str, 0, str.length);
-
-        int start = 0;
-        for (int i=0; i<str.length; i++) {
-            if (str[i] == ' ') {
-                reverse(str, start, i);
-                start = i + 1;
-            }
-        }
-
-        reverse(str, start, str.length);
+  public void reverseWords(char[] s) {
+    reverse(s, 0, s.length - 1);
+    int startIdx = 0;
+    for (int i = 0; i < s.length; i++) {
+      if (s[i] == ' ' || i == s.length - 1) {
+        reverse(s, startIdx, (i == s.length - 1 ? i : i - 1));
+        startIdx = i + 1;
+      }
     }
-
-    private static void reverse(char[] str, int start, int end) {
-        for (int i=0; i<(end - start)/2; i++) {
-            swap(str, start+i, end-i-1);
-        }
+  }
+  
+  private void reverse(char[] s, int startIdx, int endIdx) {
+    while (startIdx < endIdx) {
+      char temp = s[startIdx];
+      s[startIdx] = s[endIdx];
+      s[endIdx] = temp;
+      startIdx++;
+      endIdx--;
     }
-
-    private static void swap(char[] str, int i, int j) {
-        char temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-    }
+  }
 }
