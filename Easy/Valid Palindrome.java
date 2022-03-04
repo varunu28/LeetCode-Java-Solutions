1fede887488542a204ee8282abd830b29af496f3
Varun Upadhyay <varun.u28@gmail.com>
2022-03-03 16:49:22 -0800

    Update Valid Palindrome.java

--- Easy/Valid Palindrome.java
+++ Easy/Valid Palindrome.java
@@ -1,25 +1,23 @@
 class Solution {
   public boolean isPalindrome(String s) {
-    int start = 0;
-    int end = s.length() - 1;
-    while (start < end) {
-      if (Character.isLetterOrDigit(s.charAt(start)) && Character.isLetterOrDigit(s.charAt(end))) {
-        char c1 = Character.isLetter(s.charAt(start)) ? Character.toLowerCase(s.charAt(start)) : s.charAt(start);
-        char c2 = Character.isLetter(s.charAt(end)) ? Character.toLowerCase(s.charAt(end)) : s.charAt(end);
-        if (c1 != c2) {
-          return false;
-        }
-        start++;
-        end--;
+    int startIdx = 0;
+    int endIdx = s.length() - 1;
+    while (startIdx < endIdx) {
+      if (!Character.isLetterOrDigit(s.charAt(startIdx))) {
+        startIdx++;
+        continue;
       }
-      else {
-        if (!Character.isLetterOrDigit(s.charAt(start))) {
-          start++;
-        }
-        if (!Character.isLetterOrDigit(s.charAt(end))) {
-          end--;
-        }
+      if (!Character.isLetterOrDigit(s.charAt(endIdx))) {
+        endIdx--;
+        continue;
       }
+      char startChar = Character.isDigit(s.charAt(startIdx)) ? s.charAt(startIdx) : Character.toLowerCase(s.charAt(startIdx));
+      char endChar = Character.isDigit(s.charAt(endIdx)) ? s.charAt(endIdx) : Character.toLowerCase(s.charAt(endIdx));
+      if (startChar != endChar) {
+        return false;
+      }
+      startIdx++;
+      endIdx--;
     }
     return true;
   }
