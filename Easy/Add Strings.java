d78c7f03633ec3e338d12a8876a6ae38b1c07564
Varun Upadhyay <varun.u28@gmail.com>
2022-03-03 20:18:48 -0800

    Update Add Strings.java

--- Easy/Add Strings.java
+++ Easy/Add Strings.java
@@ -1,21 +1,19 @@
 class Solution {
   public String addStrings(String num1, String num2) {
+    int carry = 0;
     StringBuilder sb = new StringBuilder();
     int idxOne = num1.length() - 1;
     int idxTwo = num2.length() - 1;
-    int carry = 0;
     while (idxOne >= 0 || idxTwo >= 0 || carry > 0) {
-      int temp = carry;
       if (idxOne >= 0 && idxTwo >= 0) {
-        temp += Character.getNumericValue(num1.charAt(idxOne--)) + Character.getNumericValue(num2.charAt(idxTwo--));
+        carry += Character.getNumericValue(num1.charAt(idxOne--)) + Character.getNumericValue(num2.charAt(idxTwo--));
       } else if (idxOne >= 0 && idxTwo < 0) {
-        temp += Character.getNumericValue(num1.charAt(idxOne--));
+        carry += Character.getNumericValue(num1.charAt(idxOne--));
       } else if (idxOne < 0 && idxTwo >= 0) {
-        temp += Character.getNumericValue(num2.charAt(idxTwo--));
+        carry += Character.getNumericValue(num2.charAt(idxTwo--));
       }
-      carry = temp > 9 ? 1 : 0;
-      temp = temp > 9 ? temp % 10 : temp;
-      sb.append(temp);
+      sb.append(carry % 10);
+      carry = carry > 9 ? carry / 10 : 0;
     }
     return sb.reverse().toString();
   }
