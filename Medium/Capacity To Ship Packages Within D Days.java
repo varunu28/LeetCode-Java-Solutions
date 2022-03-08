b96fb2607b623c6a16cfcc9ddf6ec8ec324f0603
Varun Upadhyay <varun.u28@gmail.com>
2022-03-08 13:06:10 -0800

    Update Capacity To Ship Packages Within D Days.java

--- Medium/Capacity To Ship Packages Within D Days.java
+++ Medium/Capacity To Ship Packages Within D Days.java
@@ -1,29 +1,31 @@
 class Solution {
-  public int shipWithinDays(int[] weights, int D) {
-    int maxWeight = weights[0];
-    int totalWeight = 0;
+  public int shipWithinDays(int[] weights, int days) {
+    int maximumWeight = 0;
+    int minimumWeight = weights[0];
     for (int weight : weights) {
-      maxWeight = Math.max(maxWeight, weight);
-      totalWeight += weight;
+      maximumWeight += weight;
+      minimumWeight = Math.max(minimumWeight, weight);
     }
-    while (maxWeight < totalWeight) {
-      int midWeight = (maxWeight + totalWeight) / 2;
-      int need = 1;
-      int curr = 0;
-      for (int weight : weights) {
-        if (curr + weight > midWeight) {
-            need += 1;
-            curr = 0;
-        }
-        curr += weight;
-      }
-      if (need > D) {
-        maxWeight = midWeight + 1;
-      }
-      else {
-        totalWeight = midWeight;
+    while (minimumWeight <= maximumWeight) {
+      int currCapacity = (maximumWeight + minimumWeight) / 2;
+      if (isShipmentPossible(weights, days, currCapacity)) {
+        maximumWeight = currCapacity - 1;
+      } else {
+        minimumWeight = currCapacity + 1;
       }
     }
-    return maxWeight;
+    return minimumWeight;
+  }
+
+  private boolean isShipmentPossible(int[] weights, int days, int capacity) {
+    int idx = 0;
+    while (idx < weights.length) {
+      int currWeight = 0;
+      while (idx < weights.length && currWeight + weights[idx] <= capacity) {
+        currWeight += weights[idx++];
+      }
+      days--;
+    }
+    return days >= 0;
   }
 }
