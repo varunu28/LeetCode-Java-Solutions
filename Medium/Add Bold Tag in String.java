b2996e0e07450de6393966577bfbaeb76a4af563
Varun Upadhyay <varun.u28@gmail.com>
2022-03-10 08:37:33 -0800

    Update Add Bold Tag in String.java

--- Medium/Add Bold Tag in String.java
+++ Medium/Add Bold Tag in String.java
@@ -1,30 +1,44 @@
 class Solution {
-  public String addBoldTag(String S, String[] dict) {
-    int N = S.length();
-    boolean[] mask = new boolean[N];
-    for (int i = 0; i < N; ++i) {
-      for (String word: dict) search: {
-        for (int k = 0; k < word.length(); ++k) {
-          if (k + i >= S.length() || S.charAt(k + i) != word.charAt(k)) {
-            break search;
-          }
-        }
-        for (int j = i; j < i + word.length(); ++j) {
-          mask[j] = true;
-        }
+  public String addBoldTag(String s, String[] words) {
+    List<int[]> indexes = new ArrayList<>();
+    for (String word : words) {
+      indexes.addAll(getIndexPair(s, word));
+    }
+    Collections.sort(indexes,
+        Comparator.comparingInt((int[] o) -> o[0]).thenComparingInt(o -> o[1]));
+    Set<Integer> startIdx = new HashSet<>();
+    Set<Integer> endIdx = new HashSet<>();
+    int idx = 0;
+    while (idx < indexes.size()) {
+      int currStart = indexes.get(idx)[0];
+      int currEnd = indexes.get(idx)[1];
+      idx++;
+      while (idx < indexes.size() && indexes.get(idx)[0] <= currEnd + 1) {
+        currEnd = Math.max(currEnd, indexes.get(idx++)[1]);
+      }
+      startIdx.add(currStart);
+      endIdx.add(currEnd);
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      if (startIdx.contains(i)) {
+        sb.append("<b>");
+      }
+      sb.append(s.charAt(i));
+      if (endIdx.contains(i)) {
+        sb.append("</b>");
       }
     }
-    StringBuilder ans = new StringBuilder();
-    int anchor = 0;
-    for (int i = 0; i < N; ++i) {
-      if (mask[i] && (i == 0 || !mask[i - 1])) {
-        ans.append("<b>");
-      }
-      ans.append(S.charAt(i));
-      if (mask[i] && (i == N - 1 || !mask[i + 1])) {
-        ans.append("</b>");
+    return sb.toString();
+  }
+
+  private List<int[]> getIndexPair(String s, String word) {
+    List<int[]> indexPairs = new ArrayList<>();
+    for (int i = 0; i < s.length() - word.length() + 1; i++) {
+      if (s.startsWith(word, i)) {
+        indexPairs.add(new int[]{i, i + word.length() - 1});
       }
     }
-    return ans.toString();
+    return indexPairs;
   }
 }
