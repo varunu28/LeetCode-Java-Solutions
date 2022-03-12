25d2fb29a89e63c5dcca684ff21d6e41a39163e2
Varun Upadhyay <varun.u28@gmail.com>
2022-03-12 07:37:35 -0800

    Update Monotonic Array.java

--- Easy/Monotonic Array.java
+++ Easy/Monotonic Array.java
@@ -1,25 +1,18 @@
 class Solution {
-  public boolean isMonotonic(int[] A) {
-    if (A.length <= 1) {
+  public boolean isMonotonic(int[] nums) {
+    if (nums.length < 2) {
       return true;
     }
-    int dir = 0;
-    int idx = 0;
-    while (idx < A.length - 1 && dir == 0) {
-      dir = getDir(A, idx);
-      idx++;
-    }
-    while (idx < A.length - 1) {
-      int tempDir = getDir(A, idx);
-      if (tempDir != 0 && tempDir != dir) {
+    Boolean increasing = null;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == nums[i - 1] && increasing == null) {
+        continue;
+      }
+      increasing = increasing == null ? nums[i] > nums[i - 1] : increasing;
+      if ((nums[i] > nums[i - 1] && !increasing) || (nums[i] < nums[i - 1] && increasing)) {
         return false;
       }
-      idx++;
     }
     return true;
-  }
-  
-  private int getDir(int[] A, int idx) {
-    return A[idx] < A[idx + 1] ? -1 : (A[idx] > A[idx + 1] ? 1 : 0);
   }
 }
