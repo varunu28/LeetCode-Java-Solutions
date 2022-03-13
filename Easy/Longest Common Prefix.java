8e0759dce6b7c400d3cf33f42e98fe5bd7606050
Varun Upadhyay <varun.u28@gmail.com>
2022-03-12 19:02:38 -0800

    Update Longest Common Prefix.java

--- Easy/Longest Common Prefix.java
+++ Easy/Longest Common Prefix.java
@@ -1,30 +1,22 @@
 class Solution {
   public String longestCommonPrefix(String[] strs) {
-    if (strs.length == 0) {
-      return "";
-    }
     int minLength = Integer.MAX_VALUE;
-    int idx = -1;
+    int minLengthIdx = -1;
     for (int i = 0; i < strs.length; i++) {
       if (strs[i].length() < minLength) {
         minLength = strs[i].length();
-        idx = i;
+        minLengthIdx = i;
       }
     }
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < strs[idx].length(); i++) {
-      boolean flag = true;
+    for (int i = 0; i < strs[minLengthIdx].length(); i++) {
       for (String str : strs) {
-        if (str.charAt(i) != strs[idx].charAt(i)) {
-          flag = false;
-          break;
+        if (str.charAt(i) != strs[minLengthIdx].charAt(i)) {
+          return sb.toString();
         }
       }
-      if (!flag) {
-        break;
-      }
-      sb.append(strs[idx].charAt(i));
+      sb.append(strs[minLengthIdx].charAt(i));
     }
     return sb.toString();
-  }
+  }   
 }
