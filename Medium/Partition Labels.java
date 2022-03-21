9e1c92a3d38302235091cd5afd94c682846d94ba
Varun Upadhyay <varun.u28@gmail.com>
2022-03-21 08:22:48 +0530

    Update Partition Labels.java

--- Medium/Partition Labels.java
+++ Medium/Partition Labels.java
@@ -1,22 +1,19 @@
 class Solution {
-  public List<Integer> partitionLabels(String S) {
-    if (S == null || S.length() == 0) {
-      return new ArrayList<>();
+  public List<Integer> partitionLabels(String s) {
+    Map<Character, Integer> lastIndex = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      lastIndex.put(s.charAt(i), i);
     }
-    int[] count = new int[26];
-    for (int i = 0; i < S.length(); i++) {
-      count[S.charAt(i) - 'a'] = i;
-    }
-    int start = -1;
-    int end = 0;
-    List<Integer> list = new ArrayList<>();
-    for (int i = 0; i < S.length(); i++) {
-      end = Math.max(end, count[S.charAt(i) - 'a']);
-      if (end == i) {
-        list.add(end - start);
-        start = end;
+    List<Integer> partitionIndices = new ArrayList<>();
+    int currMaxIdx = -1;
+    int prevIdx = 0;
+    for (int i = 0; i < s.length(); i++) {
+      currMaxIdx = Math.max(currMaxIdx, lastIndex.get(s.charAt(i)));
+      if (i == currMaxIdx) {
+        partitionIndices.add(i - prevIdx + 1);
+        prevIdx = i + 1;
       }
     }
-    return list;
+    return partitionIndices;
   }
 }
