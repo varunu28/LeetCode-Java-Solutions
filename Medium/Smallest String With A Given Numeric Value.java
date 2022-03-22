38c9b25378168ed996f9ef91951b17a53f10b9d5
Varun Upadhyay <varun.u28@gmail.com>
2022-03-22 08:36:33 +0530

    Update Smallest String With A Given Numeric Value.java

--- Medium/Smallest String With A Given Numeric Value.java
+++ Medium/Smallest String With A Given Numeric Value.java
@@ -1,14 +1,11 @@
 class Solution {
   public String getSmallestString(int n, int k) {
     StringBuilder sb = new StringBuilder();
-    for (int idx = 0; idx < n; idx++) {
-      for (int currChar = 26; currChar > 0; currChar--) {
-        if (currChar <= k && k - currChar >= (n - idx - 1)) {
-          sb.append((char) (97 + currChar - 1));
-          k -= currChar;
-          break;
-        }
-      }
+    while (k > 0) {
+      int maxPossible = Math.min(k - n + 1, 26);
+      sb.append((char) ('a' + maxPossible - 1));
+      k -= maxPossible;
+      n--;
     }
     return sb.reverse().toString();
   }
