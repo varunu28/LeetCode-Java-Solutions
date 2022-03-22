f5ee3f0bc7352864af3b29c6ef61059b5ef93246
Varun Upadhyay <varun.u28@gmail.com>
2022-03-23 02:01:38 +0530

    Update Reorder Data in Log Files.java

--- Easy/Reorder Data in Log Files.java
+++ Easy/Reorder Data in Log Files.java
@@ -3,33 +3,23 @@ class Solution {
     List<String> letterLogs = new ArrayList<>();
     List<String> digitLogs = new ArrayList<>();
     for (String log : logs) {
-      String[] strs = log.split("\\s+");
-      if (Character.isDigit(strs[1].charAt(0))) {
+      String[] split = log.split("\\s+");
+      if (Character.isDigit(split[1].charAt(0))) {
         digitLogs.add(log);
-      }
-      else {
+      } else {
         letterLogs.add(log);
       }
     }
-    Collections.sort(letterLogs, new Comparator<String>(){
-      public int compare(String s1, String s2) {
-        String key1 = s1.substring(s1.indexOf(' ') + 1);
-        String key2 = s2.substring(s2.indexOf(' ') + 1);
-        int c = key1.compareTo(key2);
-        if (c != 0) {
-          return c;
-        }
-        return s1.substring(0, s1.indexOf(' ')).compareTo(s2.substring(0, s2.indexOf(' ')));
+    letterLogs.sort((o1, o2) -> {
+      String contentOne = o1.substring(o1.indexOf(" ") + 1);
+      String contentTwo = o2.substring(o2.indexOf(" ") + 1);
+      int c = contentOne.compareTo(contentTwo);
+      if (c != 0) {
+        return c;
       }
+      return o1.substring(0, o1.indexOf(" ")).compareTo(o2.substring(0, o2.indexOf(" ")));
     });
-    String[] ans = new String[logs.length];
-    int idx = 0;
-    for (String log : letterLogs) {
-      ans[idx++] = log;
-    }
-    for (String log : digitLogs) {
-      ans[idx++] = log;
-    }
-    return ans;
+    letterLogs.addAll(digitLogs);
+    return letterLogs.toArray(new String[0]);
   }
 }
