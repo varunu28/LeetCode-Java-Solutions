ee812ba77c0b4fd239e670b3376a4fd0b991eb8c
Varun Upadhyay <varun.u28@gmail.com>
2022-03-29 23:34:14 +0530

    Update Climbing Stairs.java

--- Easy/Climbing Stairs.java
+++ Easy/Climbing Stairs.java
@@ -1,20 +1,14 @@
 class Solution {
-  int[] memo;
   public int climbStairs(int n) {
     if (n == 1) {
       return 1;
     }
-    memo = new int[n + 1];
-    memo[1] = 1;
-    memo[2] = 2;
-    return helper(n);
-  }
-  
-  private int helper(int n) {
-    if (memo[n] != 0) {
-      return memo[n];
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
     }
-    memo[n] = helper(n - 1) + helper(n - 2);
-    return memo[n];
+    return dp[n];
   }
 }
