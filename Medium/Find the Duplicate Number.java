dbf5f10f181595f8977e83536765b1d3a39786fb
Varun Upadhyay <varun.u28@gmail.com>
2022-03-29 10:23:13 +0530

    Update and rename Find the duplicate number.java to Find the Duplicate Number.java

---
+++ Medium/Find the Duplicate Number.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int findDuplicate(int[] nums) {
+    int slow = nums[0];
+    int fast = nums[0];
+    do {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    } while (slow != fast);
+    slow = nums[0];
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+    return fast;
+  }
+}
