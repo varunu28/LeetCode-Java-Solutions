f1ed2bc744460d93bb250754478241c9bacdaaf9
Varun Upadhyay <varun.u28@gmail.com>
2022-03-30 08:57:26 +0530

    Update Search a 2D Matrix.java

--- Medium/Search a 2D Matrix.java
+++ Medium/Search a 2D Matrix.java
@@ -1,23 +1,18 @@
 class Solution {
   public boolean searchMatrix(int[][] matrix, int target) {
-    if (matrix.length == 0 || matrix[0].length == 0) {
-      return false;
-    }
     int rowIdx = matrix.length - 1;
     int colIdx = matrix[0].length - 1;
     while (rowIdx >= 0 && colIdx >= 0) {
-      int val = matrix[rowIdx][colIdx];
-      if (val == target) {
+      if (matrix[rowIdx][colIdx] == target) {
         return true;
       }
-      if (val >= matrix[rowIdx][0]) {
+      if (matrix[rowIdx][colIdx] > target) {
         colIdx--;
         if (colIdx < 0) {
           colIdx = matrix[0].length - 1;
           rowIdx--;
         }
-      }
-      else {
+      } else {
         rowIdx--;
       }
     }
