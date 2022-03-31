b7f991fc70349fb80607a22a525724b7335ddbeb
Varun Upadhyay <varun.u28@gmail.com>
2022-03-31 21:05:41 +0530

    Update Flip Game.java

--- Easy/Flip Game.java
+++ Easy/Flip Game.java
@@ -1,30 +1,11 @@
 class Solution {
-    public static List<String> generatePossibleNextMoves(String s) {
-        List<String> ans = new ArrayList<>();
-
-        if (s.length() < 2) {
-            return ans;
-        }
-
-        if (s.equals("--")) {
-            return ans;
-        }
-
-        int i = 0;
-        while (i < s.length()-1) {
-            if (s.charAt(i) == s.charAt(i+1) && s.charAt(i) == '+') {
-                StringBuilder sb = new StringBuilder();
-                sb.append(s.substring(0, i));
-                sb.append('-');
-                sb.append('-');
-                sb.append(s.substring(i+2));
-
-                ans.add(sb.toString());
-            }
-
-            i++;
-        }
-
-        return ans;
+  public List<String> generatePossibleNextMoves(String currentState) {
+    List<String> list = new ArrayList<>();
+    for (int i = 1; i < currentState.length(); i++) {
+      if (currentState.charAt(i) == '+' && currentState.charAt(i - 1) == '+') {
+        list.add(currentState.substring(0, i - 1) + "--" + currentState.substring(i + 1));
+      }
     }
+    return list;
+  }
 }
