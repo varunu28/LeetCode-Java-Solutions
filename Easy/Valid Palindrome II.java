8a945a7197645e7a1601707bd23a3993e93d7fe5
Varun Upadhyay <varun.u28@gmail.com>
2022-04-02 09:44:22 +0530

    Update Valid Palindrome II.java

--- Easy/Valid Palindrome II.java
+++ Easy/Valid Palindrome II.java
@@ -1,21 +1,26 @@
 class Solution {
   public boolean validPalindrome(String s) {
-    int[] firstPalindromeCheck = isPalindromHelper(s);
-    if (firstPalindromeCheck[0] == -1) {
-      return true;
-    }
-    int[] skipLeft = isPalindromHelper(s.substring(firstPalindromeCheck[0] + 1, firstPalindromeCheck[1] + 1));
-    int[] skipRight = isPalindromHelper(s.substring(firstPalindromeCheck[0], check[1]));
-    return skipLeft[0] == -1 || skipRight[0] == -1;
-  }
-
-  private int[] isPalindromHelper(String s) {
     int startIdx = 0;
     int endIdx = s.length() - 1;
-    while (startIdx < endIdx && s.charAt(startIdx) == s.charAt(endIdx)) {
+    while (startIdx < endIdx) {
+      if (s.charAt(startIdx) != s.charAt(endIdx)) {
+        return isPalindromeWithDeletion(s, startIdx + 1, endIdx) || 
+          isPalindromeWithDeletion(s, startIdx, endIdx - 1);
+      }
       startIdx++;
       endIdx--;
     }
-    return startIdx >= endIdx ? new int[]{-1} : new int[]{startIdx, endIdx};
+    return true;
+  } 
+  
+  private boolean isPalindromeWithDeletion(String s, int startIdx, int endIdx) {
+    while (startIdx < endIdx) {
+      if (s.charAt(startIdx) != s.charAt(endIdx)) {
+        return false;
+      }
+      startIdx++;
+      endIdx--;
+    }
+    return true;
   }
 }
