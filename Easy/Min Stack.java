8bf6e50eda11c4dee38e14bc661837cab31c6f38
Varun Upadhyay <varun.u28@gmail.com>
2022-04-02 12:57:35 +0530

    Update Min Stack.java

--- Easy/Min Stack.java
+++ Easy/Min Stack.java
@@ -1,36 +1,32 @@
 class MinStack {
-
-    /** initialize your data structure here. */
-  Stack<Integer> stack;
-  Stack<Integer> min;
+  private final Stack<int[]> stack;
+  
   public MinStack() {
-    stack = new Stack<>();
-    min = new Stack<>();
-  }
+    this.stack = new Stack<>();
+  } 
 
-  public void push(int x) {
-    stack.push(x);
-    min.push(Math.min(x, min.isEmpty() ? Integer.MAX_VALUE : min.peek()));
+  public void push(int val) {
+    int min = stack.isEmpty() ? val : Math.min(val, stack.peek()[1]);
+    stack.push(new int[]{val, min});
   }
 
   public void pop() {
     stack.pop();
-    min.pop();
   }
 
   public int top() {
-    return stack.peek();
+    return stack.peek()[0];
   }
 
   public int getMin() {
-    return min.peek();
+    return stack.peek()[1];
   }
 }
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(x);
+ * obj.push(val);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
