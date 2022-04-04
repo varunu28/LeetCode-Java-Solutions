1e41ec3004df116a68f4614be1df47ed9ccea18b
Varun Upadhyay <varun.u28@gmail.com>
2022-04-04 23:25:49 +0530

    Update Course Schedule II.java

--- Medium/Course Schedule II.java
+++ Medium/Course Schedule II.java
@@ -1,29 +1,30 @@
 class Solution {
   public int[] findOrder(int numCourses, int[][] prerequisites) {
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    int[] prereqCount = new int[numCourses];
+    Map<Integer, List<Integer>> prerequisiteToCourseDependency = new HashMap<>();
+    int[] prerequisiteCount = new int[numCourses];
     for (int[] prerequisite : prerequisites) {
-      prereqCount[prerequisite[0]]++;
-      map.computeIfAbsent(prerequisite[1], k -> new ArrayList<>()).add(prerequisite[0]);
+      prerequisiteToCourseDependency.computeIfAbsent(prerequisite[1], k -> new ArrayList<>())
+        .add(prerequisite[0]);
+      prerequisiteCount[prerequisite[0]]++;
     }
     Queue<Integer> queue = new LinkedList<>();
     for (int i = 0; i < numCourses; i++) {
-      if (prereqCount[i] == 0) {
+      if (prerequisiteCount[i] == 0) {
         queue.add(i);
       }
     }
-    int[] ans = new int[numCourses];
-    int idx = 0;
+    int[] courseOrder = new int[numCourses];
+    int courseOrderIdx = 0;
     while (!queue.isEmpty()) {
-      int removed = queue.remove();
-      for (Integer dependentCourse : map.getOrDefault(removed, new ArrayList<>())) {
-        prereqCount[dependentCourse]--;
-        if (prereqCount[dependentCourse] == 0) {
+      int course = queue.remove();
+      courseOrder[courseOrderIdx++] = course;
+      for (Integer dependentCourse : prerequisiteToCourseDependency.getOrDefault(course, new ArrayList<>())) {
+        prerequisiteCount[dependentCourse]--;
+        if (prerequisiteCount[dependentCourse] == 0) {
           queue.add(dependentCourse);
         }
       }
-      ans[idx++] = removed;
     }
-    return idx == numCourses ? ans : new int[]{};
+    return courseOrderIdx == numCourses ? courseOrder : new int[]{};
   }
 }
