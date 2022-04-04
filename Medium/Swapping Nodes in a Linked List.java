979fdd6146bdbe91b287260a8822f0e2255f78f3
Varun Upadhyay <varun.u28@gmail.com>
2022-04-04 12:03:57 +0530

    Update Swapping Nodes in a Linked List.java

--- Medium/Swapping Nodes in a Linked List.java
+++ Medium/Swapping Nodes in a Linked List.java
@@ -10,22 +10,44 @@
  */
 class Solution {
   public ListNode swapNodes(ListNode head, int k) {
-    ListNode fast = null;
+    int listLength = 0;
     ListNode curr = head;
-    int currCount = 1;
-    while (currCount != k) {
-      curr = curr.next;
-      currCount++;
-    }
-    fast = curr;
-    ListNode slow = head;
-    while (curr.next != null) {
-      slow = slow.next;
+    while (curr != null) {
+      listLength++;
       curr = curr.next;
     }
-    int temp = fast.val;
-    fast.val = slow.val;
-    slow.val = temp;
+    ListNode startPrev = null;
+    ListNode start = head;
+    ListNode endPrev = null;
+    ListNode end = head;
+    int count = k - 1;
+    while (count > 0) {
+      startPrev = start;
+      start = start.next;
+      count--;
+    }
+    count = listLength - k;
+    while (count > 0) {
+      endPrev = end;
+      end = end.next;
+      count--;
+    }
+    if (startPrev != null) {
+      startPrev.next = end;
+    }
+    if (endPrev != null) {
+      endPrev.next = start;
+    }
+    ListNode temp = start.next;
+    start.next = end.next;
+    end.next = temp;
+    // Updating head pointers if required
+    if (k == 1) {
+      head = end;
+    } 
+    if (k == listLength) {
+      head = start;
+    }
     return head;
   }
 }
