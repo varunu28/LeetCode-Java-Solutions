328a063b06c410439d6af2d0d3b868b523a77537
Varun Upadhyay <varun.u28@gmail.com>
2022-04-05 14:01:54 +0530

    Update Rank Teams by Votes.java

--- Medium/Rank Teams by Votes.java
+++ Medium/Rank Teams by Votes.java
@@ -1,32 +1,27 @@
 class Solution {
   public String rankTeams(String[] votes) {
-    Map<Character, int[]> map = new HashMap<>();
-    int n = votes[0].length();
-    for(String vote : votes) {
-      for (int i = 0; i < n; i++) {
-        if (!map.containsKey(vote.charAt(i))) {
-          map.put(vote.charAt(i), new int[n]);
-        }
-        map.get(vote.charAt(i))[i]++;
+    Map<Character, Map<Integer, Integer>> map = new HashMap<>();
+    int positionCount = votes[0].length();
+    for (String vote : votes) {
+      for (int i = 0; i < vote.length(); i++) {
+        char c = vote.charAt(i);
+        map.computeIfAbsent(c, k -> new HashMap<>());
+        map.get(c).put(i, map.get(c).getOrDefault(i, 0) + 1);
       }
     }
-    PriorityQueue<Character> pq = new PriorityQueue<>(new Comparator<Character>(){
-      public int compare(Character c1, Character c2) {
-        for (int i = 0; i < n; i++) {
-          int c = map.get(c2)[i] - map.get(c1)[i];
-          if (c != 0) {
-            return c;
-          }
+    PriorityQueue<Character> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+      for (int i = 0; i < positionCount; i++) {
+        int c = map.get(o2).getOrDefault(i, 0) - map.get(o1).getOrDefault(i, 0);
+        if (c != 0) {
+          return c;
         }
-        return c1 - c2;
       }
+      return o1 - o2;
     });
-    for (char c : votes[0].toCharArray()) {
-      pq.add(c);
-    }
+    priorityQueue.addAll(map.keySet());
     StringBuilder sb = new StringBuilder();
-    while (!pq.isEmpty()) {
-      sb.append(pq.poll());
+    while (!priorityQueue.isEmpty()) {
+      sb.append(priorityQueue.poll());
     }
     return sb.toString();
   }
