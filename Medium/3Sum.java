01c7d2c13a410f366181d2fda0a2cd71bcf955b2
Varun Upadhyay <varun.u28@gmail.com>
2022-04-06 08:44:32 +0530

    Update 3Sum.java

--- Medium/3Sum.java
+++ Medium/3Sum.java
@@ -1,29 +1,22 @@
 class Solution {
   public List<List<Integer>> threeSum(int[] nums) {
     Arrays.sort(nums);
-    List<List<Integer>> list = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
     for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
       if (i == 0 || nums[i - 1] != nums[i]) {
-        twoSumHelper(nums, i, list);
+        Set<Integer> set = new HashSet<>();
+        for (int j = i + 1; j < nums.length; j++) {
+          int target = -1 * (nums[i] + nums[j]);
+          if (set.contains(target)) {
+            result.add(Arrays.asList(nums[i], nums[j], target));
+            while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+              j++;
+            }
+          }
+          set.add(nums[j]);
+        }
       }
     }
-    return list;
-  }
-  
-  private void twoSumHelper(int[] nums, int i, List<List<Integer>> list) {
-    int low = i + 1;
-    int high = nums.length - 1;
-    while (low < high) {
-      int sum = nums[i] + nums[low] + nums[high];
-      if (sum < 0 || (low > i + 1 && nums[low] == nums[low - 1])) {
-        low++;
-      }
-      else if (sum > 0 || (high < nums.length - 1 && nums[high] == nums[high + 1])) {
-        high--;
-      }
-      else {
-        list.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
-      }
-    }
+    return result;
   }
 }
