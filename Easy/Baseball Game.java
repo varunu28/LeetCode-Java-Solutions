ff59c63575cffa067ea4c78d3087be04d7f816dd
Varun Upadhyay <varun.u28@gmail.com>
2022-04-13 13:38:06 +0530

    Update Baseball Game.java

--- Easy/Baseball Game.java
+++ Easy/Baseball Game.java
@@ -1,28 +1,25 @@
 class Solution {
   public int calPoints(String[] ops) {
     Stack<Integer> stack = new Stack<>();
-    int sum = 0;
+    int totalScore = 0;
     for (String op : ops) {
-      if (op.equals("+")) {
-        int prev = stack.pop();
-        int prevToPrev = stack.peek();
-        int newScore = prev + prevToPrev;
-        sum += newScore;
-        stack.push(prev);
+      if (op.equals("D")) {
+        int newScore = 2 * stack.peek();
+        totalScore += newScore;
         stack.push(newScore);
-      }
-      else if (op.equals("C")) {
-        sum -= stack.pop();
-      }
-      else if (op.equals("D")) {
-        sum += stack.peek() * 2;
-        stack.push(stack.peek() * 2);
-      }
-      else {
+      } else if (op.equals("C")) {
+        totalScore -= stack.pop();
+      } else if (op.equals("+")) {
+        int scoreTwo = stack.pop();
+        int newScore = scoreTwo + stack.peek();
+        stack.push(scoreTwo);
+        stack.push(newScore);
+        totalScore += newScore;
+      } else {
         stack.push(Integer.parseInt(op));
-        sum += stack.peek();
+        totalScore += Integer.parseInt(op);
       }
     }
-    return sum;
+    return totalScore;
   }
 }
