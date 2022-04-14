4766ba505d22243e9c04a6eb0bf32cfe7bc23c0a
Varun Upadhyay <varun.u28@gmail.com>
2022-04-14 06:43:08 +0530

    Update Search in a Binary Search Tree.java

--- Easy/Search in a Binary Search Tree.java
+++ Easy/Search in a Binary Search Tree.java
@@ -16,16 +16,11 @@
 class Solution {
   public TreeNode searchBST(TreeNode root, int val) {
     if (root == null) {
-      return null;
+      return root;
     }
     if (root.val == val) {
       return root;
     }
-    else if (root.val > val) {
-      return searchBST(root.left, val);
-    }
-    else {
-      return searchBST(root.right, val);
-    }
+    return root.val > val ? searchBST(root.left, val) : searchBST(root.right, val);
   }
 }
