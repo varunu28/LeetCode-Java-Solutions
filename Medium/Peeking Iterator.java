ca2f90813f6c01fa90cac0d40e5d03bfbb1b298d
Varun Upadhyay <varun.u28@gmail.com>
2022-04-24 18:00:51 -0700

    Update Peeking Iterator.java

--- Medium/Peeking Iterator.java
+++ Medium/Peeking Iterator.java
@@ -2,34 +2,35 @@
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
 class PeekingIterator implements Iterator<Integer> {
-  Integer nextElement;
-  Iterator<Integer> iterator;
+  private Iterator<Integer> iterator;
+  private Integer topElement;
+
   public PeekingIterator(Iterator<Integer> iterator) {
     // initialize any member here.
     this.iterator = iterator;
-    updateNext();
+    populateTopElement();
   }
-  
-  private void updateNext() {
-    nextElement = iterator.hasNext() ? iterator.next() : null;
-  }
-	
+
   // Returns the next element in the iteration without advancing the iterator.
   public Integer peek() {
-    return nextElement;
+    return this.topElement;
   }
-	
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
-    Integer returnValue = nextElement;
-    updateNext();
-    return returnValue;
-	}
-	
-	@Override
-	public boolean hasNext() {
-	  return nextElement != null;
-	}
+
+  // hasNext() and next() should behave the same as in the Iterator interface.
+  // Override them if needed.
+  @Override
+  public Integer next() {
+    Integer nextValue = this.topElement;
+    populateTopElement();
+    return nextValue;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return this.topElement != null;
+  }
+
+  private void populateTopElement() {
+    this.topElement = this.iterator.hasNext() ? this.iterator.next() : null;
+  }
 }
