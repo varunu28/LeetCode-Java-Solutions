1b1755b0297415fcd71eadd8d5f7dfb1ad60f6c7
Varun Upadhyay <varun.u28@gmail.com>
2022-04-26 12:08:55 -0700

    Update As Far from Land as Possible.java

--- Medium/As Far from Land as Possible.java
+++ Medium/As Far from Land as Possible.java
@@ -1,36 +1,37 @@
 class Solution {
-    public int maxDistance(int[][] grid) {
-        if (grid.length == 0 || grid[0].length == 0) {
-            return -1;
+  private final int[][] DIRS = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+  
+  public int maxDistance(int[][] grid) {
+    Queue<int[]> queue = new LinkedList<>();
+    int numRows = grid.length;
+    int numCols = grid[0].length;
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+        if (grid[i][j] == 1) {
+          queue.add(new int[]{i, j});
         }
-        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    queue.add(new int[]{i, j});
-                    visited[i][j] = true;
-                }
-            }
-        }
-        int maxDist = -1;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
-                int[] curr = queue.remove();
-                for (int[] dir : dirs) {
-                    int x = curr[0] + dir[0];
-                    int y = curr[1] + dir[1];
-                    if (x >= 0 &&  x < grid.length && y >= 0 && y < grid[0].length && !visited[x][y] && grid[x][y] == 0) {
-                        visited[x][y] = true;
-                        queue.add(new int[]{x, y});
-                    }
-                }
-            }
-            maxDist++;
-        }
-
-        return maxDist == 0 ? -1 : maxDist;
+      }
     }
+    if (queue.isEmpty() || queue.size() == numRows * numCols) {
+      return -1;
+    }
+    int distance = 0;
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      while (size-- > 0) {
+        int[] removed = queue.remove();
+        for (int[] dir : DIRS) {
+          int x = removed[0] + dir[0];
+          int y = removed[1] + dir[1];
+          if (x < 0 || y < 0 || x >= numRows || y >= numCols || grid[x][y] == 1) {
+            continue;
+          }
+          grid[x][y] = 1;
+          queue.add(new int[]{x, y});
+        }
+      }
+      distance++;
+    }
+    return distance - 1; 
+  }
 }
