859072dd3fd786f565b549e4319d2d7613a89749
Varun Upadhyay <varun.u28@gmail.com>
2022-04-28 07:42:27 -0700

    Update Path With Minimum Effort.java

--- Medium/Path With Minimum Effort.java
+++ Medium/Path With Minimum Effort.java
@@ -1,38 +1,47 @@
 class Solution {
+  
+  private final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  
   public int minimumEffortPath(int[][] heights) {
-    int rows = heights.length;
-    int cols = heights[0].length;
-    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    int[][] distance = new int[rows][cols];
-    for (int i = 0; i < rows; i++) {
-      Arrays.fill(distance[i], Integer.MAX_VALUE);
+    if (heights.length == 1 && heights[0].length == 1) {
+      return 0;
     }
-    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-    pq.add(new int[]{0, 0, 0});
-    while (!pq.isEmpty()) {
-      int[] removed = pq.poll();
-      int currDistance = removed[0];
-      int x = removed[1];
-      int y = removed[2];
-      if (currDistance > distance[x][y]) {
-        continue;
-      }
-      if (x == rows - 1 && y == cols - 1) {
-        return currDistance;
-      }
-      for (int[] direction : directions) {
-        int newX = x + direction[0];
-        int newY = y + direction[1];
-        if (newX < 0 || newX >= rows || newY < 0 || newY >= cols) {
-          continue;
-        }
-        int newDistance = Math.max(currDistance, Math.abs(heights[newX][newY] - heights[x][y]));
-        if (distance[newX][newY] > newDistance) {
-          distance[newX][newY] = newDistance;
-          pq.add(new int[]{newDistance, newX, newY});
-        }
+    int leftBound = 0;
+    int rightBound = 1000000;
+    while (leftBound < rightBound) {
+      int mid = (leftBound + rightBound) / 2;
+      if (connected(heights, mid)) {
+        rightBound = mid;
+      } else {
+        leftBound = mid + 1;
       }
     }
-    return -1;
+    return leftBound;
+  }
+  
+  private boolean connected(int[][] heights, int targetEffort) {
+    int numRows = heights.length;
+    int numCols = heights[0].length;
+    Queue<int[]> queue = new LinkedList<>();
+    boolean[][] seen = new boolean[numRows][numCols];
+    queue.add(new int[]{0, 0});
+    seen[0][0] = true;
+    while (!queue.isEmpty()) {
+      int[] removed = queue.remove();
+      int x = removed[0];
+      int y = removed[1];
+      if (x == numRows - 1 && y == numCols - 1) {
+        return true;
+      }
+      for (int[] dir : DIRS) {
+        int newX = x + dir[0];
+        int newY = y + dir[1];
+        if (newX >= 0 && newY >= 0 && newX < numRows && newY < numCols && !seen[newX][newY] && Math.abs(heights[x][y] - heights[newX][newY]) <= targetEffort) {
+          queue.add(new int[]{newX, newY});
+          seen[newX][newY] = true;
+        }
+      }
+    }
+    return false;
   }
 }
