5be1437bd2b37ef515fa1c72e973f179f1860481
Varun Upadhyay <varun.u28@gmail.com>
2022-04-28 18:15:03 -0700

    Update Design Tic-Tac-Toe.java

--- Medium/Design Tic-Tac-Toe.java
+++ Medium/Design Tic-Tac-Toe.java
@@ -1,66 +1,50 @@
 class TicTacToe {
 
-  /** Initialize your data structure here. */
-  int[][] board;
-  Map<Integer, Map<Integer, Integer>> rowMap;
-  Map<Integer, Map<Integer, Integer>> colMap;
-  Map<Integer, Integer> leftDiagonalMap;
-  Map<Integer, Integer> rightDiagonalMap;
-  int n;
+  private int n;
+  private Map<Integer, Map<Integer, Integer>> rowMapping;
+  private Map<Integer, Map<Integer, Integer>> colMapping;
+  private Map<Integer, Map<Integer, Integer>> diagonalMapping;
+  
+  private final int LEFT_DIAGONAL = 1;
+  private final int RIGHT_DIAGONAL = -1;
+  
   public TicTacToe(int n) {
-    board = new int[n][n];
-    rowMap = new HashMap<>();
-    colMap = new HashMap<>();
-    leftDiagonalMap = new HashMap<>();
-    rightDiagonalMap = new HashMap<>();
     this.n = n;
+    this.rowMapping = new HashMap<>();
+    this.colMapping = new HashMap<>();
+    this.diagonalMapping = new HashMap<>();
+    this.diagonalMapping.put(LEFT_DIAGONAL, new HashMap<>());
+    this.diagonalMapping.put(RIGHT_DIAGONAL, new HashMap<>());
   }
 
-  /** Player {player} makes a move at ({row}, {col}).
-      @param row The row of the board.
-      @param col The column of the board.
-      @param player The player, can be either 1 or 2.
-      @return The current winning condition, can be either:
-              0: No one wins.
-              1: Player 1 wins.
-              2: Player 2 wins. */
   public int move(int row, int col, int player) {
-    if (!rowMap.containsKey(row)) {
-      Map<Integer, Integer> map = new HashMap<>();
-      rowMap.put(row, map);
-    }
-    rowMap.get(row).put(player, rowMap.get(row).getOrDefault(player, 0) + 1);
-    if (ifWinner(rowMap.get(row), player)) {
-      return player;
-    }
-    if (!colMap.containsKey(col)) {
-      Map<Integer, Integer> map = new HashMap<>();
-      colMap.put(col, map);
-    }
-    colMap.get(col).put(player, colMap.get(col).getOrDefault(player, 0) + 1);
-    if (ifWinner(colMap.get(col), player)) {
-      return player;
-    }
-    if (row == col) {
-      leftDiagonalMap.put(player, leftDiagonalMap.getOrDefault(player, 0) + 1);
-    }
-    if (ifWinner(leftDiagonalMap, player)) {
-      return player;
-    }
-    if (row + col == n - 1) {
-      rightDiagonalMap.put(player, rightDiagonalMap.getOrDefault(player, 0) + 1);
-    }
-    if (ifWinner(rightDiagonalMap, player)) {
+    recordMove(row, col, player);
+    if (isWinnerFound(row, col, player)) {
       return player;
     }
     return 0;
   }
   
-  private boolean ifWinner(Map<Integer, Integer> map, int player) {
-    if (map.size() > 1) {
-      return false;
+  private void recordMove(int row, int col, int player) {
+    this.rowMapping.computeIfAbsent(row, k -> new HashMap<>());
+    this.colMapping.computeIfAbsent(col, k -> new HashMap<>());
+    this.rowMapping.get(row).put(player, this.rowMapping.get(row).getOrDefault(player, 0) + 1);
+    this.colMapping.get(col).put(player, this.colMapping.get(col).getOrDefault(player, 0) + 1);
+    if (row == col) {
+      this.diagonalMapping.get(LEFT_DIAGONAL)
+        .put(player, this.diagonalMapping.get(LEFT_DIAGONAL).getOrDefault(player, 0) + 1);
+    } 
+    if (row + col + 1 == this.n) {
+      this.diagonalMapping.get(RIGHT_DIAGONAL)
+        .put(player, this.diagonalMapping.get(RIGHT_DIAGONAL).getOrDefault(player, 0) + 1);
     }
-    return map.values().stream().reduce(0, Integer::sum) == n;
+  }
+  
+  private boolean isWinnerFound(int row, int col, int player) {
+    return this.rowMapping.get(row).get(player) == this.n ||
+      this.colMapping.get(col).get(player) == this.n ||
+      this.diagonalMapping.get(LEFT_DIAGONAL).getOrDefault(player, 0) == this.n ||
+      this.diagonalMapping.get(RIGHT_DIAGONAL).getOrDefault(player, 0) == this.n;
   }
 }
 
