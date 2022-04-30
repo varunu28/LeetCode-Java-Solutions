1465f4041408af759db71b314207341aaabaaa08
Varun Upadhyay <varun.u28@gmail.com>
2022-04-30 06:42:41 -0700

    Update Custom Sort String.java

--- Medium/Custom Sort String.java
+++ Medium/Custom Sort String.java
@@ -1,16 +1,23 @@
 class Solution {
   public String customSortString(String order, String s) {
-    Map<Character, Integer> map = new HashMap<>();
-    int position = 1;
-    for (char c : order.toCharArray()) {
-      map.put(c, position++);
+    Map<Character, Integer> frequency = new HashMap<>();
+    for (char c : s.toCharArray()) {
+      frequency.put(c, frequency.getOrDefault(c, 0) + 1);
     }
-    return s.chars()
-        .mapToObj(c -> (char) c)
-        .sorted(Comparator.comparing(o -> map.getOrDefault(o, 0)))
-        .collect(Collector.of(StringBuilder::new,
-          StringBuilder::append,
-          StringBuilder::append,
-          StringBuilder::toString));
+    StringBuilder sb = new StringBuilder();
+    for (char c : order.toCharArray()) {
+      int count = frequency.getOrDefault(c, 0);
+      while (count-- > 0) {
+        sb.append(c);
+      }
+      frequency.put(c, 0);
+    }
+    for (Character key : frequency.keySet()) {
+      int count = frequency.getOrDefault(key, 0);
+      while (count-- > 0) {
+        sb.append(key);
+      }
+    }
+    return sb.toString();
   }
 }
