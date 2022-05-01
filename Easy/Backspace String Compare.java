5541c9340b5c71e8964bf7e97327ec2afed63a87
Varun Upadhyay <varun.u28@gmail.com>
2022-04-30 19:01:12 -0700

    Update Backspace String Compare.java

--- Easy/Backspace String Compare.java
+++ Easy/Backspace String Compare.java
@@ -1,45 +1,23 @@
 class Solution {
-  public boolean backspaceCompare(String S, String T) {
-    int i = S.length() - 1;
-    int j = T.length() - 1;
-    int skipS = 0;
-    int skipT = 0;
-    while (i >= 0 || j >= 0) {
-      while (i >= 0) {
-        if (S.charAt(i) == '#') {
-          skipS++;
-          i--;
+  public boolean backspaceCompare(String s, String t) {
+    return formBackSpaceString(s).equals(formBackSpaceString(t));
+  }
+  
+  private String formBackSpaceString(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (c == '#') {
+        if (!stack.isEmpty()) {
+          stack.pop();
         }
-        else if (skipS > 0) {
-          skipS--;
-          i--;
-        }
-        else {
-          break;
-        }
+      } else {
+        stack.push(c);
       }
-      while (j >= 0) {
-        if (T.charAt(j) == '#') {
-          skipT++;
-          j--;
-        }
-        else if (skipT > 0) {
-          skipT--;
-          j--;
-        }
-        else {
-          break;
-        }
-      }
-      if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j)) {
-        return false;
-      }
-      if ((i >= 0) != (j >= 0)) {
-        return false;
-      }
-      i--;
-      j--;
     }
-    return true;
+    StringBuilder sb = new StringBuilder();
+    while (!stack.isEmpty()) {
+      sb.append(stack.pop());
+    }
+    return sb.toString();
   }
 }
