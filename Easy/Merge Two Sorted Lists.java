b2188e01c553bf7a7a8267be6d15ea31458d7ac7
Varun Upadhyay <varun.u28@gmail.com>
2022-05-02 06:57:58 -0700

    Update Merge Two Sorted Lists.java

--- Easy/Merge Two Sorted Lists.java
+++ Easy/Merge Two Sorted Lists.java
@@ -10,26 +10,38 @@
  */
 class Solution {
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    ListNode dummy = new ListNode(-1);
-    ListNode curr = dummy;
-    while (list1 != null || list2 != null) {
-      if (list1 != null && list2 != null) {
-        if (list1.val > list2.val) {
-          curr.next = new ListNode(list2.val);
-          list2 = list2.next;
-        } else {
-          curr.next = new ListNode(list1.val);
-          list1 = list1.next;
+    if (list1 == null || list2 == null) {
+      return list1 == null ? list2 : list1;
+    }
+    ListNode prev = null;
+    ListNode head = null;
+    while (list1 != null && list2 != null) {
+      if (list1.val <= list2.val) {
+        prev = list1;
+        if (head == null) {
+          head = list1;
         }
-      } else if (list1 != null && list2 == null) {
-        curr.next = new ListNode(list1.val);
         list1 = list1.next;
       } else {
-        curr.next = new ListNode(list2.val);
+        ListNode node = list2;
         list2 = list2.next;
+        if (prev == null) {
+          node.next = list1;
+          prev = node;
+          head = prev;
+        } else {
+          prev.next = node;
+          node.next = list1;
+          prev = node;
+        }
       }
-      curr = curr.next;
     }
-    return dummy.next;
+    if (list2 != null) {
+      prev.next = list2;
+    }
+    if (list1 != null) {
+      prev.next = list1;
+    }
+    return head;
   }
 }
