cd0ecac29ce2007cf950adb7f7b23b5c70d6d521
Varun Upadhyay <varun.u28@gmail.com>
2022-05-03 16:26:21 -0700

    Update Intersection of Three Sorted Arrays.java

--- Easy/Intersection of Three Sorted Arrays.java
+++ Easy/Intersection of Three Sorted Arrays.java
@@ -1,21 +1,28 @@
 class Solution {
   public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
-    int[] counter = new int[2001];
-    updateCounter(counter, arr1);
-    updateCounter(counter, arr2);
-    updateCounter(counter, arr3);
-    List<Integer> ans = new ArrayList<>();
-    for (int i = 0; i < 2001; i++) {
-      if (counter[i] == 3) {
-        ans.add(i);
+    int idxOne = 0;
+    int idxTwo = 0;
+    int idxThree = 0;
+    List<Integer> result = new ArrayList<>();
+    while (idxOne < arr1.length && idxTwo < arr2.length && idxThree < arr3.length) {
+      if (arr1[idxOne] == arr2[idxTwo] && arr2[idxTwo] == arr3[idxThree]) {
+        result.add(arr1[idxOne]);
+        idxOne++;
+        idxTwo++;
+        idxThree++;
+      } else {
+        int max = Math.max(arr1[idxOne], Math.max(arr2[idxTwo], arr3[idxThree]));
+        if (arr1[idxOne] < max) {
+          idxOne++;
+        }
+        if (arr2[idxTwo] < max) {
+          idxTwo++;
+        }
+        if (arr3[idxThree] < max) {
+          idxThree++;
+        }
       }
     }
-    return ans;
-  }
-  
-  private void updateCounter(int[] counter, int[] arr) {
-    for (int num : arr) {
-      counter[num]++;
-    }
+    return result;
   }
 }
