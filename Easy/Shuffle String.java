399f4965cc4368b487c6eddfbf5c4d77826e0609
Varun Upadhyay <varun.u28@gmail.com>
2022-05-03 13:38:56 -0700

    Update Shuffle String.java

--- Easy/Shuffle String.java
+++ Easy/Shuffle String.java
@@ -1,9 +1,24 @@
 class Solution {
   public String restoreString(String s, int[] indices) {
-    char[] letters = new char[s.length()];
+    char[] letters = s.toCharArray();
     for (int i = 0; i < indices.length; i++) {
-      letters[indices[i]] = s.charAt(i);
+      while (indices[i] != i) {
+        swapLetter(letters, i, indices[i]);
+        swapIndex(indices, i, indices[i]); 
+      }
     }
     return String.valueOf(letters);
+  }
+
+  private void swapLetter(char[] arr, int idxOne, int idxTwo) {
+    char temp = arr[idxOne];
+    arr[idxOne] = arr[idxTwo];
+    arr[idxTwo] = temp;
+  }
+
+  private void swapIndex(int[] arr, int idxOne, int idxTwo) {
+    int temp = arr[idxOne];
+    arr[idxOne] = arr[idxTwo];
+    arr[idxTwo] = temp;
   }
 }
