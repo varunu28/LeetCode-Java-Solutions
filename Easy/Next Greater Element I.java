c5acea2e79f6c8fda5f0c2599ac697d196b7df39
Varun Upadhyay <varun.u28@gmail.com>
2022-05-03 16:28:02 -0700

    Update Next Greater Element I.java

--- Easy/Next Greater Element I.java
+++ Easy/Next Greater Element I.java
@@ -1,20 +1,18 @@
 class Solution {
   public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    Map<Integer, Integer> map = new HashMap<>();
     Stack<Integer> stack = new Stack<>();
-    int[] nextMaximum = new int[nums2.length];
-    Map<Integer, Integer> indexMap = new HashMap<>();
     for (int i = nums2.length - 1; i >= 0; i--) {
       while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
         stack.pop();
       }
-      nextMaximum[i] = stack.isEmpty() ? -1 : stack.peek();
+      map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
       stack.push(nums2[i]);
-      indexMap.put(nums2[i], i);
     }
-    int[] nextMaximumResult = new int[nums1.length];
+    int[] result = new int[nums1.length];
     for (int i = 0; i < nums1.length; i++) {
-      nextMaximumResult[i] = nextMaximum[indexMap.get(nums1[i])];
+      result[i] = map.getOrDefault(nums1[i], -1);
     }
-    return nextMaximumResult;
+    return result;
   }
 }
