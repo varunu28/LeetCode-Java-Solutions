13fc282f2f8ad4ac4b2de4a11e6ef98d68bf114f
Varun Upadhyay <varun.u28@gmail.com>
2022-05-04 10:59:58 -0700

    Update Verifying an Alien Dictionary.java

--- Easy/Verifying an Alien Dictionary.java
+++ Easy/Verifying an Alien Dictionary.java
@@ -1,31 +1,27 @@
 class Solution {
   public boolean isAlienSorted(String[] words, String order) {
-    Map<Character, Integer> dictionary = new HashMap<>();
-    for (int i = 0; i < order.length(); i++) {
-      dictionary.put(order.charAt(i), i);
+    Map<Character, Integer> map = new HashMap<>();
+    int idx = 0;
+    for (char c : order.toCharArray()) {
+      map.put(c, idx++);
     }
-    for (int i = 0; i < words.length - 1; i++) {
-      if (!hasCorrectOrder(words[i], words[i + 1], dictionary)) {
+    for (int i = 1; i < words.length; i++) {
+      if (!isSorted(words[i - 1], words[i], map)) {
         return false;
       }
     }
     return true;
   }
   
-  private boolean hasCorrectOrder(String firstWord, String secondWord, Map<Character, Integer> dictionary) {
-    int idxOne = 0;
-    int idxTwo = 0;
-    boolean correctOrder = false;
-    while (idxOne < firstWord.length() && idxTwo < secondWord.length()) {
-      int dictionaryDiff = dictionary.get(firstWord.charAt(idxOne++)) - dictionary.get(secondWord.charAt(idxTwo++));
-      if (dictionaryDiff > 0) {
+  private boolean isSorted(String wordOne, String wordTwo, Map<Character, Integer> map) {
+    for (int i = 0; i < Math.min(wordOne.length(), wordTwo.length()); i++) {
+      int diff = map.get(wordOne.charAt(i)) - map.get(wordTwo.charAt(i));
+      if (diff > 0) {
         return false;
-      } 
-      if (dictionaryDiff < 0) {
-        correctOrder = true;
-        break;
+      } else if (diff < 0) {
+        return true;
       }
     }
-    return correctOrder || firstWord.length() <= secondWord.length();
+    return wordOne.length() <= wordTwo.length();
   }
 }
