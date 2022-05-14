a41e4bb94dbbbab2f0a4513c27169cd27495078a
Varun Upadhyay <varun.u28@gmail.com>
2022-05-14 17:59:40 +0530

    Update Implement Stack using Queues.java

--- Easy/Implement Stack using Queues.java
+++ Easy/Implement Stack using Queues.java
@@ -1,44 +1,52 @@
-import java.util.ArrayList;
-
 class MyStack {
+  
+  private Queue<Integer> primaryQueue;
+  private Queue<Integer> secondaryQueue;
+  
+  public MyStack() {
+    this.primaryQueue = new LinkedList<>();
+    this.secondaryQueue = new LinkedList<>();
+  }
 
-    /** Initialize your data structure here. */
-    ArrayList<Integer> arrNew = new ArrayList<>();
-    ArrayList<Integer> arrOld = new ArrayList<>();
+  public void push(int x) {
+    this.primaryQueue.add(x);
+  }
 
+  public int pop() {
+    int result = moveFromPrimaryToSecondary();
+    moveFromSecondaryToPrimary(true);
+    return result;
+  }
 
-    /** Push element x onto stack. */
-    public void push(int x) {
-        arrOld.add(x);
+  public int top() {
+    int result = moveFromPrimaryToSecondary();
+    moveFromSecondaryToPrimary(false);
+    return result;
+  }
+  
+  public boolean empty() {
+    return this.primaryQueue.isEmpty();
+  }
+  
+  private int moveFromPrimaryToSecondary() {
+    int last = this.primaryQueue.peek();
+    while (!this.primaryQueue.isEmpty()) {
+      last = this.primaryQueue.peek();
+      this.secondaryQueue.add(this.primaryQueue.remove());
     }
-
-    /** Removes the element on top of the stack and returns that element. */
-    public int pop() {
-        for (int i=0;i<=arrOld.size()-1;i++) {
-            arrNew.add(arrOld.get(i));
-        }
-        int k = arrNew.get(arrNew.size()-1);
-        arrNew.remove(arrNew.indexOf(k));
-
-        arrOld.clear();
-        return k;
+    return last;
+  }
+  
+  private void moveFromSecondaryToPrimary(boolean removeLast) {
+    while (this.secondaryQueue.size() > 1) {
+      this.primaryQueue.add(this.secondaryQueue.remove());
     }
-
-    /** Get the top element. */
-    public int top() {
-        for (int i=0;i<=arrOld.size()-1;i++) {
-            arrNew.add(arrOld.get(i));
-        }
-        int k = arrNew.get(arrNew.size()-1);
-
-        arrOld.clear();
-        return k;
+    if (removeLast) {
+      this.secondaryQueue.remove();
+    } else {
+      this.primaryQueue.add(this.secondaryQueue.remove());
     }
-
-    /** Returns whether the stack is empty. */
-    public boolean empty() {
-        return arrNew.size() == 0 && arrOld.size() == 0;
-    }
+  }
 }
 
 /**
