3d4b057d6d0e3d96bff422ca4b7944bf0d12f51f
Varun Upadhyay <varun.u28@gmail.com>
2022-05-19 05:32:43 +0530

    Update and rename Delete node in a linked list.java to Delete Node in a Linked List.java

---
+++ Easy/Delete Node in a Linked List.java
@@ -0,0 +1,15 @@
+/**
+* Definition for singly-linked list.
+* public class ListNode {
+*     int val;
+*     ListNode next;
+*     ListNode(int x) { val = x; }
+* }
+*/
+class Solution {
+  public void deleteNode(ListNode node) {
+    int nextVal = node.next.val;
+    node.next = node.next.next;
+    node.val = nextVal;
+  }
+}
