255b9a17444f937b27458de030b84307e1f1c62c
Varun Upadhyay <varun.u28@gmail.com>
2022-05-24 14:55:19 -0700

    Update Diet Plan Performance.java

--- Easy/Diet Plan Performance.java
+++ Easy/Diet Plan Performance.java
@@ -1,27 +1,16 @@
 class Solution {
-    int points;
-    public int dietPlanPerformance(int[] calories, int k, int lower, int upper) {
-        points = 0;
-        int totalCal = 0;
-        int start = 0;
-        for (int i = 0; i < k; i++) {
-            totalCal += calories[i];
-        }
-        for (int i = k; i < calories.length; i++) {    
-            updatePoints(totalCal, lower, upper);
-            totalCal -= calories[start++];
-            totalCal += calories[i];
-        }
-        updatePoints(totalCal, lower, upper);
-        return points;
+  public int dietPlanPerformance(int[] calories, int k, int lower, int upper) {
+    int points = 0;
+    int totalCalories = 0;
+    for (int i = 0; i < k - 1; i++) {
+      totalCalories += calories[i];
     }
-    
-    private void updatePoints(int currCal, int lower, int upper) {
-        if (currCal < lower) {
-            points--;
-        }
-        if (currCal > upper) {
-            points++;
-        }
+    for (int i = k - 1; i < calories.length; i++) {
+      totalCalories += calories[i];
+      points += totalCalories < lower ? -1 : 0;
+      points += totalCalories > upper ? 1 : 0;
+      totalCalories -= calories[i - k + 1];
     }
+    return points;
+  }
 }
