9845f6f98ea1538a0ef70e2941beaf160b14a7dd
Varun Upadhyay <varun.u28@gmail.com>
2022-05-24 14:43:21 -0700

    Update Strobogrammatic Number.java

--- Easy/Strobogrammatic Number.java
+++ Easy/Strobogrammatic Number.java
@@ -2,13 +2,11 @@ class Solution {
   public boolean isStrobogrammatic(String num) {
     StringBuilder sb = new StringBuilder();
     for (char c : num.toCharArray()) {
-      if (c == '0' || c == '1' || c == '8') {
-        sb.append(c);
-      }
-      else if (c == '6' || c == '9') {
+      if (c == '6' || c == '9') {
         sb.append(c == '6' ? '9' : '6');
-      }
-      else {
+      } else if (c == '0' || c == '8' || c == '1') {
+        sb.append(c);
+      } else {
         return false;
       }
     }
