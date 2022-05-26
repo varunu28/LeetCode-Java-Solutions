a887be56e2797d4cc3f3634ac66131d864a5c62a
Varun Upadhyay <varun.u28@gmail.com>
2022-05-26 15:51:09 -0700

    Update Two Sum IV - Input is a BST.java

--- Easy/Two Sum IV - Input is a BST.java
+++ Easy/Two Sum IV - Input is a BST.java
@@ -4,46 +4,29 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-        Stack<TreeNode> stack = new Stack<>();
-        while (root != null) {
-            stack.push(root);
-            root = root.left;
-        }
-        
-        Set<Integer> set = new HashSet<>();
-        int min = popStack(stack);
-        set.add(min);
-        
-        while (!stack.isEmpty()) {
-            int num = popStack(stack);
-            if (set.contains(k-num)) {
-                return true;
-            }
-            
-            if (min + num > k) {
-                return false;
-            }
-            
-            set.add(num);
-        }
-        
-        return false;
+  public boolean findTarget(TreeNode root, int k) {
+    Set<Integer> set = new HashSet<>();
+    return helper(root, k, set);
+  }
+  
+  private boolean helper(TreeNode root, int k, Set<Integer> set) {
+    if (root == null) {
+      return false;
     }
-    
-    private int popStack(Stack<TreeNode> stack) {
-        TreeNode n = stack.pop();
-        TreeNode right = n.right;
-        
-        while (right != null) {
-            stack.push(right);
-            right = right.left;
-        }
-        
-        return n.val;
+    if (set.contains(k - root.val)) {
+      return true;
     }
+    set.add(root.val);
+    return helper(root.left, k, set) || helper(root.right, k, set);
+  }
 }
