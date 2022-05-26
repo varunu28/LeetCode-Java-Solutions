2c015331641fcaf9c414ba9aee8f50845b18aa89
Varun Upadhyay <varun.u28@gmail.com>
2022-05-26 12:50:23 -0700

    Update How Many Apples Can You Put into the Basket.java

--- Easy/How Many Apples Can You Put into the Basket.java
+++ Easy/How Many Apples Can You Put into the Basket.java
@@ -1,12 +1,18 @@
 class Solution {
-  public int maxNumberOfApples(int[] arr) {
-    int weight = 0;
-    int currCount = 0;
-    Arrays.sort(arr);
-    for (int i = 0; i < arr.length && weight + arr[i] <= 5000; i++) {
-      weight += arr[i];
-      currCount++;
+  public int maxNumberOfApples(int[] weight) {
+    int[] counter = new int[1001];
+    for (int w : weight) {
+      counter[w]++;
     }
-    return currCount;
+    int numberOfApples = 0;
+    int totalWeight = 0;
+    for (int i = 1; i < counter.length && totalWeight <= 5000; i++) {
+      int count = counter[i];
+      while (count-- > 0 && totalWeight + i <= 5000) {
+        numberOfApples++;
+        totalWeight += i;
+      }
+    }
+    return numberOfApples;
   }
 }
