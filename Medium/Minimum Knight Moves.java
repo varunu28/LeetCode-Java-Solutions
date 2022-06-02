7ee88c3075731cf57b4a7d5d3b78e7243c7a7658
Varun Upadhyay <varun.u28@gmail.com>
2022-06-02 14:59:10 -0700

    Update Minimum Knight Moves.java

--- Medium/Minimum Knight Moves.java
+++ Medium/Minimum Knight Moves.java
@@ -1,35 +1,27 @@
 class Solution {
-  public final int[][] DIRS = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+  private final int[][] DIRS = {{1, 2}, {2, 1}, {1, -2}, {-2, 1}, {2, -1}, {-2, -1}, {-1, -2}, {-1, 2}};
+  
   public int minKnightMoves(int x, int y) {
-    int currX = 0;
-    int currY = 0;
-    int targetX = Math.abs(x);
-    int targetY = Math.abs(y);
-    Set<String> visited = new HashSet<>();
-    Queue<int[]> queue = new LinkedList<>();
     int numOfSteps = 0;
-    visited.add(currX + "|" + currY);
-    queue.add(new int[]{currX, currY});
+    boolean[][] visited = new boolean[607][607];
+    Queue<int[]> queue = new LinkedList<>();
+    queue.add(new int[]{0, 0});
     while (!queue.isEmpty()) {
       int size = queue.size();
-      boolean found = false;
       while (size-- > 0) {
         int[] removed = queue.remove();
-        if (removed[0] == targetX && removed[1] == targetY) {
-          found = true;
-          break;
+        if (removed[0] == x && removed[1] == y) {
+          return numOfSteps;
         }
         for (int[] dir : DIRS) {
           int newX = removed[0] + dir[0];
           int newY = removed[1] + dir[1];
-          if (!visited.contains(newX + "|" + newY) && newX >= -1 && newY >= -1) {
-            visited.add(newX + "|" + newY);
-            queue.add(new int[]{newX, newY});
+          if (visited[newX + 302][newY + 302]) {
+            continue;
           }
+          visited[newX + 302][newY + 302] = true;
+          queue.add(new int[]{newX, newY});
         }
-      }
-      if (found) {
-        break;
       }
       numOfSteps++;
     }
