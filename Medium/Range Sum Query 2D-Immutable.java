7839237825b95e83f64bd7762179fb37f97e6535
Varun Upadhyay <varun.u28@gmail.com>
2022-06-03 07:15:49 -0700

    Update Range Sum Query 2D-Immutable.java

--- Medium/Range Sum Query 2D-Immutable.java
+++ Medium/Range Sum Query 2D-Immutable.java
@@ -1,21 +1,28 @@
 class NumMatrix {
-  int[][] dp;
+
+  int rows;
+  int cols;
+  int[][] rowLevelSum;
   public NumMatrix(int[][] matrix) {
-    if (matrix.length == 0 || matrix[0].length == 0) {
-      return;
-    }
-    dp = new int[matrix.length][matrix[0].length + 1];
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[0].length; j++) {
-        dp[i][j + 1] = dp[i][j] + matrix[i][j];
+    this.rows = matrix.length;
+    this.cols = matrix[0].length;
+    this.rowLevelSum = new int[rows][cols];
+    for (int i = 0; i < rows; i++) {
+      int sum = 0;
+      for (int j = 0; j < cols; j++) {
+        sum += matrix[i][j];
+        this.rowLevelSum[i][j] = sum;
       }
     }
   }
 
   public int sumRegion(int row1, int col1, int row2, int col2) {
     int sum = 0;
-    for (int row = row1; row <= row2; row++) {
-      sum += dp[row][col2 + 1] - dp[row][col1];
+    for (int i = row1; i <= row2; i++) {
+      sum += rowLevelSum[i][col2];
+      if (col1 > 0) {
+        sum -= rowLevelSum[i][col1 - 1];
+      }
     }
     return sum;
   }
