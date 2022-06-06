1f62fb365709dfc7f75978ef60799923a957195d
Varun Upadhyay <varun.u28@gmail.com>
2022-06-06 15:10:37 -0700

    Update Relative Sort Array.java

--- Easy/Relative Sort Array.java
+++ Easy/Relative Sort Array.java
@@ -1,32 +1,21 @@
 class Solution {
   public int[] relativeSortArray(int[] arr1, int[] arr2) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < arr2.length; i++) {
-      map.put(arr2[i], i);
-    }
-    PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
-      public int compare(Integer o1, Integer o2) {
-        return map.get(o1) - map.get(o2);
-      }
-    });
-    List<Integer> notPresent = new ArrayList<>();
+    int[] frequency = new int[1001];
     for (int num : arr1) {
-      if (map.containsKey(num)) {
-        pq.add(num);
-      }
-      else {
-        notPresent.add(num);
-      }
+      frequency[num]++;
     }
-    int[] ans = new int[arr1.length];
+    int[] result = new int[arr1.length];
     int idx = 0;
-    while (!pq.isEmpty()) {
-      ans[idx++] = pq.poll();
+    for (int num : arr2) {
+      while (frequency[num]-- > 0) {
+        result[idx++] = num;
+      }
     }
-    Collections.sort(notPresent);
-    for (int num : notPresent) {
-      ans[idx++] = num;
+    for (int i = 0; i < 1001; i++) {
+      while (frequency[i]-- > 0) {
+        result[idx++] = i;
+      }
     }
-    return ans;
+    return result;
   }
 }
