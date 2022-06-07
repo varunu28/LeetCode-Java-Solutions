1e8651b810299592d9fc5ea033227a822173edc3
Varun Upadhyay <varun.u28@gmail.com>
2022-06-07 09:49:08 -0700

    Update Reverse Words in a String.java

--- Medium/Reverse Words in a String.java
+++ Medium/Reverse Words in a String.java
@@ -1,28 +1,49 @@
 class Solution {
   public String reverseWords(String s) {
-    Stack<String> stack = new Stack<>();
-    StringBuilder sb = new StringBuilder();
-    int idx = 0;
-    int n = s.length();
-    while (idx < n) {
-      if (s.charAt(idx) != ' ') {
-        sb.append(s.charAt(idx));
+    StringBuilder sb = trimSpace(s);
+    reverse(sb, 0, sb.length() - 1);
+    int start = 0;
+    int end = 0;
+    int n = sb.length();
+    while (start < n) {
+      while (end < n && sb.charAt(end) != ' ') {
+        end++;
       }
-      if (s.charAt(idx) == ' ' || idx == n - 1) {
-        if (sb.length() > 0) {
-          stack.push(sb.toString());
-          sb.setLength(0);
-        }
-      }
-      idx++;
-    }
-    sb.setLength(0);
-    while (!stack.isEmpty()) {
-      sb.append(stack.pop());
-      if (!stack.isEmpty()) {
-        sb.append(" ");
-      }
+      reverse(sb, start, end - 1);
+      start = end + 1;
+      end++;
     }
     return sb.toString();
+  }
+  
+  private void reverse(StringBuilder sb, int start, int end) {
+    while (start <= end) {
+      char temp = sb.charAt(start);
+      sb.setCharAt(start++, sb.charAt(end));
+      sb.setCharAt(end--, temp);
+    }
+  }
+  
+  private StringBuilder trimSpace(String s) {
+    StringBuilder sb = new StringBuilder();
+    int start = 0;
+    int end = s.length() - 1;
+    while (start <= end && s.charAt(start) == ' ') {
+      start++;
+    }
+    while (end >= start && s.charAt(end) == ' ') {
+      end--;
+    }
+    while (start <= end) {
+      if (s.charAt(start) != ' ') {
+        sb.append(s.charAt(start++));
+      } else {
+        if (sb.charAt(sb.length() - 1) != ' ') {
+          sb.append(s.charAt(start));
+        }
+        start++;
+      }
+    }
+    return sb;
   }
 }
