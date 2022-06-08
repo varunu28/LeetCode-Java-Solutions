404be1017861fad7c7e4e6f8dd0af52a8e95523b
Varun Upadhyay <varun.u28@gmail.com>
2022-06-08 11:35:32 -0700

    Update Longest Palindromic Substring.java

--- Medium/Longest Palindromic Substring.java
+++ Medium/Longest Palindromic Substring.java
@@ -1,26 +1,24 @@
 class Solution {
-    
-    public boolean isPalindrome(String s,int begin, int end) {
-        if (begin < 0) return false;
-        while(begin < end) {
-            if (s.charAt(begin++) != s.charAt(end--)) return false; 
-        }
-        return true;
+  public String longestPalindrome(String s) {
+    int start = 0;
+    int end = 0;
+    for (int i = 0; i < s.length(); i++) {
+      int lenOne = helper(s, i, i);
+      int lenTwo = helper(s, i, i + 1);
+      int maxLength = Math.max(lenOne, lenTwo);
+      if (maxLength > end - start) {
+        start = i - (maxLength - 1) / 2;
+        end = i + maxLength / 2;
+      }
     }
-    
-    public String longestPalindrome(String s) {
-        String res = "";
-        int currLength = 0;
-        for (int i=0;i<s.length();i++) {
-            if (isPalindrome(s,i-currLength-1,i)) {
-                res = s.substring(i-currLength-1,i+1);
-                currLength += 2;
-            } 
-            else if (isPalindrome(s,i-currLength,i)) {
-                res = s.substring(i-currLength,i+1);
-                currLength += 1;
-            }
-        }
-        return res;
+    return s.substring(start, end + 1);
+  }
+  
+  private int helper(String s, int left, int right) {
+    while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+      left--;
+      right++;
     }
+    return right - left - 1;
+  }
 }
