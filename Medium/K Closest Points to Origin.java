927d5a48cf82a965ef9b0cac6616556c08896cec
Varun Upadhyay <varun.u28@gmail.com>
2022-06-08 11:34:18 -0700

    Update K Closest Points to Origin.java

--- Medium/K Closest Points to Origin.java
+++ Medium/K Closest Points to Origin.java
@@ -1,15 +1,20 @@
 class Solution {
   public int[][] kClosest(int[][] points, int k) {
-    PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-      double diff =  Math.sqrt(o1[0] * o1[0] + o1[1] * o1[1]) - Math.sqrt(
-          o2[0] * o2[0] + o2[1] * o2[1]);
-      return diff < 0 ? -1 : (diff > 0 ? 1 : 0);
-    });
-    Collections.addAll(pq, points);
-    int[][] result = new int[k][2];
-    for (int i = 0; i < k; i++) {
+    PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> getDistance(o2) - getDistance(o1));
+    for (int[] point : points) {
+      pq.add(point);
+      if (pq.size() > k) {
+        pq.poll();
+      }
+    }
+    int[][] result = new int[pq.size()][2];
+    for (int i = 0; i < result.length; i++) {
       result[i] = pq.poll();
     }
     return result;
+  }
+
+  private int getDistance(int[] point) {
+    return (point[0] * point[0]) + (point[1] * point[1]);
   }
 }
