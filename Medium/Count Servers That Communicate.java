ead55b4b53b49331b586c3b4611f12e51189a7c2
Varun Upadhyay <varun.u28@gmail.com>
2022-06-10 16:34:04 -0700

    Update Count Servers That Communicate.java

--- Medium/Count Servers That Communicate.java
+++ Medium/Count Servers That Communicate.java
@@ -1,31 +1,25 @@
 class Solution {
-    public int countServers(int[][] grid) {
-        if (grid.length == 0 || grid[0].length == 0) {
-            return 0;
+  public int countServers(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+    int[] rowCount = new int[m];
+    int[] colCount = new int[n];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == 1) {
+          rowCount[i]++;
+          colCount[j]++;
         }
-        int numRows = grid.length;
-        int numCols = grid[0].length;
-        int[] rowCount = new int[numRows];
-        int[] colCount = new int[numCols];
-        int numOfServers = 0;
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                if (grid[i][j] == 1) {
-                    numOfServers++;
-                    rowCount[i]++;
-                    colCount[j]++;
-                }
-            }
-        }
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                if (grid[i][j] == 1) {
-                    if (rowCount[i] == 1 && colCount[j] == 1) {
-                        numOfServers--;
-                    }
-                }
-            }
-        }
-        return numOfServers;
+      }
     }
+    int connectedServerCount = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)) {
+          connectedServerCount++;
+        }
+      }
+    }
+    return connectedServerCount;
+  }
 }
