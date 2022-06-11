ca359c3435bfefb3f4e8fc50594a708e18fca15e
Varun Upadhyay <varun.u28@gmail.com>
2022-06-11 07:02:33 -0700

    Update Minimum Operations to Reduce X to Zero.java

--- Medium/Minimum Operations to Reduce X to Zero.java
+++ Medium/Minimum Operations to Reduce X to Zero.java
@@ -1,26 +1,21 @@
 class Solution {
   public int minOperations(int[] nums, int x) {
-    int n = nums.length;
-    int leftIdx = 0;
-    int sum = 0;
-    int ans = -1;
-    for (; leftIdx < n && sum < x; leftIdx++) {
-      sum += nums[leftIdx];
+    int totalSum = 0;
+    for (int num : nums) {
+      totalSum += num;
     }
-    if (sum == x) {
-      ans = leftIdx;
-    }
-    leftIdx--;
-    int rightIdx = n - 1;
-    for (; leftIdx >= 0; leftIdx--) {
-      sum -= nums[leftIdx];
-      while (rightIdx > leftIdx && sum < x) {
-        sum += nums[rightIdx--];
+    int max = -1;
+    int left = 0;
+    int current = 0;
+    for (int right = 0; right < nums.length; right++) {
+      current += nums[right];
+      while (current > totalSum - x && left <= right) {
+        current -= nums[left++];
       }
-      if (sum == x) {
-        ans = ans == -1 ? (leftIdx + n - rightIdx - 1) : Math.min(ans, (leftIdx + n - rightIdx - 1));
+      if (current == totalSum - x) {
+        max = Math.max(max, right - left + 1);
       }
     }
-    return ans;
+    return max != -1 ? nums.length - max : -1;
   }
 }
