cd4bc6c1b12b257c6aee3880d46d26429871d541
Varun Upadhyay <varun.u28@gmail.com>
2022-06-13 20:09:39 -0700

    Update Delete Operation for Two Strings.java

--- Medium/Delete Operation for Two Strings.java
+++ Medium/Delete Operation for Two Strings.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int[][] memo = new int[word1.length() + 1][word2.length() + 1];
-        for (int i = 0; i <= word1.length(); i++) {
-            for (int j = 0; j <= word2.length(); j++) {
-                if (i == 0) {
-                    memo[i][j] = j;
-                }
-                else if (j == 0) {
-                    memo[i][j] = i;
-                }
-                else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    memo[i][j] = memo[i - 1][j - 1];
-                }
-                else {
-                    memo[i][j] = Math.min(Math.min(memo[i - 1][j - 1] + 2, memo[i - 1][j] + 1), 
-                                          memo[i][j - 1] + 1);
-                }
-            }
-        }
-        
-        return memo[word1.length()][word2.length()];
+  public int minDistance(String word1, String word2) {
+    int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+    return word1.length() + word2.length() - 2 * longestCommonSubsequence(word1, word2, word1.length(), word2.length(), dp);
+  }
+  
+  private int longestCommonSubsequence(String word1, String word2, int m, int n, int[][] dp) {
+    if (m == 0 || n == 0) {
+      return 0;
     }
+    if (dp[m][n] > 0) {
+      return dp[m][n];
+    }
+    if (word1.charAt(m - 1) == word2.charAt(n - 1)) {
+      dp[m][n] = 1 + longestCommonSubsequence(word1, word2, m - 1, n - 1, dp);
+    } else {
+      dp[m][n] = Math.max(longestCommonSubsequence(word1, word2, m - 1, n, dp), longestCommonSubsequence(word1, word2, m, n - 1, dp));
+    }
+    return dp[m][n];
+  }
 }
-
