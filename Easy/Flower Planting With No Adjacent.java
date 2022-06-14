80e3f0cf8a0867461a0fb633ae37751055f8291b
Varun Upadhyay <varun.u28@gmail.com>
2022-06-14 15:25:09 -0700

    Update Flower Planting With No Adjacent.java

--- Easy/Flower Planting With No Adjacent.java
+++ Easy/Flower Planting With No Adjacent.java
@@ -1,25 +1,22 @@
 class Solution {
-  public int[] gardenNoAdj(int N, int[][] paths) {
-    Map<Integer, List<Integer>> map = new HashMap<>();
+  public int[] gardenNoAdj(int n, int[][] paths) {
+    Map<Integer, Set<Integer>> map = new HashMap<>();
     for (int[] path : paths) {
-      map.computeIfAbsent(path[0], k -> new ArrayList<>()).add(path[1]);
-      map.computeIfAbsent(path[1], k -> new ArrayList<>()).add(path[0]);
+      map.computeIfAbsent(path[0], k -> new HashSet<>()).add(path[1]);
+      map.computeIfAbsent(path[1], k -> new HashSet<>()).add(path[0]);
     }
-    int[] ans = new int[N];
-    for (int i = 0; i < N; i++) {
-      Set<Integer> neighbourPlant = new HashSet<>();
-      for (Integer neighbour : map.getOrDefault(i + 1, new ArrayList<>())) {
-        if (ans[neighbour - 1] != 0) {
-          neighbourPlant.add(ans[neighbour - 1]);
-        }
-      }
-      for (int j = 1; j <= 4; j++) {
-        if (!neighbourPlant.contains(j)) {
-          ans[i] = j;
-          break;
+    int[] result = new int[n];
+    for (int i = 1; i <= n; i++) {
+      int[] colors = new int[5];
+      for (int neighbor : map.getOrDefault(i, new HashSet<>())) {
+        colors[result[neighbor - 1]] = 1;
+      }      
+      for (int c = 4; c > 0; c--) {
+        if (colors[c] != 1) {
+          result[i - 1] = c;
         }
       }
     }
-    return ans;
+    return result;
   }
 }
