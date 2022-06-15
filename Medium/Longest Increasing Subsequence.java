71611f06830b1b0d3bcba4a0d733160f7f2fdca4
Varun Upadhyay <varun.u28@gmail.com>
2022-06-15 07:36:59 -0700

    Update Longest Increasing Subsequence.java

--- Medium/Longest Increasing Subsequence.java
+++ Medium/Longest Increasing Subsequence.java
@@ -1,21 +1,19 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        
-        for (int i = 1; i < nums.length; i ++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
+  public int lengthOfLIS(int[] nums) {
+    List<Integer> subsequence = new ArrayList<>();
+    subsequence.add(nums[0]);
+    for (int i = 1; i < nums.length; i++) {
+      int currNum = nums[i];
+      if (currNum > subsequence.get(subsequence.size() - 1)) {
+        subsequence.add(currNum);
+      } else {
+        int idx = 0;
+        while (currNum > subsequence.get(idx)) {
+          idx++;
         }
-        
-        int max = Integer.MIN_VALUE;
-        for (int num : dp) {
-            max = Math.max(num, max);
-        }
-        
-        return max == Integer.MIN_VALUE ? 0 : max;
+        subsequence.set(idx, currNum);
+      }
     }
+    return subsequence.size();
+  }
 }
