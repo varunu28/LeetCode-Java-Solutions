8e44e4ee8ae8ef3d8035bbe2d2363443cd8ca33c
Varun Upadhyay <varun.u28@gmail.com>
2022-06-17 12:21:49 -0700

    Update Design Circular Deque.java

--- Medium/Design Circular Deque.java
+++ Medium/Design Circular Deque.java
@@ -1,117 +1,62 @@
-class MyCircularDeque {
-  DequeNode head;
-  DequeNode tail;
-  int k;
-  int currCapacity;
+class MyCircularQueue {
+  private int[] queue;
+  private int valueCursor;
+  private int emptyCursor;
 
-  /** Initialize your data structure here. Set the size of the deque to be k. */
-  public MyCircularDeque(int k) {
-    currCapacity = 0;
-    this.k = k;
-    head = new DequeNode(-1);
-    tail = new DequeNode(-1);
-    head.front = tail;
-    tail.back = head;
+  public MyCircularQueue(int k) {
+    this.queue = new int[k];
+    Arrays.fill(queue, -1);
+    this.emptyCursor = 0;
+    this.valueCursor = 0;
   }
 
-  /** Adds an item at the front of Deque. Return true if the operation is successful. */
-  public boolean insertFront(int value) {
-    if (isFull()) {
+  public boolean enQueue(int value) {
+    if (this.queue[this.emptyCursor] != -1) {
       return false;
     }
-    DequeNode node = new DequeNode(value);
-    node.front = head.front;
-    head.front.back = node;
-    node.back = head;
-    head.front = node;
-    currCapacity++;
+    this.queue[this.emptyCursor++] = value;
+    if (this.emptyCursor == this.queue.length) {
+      this.emptyCursor = 0;
+    }
     return true;
   }
 
-  /** Adds an item at the rear of Deque. Return true if the operation is successful. */
-  public boolean insertLast(int value) {
-    if (isFull()) {
+  public boolean deQueue() {
+    if (this.queue[this.valueCursor] == -1) {
       return false;
     }
-    DequeNode node = new DequeNode(value);
-    node.front = tail;
-    node.back = tail.back;
-    tail.back.front = node;
-    tail.back = node;
-    currCapacity++;
+    this.queue[this.valueCursor++] = -1;
+    if (this.valueCursor == this.queue.length) {
+      this.valueCursor = 0;
+    }
     return true;
   }
 
-  /** Deletes an item from the front of Deque. Return true if the operation is successful. */
-  public boolean deleteFront() {
-    if (isEmpty()) {
-      return false;
-    }
-    DequeNode next = head.front.front;
-    head.front = next;
-    next.back = head;
-    currCapacity--;
-    return true;
+  public int Front() {
+    return this.queue[this.valueCursor];
   }
 
-  /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
-  public boolean deleteLast() {
-    if (isEmpty()) {
-      return false;
-    }
-    DequeNode prev = tail.back.back;
-    tail.back = prev;
-    prev.front = tail;
-    currCapacity--;
-    return true;
+  public int Rear() {
+    int idx = this.emptyCursor == 0 ? this.queue.length - 1 : this.emptyCursor - 1;
+    return this.queue[idx];
   }
 
-  /** Get the front item from the deque. */
-  public int getFront() {
-    if (isEmpty()) {
-      return -1;
-    }
-    return head.front.val;
-  }
-
-  /** Get the last item from the deque. */
-  public int getRear() {
-    if (isEmpty()) {
-      return -1;
-    }
-    return tail.back.val;
-  }
-
-  /** Checks whether the circular deque is empty or not. */
   public boolean isEmpty() {
-    return currCapacity == 0;
+    return this.queue[this.valueCursor] == -1;
   }
 
-  /** Checks whether the circular deque is full or not. */
   public boolean isFull() {
-    return currCapacity == k;
-  }
-}
-
-class DequeNode {
-  int val;
-  DequeNode front;
-  DequeNode back;
-  
-  public DequeNode(int val) {
-    this.val = val;
+    return this.queue[this.emptyCursor] != -1;
   }
 }
 
 /**
- * Your MyCircularDeque object will be instantiated and called as such:
- * MyCircularDeque obj = new MyCircularDeque(k);
- * boolean param_1 = obj.insertFront(value);
- * boolean param_2 = obj.insertLast(value);
- * boolean param_3 = obj.deleteFront();
- * boolean param_4 = obj.deleteLast();
- * int param_5 = obj.getFront();
- * int param_6 = obj.getRear();
- * boolean param_7 = obj.isEmpty();
- * boolean param_8 = obj.isFull();
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
  */
