069f1dce059f9a382b57a0da69ddafd79f26a4db
Varun Upadhyay <varun.u28@gmail.com>
2022-06-17 15:41:49 -0700

    Update Graph Valid Tree.java

--- Medium/Graph Valid Tree.java
+++ Medium/Graph Valid Tree.java
@@ -1,19 +1,27 @@
 class Solution {
   public boolean validTree(int n, int[][] edges) {
-    int[] nums = new int[n];
-    Arrays.fill(nums, -1);
+    Map<Integer, Set<Integer>> map = new HashMap<>();
     for (int[] edge : edges) {
-      int rootOne = find(nums, edge[0]);
-      int rootTwo = find(nums, edge[1]);
-      if (rootOne == rootTwo) {
-        return false;
-      }
-      nums[rootOne] = rootTwo;
+      map.computeIfAbsent(edge[0], k -> new HashSet<>()).add(edge[1]);
+      map.computeIfAbsent(edge[1], k -> new HashSet<>()).add(edge[0]);
     }
-    return edges.length == n - 1;
-  }
-  
-  private int find(int[] nums, int n) {
-    return nums[n] == -1 ? n : find(nums, nums[n]);
+    Map<Integer, Integer> parent = new HashMap<>();
+    parent.put(0, -1);
+    Stack<Integer> stack = new Stack<>();
+    stack.push(0);
+    while (!stack.isEmpty()) {
+      int node = stack.pop();
+      for (int neighbor : map.getOrDefault(node, new HashSet<>())) {
+        if (parent.get(node) == neighbor) {
+          continue;
+        }
+        if (parent.containsKey(neighbor)) {
+          return false;
+        }
+        stack.push(neighbor);
+        parent.put(neighbor, node);
+      }
+    }
+    return parent.size() == n;
   }
 }
