350df3288cabeec3ed602f8b0b5d140f79ae4691
Varun Upadhyay <varun.u28@gmail.com>
2022-06-18 08:29:19 -0700

    Update Prefix and Suffix Search.java

--- Hard/Prefix and Suffix Search.java
+++ Hard/Prefix and Suffix Search.java
@@ -1,61 +1,48 @@
 class WordFilter {
 
-  Node root;
-  Map<String, Integer> weightMap;
-  public WordFilter(String[] words) {
-    root = new Node('-');
-    weightMap = new HashMap<>();
-    for (int i = 0; i < words.length; i++) {
-      root.words.add(words[i]);
-      addWord(words[i], 0, root);
-      weightMap.put(words[i], i);
-    }
-  }
+  private TrieNode root;
   
-  private void addWord(String word, int idx, Node curr) {
-    if (idx == word.length()) {
-      return;
+  public WordFilter(String[] words) {
+    this.root = new TrieNode();
+    for (int i = 0; i < words.length; i++) {
+      indexWord(words[i], i);
     }
-    char c = word.charAt(idx);
-    if (!curr.children.containsKey(c)) {
-      curr.children.put(c, new Node(c));
-    }
-    curr = curr.children.get(c);
-    curr.words.add(word);
-    addWord(word, idx + 1, curr);
   }
 
   public int f(String prefix, String suffix) {
-    Node curr = root;
-    if (prefix.length() != 0) {
-      for (char c : prefix.toCharArray()) {
-        if (!curr.children.containsKey(c)) {
-          return -1;
-        }
-        curr = curr.children.get(c);
+    TrieNode curr = root;
+    for (char c : (suffix + "{" + prefix).toCharArray()) {
+      if (curr.children[c - 'a'] == null) {
+        return -1;
       }
+      curr = curr.children[c - 'a'];
     }
-    int ans = -1;
-    for (String word : curr.words) {
-      if (suffix.length() == 0 || word.endsWith(suffix)) {
-        ans = Math.max(ans, weightMap.get(word));
-      }
-    }
-    return ans;
+    return curr.maxIdx;
   }
-}
 
-
-class Node {
-  char c;
-  Map<Character, Node> children;
-  Set<String> words;
+  private void indexWord(String word, int idx) {
+    for (int i = word.length() - 1; i >= 0; i--) {
+      String suffixAndPrefix = word.substring(i, word.length()) + "{" + word;
+      TrieNode curr = root;
+      for (char c : suffixAndPrefix.toCharArray()) {
+        if (curr.children[c - 'a'] == null) {
+          curr.children[c - 'a'] = new TrieNode();
+        }
+        curr = curr.children[c - 'a'];
+        curr.maxIdx = idx;
+      }
+    }
+  }
   
-  public Node(char c) {
-    this.c = c;
-    children = new HashMap<>();
-    words = new HashSet<>();
-  } 
+  
+  private class TrieNode {
+    TrieNode[] children;
+    int maxIdx;    
+    public TrieNode() {
+      this.children = new TrieNode[27];
+      this.maxIdx = 0;
+    }
+  }
 }
 
 /**
