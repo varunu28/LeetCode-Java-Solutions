c198cb91f5fe6bd60306de3c8023031de4c7d280
Varun Upadhyay <varun.u28@gmail.com>
2022-06-18 17:02:00 -0700

    Update Maximum Average Subarray I.java

--- Easy/Maximum Average Subarray I.java
+++ Easy/Maximum Average Subarray I.java
@@ -1,17 +1,15 @@
 class Solution {
   public double findMaxAverage(int[] nums, int k) {
-    Double max = null;
     double sum = 0;
-    for (int i = 0; i < k - 1; i++) {
+    for (int i = 0; i < k; i++) {
       sum += nums[i];
     }
-    int start = 0;
-    for (int i = k - 1; i < nums.length; i++) {
+    double maxAverage = sum / k;
+    for (int i = k; i < nums.length; i++) {
+      sum -= nums[i - k];
       sum += nums[i];
-      double avg = sum / k;
-      max = max == null ? avg : Math.max(max, avg);
-      sum -= nums[start++];
+      maxAverage = Math.max(maxAverage, sum / k);
     }
-    return max;
+    return maxAverage;
   }
 }
