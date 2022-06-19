2ec2fe2c41715a80e114d7ebcb12c37dfed557b6
Varun Upadhyay <varun.u28@gmail.com>
2022-06-19 07:27:24 -0700

    Update Search Suggestions System.java

--- Medium/Search Suggestions System.java
+++ Medium/Search Suggestions System.java
@@ -1,59 +1,46 @@
 class Solution {
   public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-    Node root = new Node('-');
-    for (String product : products) {
-      addProduct(product, root);
+    TrieNode root = new TrieNode();
+    for (String prod : products) {
+      TrieNode curr = root;
+      for (char c : prod.toCharArray()) {
+        if (curr.children[c - 'a'] == null) {
+          curr.children[c - 'a'] = new TrieNode();
+        }
+        curr = curr.children[c - 'a'];
+        curr.matchingProducts.add(prod);
+      }
     }
-    List<List<String>> list = new ArrayList<>();
-    Node curr = root;
-    boolean notFound = false;
+    List<List<String>> result = new ArrayList<>();
+    TrieNode curr = root;
     for (char c : searchWord.toCharArray()) {
-      if (notFound) {
-        list.add(new ArrayList<>());
-      }
-      else {
-        if (!curr.children.containsKey(c)) {
-          notFound = true;
-          list.add(new ArrayList<>());
-          continue;
+      if (curr == null) {
+        result.add(new ArrayList<>());
+      } else {
+        if (curr.children[c - 'a'] == null) {
+          curr = null;
+          result.add(new ArrayList<>());
+        } else {
+          curr = curr.children[c - 'a'];
+          PriorityQueue<String> currMatchingProductsQueue = new PriorityQueue<>(curr.matchingProducts);
+          List<String> currMatchingProducts = new ArrayList<>();
+          while (!currMatchingProductsQueue.isEmpty() && currMatchingProducts.size() < 3) {
+            currMatchingProducts.add(currMatchingProductsQueue.poll());
+          }
+          result.add(currMatchingProducts);
         }
-        curr = curr.children.get(c);
-        List<String> temp = new ArrayList<>();
-        PriorityQueue<String> words = new PriorityQueue<>(curr.possibleWords);
-        for (int i = 0; i < 3 && !words.isEmpty(); i++) {
-          temp.add(words.poll());
-        }
-        Collections.reverse(temp);
-        list.add(temp);
       }
     }
-    return list;
+    return result;
   }
   
-  private void addProduct(String product, Node root) {
-    Node curr = root;
-    for (int i = 0; i < product.length(); i++) {
-      if (!curr.children.containsKey(product.charAt(i))) {
-        curr.children.put(product.charAt(i), new Node(product.charAt(i)));
-      }
-      curr = curr.children.get(product.charAt(i));
-      curr.possibleWords.add(product);
-      if (curr.possibleWords.size() > 3) {
-        curr.possibleWords.poll();
-      }
+  private class TrieNode {
+    TrieNode[] children;
+    PriorityQueue<String> matchingProducts;
+    
+    public TrieNode() {
+      this.children = new TrieNode[26];
+      this.matchingProducts = new PriorityQueue<>();
     }
-  }
-}
-
-
-class Node {
-  char c;
-  Map<Character, Node> children;
-  PriorityQueue<String> possibleWords;
-  
-  public Node(char c) {
-    this.c = c;
-    children = new HashMap<>();
-    possibleWords = new PriorityQueue<>(Comparator.reverseOrder());
   }
 }
