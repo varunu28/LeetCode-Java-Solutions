b3308b6871230a329fa18a19a7592614129dbb62
Varun Upadhyay <varun.u28@gmail.com>
2022-06-21 16:00:08 -0700

    Update Insert Delete GetRandom O(1).java

--- Medium/Insert Delete GetRandom O(1).java
+++ Medium/Insert Delete GetRandom O(1).java
@@ -1,41 +1,39 @@
 class RandomizedSet {
 
-  /** Initialize your data structure here. */
-  Map<Integer, Integer> map;
-  List<Integer> list;
+  private List<Integer> randomizedSet;
+  private Map<Integer, Integer> indexMap;
+  private int currSize;
+  
   public RandomizedSet() {
-    map = new HashMap<>();
-    list = new ArrayList<>();
+    this.randomizedSet = new ArrayList<>();
+    this.indexMap = new HashMap<>();
+    this.currSize = 0;
   }
 
-  /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
   public boolean insert(int val) {
-    if (map.containsKey(val)) {
+    if (this.indexMap.containsKey(val)) {
       return false;
     }
-    list.add(val);
-    map.put(val, list.size() - 1);
+    this.randomizedSet.add(val);
+    this.indexMap.put(val, this.currSize++);
     return true;
   }
 
-  /** Removes a value from the set. Returns true if the set contained the specified element. */
   public boolean remove(int val) {
-    if (!map.containsKey(val)) {
+    if (!this.indexMap.containsKey(val)) {
       return false;
     }
-    int idx = map.get(val);
-    int lastIdx = list.size() - 1;
-    list.set(idx, list.get(lastIdx));
-    map.put(list.get(lastIdx), idx);
-    map.remove(val);
-    list.remove(lastIdx);
+    int valIdx = this.indexMap.get(val);
+    this.indexMap.put(this.randomizedSet.get(this.currSize - 1), valIdx);
+    this.randomizedSet.set(valIdx, this.randomizedSet.get(this.currSize - 1));
+    this.randomizedSet.remove(this.currSize - 1);
+    this.indexMap.remove(val);
+    this.currSize--;
     return true;
   }
 
-  /** Get a random element from the set. */
   public int getRandom() {
-    int idx = new Random().nextInt(list.size());
-    return list.get(idx);
+    return this.randomizedSet.get(new Random().nextInt(this.currSize));
   }
 }
 
