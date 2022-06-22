7d7cdea03e27c99d545682c0959fc4b62cd44482
Varun Upadhyay <varun.u28@gmail.com>
2022-06-21 20:35:15 -0700

    Update Kth Largest Element in an Array.java

--- Medium/Kth Largest Element in an Array.java
+++ Medium/Kth Largest Element in an Array.java
@@ -1,24 +1,16 @@
 class Solution {
   public int findKthLargest(int[] nums, int k) {
-    return quickSelect(nums, 0, nums.length - 1, nums.length - k);
-  }
-
-  private int quickSelect(int[] nums, int leftIdx, int rightIdx, int k) {
-    int pivot = nums[rightIdx];
-    int idx = leftIdx;
-    for (int i = leftIdx; i < rightIdx; i++) {
-      if (nums[i] <= pivot) {
-        swap(nums, i, idx);
-        idx++;
+    int[] counter = new int[20001];
+    for (int num : nums) {
+      counter[num + 10000]++;
+    }
+    for (int i = counter.length - 1; i >= 0; i--) {
+      if (counter[i] < k) {
+        k -= counter[i];
+      } else {
+        return i - 10000;
       }
     }
-    swap(nums, idx, rightIdx);
-    return idx == k ? nums[idx] : (idx > k ? quickSelect(nums, leftIdx, idx - 1, k) : quickSelect(nums, idx + 1, rightIdx, k));
-  }
-
-  private void swap(int[] nums, int idxOne, int idxTwo) {
-    int temp = nums[idxOne];
-    nums[idxOne] = nums[idxTwo];
-    nums[idxTwo] = temp;
+    return -1;
   }
 }
