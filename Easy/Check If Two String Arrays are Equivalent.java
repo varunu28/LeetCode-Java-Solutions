f105faf04c7f4c4fdbf00c085f32acaf30936626
Varun Upadhyay <varun.u28@gmail.com>
2022-06-22 08:28:29 -0700

    Update Check If Two String Arrays are Equivalent.java

--- Easy/Check If Two String Arrays are Equivalent.java
+++ Easy/Check If Two String Arrays are Equivalent.java
@@ -1,6 +1,27 @@
 class Solution {
   public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-    return Arrays.stream(word1).reduce((a, b) -> a + b)
-        .equals(Arrays.stream(word2).reduce((a, b) -> a + b));
+    int wordIdxOne = 0;
+    int wordIdxTwo = 0;
+    int idxOne = 0;
+    int idxTwo = 0;
+    while (wordIdxOne < word1.length && wordIdxTwo < word2.length) {
+      if (idxOne == word1[wordIdxOne].length() || idxTwo == word2[wordIdxTwo].length()) {
+        if (idxOne == word1[wordIdxOne].length()) {
+          wordIdxOne++;
+          idxOne = 0;
+        }
+        if (idxTwo == word2[wordIdxTwo].length()) {
+          wordIdxTwo++;
+          idxTwo = 0;
+        }
+      } else {
+        if (word1[wordIdxOne].charAt(idxOne) != word2[wordIdxTwo].charAt(idxTwo)) {
+          return false;
+        }
+        idxOne++;
+        idxTwo++;
+      }
+    }
+    return wordIdxOne == word1.length && wordIdxTwo == word2.length;
   }
 }
