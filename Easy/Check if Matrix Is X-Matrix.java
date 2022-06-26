dab0bccbc3bc49bdd002195455f7d5f5fd97bb17
Varun Upadhyay <varun.u28@gmail.com>
2022-06-26 14:13:17 -0700

    Create Check if Matrix Is X-Matrix.java

---
+++ Easy/Check if Matrix Is X-Matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+  public boolean checkXMatrix(int[][] grid) {
+    int n = grid.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == j || i + j == n - 1) {
+          if (grid[i][j] == 0) {
+            return false;
+          }
+        } else {
+          if (grid[i][j] != 0) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+}
