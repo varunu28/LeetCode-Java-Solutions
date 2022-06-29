f92d1d65c3f928f54e71cd8b2a782c42c2d422a6
Varun Upadhyay <varun.u28@gmail.com>
2022-06-29 08:14:22 -0700

    Update Queue Reconstruction By Height.java

--- Medium/Queue Reconstruction By Height.java
+++ Medium/Queue Reconstruction By Height.java
@@ -1,20 +1,11 @@
 class Solution {
   public int[][] reconstructQueue(int[][] people) {
-    Arrays.sort(people, new Comparator<int[]>(){
-      public int compare(int[] o1, int[] o2) {
-        int c = o2[0] - o1[0];
-        if (c != 0) {
-          return c;
-        }
-        return o1[1] - o2[1]; 
-      }
-    });
-    System.out.println();
-    List<int[]> list = new ArrayList<>();
+    Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+    List<int[]> result = new ArrayList<>();
     for (int[] p : people) {
-      list.add(p[1], p);
+      result.add(p[1], p);
     }
     int n = people.length;
-    return list.toArray(new int[n][2]);
+    return result.toArray(new int[n][2]);
   }
 }
