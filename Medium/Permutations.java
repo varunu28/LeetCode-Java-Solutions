ae12464c9a458e95939b1273527a2313a66fbc78
Varun Upadhyay <varun.u28@gmail.com>
2022-06-30 07:56:33 -0700

    Update Permutations.java

--- Medium/Permutations.java
+++ Medium/Permutations.java
@@ -1,25 +1,22 @@
 class Solution {
   public List<List<Integer>> permute(int[] nums) {
-    List<List<Integer>> ans = new ArrayList <>();
-    Set<Integer> used = new HashSet <>();
-    permuteHelper(nums, nums.length, ans, used, new ArrayList<>());
-    return ans;
+    List<List<Integer>> result = new ArrayList<>();
+    helper(nums, result, new ArrayList<>(), new boolean[nums.length]);
+    return result;
   }
-
-  private void permuteHelper(int[] nums, int length, List<List<Integer>> ans,
-                                    Set<Integer> used, ArrayList<Integer> curr) {
-    if (curr.size() == length) {
-      ans.add(new ArrayList <>(curr));
-      return;
-    }
-
-    for (int i = 0; i < length; i++) {
-      if (!used.contains(nums[i])) {
-        used.add(nums[i]);
-        curr.add(nums[i]);
-        permuteHelper(nums, length, ans, used, curr);
-        used.remove(nums[i]);
-        curr.remove(curr.size() - 1);
+  
+  private void helper(int[] nums, List<List<Integer>> result, List<Integer> curr, boolean[] visited) {
+    if (curr.size() == nums.length) {
+      result.add(new ArrayList<>(curr));
+    } else {
+      for (int i = 0; i < nums.length; i++) {
+        if (!visited[i]) {
+          visited[i] = true;
+          curr.add(nums[i]);
+          helper(nums, result, curr, visited);
+          visited[i] = false;
+          curr.remove(curr.size() - 1);
+        }
       }
     }
   }
