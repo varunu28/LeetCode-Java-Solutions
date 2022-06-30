1a2857159c24727a8ac128e8a9cfe1ec42785012
Varun Upadhyay <varun.u28@gmail.com>
2022-06-30 14:44:14 -0700

    Update Sort Integers by The Number of 1 Bits.java

--- Easy/Sort Integers by The Number of 1 Bits.java
+++ Easy/Sort Integers by The Number of 1 Bits.java
@@ -1,9 +1,8 @@
 class Solution {
   public int[] sortByBits(int[] arr) {
-    return Arrays.stream(arr).boxed()
+    return Arrays.stream(arr)
+        .boxed()
         .sorted(Comparator.comparingInt(Integer::bitCount).thenComparingInt(o -> o))
-        .collect(Collectors.toList())
-        .stream()
         .mapToInt(Integer::intValue)
         .toArray();
   }
