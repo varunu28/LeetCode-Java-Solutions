e31c6a6ac1e055c1a376ded2218b31b2bd91c18a
Varun Upadhyay <varun.u28@gmail.com>
2022-07-01 08:00:06 -0700

    Update Length of last word.java

--- Easy/Length of last word.java
+++ Easy/Length of last word.java
@@ -1,14 +1,13 @@
 class Solution {
   public int lengthOfLastWord(String s) {
-    int count = 0;
     int idx = s.length() - 1;
     while (idx >= 0 && s.charAt(idx) == ' ') {
       idx--;
     }
+    int currIdx = idx;
     while (idx >= 0 && s.charAt(idx) != ' ') {
       idx--;
-      count++;
     }
-    return count;
+    return currIdx - idx;
   }
 }
