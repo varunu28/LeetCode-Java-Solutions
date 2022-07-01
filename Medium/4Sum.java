57f26849e8320d54455620b3190569d59c68deb3
Varun Upadhyay <varun.u28@gmail.com>
2022-07-01 13:36:48 -0700

    Update 4Sum.java

--- Medium/4Sum.java
+++ Medium/4Sum.java
@@ -1,25 +1,46 @@
 class Solution {
   public List<List<Integer>> fourSum(int[] nums, int target) {
     Arrays.sort(nums);
-    Set<List<Integer>> set = new HashSet<>();
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        int newTarget = target - nums[i] - nums[j];
-        int start = j + 1;
-        int end = nums.length - 1;
-        while (start < end) {
-          if ((nums[start] + nums[end]) == newTarget) {
-            set.add(Arrays.asList(nums[i], nums[j], nums[start++], nums[end--]));
-          }
-          else if ((nums[start] + nums[end]) < newTarget) {
-            start++;
-          }
-          else {
-            end--;
-          }
+    return kSum(nums, target, 0, 4);
+  }
+  
+  public List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (start == nums.length) {
+      return result;
+    }
+    long averageValue = target / k;
+    if  (nums[start] > averageValue || averageValue > nums[nums.length - 1]) {
+      return result;
+    }
+    if (k == 2) {
+      return twoSum(nums, target, start);
+    }
+    for (int i = start; i < nums.length; ++i) {
+      if (i == start || nums[i - 1] != nums[i]) {
+        for (List<Integer> subset : kSum(nums, target - nums[i], i + 1, k - 1)) {
+          result.add(new ArrayList<>(Arrays.asList(nums[i])));
+          result.get(result.size() - 1).addAll(subset);
         }
       }
     }
-    return new ArrayList<>(set);
+    return result;
+  }
+	
+  public List<List<Integer>> twoSum(int[] nums, long target, int start) {
+    List<List<Integer>> result = new ArrayList<>();
+    int low = start;
+    int high = nums.length - 1;
+    while (low < high) {
+      int currSum = nums[low] + nums[high];
+      if (currSum < target || (low > start && nums[low] == nums[low - 1])) {
+        ++low;
+      } else if (currSum > target || (high < nums.length - 1 && nums[high] == nums[high + 1])) {
+        --high;
+      } else {
+        result.add(Arrays.asList(nums[low++], nums[high--]));
+      }
+    }
+    return result;
   }
 }
