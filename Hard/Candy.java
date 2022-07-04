7412f6fb01df2b0dacb2494452534b865c4a2cad
Varun Upadhyay <varun.u28@gmail.com>
2022-07-04 14:06:43 -0700

    Update Candy.java

--- Hard/Candy.java
+++ Hard/Candy.java
@@ -1,23 +1,19 @@
 class Solution {
   public int candy(int[] ratings) {
-    int[] leftCandy = new int[ratings.length];
-    int[] rightCandy = new int[ratings.length];
-    Arrays.fill(leftCandy, 1);
-    Arrays.fill(rightCandy, 1);
+    int[] candies = new int[ratings.length];
+    Arrays.fill(candies, 1);
     for (int i = 1; i < ratings.length; i++) {
       if (ratings[i] > ratings[i - 1]) {
-        leftCandy[i] = leftCandy[i - 1] + 1;
+        candies[i] = candies[i - 1] + 1;
       }
     }
+    int totalCandies = candies[candies.length - 1];
     for (int i = ratings.length - 2; i >= 0; i--) {
       if (ratings[i] > ratings[i + 1]) {
-        rightCandy[i] = rightCandy[i + 1] + 1;
+        candies[i] = Math.max(candies[i], candies[i + 1] + 1);
       }
+      totalCandies += candies[i];
     }
-    int numOfCandies = 0;
-    for (int i = 0; i < ratings.length; i++) {
-      numOfCandies += Math.max(leftCandy[i], rightCandy[i]);
-    }
-    return numOfCandies; 
+    return totalCandies;
   }
 }
