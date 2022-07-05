872f1f472b532942d361012b703c14f09e2fb8ba
Varun Upadhyay <varun.u28@gmail.com>
2022-07-05 07:51:59 -0700

    Update Validate Binary Search Tree.java

--- Medium/Validate Binary Search Tree.java
+++ Medium/Validate Binary Search Tree.java
@@ -4,23 +4,27 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
   public boolean isValidBST(TreeNode root) {
-    long min = (long) Integer.MIN_VALUE;
-    long max = (long) Integer.MAX_VALUE;
-    return helper(root, min - 1, max + 1);
+    return isValidBSTHelper(root, (((long) Integer.MAX_VALUE)  + 1), ((long) Integer.MIN_VALUE) - 1);
   }
   
-  private boolean helper(TreeNode root, long min, long max) {
-    if (root == null) {
+  private boolean isValidBSTHelper(TreeNode node, long max, long min) {
+    if (node == null) {
       return true;
     }
-    if (root.val <= min || root.val >= max) {
+    if (node.val >= max || node.val <= min) {
       return false;
     }
-    return helper(root.left, min, (long) root.val) && helper(root.right, (long) root.val, max);
+    return isValidBSTHelper(node.left, node.val, min) && isValidBSTHelper(node.right, max, node.val);
   }
 }
