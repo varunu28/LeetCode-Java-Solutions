d27b620e65e96a771b84e88509b321289a37bfba
Varun Upadhyay <varun.u28@gmail.com>
2022-07-04 18:05:16 -0700

    Update Longest Consecutive Sequence.java

--- Medium/Longest Consecutive Sequence.java
+++ Medium/Longest Consecutive Sequence.java
@@ -1,19 +1,21 @@
 class Solution {
   public int longestConsecutive(int[] nums) {
-    Map<Integer, Integer> map = new HashMap<>();
-    int maxLength = 0;
+    Set<Integer> set = new HashSet<>();
     for (int num : nums) {
-      if (map.containsKey(num)) {
-        continue;
+      set.add(num);
+    }
+    int maxLength = 0;
+    for (int num : set) {
+      if (!set.contains(num - 1)) {
+        int currNum = num;
+        int currentLength = 1;
+        while (set.contains(currNum + 1)) {
+          currNum++;
+          currentLength++;
+        }
+        maxLength = Math.max(currentLength, maxLength);
       }
-      int left = map.containsKey(num - 1) ? map.get(num - 1) : 0;
-      int right = map.containsKey(num + 1) ? map.get(num + 1) : 0;
-      int sum = left + right + 1;
-      maxLength = Math.max(maxLength, sum);
-      map.put(num, sum);
-      map.put(num - left, sum);
-      map.put(num + right, sum);
     }
     return maxLength;
-  }   
+  }
 }
