f0f604e7b6f076d496e9480ca29545f370cc940c
Varun Upadhyay <varun.u28@gmail.com>
2022-07-05 09:47:43 -0700

    Update SqrtX.java

--- Easy/SqrtX.java
+++ Easy/SqrtX.java
@@ -3,21 +3,19 @@ class Solution {
     if (x < 2) {
       return x;
     }
-    int start = 2;
-    int end = x / 2;
-    while (start <= end) {
-      int mid = start + (end - start) / 2;
-      long num = (long) mid * mid; 
-      if (num > x) {
-        end = mid - 1;
-      }
-      else if (num < x) {
-        start = mid + 1;
-      }
-      else {
+    int left = 2;
+    int right = x / 2;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      long square = ((long) mid) * mid; 
+      if (square > x) {
+        right = mid - 1;
+      } else if (square < x) {
+        left = mid + 1;
+      } else {
         return mid;
       }
     }
-    return end;
+    return right;
   }
 }
