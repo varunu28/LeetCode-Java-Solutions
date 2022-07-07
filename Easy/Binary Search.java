d4cd4cf44a9899b80633593e833047971a6a5f7a
Varun Upadhyay <varun.u28@gmail.com>
2022-07-07 12:36:50 -0700

    Update Binary Search.java

--- Easy/Binary Search.java
+++ Easy/Binary Search.java
@@ -1,17 +1,15 @@
 class Solution {
   public int search(int[] nums, int target) {
-    int left = 0;
-    int right = nums.length - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
+    int start = 0;
+    int end = nums.length - 1;
+    while (start <= end) {
+      int mid = (start + end) / 2;
       if (nums[mid] == target) {
         return mid;
-      }
-      else if (nums[mid] > target) {
-        right = mid - 1;
-      }
-      else {
-        left = mid + 1;
+      } else if (nums[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
       }
     }
     return -1;
