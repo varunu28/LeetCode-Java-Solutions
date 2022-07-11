343aa2601654e701bd7e08c590d631e2ccabd4f5
Varun Upadhyay <varun.u28@gmail.com>
2022-07-11 16:46:27 -0700

    Update Can Place Flowers.java

--- Easy/Can Place Flowers.java
+++ Easy/Can Place Flowers.java
@@ -2,15 +2,13 @@ class Solution {
   public boolean canPlaceFlowers(int[] flowerbed, int n) {
     for (int i = 0; i < flowerbed.length && n > 0; i++) {
       if (flowerbed[i] == 0) {
-        if (i == 0 && (i + 1 < flowerbed.length && flowerbed[i + 1] == 0)
-            || flowerbed.length == 1) {
+        if (i == 0 && (flowerbed.length == 1 || flowerbed[i + 1] == 0)) {
           flowerbed[i] = 1;
           n--;
-        } else if (i == flowerbed.length - 1 && i - 1 >= 0 && flowerbed[i - 1] == 0) {
+        } else if (i == flowerbed.length - 1 && flowerbed[i - 1] == 0) {
           flowerbed[i] = 1;
           n--;
-        } else if (i + 1 < flowerbed.length && i - 1 >= 0 && flowerbed[i + 1] == 0
-            && flowerbed[i - 1] == 0) {
+        } else if (i + 1 < flowerbed.length && flowerbed[i + 1] == 0 && i - 1 >= 0 && flowerbed[i - 1] == 0) {
           flowerbed[i] = 1;
           n--;
         }
