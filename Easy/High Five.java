21775562b08f0a8cfae8e866e288a36658112e48
Varun Upadhyay <varun.u28@gmail.com>
2022-07-12 08:43:55 -0700

    Update High Five.java

--- Easy/High Five.java
+++ Easy/High Five.java
@@ -1,37 +1,23 @@
 class Solution {
-    public int[][] highFive(int[][] items) {
-        final int COUNT = 5;
-        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-        Set<Integer> set = new TreeSet<>();
-
-        for (int[] item : items) {
-            int id = item[0];
-            int score = item[1];
-
-            map.computeIfAbsent(id, k -> new PriorityQueue<>(COUNT, Comparator.naturalOrder())).add(score);
-
-            if (map.get(id).size() > COUNT) {
-                map.get(id).poll();
-            }
-
-            set.add(id);
-        }
-
-        int[][] ans = new int[map.size()][2];
-        int idx = 0;
-        Iterator<Integer> iterator = set.iterator();
-        while (iterator.hasNext()) {
-            int id = iterator.next();
-            PriorityQueue<Integer> scores = map.get(id);
-            int sum = 0;
-
-            while (!scores.isEmpty()) {
-                sum += scores.poll();
-            }
-
-            ans[idx++] = new int[]{id, sum / COUNT};
-        }
-
-        return ans;
+  public int[][] highFive(int[][] items) {
+    Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+    for (int[] item : items) {
+      int id = item[0];
+      int score = item[1];
+      map.computeIfAbsent(id, k -> new PriorityQueue<>((a, b) -> b - a)).add(score);
     }
+    int[][] result = new int[map.size()][2];
+    int idx = 0;
+    for (Integer id : map.keySet()) {
+      PriorityQueue<Integer> scoresInOrder = map.get(id);
+      int totalScore = 0;
+      int count = Math.min(5, scoresInOrder.size());
+      for (int i = 0; i < 5 && !scoresInOrder.isEmpty(); i++) {
+        totalScore += scoresInOrder.poll();
+      }
+      result[idx++] = new int[]{id, totalScore / count};
+    }
+    Arrays.sort(result, (a, b) -> a[0] - b[0]);
+    return result;
+  }
 }
