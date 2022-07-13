30ea21bf9a3335c613cfa02d76361abdc41c54c3
Varun Upadhyay <varun.u28@gmail.com>
2022-07-13 07:43:38 -0700

    Update Valid Palindrome II.java

--- Easy/Valid Palindrome II.java
+++ Easy/Valid Palindrome II.java
@@ -1,22 +1,18 @@
 class Solution {
   public boolean validPalindrome(String s) {
+    return validPalindromeHelper(s, false);
+  }
+  
+  private boolean validPalindromeHelper(String s, boolean deleted) {
     int startIdx = 0;
     int endIdx = s.length() - 1;
     while (startIdx < endIdx) {
       if (s.charAt(startIdx) != s.charAt(endIdx)) {
-        return isPalindromeWithDeletion(s, startIdx + 1, endIdx) || 
-          isPalindromeWithDeletion(s, startIdx, endIdx - 1);
-      }
-      startIdx++;
-      endIdx--;
-    }
-    return true;
-  } 
-  
-  private boolean isPalindromeWithDeletion(String s, int startIdx, int endIdx) {
-    while (startIdx < endIdx) {
-      if (s.charAt(startIdx) != s.charAt(endIdx)) {
-        return false;
+        if (deleted) {
+          return false;
+        }
+        return validPalindromeHelper(s.substring(0, startIdx) + s.substring(startIdx + 1), true) ||
+               validPalindromeHelper(s.substring(0, endIdx) + s.substring(endIdx + 1), true);
       }
       startIdx++;
       endIdx--;
