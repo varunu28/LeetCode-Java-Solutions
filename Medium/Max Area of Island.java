5c3a130a402bc975de45bd7767e4986ce1f6d58e
Varun Upadhyay <varun.u28@gmail.com>
2022-07-14 18:08:28 -0700

    Update Max Area of Island.java

--- Medium/Max Area of Island.java
+++ Medium/Max Area of Island.java
@@ -1,45 +1,34 @@
 class Solution {
-  
-  private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  private final int[][] DIRS = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
   
   public int maxAreaOfIsland(int[][] grid) {
-    int rows = grid.length;
+    int area = 0;
+    int rows = grid.length; 
     int cols = grid[0].length;
-    int maxArea = 0;
+    boolean[][] visited = new boolean[rows][cols];
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-        if (grid[i][j] != 0) {
-          grid[i][j] = 0;
-          int currentArea = getArea(grid, i, j);
-          maxArea = Math.max(maxArea, currentArea);
+        if (!visited[i][j] && grid[i][j] == 1) {
+          Queue<int[]> queue = new LinkedList<>();
+          int currArea = 0;
+          queue.add(new int[]{i, j});
+          visited[i][j] = true;
+          while (!queue.isEmpty()) {
+            int[] removed = queue.remove();
+            currArea++;
+            for (int[] dir : DIRS) {
+              int x = removed[0] + dir[0];
+              int y = removed[1] + dir[1];
+              if (x >= 0 && y >= 0 && x < rows && y < cols && !visited[x][y] && grid[x][y] == 1) {
+                queue.add(new int[]{x, y});
+                visited[x][y] = true;
+              }
+            }
+          }
+          area = Math.max(area, currArea);
         }
       }
     }
-    return maxArea;
-  }
-  
-  private int getArea(int[][] grid, int i, int j) {
-    int currentArea = 0;
-    Queue<int[]> queue = new LinkedList<>();
-    queue.add(new int[]{i, j});
-    while (!queue.isEmpty()) {
-      int[] removed = queue.remove();
-      int currX = removed[0];
-      int currY = removed[1];
-      currentArea++;
-      for (int[] dir : DIRS) {
-        int newX = dir[0] + currX;
-        int newY = dir[1] + currY;
-        if (isValidCoordinate(grid, newX, newY) && grid[newX][newY] == 1) {
-          queue.add(new int[]{newX, newY});
-          grid[newX][newY] = 0;
-        }
-      }
-    }
-    return currentArea;
-  }
-  
-  private boolean isValidCoordinate(int[][] grid, int i, int j) {
-    return i >= 0 && j >= 0 && i < grid.length && j < grid[0].length;
+    return area;
   }
 }
