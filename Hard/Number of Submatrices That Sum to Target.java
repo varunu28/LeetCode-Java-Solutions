e46df51a30ff95a6c5cb033956ed89be7081536e
Varun Upadhyay <varun.u28@gmail.com>
2022-07-18 08:59:15 -0700

    Update Number of Submatrices That Sum to Target.java

--- Hard/Number of Submatrices That Sum to Target.java
+++ Hard/Number of Submatrices That Sum to Target.java
@@ -1,29 +1,26 @@
 class Solution {
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int res = 0;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                matrix[i][j] += matrix[i][j - 1];
-            }
-        }
-        
-        for (int i = 0; i < cols; i++) {
-            for (int j = i; j < cols; j++) {
-                Map<Integer, Integer> map = new HashMap<>();
-                map.put(0, 1);
-                int sum = 0;
-                for (int k = 0; k < rows; k++) {
-                    sum += matrix[k][j] - (i > 0 ? matrix[k][i - 1] : 0);
-                    res += map.getOrDefault(sum - target, 0);
-                    
-                    map.put(sum, map.getOrDefault(sum, 0) + 1);
-                }
-            }
-        }
-        
-        return res;
+  public int numSubmatrixSumTarget(int[][] matrix, int target) {
+    int rows = matrix.length;
+    int cols = matrix[0].length;
+    int[][] prefixSum = new int[rows + 1][cols + 1];
+    for (int i = 1; i <= rows; i++) {
+      for (int j = 1; j <= cols; j++) {
+        prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + matrix[i - 1][j - 1];
+      }
     }
+    int count = 0;
+    int currSum = 0;
+    for (int i = 1; i <= rows; i++) {
+      for (int j = i; j <= rows; j++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int k = 1; k <= cols; k++) {
+          currSum = prefixSum[j][k] - prefixSum[i - 1][k];
+          count += map.getOrDefault(currSum - target, 0);
+          map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        }
+      }
+    }
+    return count;
+  }
 }
