4509f047ab7c4fdf2d60a0560179a2a2e306f330
Varun Upadhyay <varun.u28@gmail.com>
2022-07-21 18:56:42 -0700

    Update Partition List.java

--- Medium/Partition List.java
+++ Medium/Partition List.java
@@ -3,35 +3,44 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode partition(ListNode head, int x) {
-        Queue<Integer> small = new LinkedList<>();
-        Queue<Integer> bigOrEqual = new LinkedList<>();
-        ListNode curr = head;
-        while (curr != null){
-            if (curr.val < x) {
-                small.add(curr.val);
-            }
-            else {
-                bigOrEqual.add(curr.val);
-            }
-            curr = curr.next;
+  public ListNode partition(ListNode head, int x) {
+    ListNode lessThanHead = null;
+    ListNode lessThan = null;
+    ListNode greaterThan = null;
+    ListNode greaterThanHead = null;
+    while (head != null) {
+      ListNode nextNode = head.next;
+      if (head.val < x) {
+        if (lessThan == null) {
+          lessThan = head;
+          lessThanHead = lessThan;
+        } else {
+          lessThan.next = head;
+          lessThan = lessThan.next;
         }
-        
-        curr = head;
-        while (curr != null) {
-            if (!small.isEmpty()) {
-                curr.val = small.remove();
-            }
-            else {
-                curr.val = bigOrEqual.remove();
-            }
-            curr = curr.next;
+        lessThan.next = null;
+      } else {
+        if (greaterThan == null) {
+          greaterThan = head;
+          greaterThanHead = greaterThan;
+        } else {
+          greaterThan.next = head;
+          greaterThan = greaterThan.next;
         }
-        
-        return head;
+        greaterThan.next = null;
+      }
+      head = nextNode;
     }
+    if (lessThanHead == null || greaterThanHead == null) {
+      return lessThan == null ? greaterThanHead : lessThanHead;
+    }
+    lessThan.next = greaterThanHead;
+    return lessThanHead;
+  }
 }
