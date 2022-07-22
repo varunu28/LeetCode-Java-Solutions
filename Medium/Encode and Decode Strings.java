66408f0a279172e4d33768c1df8d0b14dffbc3d6
Varun Upadhyay <varun.u28@gmail.com>
2022-07-22 10:01:06 -0700

    Update Encode and Decode Strings.java

--- Medium/Encode and Decode Strings.java
+++ Medium/Encode and Decode Strings.java
@@ -3,8 +3,8 @@ public class Codec {
   // Encodes a list of strings to a single string.
   public String encode(List<String> strs) {
     StringBuilder sb = new StringBuilder();
-    for (String str : strs) {
-      sb.append(lengthEncode(str)).append(str);
+    for (String s : strs) {
+      sb.append(lengthEncoding(s)).append(s);
     }
     return sb.toString();
   }
@@ -12,24 +12,34 @@ public class Codec {
   // Decodes a single string to a list of strings.
   public List<String> decode(String s) {
     int idx = 0;
+    List<String> result = new ArrayList<>();
     int n = s.length();
-    List<String> list = new ArrayList<>();
     while (idx < n) {
-      int length = Integer.parseInt(s.substring(idx, idx + 10));
-      idx += 10;
-      list.add(s.substring(idx, idx + length));
-      idx += length;
+      int currStringLength = lengthDecoding(s.substring(idx, idx + 4));
+      idx += 4;
+      result.add(s.substring(idx, idx + currStringLength));
+      idx += currStringLength;
     }
-    return list;
+    return result;
   }
   
-  private String lengthEncode(String s) {
+  private String lengthEncoding(String s) {
     int n = s.length();
-    return String.join("", Collections.nCopies(10 - String.valueOf(n).length(), "0")) + String.valueOf(n);
+    char[] bytes = new char[4];
+    for(int i = 3; i >= 0; i--) {
+      bytes[3 - i] = (char) (n >> (i * 8) & 0xff);
+    }
+    return new String(bytes);
+  }
+  
+  private int lengthDecoding(String bytesStr) {
+    int result = 0;
+    for(char b : bytesStr.toCharArray())
+      result = (result << 8) + (int)b;
+    return result;
   }
 }
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.decode(codec.encode(strs));
-
