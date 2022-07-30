34ffbccdaed47f9432fad0ff24b9d091c49e9d26
Varun Upadhyay <varun.u28@gmail.com>
2022-07-30 15:22:45 -0700

    Update and rename Easy/Reorder Data in Log Files.java to Medium/Reorder Data in Log Files.java

--- Medium/Reorder Data in Log Files.java
+++ Medium/Reorder Data in Log Files.java
@@ -3,16 +3,15 @@ class Solution {
     List<String> letterLogs = new ArrayList<>();
     List<String> digitLogs = new ArrayList<>();
     for (String log : logs) {
-      String[] split = log.split("\\s+");
-      if (Character.isDigit(split[1].charAt(0))) {
+      if (Character.isDigit(log.split("\\s+")[1].charAt(0))) {
         digitLogs.add(log);
       } else {
         letterLogs.add(log);
       }
     }
     letterLogs.sort(Comparator.comparing((String o) -> o.substring(o.indexOf(' ') + 1))
-        .thenComparing(o -> o.substring(0, o.indexOf(' '))));
+        .thenComparing(o -> o.split("\\s+")[0]));
     letterLogs.addAll(digitLogs);
-    return letterLogs.toArray(new String[]{});
-  } 
+    return letterLogs.toArray(new String[0]);
+  }
 }
