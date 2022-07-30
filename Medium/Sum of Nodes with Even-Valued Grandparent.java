617557c732c039a695c468d42649cd2c138dd75d
Varun Upadhyay <varun.u28@gmail.com>
2022-07-30 15:22:08 -0700

    Update Sum of Nodes with Even-Valued Grandparent.java

--- Medium/Sum of Nodes with Even-Valued Grandparent.java
+++ Medium/Sum of Nodes with Even-Valued Grandparent.java
@@ -4,25 +4,38 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  int sum;
   public int sumEvenGrandparent(TreeNode root) {
-    sum = 0;
-    helper(root, null, null);
+    int sum = 0;
+    Queue<TreeNode[]> queue = new LinkedList<>();
+    queue.add(new TreeNode[]{root, null, null});
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      while (size-- > 0) {
+        TreeNode[] removed = queue.remove();
+        TreeNode node = removed[0];
+        TreeNode parent = removed[1];
+        TreeNode grandParent = removed[2];
+        if (grandParent != null && grandParent.val % 2 == 0) {
+          sum += node.val;
+        }
+        if (node.left != null) {
+          queue.add(new TreeNode[]{node.left, node, parent});
+        }
+        if (node.right != null) {
+          queue.add(new TreeNode[]{node.right, node, parent});
+        }
+      }
+    }
     return sum;
-  }
-  
-  private void helper(TreeNode root, TreeNode parent, TreeNode grandparent) {
-    if (root == null) {
-      return;
-    }
-    if (grandparent != null && grandparent.val % 2 == 0) {
-      sum += root.val;
-    }
-    helper(root.left, root, parent);
-    helper(root.right, root, parent);
   }
 }
