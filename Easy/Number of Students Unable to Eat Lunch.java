70bead34461690567182db34b1a2a3110afde5d5
Varun Upadhyay <varun.u28@gmail.com>
2022-07-30 16:39:32 -0700

    Update Number of Students Unable to Eat Lunch.java

--- Easy/Number of Students Unable to Eat Lunch.java
+++ Easy/Number of Students Unable to Eat Lunch.java
@@ -1,21 +1,13 @@
 class Solution {
   public int countStudents(int[] students, int[] sandwiches) {
-    int studentIdx = 0;
-    int sandwichIdx = 0;
-    Map<Integer, Integer> studentState = new HashMap<>();
-    while (sandwichIdx < sandwiches.length) {
-      if (sandwiches[sandwichIdx] == students[studentIdx]) {
-        sandwichIdx++;
-        students[studentIdx] = -1;
-      } else if (students[studentIdx] != -1) {
-        // Check to see if we have finished one complete iteration without consuming any sandwich
-        if (studentState.containsKey(studentIdx) && studentState.get(studentIdx) == sandwichIdx) {
-          break;
-        }
-        studentState.put(studentIdx, sandwichIdx);
-      }
-      studentIdx = studentIdx == students.length - 1 ? 0 : studentIdx + 1;
+    int[] preferenceCount = new int[2];
+    for (int preference : students) {
+      preferenceCount[preference]++;
     }
-    return sandwichIdx == sandwiches.length ? 0 : sandwiches.length - sandwichIdx;
+    int i = 0;
+    for (i = 0; i < sandwiches.length && preferenceCount[sandwiches[i]] > 0; i++) {
+      preferenceCount[sandwiches[i]]--;
+    }
+    return sandwiches.length - i; 
   }
 }
