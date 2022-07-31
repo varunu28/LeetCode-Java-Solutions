6a8443fcb6f617dde928d2ee7ef708700f6f1071
Varun Upadhyay <varun.u28@gmail.com>
2022-07-31 07:34:27 -0700

    Update Range Sum Query - Mutable.java

--- Medium/Range Sum Query - Mutable.java
+++ Medium/Range Sum Query - Mutable.java
@@ -1,71 +1,50 @@
 class NumArray {
-    int[] fen;
-    int[] nums;
-    int n;
-    public NumArray(int[] nums) {
-        this.nums = nums;
-        n = nums.length;
-        fen = new int[n + 1];
-        init();
+  
+  private int[] nums;
+  private int[] indexTree;
+  private int n;
+  
+  public NumArray(int[] nums) {
+    this.nums = nums;
+    this.n = nums.length;
+    this.indexTree = new int[n + 1];
+    for (int i = 0; i < n; i++) {
+      initialize(i, nums[i]);
     }
+  }
 
-    private void init () {
-        if (n == 0) {
-            return;
-        }
+  public void update(int index, int val) {
+    int diff = val - this.nums[index];
+		this.nums[index] = val;
+		initialize(index, diff);
+  }
 
-        // Compute the pre-sum
-        fen[1] = nums[0];
-        for (int i = 1; i < n; i++) {
-            fen[i + 1] = fen[i] + nums[i];
-        }
-
-        for (int i = n; i > 0; i--) {
-            // Removing the value of parent for each node
-            int parent = i - (i & -i);
-            if (parent >= 0) {
-                fen[i] -= fen[parent];
-            }
-        }
-    }
-
-    public void update(int i, int val) {
-        // Find the difference between previous and current value
-        int extra = val - nums[i];
-
-        // Update the actual array with the new val
-        nums[i] = val;
-
-        // Update the tree with the extra value to all the parents 
-        increment(i, extra);
-    }
-
-    private void increment (int i, int extra) {
-        i++;
-        while (i <= n) {
-            fen[i] += extra;
-            i = i + (i & -i);
-        }
-    }
-
-    public int sumRange(int i, int j) {
-        // Similar to pre sum from 0 to j - pre sum from 0 to i
-        return getSum(j + 1) - getSum(i);
-    }
-
-    private int getSum (int i) {
-        int res = 0;
-        while (i > 0) {
-            res += fen[i];
-            i = i - (i & -i);
-        }
-
-        return res;
-    }
+  public int sumRange(int left, int right) {
+    return getSum(right) - getSum(left - 1);
+  }
+  
+  private void initialize(int idx, int value) {
+		idx++;
+		while (idx <= this.n) {
+			this.indexTree[idx] += value;
+			idx += (idx & -idx);
+		}
+  }
+  
+  private int getSum(int i) {
+		int sum = 0;
+		i++;
+		while (i > 0) {
+			sum += this.indexTree[i];
+			i -= (i & -i);
+		}
+		return sum;
+	}
 }
+
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
- * obj.update(i,val);
- * int param_2 = obj.sumRange(i,j);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
  */
