759fd3a55b7da740915eb668941c58168ce00c73
Varun Upadhyay <varun.u28@gmail.com>
2022-08-01 13:49:00 -0700

    Update Fruit Into Baskets.java

--- Medium/Fruit Into Baskets.java
+++ Medium/Fruit Into Baskets.java
@@ -1,22 +1,21 @@
 class Solution {
-  public int totalFruit(int[] tree) {
+  public int totalFruit(int[] fruits) {
     Map<Integer, Integer> map = new HashMap<>();
-    int maxCount = 0;
-    int start = 0;
-    int end = 0;
-    int n = tree.length;
-    while (end < n) {
-      map.put(tree[end], map.getOrDefault(tree[end], 0) + 1);
-      while (map.size() > 2) {
-        map.put(tree[start], map.get(tree[start]) - 1);
-        if (map.get(tree[start]) == 0) {
-          map.remove(tree[start]);
+    int startIdx = 0;
+    int endIdx = 0;
+    int n = fruits.length;
+    int maxPickedCount = 0;
+    while (endIdx < n) {
+      map.put(fruits[endIdx], map.getOrDefault(fruits[endIdx++], 0) + 1);
+      while (startIdx < endIdx && map.size() > 2) {
+        map.put(fruits[startIdx], map.get(fruits[startIdx]) - 1);
+        if (map.get(fruits[startIdx]) == 0) {
+          map.remove(fruits[startIdx]);
         }
-        start++;
+        startIdx++;
       }
-      end++;
-      maxCount = Math.max(maxCount, end - start);
+      maxPickedCount = Math.max(maxPickedCount, endIdx - startIdx);
     }
-    return maxCount;
-  }
+    return maxPickedCount;
+  }   
 }
