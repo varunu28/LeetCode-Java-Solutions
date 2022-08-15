93d7ac2a3780e2ab9e4c23b6e2523cb94b00c06f
Varun Upadhyay <varun.u28@gmail.com>
2022-08-15 08:16:44 -0700

    Update Roman to Integer.java

--- Easy/Roman to Integer.java
+++ Easy/Roman to Integer.java
@@ -1,25 +1,23 @@
 class Solution {
+  
   public int romanToInt(String s) {
-    String[] keys = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-    int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    String[] strings = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    int[] value = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
     Map<String, Integer> map = new HashMap<>();
-    for (int i = 0; i < keys.length; i++) {
-      map.put(keys[i], values[i]);
-    }
-    int val = 0;
+    for (int i = 0; i < strings.length; i++) {
+      map.put(strings[i], value[i]);
+    } 
     int idx = 0;
-    int n = s.length();
-    while (idx < n) {
-      char c = s.charAt(idx);
-      if (idx + 1 < n && map.containsKey(s.substring(idx, idx + 2))) {
-        val += map.get(s.substring(idx, idx + 2));
+    int num = 0;
+    while (idx < s.length()) {
+      if (idx + 1 < s.length() && map.containsKey(s.substring(idx, idx + 2))) {
+        num += map.get(s.substring(idx, idx + 2));
         idx += 2;
-      }
-      else {
-        val += map.get(String.valueOf(c));
+      } else {
+        num += map.get(s.substring(idx, idx + 1));
         idx++;
       }
     }
-    return val;
+    return num;
   }
 }
