b74a7649e064e491f06d0a2fcee1d960afe4b7ab
Varun Upadhyay <varun.u28@gmail.com>
2022-08-15 15:05:29 -0700

    Update Pairs of Songs With Total Durations Divisible by 60.java

--- Easy/Pairs of Songs With Total Durations Divisible by 60.java
+++ Easy/Pairs of Songs With Total Durations Divisible by 60.java
@@ -3,9 +3,7 @@ class Solution {
     Map<Integer, Integer> map = new HashMap<>();
     int count = 0;
     for (int t : time) {
-      if (map.containsKey(((60 - t % 60)) % 60)) {
-        count += map.get((60 - t % 60) % 60);
-      }
+      count += map.getOrDefault((60 - t % 60) % 60, 0);
       map.put(t % 60, map.getOrDefault(t % 60, 0) + 1);
     }
     return count;
