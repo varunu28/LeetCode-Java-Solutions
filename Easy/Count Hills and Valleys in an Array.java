4e7200421ca8715b00e1475729ec99961e89d50e
Varun Upadhyay <varun.u28@gmail.com>
2022-08-18 15:13:11 -0700

    Update Count Hills and Valleys in an Array.java

--- Easy/Count Hills and Valleys in an Array.java
+++ Easy/Count Hills and Valleys in an Array.java
@@ -1,19 +1,13 @@
 class Solution {
   public int countHillValley(int[] nums) {
-    List<Integer> list = new ArrayList<>();
-    list.add(nums[0]);
-    for (int i = 1; i < nums.length; i++) {
-      if (nums[i] != nums[i - 1]) {
-        list.add(nums[i]);
+    int count = 0;
+    int left = nums[0];
+    for (int i = 1; i < nums.length - 1; i++) {
+      if ((left < nums[i] && nums[i] > nums[i + 1]) || (left > nums[i] && nums[i] < nums[i + 1])) {
+        left = nums[i];
+        count++;
       }
     }
-    int hillsAndVallies = 0;
-    for (int i = 1; i < list.size() - 1; i++) {
-      if ((list.get(i) < list.get(i - 1) && list.get(i) < list.get(i + 1)) || 
-          (list.get(i) > list.get(i - 1) && list.get(i) > list.get(i + 1))) {
-        hillsAndVallies++;
-      }
-    }
-    return hillsAndVallies;
+    return count;
   }
 }
