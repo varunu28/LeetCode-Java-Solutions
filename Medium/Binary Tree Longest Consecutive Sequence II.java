9a98191ac2ad61e9276f359abebf080d7d3c9c3d
Varun Upadhyay <varun.u28@gmail.com>
2022-08-22 12:40:12 -0700

    Update Binary Tree Longest Consecutive Sequence II.java

--- Medium/Binary Tree Longest Consecutive Sequence II.java
+++ Medium/Binary Tree Longest Consecutive Sequence II.java
@@ -4,46 +4,45 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public int longestConsecutive(TreeNode root) {   
-        int[] ans = new int[1];
-        helper(root, ans);
-        return ans[0];
+  public int longestConsecutive(TreeNode root) {
+    int[] result = {0};
+    helper(root, result);
+    return result[0];
+  }
+  
+  private int[] helper(TreeNode root, int[] result) {
+    if (root == null) {
+      return new int[]{0, 0};
     }
-    
-    private int[] helper(TreeNode root, int[] ans) {
-        if (root == null) {
-            return new int[]{0,0};
-        }
-        
-        int increase = 1;
-        int decrease = 1;
-        
-        if (root.left != null) {
-            int[] left = helper(root.left, ans);
-            if (root.val == root.left.val - 1) {
-                increase = left[0] + 1;
-            }
-            if (root.val == root.left.val + 1) {
-                decrease = left[1] + 1;
-            }
-        }
-        
-        if (root.right != null) {
-            int[] right = helper(root.right, ans);
-            if (root.val == root.right.val - 1) {
-                increase = Math.max(right[0] + 1, increase);
-            }
-            if (root.val == root.right.val + 1) {
-                decrease = Math.max(right[1] + 1, decrease);
-            }
-        }
-        
-        ans[0] = Math.max(ans[0], increase + decrease - 1);
-        
-        return new int[]{increase, decrease};
+    int increment = 1;
+    int decrement = 1;
+    if (root.left != null) {
+      int[] left = helper(root.left, result);
+      if (root.val == root.left.val + 1) {
+        decrement = left[1] + 1;
+      } else if (root.val == root.left.val - 1) {
+        increment = left[0] + 1;
+      }
     }
+    if (root.right != null) {
+      int[] right = helper(root.right, result);
+      if (root.val == root.right.val + 1) {
+        decrement = Math.max(decrement, right[1] + 1);
+      } else if (root.val == root.right.val - 1) {
+        increment = Math.max(increment, right[0] + 1);
+      }
+    }
+    result[0] = Math.max(result[0], increment + decrement - 1);
+    return new int[]{increment, decrement};
+  }
 }
