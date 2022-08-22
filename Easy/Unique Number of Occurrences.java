d34c42b0630d49bcb2af3cbecc82f696ba6cb27d
Varun Upadhyay <varun.u28@gmail.com>
2022-08-22 16:41:47 -0700

    Update Unique Number of Occurrences.java

--- Easy/Unique Number of Occurrences.java
+++ Easy/Unique Number of Occurrences.java
@@ -1,10 +1,9 @@
 class Solution {
-    public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : arr) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        Set<Integer> set = new HashSet<>(map.values());
-        return map.size() == set.size();
+  public boolean uniqueOccurrences(int[] arr) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : arr) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
     }
+    return map.size() == new HashSet<>(map.values()).size();
+  }
 }
