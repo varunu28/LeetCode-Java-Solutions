89781e1ef1b6a7dd91ede9316568ec1fe8828bfa
Varun Upadhyay <varun.u28@gmail.com>
2022-08-23 14:52:03 -0700

    Update Reformat Date.java

--- Easy/Reformat Date.java
+++ Easy/Reformat Date.java
@@ -1,16 +1,22 @@
 class Solution {
+  private static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  
   public String reformatDate(String date) {
-    String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-    Map<String, String> monthMap = new HashMap<>();
-    for (int i = 1; i <= 12; i++) {
-      monthMap.put(months[i - 1], (i > 9 ? "" : "0") + i);
+    String[] split = date.split("\\s+");
+    StringBuilder sb = new StringBuilder();
+    sb.append(split[2]).append("-");
+    int monthIdx = getMonthIdx(split[1]);
+    sb.append(monthIdx > 9 ? "" : "0").append(monthIdx).append("-");
+    sb.append(Character.isDigit(split[0].charAt(1)) ? split[0].substring(0, 2) : ("0" + split[0].charAt(0)));
+    return sb.toString();
+  }
+  
+  private int getMonthIdx(String month) {
+    for (int i = 0; i < MONTHS.length; i++) {
+      if (month.equals(MONTHS[i])) {
+        return i + 1;
+      }
     }
-    String[] strs = date.split("\\s+");
-    int dayEndIdx = 0;
-    while (dayEndIdx < strs[0].length() && Character.isDigit(strs[0].charAt(dayEndIdx))) {
-      dayEndIdx++;
-    }
-    String dayStr = strs[0].substring(0, dayEndIdx);
-    return strs[2] + "-" + monthMap.get(strs[1]) + "-" + (dayStr.length() == 2 ? dayStr : "0" + dayStr);
+    return -1;
   }
 }
