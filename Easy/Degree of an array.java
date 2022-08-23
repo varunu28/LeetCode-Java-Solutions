c7a8b7596475cea3017968e18129474d5a2ec531
Varun Upadhyay <varun.u28@gmail.com>
2022-08-23 15:56:28 -0700

    Update Degree of an array.java

--- Easy/Degree of an array.java
+++ Easy/Degree of an array.java
@@ -1,24 +1,24 @@
 class Solution {
   public int findShortestSubArray(int[] nums) {
-    Map<Integer, Integer> count = new HashMap<>();
-    Map<Integer, Integer> left = new HashMap<>();
-    Map<Integer, Integer> right = new HashMap<>();
-    int degree = 0;
-    for (int i = 0; i < nums.length; i++) {
-      int num = nums[i];
-      if (!left.containsKey(num)) {
-        left.put(num, i);
-      }
-      right.put(num, i);
-      count.put(num, count.getOrDefault(num, 0) + 1);
-      degree = Math.max(degree, count.get(num));
+    Map<Integer, Integer> map = new HashMap<>();
+    int maxCount = 0;
+    for (int num : nums) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+      maxCount = Math.max(maxCount, map.get(num));
     }
-    int ans = nums.length;
-    for (int key : count.keySet()) {
-      if (count.get(key) == degree) {
-        ans = Math.min(ans, right.get(key) - left.get(key) + 1);
+    int start = 0;
+    int end = 0;
+    map.clear();
+    int minLength = Integer.MAX_VALUE;
+    while (end < nums.length) {
+      map.put(nums[end], map.getOrDefault(nums[end], 0) + 1);
+      while (start <= end && map.get(nums[end]) == maxCount) {
+        minLength = Math.min(minLength, end - start + 1);
+        map.put(nums[start], map.get(nums[start]) - 1);
+        start++;
       }
+      end++;
     }
-    return ans;
+    return minLength;
   }
 }
