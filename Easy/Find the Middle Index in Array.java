e778d07a19b922b1e2eef69fc2843df95001f866
Varun Upadhyay <varun.u28@gmail.com>
2022-08-24 11:44:06 -0700

    Update Find the Middle Index in Array.java

--- Easy/Find the Middle Index in Array.java
+++ Easy/Find the Middle Index in Array.java
@@ -1,21 +1,19 @@
 class Solution {
   public int findMiddleIndex(int[] nums) {
-    int[] leftSum = new int[nums.length];
-    int[] rightSum = new int[nums.length];
+    int n = nums.length;
+    int[] rightSum = new int[n];
     int currSum = 0;
-    for (int i = 0; i < nums.length; i++) {
-      leftSum[i] = currSum;
+    for (int i = n - 1; i >= 0; i--) {
       currSum += nums[i];
+      rightSum[i] = currSum;
     }
     currSum = 0;
-    for (int i = nums.length - 1; i >= 0; i--) {
-      rightSum[i] = currSum;
-      currSum += nums[i];
-    }
-    for (int i = 0; i < nums.length; i++) {
-      if (leftSum[i] == rightSum[i]) {
+    for (int i = 0; i < n; i++) {
+      int rightCurrSum = i != n - 1 ? rightSum[i + 1] : 0;
+      if (currSum == rightCurrSum) {
         return i;
       }
+      currSum += nums[i];
     }
     return -1;
   }
