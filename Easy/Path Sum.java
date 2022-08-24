b39622201c105f3e39c3cf8f9554b7f831762869
Varun Upadhyay <varun.u28@gmail.com>
2022-08-24 12:56:37 -0700

    Update Path Sum.java

--- Easy/Path Sum.java
+++ Easy/Path Sum.java
@@ -4,17 +4,39 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-        
-        if (root == null) return false;
-        if (root.left == null && root.right == null) {
-            return sum - root.val == 0;
-        }
-        
-        return (root.left != null ? hasPathSum(root.left, sum-root.val) : false) | (root.right != null ? hasPathSum(root.right, sum-root.val) : false);
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    if (root == null) {
+      return false;
     }
+    Queue<Integer> sumQueue = new LinkedList<>();
+    Queue<TreeNode> nodeQueue = new LinkedList<>();
+    sumQueue.add(targetSum - root.val);
+    nodeQueue.add(root);
+    while (!nodeQueue.isEmpty()) {
+      TreeNode removedNode = nodeQueue.remove();
+      int removedSum = sumQueue.remove();
+      if (removedNode.left == null && removedNode.right == null && removedSum == 0) {
+        return true;
+      }
+      if (removedNode.left != null) {
+        nodeQueue.add(removedNode.left);
+        sumQueue.add(removedSum - removedNode.left.val);
+      }
+      if (removedNode.right != null) {
+        nodeQueue.add(removedNode.right);
+        sumQueue.add(removedSum - removedNode.right.val);
+      }
+    }
+    return false;
+  }
 }
