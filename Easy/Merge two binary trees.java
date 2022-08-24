4e75613375432fe32be606c0c28da6f370c32814
Varun Upadhyay <varun.u28@gmail.com>
2022-08-24 11:32:41 -0700

    Update Merge two binary trees.java

--- Easy/Merge two binary trees.java
+++ Easy/Merge two binary trees.java
@@ -4,24 +4,24 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-    if (t1 == null && t2 == null) {
+  public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+    if (root1 == null && root2 == null) {
       return null;
     }
-    int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val); 
-    TreeNode root = new TreeNode(val);
-    root.left = mergeTrees(
-      t1 == null ? null : t1.left, 
-      t2 == null ? null : t2.left
-    );
-    root.right = mergeTrees(
-      t1 == null ? null : t1.right, 
-      t2 == null ? null : t2.right
-    );
+    int value = (root1 == null ? 0 : root1.val) + (root2 == null ? 0 : root2.val);
+    TreeNode root = new TreeNode(value);
+    root.left = mergeTrees((root1 == null ? null : root1.left), (root2 == null ? null : root2.left));
+    root.right = mergeTrees((root1 == null ? null : root1.right), (root2 == null ? null : root2.right));
     return root;
   }
 }
