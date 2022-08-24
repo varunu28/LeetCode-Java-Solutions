b60dcad9cc0eb548ec10bbc9469e6788031a7f63
Varun Upadhyay <varun.u28@gmail.com>
2022-08-23 17:01:34 -0700

    Update Reverse Bits.java

--- Easy/Reverse Bits.java
+++ Easy/Reverse Bits.java
@@ -1,11 +1,17 @@
 public class Solution {
+    // you need treat n as an unsigned value
   public int reverseBits(int n) {
-    int res = 0;
+    if (n == 0) {
+      return 0;
+    }
+    int result = 0;
     for (int i = 0; i < 32; i++) {
-      res <<= 1;
-      res = res | (n & 1);
+      result <<= 1;
+      if ((n & 1) == 1) {
+        result++;
+      }
       n >>= 1;
     }
-    return res;
+    return result;
   }
 }
