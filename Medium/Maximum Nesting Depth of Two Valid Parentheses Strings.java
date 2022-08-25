f965c45cab4ef662be975604a3f263299d6fed56
Varun Upadhyay <varun.u28@gmail.com>
2022-08-25 14:24:48 -0700

    Update Maximum Nesting Depth of Two Valid Parentheses Strings.java

--- Medium/Maximum Nesting Depth of Two Valid Parentheses Strings.java
+++ Medium/Maximum Nesting Depth of Two Valid Parentheses Strings.java
@@ -1,16 +1,11 @@
 class Solution {
   public int[] maxDepthAfterSplit(String seq) {
-    int[] maxDepth = new int[seq.length()];
-    int currDepth = 0;
-    for (int i = 0; i < seq.length(); i++) {
-      if (seq.charAt(i) == '(') {
-        currDepth++;
-      }
-      maxDepth[i] = currDepth % 2;
-      if (seq.charAt(i) != '(') {
-        currDepth--;
-      }
+    int[] result = new int[seq.length()];
+    int idx = 0;
+    int depth = 0;
+    for (char c : seq.toCharArray()) {
+      result[idx++] = c == '(' ? (depth++ % 2) : (--depth % 2);
     }
-    return maxDepth;
+    return result;
   }
 }
