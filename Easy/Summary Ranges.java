f47b20d24b55e6b35aa7e9d7bb6de8dee8ef6086
Varun Upadhyay <varun.u28@gmail.com>
2022-08-28 14:23:57 -0700

    Update Summary Ranges.java

--- Easy/Summary Ranges.java
+++ Easy/Summary Ranges.java
@@ -1,19 +1,18 @@
 class Solution {
   public List<String> summaryRanges(int[] nums) {
-    List<String> ranges = new ArrayList<>();
+    List<String> result = new ArrayList<>();
     int idx = 0;
-    while (idx < nums.length) {
-      int start = nums[idx++];
+    int n = nums.length;
+    while (idx < n) {
+      int start = nums[idx];
       int end = start;
-      while (idx < nums.length && (nums[idx] - end) == 1) {
-        end = nums[idx++];
+      idx++;
+      while (idx < n && nums[idx] == end + 1) {
+        end = nums[idx];
+        idx++;
       }
-      if (start == end) {
-        ranges.add(String.valueOf(start));
-      } else {
-        ranges.add(String.format("%d->%d", start, end));
-      }
+      result.add(start == end ? String.valueOf(start) : (start + "->" + end));
     }
-    return ranges;
+    return result;
   }
 }
