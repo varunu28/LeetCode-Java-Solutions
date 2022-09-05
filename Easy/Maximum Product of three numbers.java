a925b55250fbccf8070db1e3deccbccb14a8b7ad
Varun Upadhyay <varun.u28@gmail.com>
2022-09-05 14:45:28 -0700

    Update Maximum Product of three numbers.java

--- Easy/Maximum Product of three numbers.java
+++ Easy/Maximum Product of three numbers.java
@@ -2,9 +2,8 @@ class Solution {
   public int maximumProduct(int[] nums) {
     Arrays.sort(nums);
     int n = nums.length;
-    int rightSideProd = nums[n - 1] * nums[n - 2] * nums[n - 3];
-    int leftSideProd = nums[0] * nums[1] * nums[2];
-    int mixedSideProd = nums[0] * nums[1] * nums[n - 1];
-    return Math.max(rightSideProd, Math.max(leftSideProd, mixedSideProd));
+    int lastThree = nums[n - 1] * nums[n - 2] * nums[n - 3];
+    int firstTwoAndLast = nums[0] * nums[1] * nums[n - 1];
+    return Math.max(lastThree, firstTwoAndLast);
   }
 }
