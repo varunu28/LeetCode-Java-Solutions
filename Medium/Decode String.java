b1c3edd9496a7cc3c750a6d1a6045666de37dc3c
Varun Upadhyay <varun.u28@gmail.com>
2022-09-07 11:05:08 -0700

    Update Decode String.java

--- Medium/Decode String.java
+++ Medium/Decode String.java
@@ -1,34 +1,32 @@
 class Solution {
   public String decodeString(String s) {
-    Stack<Integer> countStack = new Stack<>();
-    Stack<String> wordStack = new Stack<>();
-    StringBuilder sb = new StringBuilder();
-    int idx = 0;
-    while (idx < s.length()) {
-      if (Character.isDigit(s.charAt(idx))) {
-        int count = 0;
-        while (idx < s.length() && Character.isDigit(s.charAt(idx))) {
-          count = count * 10 + Character.getNumericValue(s.charAt(idx++));
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (c == ']') {
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty() && stack.peek() != '[') {
+          sb.append(stack.pop());
         }
-        countStack.push(count);
-      } else if (Character.isLetter(s.charAt(idx))) {
-        sb.append(s.charAt(idx++));
-      } else if (s.charAt(idx) == '[') {
-        wordStack.push(sb.toString());
+        stack.pop();
+        String temp = sb.toString();
         sb.setLength(0);
-        idx++;
-      } else {
-        StringBuilder temp = new StringBuilder(wordStack.pop());
-        String currentString = sb.toString();
-        sb.setLength(0);
-        int count = countStack.pop();
+        while (!stack.isEmpty() && Character.isDigit(stack.peek())) {
+          sb.append(stack.pop());
+        }
+        int count = Integer.parseInt(sb.reverse().toString());
         while (count-- > 0) {
-          temp.append(currentString);
+          for (int i = temp.length() - 1; i >= 0; i--) {
+            stack.push(temp.charAt(i));
+          }
         }
-        sb.append(temp);
-        idx++;
+      } else {
+        stack.push(c);
       }
     }
-    return sb.toString();
+    StringBuilder sb = new StringBuilder();
+    while (!stack.isEmpty()) {
+      sb.append(stack.pop());
+    }
+    return sb.reverse().toString();
   }
 }
