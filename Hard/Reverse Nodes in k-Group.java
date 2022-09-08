0a4d2c6ddfbef697b602fae32b17c2f6f3005a34
Varun Upadhyay <varun.u28@gmail.com>
2022-09-08 07:01:09 -0700

    Update Reverse Nodes in k-Group.java

--- Hard/Reverse Nodes in k-Group.java
+++ Hard/Reverse Nodes in k-Group.java
@@ -10,47 +10,40 @@
  */
 class Solution {
   public ListNode reverseKGroup(ListNode head, int k) {
-    if (head == null) {
-      return null;
-    }
-    ListNode start = head;
+    ListNode newHead = null;
+    ListNode prev = null;
     ListNode curr = head;
-    ListNode prev = null;
-    ListNode revHead = null;
     while (curr != null) {
-      int count = 1;
-      while (curr.next != null && count < k) {
-        curr = curr.next;
-        count++;
-      }
-      if (count == k) {
-        ListNode next = curr.next;
-        curr.next = null;
-        if (revHead == null) {
-          revHead = curr;
-        }
-        ListNode reverse = reverse(start);
-        if (prev != null) {
-          prev.next = reverse;
-        }
-        start.next = next;
-        prev = start;
-        start = next;
-        curr = next;
-      }
-      else {
+      ListNode start = curr;
+      int idx = 1;
+      for (idx = 1; idx < k && curr.next != null; idx++) {
         curr = curr.next;
       }
+      if (idx != k) {
+        curr = null;
+        continue;
+      }
+      ListNode next = curr.next;
+      curr.next = null;
+      ListNode reversedNode = reverse(start);
+      if (newHead == null) {
+        newHead = reversedNode;
+      }
+      if (prev != null) {
+        prev.next = reversedNode;
+      }
+      prev = start;
+      start.next = next;
+      curr = next;
     }
-    return revHead == null ? head : revHead;
-  } 
+    return newHead;
+  }
   
-  private ListNode reverse(ListNode root) {
-    ListNode curr = root;
-    ListNode next = null;
+  private ListNode reverse(ListNode node) {
+    ListNode curr = node;
     ListNode prev = null;
     while (curr != null) {
-      next = curr.next;
+      ListNode next = curr.next;
       curr.next = prev;
       prev = curr;
       curr = next;
