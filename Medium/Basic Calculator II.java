ab71ab7fc99b4b63d8759b4b05216209e86372b2
Varun Upadhyay <varun.u28@gmail.com>
2022-09-08 15:54:06 -0700

    Update Basic Calculator II.java

--- Medium/Basic Calculator II.java
+++ Medium/Basic Calculator II.java
@@ -1,41 +1,28 @@
 class Solution {
   public int calculate(String s) {
-    char sign = '+';
     int currNum = 0;
-    Stack<Integer> stack = new Stack<>();
+    int prevNum = 0;
+    int result = 0;
+    char operation = '+';
     for (int i = 0; i < s.length(); i++) {
-      if (Character.isDigit(s.charAt(i))) {
-        currNum = currNum * 10 + Character.getNumericValue(s.charAt(i));
+      char c = s.charAt(i);
+      if (Character.isDigit(c)) {
+        currNum = currNum * 10 + Character.getNumericValue(c);
       } 
-      if ((!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ') || i == s.length() - 1) {
-        updateStackForPreviousSign(sign, stack, currNum);
+      if ((!Character.isDigit(c) && !Character.isWhitespace(c)) || i == s.length() - 1) {
+        if (operation == '-' || operation == '+') {
+          result += prevNum;
+          prevNum = operation == '+' ? currNum : -currNum;
+        } else if (operation == '*') {
+          prevNum = prevNum * currNum;
+        } else {
+          prevNum = prevNum / currNum;
+        }
+        operation = c;
         currNum = 0;
-        sign = s.charAt(i);
       }
     }
-    int result = 0;
-    while (!stack.isEmpty()) {
-      result += stack.pop();
-    }
+    result += prevNum;
     return result;
-  }
-
-  private void updateStackForPreviousSign(char sign, Stack<Integer> stack, int currNum) {
-    switch (sign) {
-      case '+':
-        stack.push(currNum);
-        break;
-      case '-':
-        stack.push(-1 * currNum);
-        break;
-      case '/':
-        stack.push(stack.pop() / currNum);
-        break;
-      case '*':
-        stack.push(stack.pop() * currNum);
-        break;
-      default:
-        break;
-    }
   }
 }
