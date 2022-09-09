b89443509ab0b984a78458bc659cfd26b8e9bff6
Varun Upadhyay <varun.u28@gmail.com>
2022-09-09 10:25:48 -0700

    Update Fraction to Recurring Decimal.java

--- Medium/Fraction to Recurring Decimal.java
+++ Medium/Fraction to Recurring Decimal.java
@@ -4,22 +4,22 @@ class Solution {
       return "0";
     }
     StringBuilder fraction = new StringBuilder();
-    if (numerator < 0 ^ denominator < 0) {
-      fraction.append("-");
+    if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0)) {
+      fraction.append('-');
     }
-    long dividend = Math.abs((long) numerator);
-    long divisor = Math.abs((long) denominator);
+    long dividend = Math.abs(Long.valueOf(numerator));
+    long divisor = Math.abs(Long.valueOf(denominator));
     fraction.append(String.valueOf(dividend / divisor));
     long remainder = dividend % divisor;
     if (remainder == 0) {
       return fraction.toString();
     }
-    fraction.append(".");
+    fraction.append('.');
     Map<Long, Integer> map = new HashMap<>();
     while (remainder != 0) {
       if (map.containsKey(remainder)) {
         fraction.insert(map.get(remainder), "(");
-        fraction.append(")");
+        fraction.append(')');
         break;
       }
       map.put(remainder, fraction.length());
