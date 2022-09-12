c6f4a2a1ede125f9db16e34cf2de614cca1765c9
Varun Upadhyay <varun.u28@gmail.com>
2022-09-12 14:50:29 -0700

    Update 3Sum Closest.java

--- Medium/3Sum Closest.java
+++ Medium/3Sum Closest.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        int sum = Integer.MAX_VALUE;
-        Arrays.sort(nums);
-        
-        for (int i=0; i<nums.length; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
-            
-            while (j < k) {
-                int tempSum = nums[i] + nums[j] + nums[k];
-                if (sum == Integer.MAX_VALUE || Math.abs(sum - target) > Math.abs(target - tempSum)) {
-                    sum = tempSum;
-                }
-                
-                if (tempSum > target) {
-                    k--;
-                }
-                else {
-                    j++;
-                }    
-            }
+  public int threeSumClosest(int[] nums, int target) {
+    Arrays.sort(nums);
+    int closestDiff = Integer.MAX_VALUE;
+    for (int i = 0; i < nums.length; i++) {
+      int start = i + 1;
+      int end = nums.length - 1;
+      while (start < end) {
+        int currSum = nums[i] + nums[start] + nums[end];
+        if (Math.abs(target - currSum) < Math.abs(closestDiff)) {
+          closestDiff = target - currSum;
         }
-        
-        return sum;
+        if (currSum < target) {
+          start++;
+        } else {
+          end--;
+        }
+      }
     }
+    return target - closestDiff;
+  }
 }
