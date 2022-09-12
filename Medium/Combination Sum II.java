d1c76836d08f9ba35581e4167b1e6c0e7660efa1
Varun Upadhyay <varun.u28@gmail.com>
2022-09-12 15:33:25 -0700

    Update Combination Sum II.java

--- Medium/Combination Sum II.java
+++ Medium/Combination Sum II.java
@@ -1,26 +1,25 @@
 class Solution {
   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-    List<List<Integer>> list = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
     Arrays.sort(candidates);
-    helper(candidates, 0, target, new ArrayList<>(), list);
-    return list;
+    helper(candidates, target, result, new ArrayList<>(), 0);
+    return result;
   }
   
-  private void helper(int[] candidates, int idx, int target, List<Integer> temp, List<List<Integer>> list) {
-    if (target == 0) {
-      list.add(new ArrayList<>(temp));
-      return;
-    }
-    if (target < 0) {
+  private void helper(int[] candidates, int target, List<List<Integer>> result, List<Integer> currCombination, int idx) {
+    if (target <= 0) {
+      if (target == 0) {
+        result.add(new ArrayList<>(currCombination));
+      }
       return;
     }
     for (int i = idx; i < candidates.length; i++) {
-      if (i > idx && candidates[i] == candidates[i - 1]) { 
+      if (i > idx && candidates[i] == candidates[i - 1]) {
         continue;
       }
-      temp.add(candidates[i]);
-      helper(candidates, i + 1, target - candidates[i], temp, list);
-      temp.remove(temp.size() - 1);
+      currCombination.add(candidates[i]);
+      helper(candidates, target - candidates[i], result, currCombination, i + 1);
+      currCombination.remove(currCombination.size() - 1);
     }
   }
 }
