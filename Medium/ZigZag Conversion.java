3e693f3abded64a38adb6f65c29da97fa621bf44
Varun Upadhyay <varun.u28@gmail.com>
2022-09-12 07:30:32 -0700

    Update ZigZag Conversion.java

--- Medium/ZigZag Conversion.java
+++ Medium/ZigZag Conversion.java
@@ -1,26 +1,25 @@
 class Solution {
-    public String convert(String s, int numRows) {
-        List<StringBuilder> list = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            list.add(new StringBuilder());
-        }
-        
-        int idx = 0;
-        while (idx < s.length()) {
-            for (int i = 0; i < numRows && idx < s.length(); i++) {
-                list.get(i).append(s.charAt(idx++));
-            }
-            
-            for (int i = numRows - 2; i > 0 && idx < s.length(); i--) {
-                list.get(i).append(s.charAt(idx++));
-            }
-        }
-        
-        StringBuilder ans = new StringBuilder();
-        for (StringBuilder sb : list) {
-            ans.append(sb.toString());
-        }
-        
-        return ans.toString();
+  public String convert(String s, int numRows) {
+    if (numRows == 1) {
+      return s;
     }
+    List<StringBuilder> list = new ArrayList<>();
+    for (int i = 0; i < numRows; i++) {
+      list.add(new StringBuilder());
+    }
+    int idx = 0;
+    boolean downDirection = false;
+    for (char c : s.toCharArray()) {
+      list.get(idx).append(c);
+      if (idx == 0 || idx == numRows - 1) {
+        downDirection = !downDirection;
+      }
+      idx += downDirection ? 1 : -1;
+    }
+    StringBuilder result = new StringBuilder();
+    for (StringBuilder sb : list) {
+      result.append(sb.toString());
+    }
+    return result.toString();
+  }
 }
