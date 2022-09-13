722af37143ca4e3b7edefea0ee8b020631b48262
Varun Upadhyay <varun.u28@gmail.com>
2022-09-13 16:00:56 -0700

    Update Next Greater Element II.java

--- Medium/Next Greater Element II.java
+++ Medium/Next Greater Element II.java
@@ -1,23 +1,23 @@
 class Solution {
   public int[] nextGreaterElements(int[] nums) {
-    int[] ans = new int[nums.length];
+    int[] result = new int[nums.length];
     Stack<Integer> stack = new Stack<>();
     for (int i = nums.length - 1; i >= 0; i--) {
       while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
         stack.pop();
       }
-      ans[i] = stack.isEmpty() ? -1 : nums[stack.peek()];
+      result[i] = stack.isEmpty() ? -1 : nums[stack.peek()];
       stack.push(i);
     }
     for (int i = nums.length - 1; i >= 0; i--) {
-      if (ans[i] == -1) {
+      if (result[i] == -1) {
         while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
           stack.pop();
         }
-        ans[i] = stack.isEmpty() ? -1 : nums[stack.peek()];
+        result[i] = stack.isEmpty() ? -1 : nums[stack.peek()];
       }
       stack.push(i);
     }
-    return ans;
+    return result;
   }
 }
