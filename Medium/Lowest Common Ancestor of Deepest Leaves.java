e84f22ba27be858d3241646af5f7f4e9e50d4130
Varun Upadhyay <varun.u28@gmail.com>
2022-09-15 16:23:26 -0700

    Update Lowest Common Ancestor of Deepest Leaves.java

--- Medium/Lowest Common Ancestor of Deepest Leaves.java
+++ Medium/Lowest Common Ancestor of Deepest Leaves.java
@@ -4,40 +4,42 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-class Solution {    
-    public TreeNode lcaDeepestLeaves(TreeNode root) {
-        return helper(root).lowesetCommonAncestor;
+class Solution {
+  public TreeNode lcaDeepestLeaves(TreeNode root) {
+    return dfs(root).node;
+  }
+  
+  private NodeResult dfs(TreeNode root) {
+    if (root == null) {
+      return new NodeResult(null, 0);
     }
+    NodeResult leftResult = dfs(root.left);
+    NodeResult rightResult = dfs(root.right);
+    if (leftResult.distance > rightResult.distance) {
+      return new NodeResult(leftResult.node, leftResult.distance + 1);
+    }
+    if (leftResult.distance < rightResult.distance) {
+      return new NodeResult(rightResult.node, rightResult.distance + 1);
+    }
+    return new NodeResult(root, rightResult.distance + 1);
+  }
+  
+  private static class NodeResult {
+    TreeNode node;
+    int distance;
     
-    private UpdatedTreeNode helper(TreeNode root) {       
-        if (root == null) {
-            return new UpdatedTreeNode(0, null);
-        }
-        
-        UpdatedTreeNode leftUpdated = helper(root.left);
-        UpdatedTreeNode rightUpdated = helper(root.right);
-        
-        if (leftUpdated.depth == rightUpdated.depth) {
-            return new UpdatedTreeNode(leftUpdated.depth + 1, root);
-        }
-        else if (leftUpdated.depth > rightUpdated.depth) {
-            return new UpdatedTreeNode(leftUpdated.depth + 1, leftUpdated.lowesetCommonAncestor);
-        }
-        else {
-            return new UpdatedTreeNode(rightUpdated.depth + 1, rightUpdated.lowesetCommonAncestor);
-        }
+    public NodeResult(TreeNode node, int distance) {
+      this.node = node;
+      this.distance = distance;
     }
-}
-
-class UpdatedTreeNode {
-    public int depth;
-    public TreeNode lowesetCommonAncestor;
-
-    public UpdatedTreeNode(int depth, TreeNode node) {
-        this.depth = depth;
-        this.lowesetCommonAncestor = node;
-    }
+  }
 }
