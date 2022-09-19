c1e28be546847404bc46dad2f2f62f0feb3080e9
Varun Upadhyay <varun.u28@gmail.com>
2022-09-18 17:22:27 -0700

    Update Find Duplicate File in System.java

--- Medium/Find Duplicate File in System.java
+++ Medium/Find Duplicate File in System.java
@@ -1,17 +1,20 @@
 class Solution {
   public List<List<String>> findDuplicate(String[] paths) {
-    Map<String, Set<String>> map = new HashMap<>();
+    Map<String, Set<String>> contentToDirectoryMapping = new HashMap<>();
     for (String path : paths) {
-      String[] splitPath = path.split("\\s+");
-      String directory = splitPath[0];
-      for (int i = 1; i < splitPath.length; i++) {
-        String fileName = splitPath[i].substring(0, splitPath[i].indexOf('('));
-        String fileContent = splitPath[i]
-            .substring(splitPath[i].indexOf('(') + 1, splitPath[i].indexOf(')'));
-        map.computeIfAbsent(fileContent, k -> new HashSet<>()).add(directory + "/" + fileName);
+      String[] splits = path.split("\\s+");
+      String directoryName = splits[0];
+      for (int i = 1; i < splits.length; i++) {
+        int contentStartIdx = splits[i].indexOf('(');
+        String fileName = splits[i].substring(0, contentStartIdx);
+        String content = splits[i].substring(contentStartIdx + 1, splits[i].length() - 1);
+        contentToDirectoryMapping.computeIfAbsent(content, k -> new HashSet<>())
+          .add(directoryName + "/" + fileName);
       }
     }
-    return map.values().stream().filter(entry -> entry.size() > 1).map(ArrayList::new)
-        .collect(Collectors.toList());
+    return contentToDirectoryMapping.values().stream()
+      .filter(e -> e.size() > 1)
+      .map(ArrayList::new)
+      .collect(Collectors.toList());
   }
 }
