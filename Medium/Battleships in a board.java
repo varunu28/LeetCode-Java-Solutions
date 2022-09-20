ede04e0150f07e34916f326458993eae5326ec7c
Varun Upadhyay <varun.u28@gmail.com>
2022-09-20 13:05:14 -0700

    Update Battleships in a board.java

--- Medium/Battleships in a board.java
+++ Medium/Battleships in a board.java
@@ -1,20 +1,17 @@
 class Solution {
-    public int countBattleships(char[][] board) {
-        int n = board.length;
-        if (n == 0) return 0;
-        int m = board[0].length;
-        
-        int count = 0;
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<m;j++) {
-                if (board[i][j] == '.') continue;
-                if (i > 0 && board[i-1][j] == 'X')continue;
-                if (j > 0 && board[i][j-1] == 'X')continue;
-                
-                count++;
-            }
+  
+  public int countBattleships(char[][] board) {
+    int count = 0;
+    int rows = board.length;
+    int cols = board[0].length;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (board[i][j] == '.' || (i > 0 && board[i - 1][j] == 'X') || (j > 0 && board[i][j - 1] == 'X')) {
+          continue;
         }
-        
-        return count;
+        count++;
+      }
     }
+    return count;
+  }
 }
