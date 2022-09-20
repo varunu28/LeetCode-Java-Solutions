9fa41cfdd5b1f2ba38a5824197316843c2eb6cfd
Varun Upadhyay <varun.u28@gmail.com>
2022-09-20 15:21:12 -0700

    Update Maximize Distance to Closest Person.java

--- Medium/Maximize Distance to Closest Person.java
+++ Medium/Maximize Distance to Closest Person.java
@@ -1,30 +1,14 @@
 class Solution {
   public int maxDistToClosest(int[] seats) {
-    int numberOfSeats = seats.length;
-    int[] distanceFromRight = new int[numberOfSeats];
-    int[] distanceFromLeft = new int[numberOfSeats];
-    for (int i = 1; i < numberOfSeats; i++) {
-      if (seats[i - 1] == 0 && seats[i] == 0) {
-          distanceFromLeft[i] = distanceFromLeft[i - 1] + 1;
-      }
-    }
-    for (int i = numberOfSeats - 2; i >= 0; i--) {
-      if (seats[i + 1] != 1 && seats[i] == 0) {
-          distanceFromRight[i] = distanceFromRight[i + 1] + 1;
-      }
-    }
+    int n = seats.length;
     int maxDistance = 0;
-    for (int i = 0; i < numberOfSeats; i++) {
-      if (i == 0 || i == numberOfSeats - 1) {
-        maxDistance = Math.max(
-          maxDistance, Math.max(distanceFromLeft[i], distanceFromRight[i])
-        );
-      } else {
-        maxDistance = Math.max(
-          maxDistance, Math.min(distanceFromLeft[i], distanceFromRight[i])
-        );
+    int start = -1;
+    for (int i = 0; i < n; i++) {
+      if (seats[i] == 1) {
+        maxDistance = start == -1 ? i : Math.max(maxDistance, (i - start) / 2);
+        start = i;
       }
     }
-    return maxDistance + 1;
+    return Math.max(maxDistance, n - 1 - start);
   }
 }
