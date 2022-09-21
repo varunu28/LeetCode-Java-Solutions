f6a8777afae84facfdb3a62d26c2527c11642c18
Varun Upadhyay <varun.u28@gmail.com>
2022-09-21 07:03:45 -0700

    Update Set Matrix Zeroes.java

--- Medium/Set Matrix Zeroes.java
+++ Medium/Set Matrix Zeroes.java
@@ -1,55 +1,50 @@
 class Solution {
   public void setZeroes(int[][] matrix) {
+    int rows = matrix.length;
+    int cols = matrix[0].length;
     boolean firstRowZero = false;
     boolean firstColZero = false;
-    int numRows = matrix.length;
-    int numCols = matrix[0].length;
-    // Mark if first column needs to be set zero
-    for (int i = 0; i < numRows; i++) {
+    for (int i = 0; i < rows; i++) {
       if (matrix[i][0] == 0) {
         firstColZero = true;
+        break;
       }
     }
-    // Mark if first row needs to be set zero
-    for (int i = 0; i < numCols; i++) {
+    for (int i = 0; i < cols; i++) {
       if (matrix[0][i] == 0) {
         firstRowZero = true;
+        break;
       }
     }
-    // If a value in matrix is zero set the value at first row and column to be zero
-    for (int i = 1; i < numRows; i++) {
-      for (int j = 1; j < numCols; j++) {
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
         if (matrix[i][j] == 0) {
           matrix[0][j] = 0;
           matrix[i][0] = 0;
         }
       }
     }
-    // Update the complete column to be zero if first item of row is zero
-    for (int i = 1; i < numRows; i++) {
+    for (int i = 1; i < rows; i++) {
       if (matrix[i][0] == 0) {
-        for (int j = 1; j < numCols; j++) {
+        for (int j = 0; j < cols; j++) {
           matrix[i][j] = 0;
         }
       }
     }
-    // Update the complete row to be zero if first item of column is zero
-    for (int i = 1; i < numCols; i++) {
+    for (int i = 1; i < cols; i++) {
       if (matrix[0][i] == 0) {
-        for (int j = 1; j < numRows; j++) {
+        for (int j = 0; j < rows; j++) {
           matrix[j][i] = 0;
         }
-      } 
+      }
     }
-    // Set the first row to zero if flag is set
     if (firstRowZero) {
-      for (int i = 0; i < numCols; i++) {
+      for (int i = 0; i < cols; i++) {
         matrix[0][i] = 0;
       }
     }
-    // Set the first column to zero if flag is set
     if (firstColZero) {
-      for (int i = 0; i < numRows; i++) {
+      for (int i = 0; i < rows; i++) {
         matrix[i][0] = 0;
       }
     }
