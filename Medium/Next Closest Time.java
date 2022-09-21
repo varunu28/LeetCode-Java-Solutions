2ba8f62ad50f50724cef165f9906c905023424d2
Varun Upadhyay <varun.u28@gmail.com>
2022-09-21 12:59:24 -0700

    Update Next Closest Time.java

--- Medium/Next Closest Time.java
+++ Medium/Next Closest Time.java
@@ -1,42 +1,23 @@
 class Solution {
-    public String nextClosestTime(String time) {
-        char[] timeDigits = time.toCharArray();
-        char[] sorted = time.toCharArray();
-        Arrays.sort(sorted);
-        
-        timeDigits[4] = findNext(timeDigits[4], (char)('9' + 1), sorted);
-        if (timeDigits[4] > time.charAt(4)) {
-            return String.valueOf(timeDigits);
-        }
-        
-        timeDigits[3] = findNext(timeDigits[3], '5', sorted);
-        if (timeDigits[3] > time.charAt(3)) {
-            return String.valueOf(timeDigits);
-        }
-        
-        timeDigits[1] = timeDigits[0] == '2' ? 
-                                findNext(timeDigits[1], '3', sorted) : 
-                                findNext(timeDigits[1], (char)('9' + 1), sorted);
-        if (timeDigits[1] > time.charAt(1)) {
-            return String.valueOf(timeDigits);
-        }
-        
-        timeDigits[0] = findNext(timeDigits[0], '2', sorted);
-        
-        return String.valueOf(timeDigits);
+  public String nextClosestTime(String time) {
+    int minutes = Integer.parseInt(time.substring(0, 2)) * 60 + Integer.parseInt(time.substring(3));
+    Set<Integer> digits = new HashSet<>();
+    for (char c : time.toCharArray()) {
+      digits.add(c - '0');
     }
-    
-    private char findNext(char current, char limit, char[] sorted) {
-        if (current == limit) {
-            return sorted[0];
+    while (true) {
+      minutes = (minutes + 1) % (24 * 60);
+      int[] nextTime = {minutes / 60 / 10, minutes / 60 % 10, minutes % 60 / 10, minutes % 60 % 10};
+      boolean found = true;
+      for (int unit : nextTime) {
+        if (!digits.contains(unit)) {
+          found = false;
+          break;
         }
-        
-        int pos = Arrays.binarySearch(sorted, current) + 1;
-        
-        while (pos < 4 && (sorted[pos] > limit || sorted[pos] == current)) {
-            pos++;
-        }
-        
-        return pos == 4 ? sorted[0] : sorted[pos];
+      }
+      if (found) {
+        return String.format("%02d:%02d", minutes / 60, minutes % 60);
+      }
     }
+  }
 }
