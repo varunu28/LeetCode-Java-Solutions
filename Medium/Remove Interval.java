a9e088d67ac8a96d0ae599c6f0f524b6bd4d4ac5
Varun Upadhyay <varun.u28@gmail.com>
2022-09-21 18:56:56 -0700

    Update Remove Interval.java

--- Medium/Remove Interval.java
+++ Medium/Remove Interval.java
@@ -1,41 +1,18 @@
 class Solution {
-    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
-        List<List<Integer>> list = new ArrayList<>();
-        int removeStart = toBeRemoved[0];
-        int removeEnd = toBeRemoved[1];
-        for (int[] interval : intervals) {
-            int start = interval[0];
-            int end = interval[1];
-            // Case 1: Overlap the removal interval. Skip this interval
-            if (start >= removeStart && end <= removeEnd) {
-                continue;
-            }
-            // Case 2 and 3: Completely miss out the removal interval either on left side or right side of number line.
-            // Add the complete interval
-            if ((start <= removeStart && end <= removeStart) ||
-                (start >= removeEnd && end >= removeEnd)) {
-                list.add(Arrays.asList(start, end));
-                continue;
-            }
-            // Case 4: Interval partially overlaps on left side of removal interval on number line. 
-            // Add the modified interval
-            if (start <= removeStart) {
-                int tempStart = Math.min(start, removeStart);
-                int tempEnd = Math.max(start, removeStart);
-                if (tempStart != tempEnd) {
-                    list.add(Arrays.asList(tempStart, tempEnd));
-                }
-            }
-            // Case 5: Interval partially overlaps on right side of removal interval on number line. 
-            // Add the modified interval
-            if (end >= removeEnd) {
-                int tempStart = Math.min(end, removeEnd);
-                int tempEnd = Math.max(end, removeEnd);
-                if (tempStart != tempEnd) {
-                    list.add(Arrays.asList(tempStart, tempEnd));
-                }
-            }
+  public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
+    List<List<Integer>> result = new ArrayList<>();
+    for (int[] interval : intervals) {
+      if (interval[0] > toBeRemoved[1] || interval[1] < toBeRemoved[0]) {
+        result.add(Arrays.asList(interval[0], interval[1]));
+      } else {
+        if (interval[0] < toBeRemoved[0]) {
+          result.add(Arrays.asList(interval[0], toBeRemoved[0]));
         }
-        return list;
+        if (interval[1] > toBeRemoved[1]) {
+          result.add(Arrays.asList(toBeRemoved[1], interval[1]));
+        }
+      }
     }
+    return result;
+  } 
 }
