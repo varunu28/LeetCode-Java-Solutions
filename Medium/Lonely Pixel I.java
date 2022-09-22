6b1710eec2b6bc96ac7d5dc4a77830c0ed38fce0
Varun Upadhyay <varun.u28@gmail.com>
2022-09-21 19:38:56 -0700

    Update Lonely Pixel I.java

--- Medium/Lonely Pixel I.java
+++ Medium/Lonely Pixel I.java
@@ -1,37 +1,25 @@
 class Solution {
-    public int findLonelyPixel(char[][] picture) {
-        int count = 0;
-        Map<Integer, Integer> rowMap = new HashMap<>();
-
-        for (int i=0; i<picture.length; i++) {
-            rowMap.put(i, 0);
-            for (int j=0; j<picture[i].length; j++) {
-                if (picture[i][j] == 'B') {
-                    rowMap.put(i, rowMap.get(i) + 1);
-                }
-            }
+  public int findLonelyPixel(char[][] picture) {
+    int rows = picture.length;
+    int cols = picture[0].length;
+    Map<Integer, Integer> rowMap = new HashMap<>();
+    Map<Integer, Integer> colMap = new HashMap<>();
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (picture[i][j] == 'B') {
+          rowMap.put(i, rowMap.getOrDefault(i, 0) + 1);
+          colMap.put(j, colMap.getOrDefault(j, 0) + 1);
         }
-
-        Map<Integer, Integer> colMap = new HashMap<>();
-        for (int i=0; i<picture[0].length; i++) {
-            colMap.put(i, 0);
-            for (int j=0; j<picture.length; j++) {
-                if (picture[j][i] == 'B') {
-                    colMap.put(i, colMap.get(i) + 1);
-                }
-            }
-        }
-
-        for (int i=0; i<picture.length; i++) {
-            for (int j=0; j<picture[0].length; j++) {
-                if (picture[i][j] == 'B') {
-                    if (rowMap.get(i) == 1 && colMap.get(j) == 1) {
-                        count++;
-                    }
-                }
-            }
-        }
-
-        return count;
+      }
     }
+    int count = 0;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (picture[i][j] == 'B' && rowMap.get(i) == 1 && colMap.get(j) == 1) {
+          count++;
+        }
+      }
+    }
+    return count;
+  } 
 }
