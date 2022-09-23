bdbce0f828ecae41857afe3dd3c05281c92868db
Varun Upadhyay <varun.u28@gmail.com>
2022-09-23 06:53:26 -0700

    Update Finding the Users Active Minutes.java

--- Medium/Finding the Users Active Minutes.java
+++ Medium/Finding the Users Active Minutes.java
@@ -1,24 +1,16 @@
 class Solution {
   public int[] findingUsersActiveMinutes(int[][] logs, int k) {
     Map<Integer, Set<Integer>> map = new HashMap<>();
-    List<Set<Integer>> setList = new ArrayList<>();
-    for (int i = 0; i < k; i++) {
-      setList.add(new HashSet<>());
-    }
     for (int[] log : logs) {
-      map.computeIfAbsent(log[0], j -> new HashSet<>()).add(log[1]);
-      int currActiveTime = map.get(log[0]).size();
-      if (currActiveTime <= k) {
-        setList.get(currActiveTime - 1).add(log[0]);
-        if (currActiveTime != 1) {
-          setList.get(currActiveTime - 2).remove(log[0]);
-        }
-      }
+      int id = log[0];
+      int minute = log[1];
+      map.computeIfAbsent(id, j -> new HashSet<>()).add(minute);
     }
-    int[] ans = new int[k];
-    for (int i = 0; i < k; i++) {
-      ans[i] = setList.get(i).size();
+    int[] result = new int[k];
+    for (Integer key : map.keySet()) {
+      int uam = map.get(key).size();
+      result[uam - 1]++;
     }
-    return ans;
+    return result;
   }
 }
