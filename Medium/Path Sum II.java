a76240abf6fe3e5d8aa9edfd1e0d3cdf64aef551
Varun Upadhyay <varun.u28@gmail.com>
2022-09-24 08:20:01 -0700

    Update Path Sum II.java

--- Medium/Path Sum II.java
+++ Medium/Path Sum II.java
@@ -15,25 +15,56 @@
  */
 class Solution {
   public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-    List<List<Integer>> list = new ArrayList<>();
-    helper(root, targetSum, new ArrayList<>(), list);
-    return list;
-  }
-  
-  private void helper(TreeNode root, int targetSum, List<Integer> curr, List<List<Integer>> list) {
+    List<List<Integer>> result = new ArrayList<>();
     if (root == null) {
-      return;
+      return result;
     }
-    curr.add(root.val);
-    if (isLeaf(root) && (targetSum - root.val) == 0) {
-      list.add(new ArrayList<>(curr));
+    Queue<NodePathSumWrapper> queue = new LinkedList<>();
+    NodePathSumWrapper currNode = new NodePathSumWrapper(root, 0);
+    currNode.addToPath(root.val);
+    queue.add(currNode);
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      while (size-- > 0) {
+        NodePathSumWrapper removed = queue.remove();
+        TreeNode node = removed.node;
+        if (node.left == null && node.right == null) {
+          if (removed.currSum == targetSum) {
+            result.add(new ArrayList<>(removed.path));
+          }
+        } else {
+          if (node.left != null) {
+            NodePathSumWrapper leftNodeWrapper = new NodePathSumWrapper(node.left, removed.currSum);
+            leftNodeWrapper.path.addAll(removed.path);
+            leftNodeWrapper.addToPath(node.left.val);
+            queue.add(leftNodeWrapper);
+          }
+          if (node.right != null) {
+            NodePathSumWrapper rightNodeWrapper = new NodePathSumWrapper(node.right, removed.currSum);
+            rightNodeWrapper.path.addAll(removed.path);
+            rightNodeWrapper.addToPath(node.right.val);
+            queue.add(rightNodeWrapper);
+          }
+        }
+      }
     }
-    helper(root.left, targetSum - root.val, curr, list);
-    helper(root.right, targetSum - root.val, curr, list);
-    curr.remove(curr.size() - 1);
+    return result;
   }
   
-  private boolean isLeaf(TreeNode root) {
-    return root.left == null && root.right == null;
+  private static class NodePathSumWrapper {
+    TreeNode node;
+    int currSum;
+    List<Integer> path;
+    
+    public NodePathSumWrapper(TreeNode node, int currSum) {
+      this.node = node;
+      this.currSum = currSum;
+      this.path = new ArrayList<>();
+    }
+    
+    public void addToPath(int value) {
+      this.path.add(value);
+      this.currSum += value;
+    }
   }
 }
