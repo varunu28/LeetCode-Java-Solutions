efb31e899f355bd330b829d2743d7b4763b77b61
Varun Upadhyay <varun.u28@gmail.com>
2022-09-25 07:26:34 -0700

    Update Design Circular Queue.java

--- Medium/Design Circular Queue.java
+++ Medium/Design Circular Queue.java
@@ -1,60 +1,52 @@
 class MyCircularQueue {
+  
+  private Integer[] queue;
+  private int valueCursor;
+  private int emptyCursor;
 
-  /** Initialize your data structure here. Set the size of the queue to be k. */
-  int[] queue;
-  int start;
-  int end;
-  int k;
   public MyCircularQueue(int k) {
-    this.k = k;
-    queue = new int[k];
-    start = 0;
-    end = 0;
-    Arrays.fill(queue, -1);
+    this.queue = new Integer[k];
+    this.valueCursor = 0;
+    this.emptyCursor = 0;
   }
 
-  /** Insert an element into the circular queue. Return true if the operation is successful. */
   public boolean enQueue(int value) {
-    if (end == start && queue[end] != -1) {
+    if (queue[emptyCursor] != null) {
       return false;
     }
-    queue[end++] = value;
-    if (end == k) {
-      end = 0;
+    queue[emptyCursor++] = value;
+    if (emptyCursor == queue.length) {
+      emptyCursor = 0;
     }
     return true;
   }
 
-  /** Delete an element from the circular queue. Return true if the operation is successful. */
   public boolean deQueue() {
-    if (queue[start] == -1) {
+    if (queue[valueCursor] == null) {
       return false;
     }
-    queue[start++] = -1;
-    if (start == k) {
-      start = 0;
+    queue[valueCursor++] = null;
+    if (valueCursor == queue.length) {
+      valueCursor = 0;
     }
     return true;
   }
 
-  /** Get the front item from the queue. */
   public int Front() {
-    return queue[start];
+    return queue[valueCursor] == null ? -1 : queue[valueCursor];
   }
 
-  /** Get the last item from the queue. */
   public int Rear() {
-    return end == 0 ? queue[k - 1] : queue[end - 1];
+    int idx = emptyCursor == 0 ? queue.length - 1 : emptyCursor - 1;
+    return queue[idx] == null ? -1 : queue[idx];
   }
 
-  /** Checks whether the circular queue is empty or not. */
   public boolean isEmpty() {
-    return start == end && queue[end] == -1;
+    return queue[valueCursor] == null;
   }
 
-  /** Checks whether the circular queue is full or not. */
   public boolean isFull() {
-    return start == end && queue[end] != -1;
+    return queue[emptyCursor] != null;
   }
 }
 
