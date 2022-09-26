2f9945adc8b4bd81430d4043e9e80a2632cfe9b5
Varun Upadhyay <varun.u28@gmail.com>
2022-09-26 09:09:46 -0700

    Update Check If a Number Is Majority Element in a Sorted Array.java

--- Easy/Check If a Number Is Majority Element in a Sorted Array.java
+++ Easy/Check If a Number Is Majority Element in a Sorted Array.java
@@ -1,13 +1,38 @@
 class Solution {
-    public boolean isMajorityElement(int[] nums, int target) {
-        if (nums.length == 1) {
-            return nums[0] == target;
-        }
-        
-        if (nums.length == 2) {
-            return nums[0] == target && nums[1] == target;
-        }
-        
-        return nums[nums.length / 2 - 1] == target && nums[nums.length / 2 + 1] == target;
+  
+  private static enum DIRECTION {
+    LEFT, RIGHT;
+  }
+  
+  public boolean isMajorityElement(int[] nums, int target) {
+    int rightIdx = getIndex(nums, target, DIRECTION.RIGHT);
+    int leftIdx = getIndex(nums, target, DIRECTION.LEFT);
+    if (leftIdx == -1 || rightIdx == -1) {
+      return false;
     }
+    int occurrences = rightIdx - leftIdx + 1;
+    return occurrences > nums.length / 2;
+  }
+  
+  private int getIndex(int[] nums, int target, DIRECTION direction) {
+    int resultIdx = -1;
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (nums[mid] == target) {
+        resultIdx = mid;
+        if (direction == DIRECTION.LEFT) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return resultIdx;
+  }
 }
