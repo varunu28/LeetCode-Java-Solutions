02a173e1c2409549fd772f46c9975cc9c4bc4ab7
Varun Upadhyay <varun.u28@gmail.com>
2022-09-26 10:48:05 -0700

    Update Average Waiting Time.java

--- Medium/Average Waiting Time.java
+++ Medium/Average Waiting Time.java
@@ -1,13 +1,20 @@
 class Solution {
   public double averageWaitingTime(int[][] customers) {
-    long totalWaitingTime = 0;
-    int currChefTime = 0;
+    double totalWaitingTime = 0.0;
+    int lastFreeTime = 0;
     for (int[] customer : customers) {
-      currChefTime = Math.max(currChefTime, customer[0]);
-      int endTime = currChefTime + customer[1];
-      totalWaitingTime += endTime - customer[0];
-      currChefTime = endTime;
+      int arrivalTime = customer[0];
+      // Wait time before chef finishes previous task
+      if (lastFreeTime > arrivalTime) {
+        totalWaitingTime += lastFreeTime - arrivalTime;
+      } else {
+        lastFreeTime = arrivalTime;
+      }
+      // Wait time to finish task for current customer
+      int taskTime = customer[1];
+      totalWaitingTime += taskTime;
+      lastFreeTime += taskTime;
     }
-    return totalWaitingTime / (double) customers.length;
+    return totalWaitingTime / customers.length;
   }
 }
