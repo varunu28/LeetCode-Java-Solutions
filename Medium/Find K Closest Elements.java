48560d5ffd3000dc4da87b99a0810326b6e9a438
Varun Upadhyay <varun.u28@gmail.com>
2022-09-28 17:19:06 -0700

    Update Find K Closest Elements.java

--- Medium/Find K Closest Elements.java
+++ Medium/Find K Closest Elements.java
@@ -1,11 +1,38 @@
 class Solution {
   public List<Integer> findClosestElements(int[] arr, int k, int x) {
-     return Arrays.stream(arr)
-        .mapToObj(e -> new int[]{e, Math.abs(e - x)})
-        .sorted((o1, o2) -> (o1[1] - o2[1] != 0) ? (o1[1] - o2[1]) : o1[0] - o2[0])
-        .map(e -> e[0])
-        .limit(k)
-        .sorted()
-        .collect(Collectors.toList());
+    List<Integer> result = new ArrayList<>();
+    if (arr.length == k) {
+      for (int num : arr) {
+        result.add(num);
+      }
+      return result;
+    }
+    int left = 0;
+    int right = arr.length;
+    while (left < right) {
+      int mid = (left + right) / 2;
+      if (arr[mid] >= x) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    left--;
+    right = left + 1;
+    while (right - left - 1 < k) {
+      if (left == -1) {
+        right++;
+        continue;
+      }
+      if (right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+        left--;
+      } else {
+        right++;
+      }
+    }
+    for (int i = left + 1; i < right; i++) {
+      result.add(arr[i]);
+    }
+    return result;
   }
 }
