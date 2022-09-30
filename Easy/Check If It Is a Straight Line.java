8368cad9032a47e77b274f6eb185dcfa11c05428
Varun Upadhyay <varun.u28@gmail.com>
2022-09-30 16:32:17 -0700

    Update Check If It Is a Straight Line.java

--- Easy/Check If It Is a Straight Line.java
+++ Easy/Check If It Is a Straight Line.java
@@ -1,24 +1,16 @@
 class Solution {
   public boolean checkStraightLine(int[][] coordinates) {
-    if (coordinates.length <= 2) {
-      return true;
-    }
-    int[] p1 = coordinates[0];
-    int[] p2 = coordinates[1];
-    for (int i = 2; i < coordinates.length; i++) {
-      if (!isCollinear(coordinates[i], p1, p2)) {
+    int xOne = coordinates[1][0]; 
+    int yOne = coordinates[1][1];
+    int dx = xOne - coordinates[0][0];
+    int dy = yOne - coordinates[0][1];
+    for (int[] coordinate : coordinates) {
+      int x = coordinate[0];
+      int y = coordinate[1];
+      if (dx * (y - yOne) != dy * (x - xOne)) {
         return false;
       }
     }
     return true;
-  }
-  
-  private boolean isCollinear(int[] c, int[] p1, int[] p2) {
-    if (p1[0] == c[0] || p2[0] == c[0]) {
-      return false;
-    }
-    int slope1 = (p1[1] - c[1]) / (p1[0] - c[0]);
-    int slope2 = (p2[1] - c[1]) / (p2[0] - c[0]);
-    return slope1 == slope2;
   }
 }
