bfb2be8bdc836d9f9a791680e445017af32c62bd
Varun Upadhyay <varun.u28@gmail.com>
2022-10-05 06:48:30 -0700

    Update Add One Row to Tree.java

--- Medium/Add One Row to Tree.java
+++ Medium/Add One Row to Tree.java
@@ -4,40 +4,48 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public TreeNode addOneRow(TreeNode root, int v, int d) {
-        if (d == 1) {
-            TreeNode t = new TreeNode(v);
-            t.left = root;
-            return t;
-        }
-        
-        helper(root, v, d, 1);
-        return root;
+  public TreeNode addOneRow(TreeNode root, int val, int depth) {
+    if (depth == 1) {
+      TreeNode newRoot = new TreeNode(val);
+      newRoot.left = root;
+      return newRoot;
     }
-    
-    private void helper(TreeNode root, int v, int d, int currLevel) {
-        if (currLevel == d-1) {
-            TreeNode t1 = new TreeNode(v);
-            TreeNode t2 = new TreeNode(v);
-            
-            t1.left = root.left;
-            t2.right = root.right;
-            root.left = t1;
-            root.right = t2;
-            
-            return;
+    int currDepth = 1;
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (currDepth < depth - 1) {
+      Queue<TreeNode> temp = new LinkedList<>();
+      while (!queue.isEmpty()) {
+        TreeNode removed = queue.remove();
+        if (removed.left != null) {
+          temp.add(removed.left);
         }
-        
-        if (root.left != null) {
-            helper(root.left, v, d, currLevel + 1);
+        if (removed.right != null) {
+          temp.add(removed.right);
         }
-        
-        if (root.right != null) {
-            helper(root.right, v, d, currLevel + 1);
-        }
+      }
+      queue = temp;
+      currDepth++;
     }
+    while (!queue.isEmpty()) {
+      TreeNode node = queue.remove();
+      TreeNode temp = node.left;
+      node.left = new TreeNode(val);
+      node.left.left = temp;
+      temp = node.right;
+      node.right = new TreeNode(val);
+      node.right.right = temp;
+    }
+    return root;
+  }
 }
