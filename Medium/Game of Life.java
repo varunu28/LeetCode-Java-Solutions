85b3cd517cdfe88a37db7988202f3199879c641c
Varun Upadhyay <varun.u28@gmail.com>
2022-10-07 07:59:54 -0700

    Update Game of Life.java

--- Medium/Game of Life.java
+++ Medium/Game of Life.java
@@ -1,39 +1,48 @@
 class Solution {
-  int[][] dirs = {{1, 0}, {0, 1}, {1, 1}, {-1, -1}, {-1, 0}, {0, -1}, {1, -1}, {-1, 1}};
+  
+  private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
   public void gameOfLife(int[][] board) {
     int rows = board.length;
     int cols = board[0].length;
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-        int count = getLiveNeighbourCount(board, i, j);
+        int liveNeighborCount = getLiveNeighbours(board, i, j);
         if (board[i][j] == 1) {
-          if (count < 2 || count > 3) {
-            board[i][j] = -1;
-          }
-        }
-        else {
-          if (count == 3) {
+          if (liveNeighborCount < 2 || liveNeighborCount > 3) {
             board[i][j] = 2;
+          }
+        } else {
+          if (liveNeighborCount == 3) {
+            board[i][j] = 3;
           }
         }
       }
     }
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-        board[i][j] = board[i][j] > 0 ? 1 : 0;
+        if (board[i][j] == 2) {
+          board[i][j] = 0;
+        } else if (board[i][j] == 3) {
+          board[i][j] = 1;
+        }
       }
     }
   }
   
-  private int getLiveNeighbourCount(int[][] board, int x, int y) {
+  private static int getLiveNeighbours(int[][] board, int i, int j) {
     int count = 0;
-    for (int[] dir : dirs) {
-      int newX = x + dir[0];
-      int newY = y + dir[1];
-      if (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length && Math.abs(board[newX][newY]) == 1) {
+    for (int[] dir : DIRS) {
+      int x = i + dir[0];
+      int y = j + dir[1];
+      if (isValidCell(board, x, y) && (board[x][y] == 1 || board[x][y] == 2)) {
         count++;
       }
     }
     return count;
+  }
+  
+  private static boolean isValidCell(int[][] board, int i, int j) {
+    return i >= 0 && j >= 0 && i < board.length && j < board[0].length;
   }
 }
