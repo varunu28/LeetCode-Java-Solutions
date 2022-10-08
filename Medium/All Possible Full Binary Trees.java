046236417c41462d88d1f3d69476a10a15c05668
Varun Upadhyay <varun.u28@gmail.com>
2022-10-08 10:14:32 -0700

    Update All Possible Full Binary Trees.java

--- Medium/All Possible Full Binary Trees.java
+++ Medium/All Possible Full Binary Trees.java
@@ -4,40 +4,42 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public List<TreeNode> allPossibleFBT(int N) {
-        List<TreeNode> ans = new LinkedList<>();
-        if (N % 2 == 0) {
-            return ans;
-        }
-        
-        return helper(N);
+  public List<TreeNode> allPossibleFBT(int n) {
+    Map<Integer, List<TreeNode>> dp = new HashMap<>();
+    return allPossibleFBTHelper(n, dp);
+  }
+  
+  private List<TreeNode> allPossibleFBTHelper(int n, Map<Integer, List<TreeNode>> dp) {
+    if (dp.containsKey(n)) {
+      return dp.get(n);
     }
-    
-    private List<TreeNode> helper(int N) {
-        List<TreeNode> ans = new LinkedList<>();
-        if (N == 1) {
-            ans.add(new TreeNode(0));
-            return ans;
+    List<TreeNode> result = new ArrayList<>();
+    if (n == 1) {
+      result.add(new TreeNode(0));
+    } else if (n % 2 == 1) {
+      for (int i = 0; i < n; i++) {
+        int j = n - 1 - i;
+        for (TreeNode left : allPossibleFBTHelper(i, dp)) {
+          for (TreeNode right : allPossibleFBTHelper(j, dp)) {
+            TreeNode root = new TreeNode(0);
+            root.left = left;
+            root.right = right;
+            result.add(root);
+          }
         }
-        
-        for (int i = 1; i <= N - 2; i += 2) {
-            List<TreeNode> left = helper(i);
-            List<TreeNode> right = helper(N - i - 1);
-            
-            for (TreeNode leftNode : left) {
-                for (TreeNode rightNode : right) {
-                    TreeNode node = new TreeNode(0);
-                    node.left = leftNode;
-                    node.right = rightNode;
-                    ans.add(node);
-                }
-            }
-        }
-        
-        return ans;
+      }
     }
+    dp.put(n, result);
+    return dp.get(n);
+  }
 }
