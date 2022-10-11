06947197969996aecc0998205b20c5ab75854e5f
Varun Upadhyay <varun.u28@gmail.com>
2022-10-11 14:30:38 -0700

    Update Check if One String Swap Can Make Strings Equal.java

--- Easy/Check if One String Swap Can Make Strings Equal.java
+++ Easy/Check if One String Swap Can Make Strings Equal.java
@@ -1,22 +1,16 @@
 class Solution {
-  public final int UNSET_INDEX = -1;
-  public final int SET_INDEX = 101;
   public boolean areAlmostEqual(String s1, String s2) {
-    int swapIndex = UNSET_INDEX;
+    int mismatchIdx = -1;
     for (int i = 0; i < s1.length(); i++) {
       if (s1.charAt(i) != s2.charAt(i)) {
-        if (swapIndex == SET_INDEX) {
-          return false;
-        } else if (swapIndex == UNSET_INDEX) {
-          swapIndex = i;
-        } else {
-          if (!(s1.charAt(swapIndex) == s2.charAt(i) && s1.charAt(i) == s2.charAt(swapIndex))) {
-            return false;
-          }
-          swapIndex = SET_INDEX;
+        if (mismatchIdx != -1) {
+          return s1.charAt(mismatchIdx) == s2.charAt(i) && 
+            s1.charAt(i) == s2.charAt(mismatchIdx) && 
+            s1.substring(i + 1).equals(s2.substring(i + 1));
         }
+        mismatchIdx = i;
       }
     }
-    return swapIndex == UNSET_INDEX || swapIndex == SET_INDEX;
+    return mismatchIdx == -1;
   }
 }
