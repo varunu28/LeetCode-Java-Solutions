814554b1fb135cb260bf3e19610ce0f2e966bc47
Varun Upadhyay <varun.u28@gmail.com>
2022-10-12 07:25:51 -0700

    Update Design Circular Deque.java

--- Medium/Design Circular Deque.java
+++ Medium/Design Circular Deque.java
@@ -1,62 +1,115 @@
-class MyCircularQueue {
-  private int[] queue;
-  private int valueCursor;
-  private int emptyCursor;
+class MyCircularDeque {
+  
+  private Node head;
+  private Node tail;
+  private int k;
+  private int currSize;
 
-  public MyCircularQueue(int k) {
-    this.queue = new int[k];
-    Arrays.fill(queue, -1);
-    this.emptyCursor = 0;
-    this.valueCursor = 0;
+  public MyCircularDeque(int k) {
+    this.k = k;
+    this.currSize = 0;
+    this.head = new Node(-1);
+    this.tail = new Node(-1);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
   }
 
-  public boolean enQueue(int value) {
-    if (this.queue[this.emptyCursor] != -1) {
+  public boolean insertFront(int value) {
+    if (isFull()) {
       return false;
     }
-    this.queue[this.emptyCursor++] = value;
-    if (this.emptyCursor == this.queue.length) {
-      this.emptyCursor = 0;
-    }
+    Node node = new Node(value);
+    Node nextToHead = head.next;
+    head.next = node;
+    node.prev = head;
+    node.next = nextToHead;
+    nextToHead.prev = node;
+    currSize++;
     return true;
   }
 
-  public boolean deQueue() {
-    if (this.queue[this.valueCursor] == -1) {
+  public boolean insertLast(int value) {
+    if (isFull()) {
       return false;
     }
-    this.queue[this.valueCursor++] = -1;
-    if (this.valueCursor == this.queue.length) {
-      this.valueCursor = 0;
-    }
+    Node node = new Node(value);
+    Node prevToTail = tail.prev;
+    tail.prev = node;
+    node.next = tail;
+    prevToTail.next = node;
+    node.prev = prevToTail;
+    currSize++;
     return true;
   }
 
-  public int Front() {
-    return this.queue[this.valueCursor];
+  public boolean deleteFront() {
+    if (isEmpty()) {
+      return false;
+    }
+    Node toDelete = head.next;
+    deleteNode(toDelete);
+    return true;
   }
 
-  public int Rear() {
-    int idx = this.emptyCursor == 0 ? this.queue.length - 1 : this.emptyCursor - 1;
-    return this.queue[idx];
+  public boolean deleteLast() {
+    if (isEmpty()) {
+      return false;
+    }
+    Node toDelete = tail.prev;
+    deleteNode(toDelete);
+    return true;
+  }
+
+  public int getFront() {
+    if (isEmpty()) {
+      return -1;
+    }
+    return head.next.val;
+  }
+
+  public int getRear() {
+    if (isEmpty()) {
+      return -1;
+    }
+    return tail.prev.val;
   }
 
   public boolean isEmpty() {
-    return this.queue[this.valueCursor] == -1;
+    return currSize == 0;
   }
 
   public boolean isFull() {
-    return this.queue[this.emptyCursor] != -1;
+    return currSize == k;
+  }
+  
+  private void deleteNode(Node node) {
+    Node prevToNode = node.prev;
+    Node nextToNode = node.next;
+    prevToNode.next = nextToNode;
+    nextToNode.prev = prevToNode;
+    currSize--;
+  }
+  
+  private static class Node {
+    int val;
+    Node next;
+    Node prev;
+    
+    public Node(int val) {
+      this.val = val;
+    }
   }
 }
 
 /**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue obj = new MyCircularQueue(k);
- * boolean param_1 = obj.enQueue(value);
- * boolean param_2 = obj.deQueue();
- * int param_3 = obj.Front();
- * int param_4 = obj.Rear();
- * boolean param_5 = obj.isEmpty();
- * boolean param_6 = obj.isFull();
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
  */
