c2b9fc5c85bd783bd45c04b5f329c4e5f933223f
Varun Upadhyay <varun.u28@gmail.com>
2022-10-12 06:37:12 -0700

    Update Minimum Path Sum.java

--- Medium/Minimum Path Sum.java
+++ Medium/Minimum Path Sum.java
@@ -1,22 +1,18 @@
 class Solution {
-    public static int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                if (i == 0 && j != 0) {
-                    grid[i][j] += grid[i][j-1];
-                }
-                if (i != 0 && j == 0) {
-                    grid[i][j] += grid[i-1][j];
-                }
-                if (i != 0 && j != 0) {
-                    grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
-                }
-            }
+  public int minPathSum(int[][] grid) {
+    int[][] dp = new int[grid.length][grid[0].length];
+    for (int i = 0; i < dp.length; i++) {
+      for (int j = 0; j < dp[i].length; j++) {
+        dp[i][j] += grid[i][j];
+        if (i > 0 && j > 0) {
+          dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+        } else if (i > 0) {
+          dp[i][j] += dp[i - 1][j];
+        } else if (j > 0) {
+          dp[i][j] += dp[i][j - 1];
         }
-
-        return grid[m-1][n-1];
+      }
     }
+    return dp[grid.length - 1][grid[0].length - 1];
+  }
 }
