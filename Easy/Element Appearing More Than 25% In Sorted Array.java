03835d34accc54de7fa3ca9bd028937d5aad1949
Varun Upadhyay <varun.u28@gmail.com>
2022-10-18 10:32:57 -0700

    Update Element Appearing More Than 25% In Sorted Array.java

--- Easy/Element Appearing More Than 25% In Sorted Array.java
+++ Easy/Element Appearing More Than 25% In Sorted Array.java
@@ -1,57 +1,42 @@
 class Solution {
+  
+  enum Direction {LEFT, RIGHT};
+  
   public int findSpecialInteger(int[] arr) {
     int n = arr.length;
-    int oneFourthNum = arr[n / 4];
-    int halfNum = arr[n / 2];
-    int threeFourthNum = arr[3 * n / 4];
-    if(Math.max(n/4, binarySearchRight(arr, 0, n - 1, oneFourthNum)) - 
-       Math.min(n/4, binarySearchLeft(arr, 0, n - 1, oneFourthNum)) >= arr.length / 4) {
-         return oneFourthNum;
-       }
-    if(Math.max(n/2, binarySearchRight(arr, 0, n - 1, halfNum)) - 
-       Math.min(n/2, binarySearchLeft(arr, 0, n - 1, halfNum)) >= arr.length / 4) {
-         return halfNum;
-       }
-    if(Math.max((3 * n)/4, binarySearchRight(arr, 0, n - 1, threeFourthNum)) - 
-       Math.min((3 * n)/4, binarySearchLeft(arr, 0, n - 1, threeFourthNum)) >= arr.length / 4) {
-         return threeFourthNum;
-       }
+    if (n == 1) {
+      return arr[0];
+    }
+    List<Integer> list = Arrays.asList(arr[n / 4], arr[n / 2], arr[3 * n / 4]);
+    for (int num : list) {
+      int firstPosition = getPosition(arr, num, Direction.LEFT);
+      int lastPosition = getPosition(arr, num, Direction.RIGHT);
+      if (lastPosition - firstPosition + 1 > n / 4) {
+        return num;
+      }
+    }
     return -1;
   }
   
-  private int binarySearchLeft(int[] arr, int start, int end, int num) {
-    int minIdx = Integer.MAX_VALUE;
-    while (start < end) {
-      int mid = (start + end) / 2;
-      if (arr[mid] == num) {
-        minIdx = Math.min(mid, minIdx);
-        end = mid - 1;
-      }
-      else if (arr[mid] < num) {
-        start = mid + 1;
-      }
-      else {
-        end = mid - 1;
-      }
-    }
-    return minIdx;
-  }
-  
-  private int binarySearchRight(int[] arr, int start, int end, int num) {
-    int maxIdx = Integer.MIN_VALUE;
-    while (start < end) {
-      int mid = (start + end) / 2;
-      if (arr[mid] == num) {
-        maxIdx = Math.max(mid, maxIdx);
-        start = mid + 1;
-      }
-      else if (arr[mid] < num) {
-        start = mid + 1;
-      }
-      else {
-        end = mid - 1;
+  private int getPosition(int[] nums, int num, Direction direction) {
+    int left = 0;
+    int right = nums.length - 1;
+    int idx = -1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (nums[mid] == num) {
+        idx = mid;
+        if (direction == Direction.LEFT) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else if (nums[mid] > num) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     }
-    return maxIdx;
+    return idx;
   }
 }
