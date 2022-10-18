5cad9a67848452fa35aad1b13247892e476ec3a8
Varun Upadhyay <varun.u28@gmail.com>
2022-10-18 12:14:20 -0700

    Update First Unique Number.java

--- Medium/First Unique Number.java
+++ Medium/First Unique Number.java
@@ -1,56 +1,67 @@
 class FirstUnique {
-  Node head;
-  Node tail;
-  Map<Integer, Node> map;
-  int MX_VAL = Integer.MAX_VALUE;
-  int MN_VAL = Integer.MIN_VALUE;
+  
+  private Node head;
+  private Node tail;
+  private Map<Integer, Node> map;
+  
   public FirstUnique(int[] nums) {
-    map = new HashMap<>();
-    head = new Node(MN_VAL);
-    tail = new Node(MX_VAL);
-    head.next = tail;
-    tail.prev = head;
+    this.head = new Node(-1);
+    this.tail = new Node(-1);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+    this.map = new HashMap<>();
     for (int num : nums) {
-      add(num);
+      if (map.containsKey(num)) {
+        deleteNode(map.get(num));
+      } else {
+        Node node = new Node(num);
+        map.put(num, node);
+        addToTail(node);
+      }
     }
   }
 
   public int showFirstUnique() {
-    return head.next.val == MX_VAL ? -1 : head.next.val;
+    return this.head.next.val;
   }
 
   public void add(int value) {
     if (map.containsKey(value)) {
-      if (map.get(value) != null) {
-        remove(map.get(value));
-        map.put(value, null);
-      }
+      deleteNode(map.get(value));
+      return;
     }
-    else {
-      Node newNode = new Node(value);
-      map.put(value, newNode);
-      newNode.prev = tail.prev;
-      tail.prev.next = newNode;
-      newNode.next = tail;
-      tail.prev = newNode;
-    }
+    Node node = new Node(value);
+    map.put(value, node);
+    addToTail(node);
   }
   
-  private void remove(Node node) {
-    Node prev = node.prev;
-    Node next = node.next;
-    prev.next = next;
-    next.prev = prev;
+  private void addToTail(Node node) {
+    Node prevToTail = this.tail.prev;
+    prevToTail.next = node;
+    node.next = this.tail;
+    node.prev = prevToTail;
+    this.tail.prev = node;
   }
-}
-
-class Node {
-  int val;
-  Node next;
-  Node prev;
   
-  public Node(int val) {
-    this.val = val;
+  private void deleteNode(Node node) {
+    if (node == null) {
+      return;
+    }
+    map.put(node.val, null);
+    Node prevToNode = node.prev;
+    Node nextToNode = node.next;
+    prevToNode.next = nextToNode;
+    nextToNode.prev = prevToNode;
+  }
+  
+  private static class Node {
+    int val;
+    Node next;
+    Node prev;
+    
+    public Node(int val) {
+      this.val = val;
+    }
   }
 }
 
