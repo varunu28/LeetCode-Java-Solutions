b1bcff17828b9a55239b3fb50f64b6aedfc91230
Varun Upadhyay <varun.u28@gmail.com>
2022-10-23 15:01:46 -0700

    Update Set Mismatch.java

--- Easy/Set Mismatch.java
+++ Easy/Set Mismatch.java
@@ -1,14 +1,18 @@
-import java.util.Map.Entry;
-
 class Solution {
   public int[] findErrorNums(int[] nums) {
-    HashMap<Integer, Long> frequencyMap = Arrays.stream(nums).boxed()
-        .collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
-    return new int[]{
-        frequencyMap.entrySet().stream().filter(entry -> entry.getValue().equals(2L))
-            .map(Entry::getKey).findFirst().orElse(-1),
-        IntStream.range(1, nums.length + 1).boxed().filter(key -> !frequencyMap.containsKey(key))
-            .findFirst().orElse(-1)
-    };
+    int[] result = new int[2];
+    Set<Integer> set = new HashSet<>();
+    int n = nums.length;
+    for (int num : nums) {
+      if (!set.add(num)) {
+        result[0] = num;
+      }
+    }
+    for (int i = 1; i <= n; i++) {
+      if (!set.contains(i)) {
+        result[1] = i;
+      }
+    }
+    return result;
   }
 }
