8a7bbfbb07984d9102d535ff62329c14c2edcd01
Varun Upadhyay <varun.u28@gmail.com>
2022-11-09 16:30:49 -0800

    Update Remove All Adjacent Duplicates In String.java

--- Easy/Remove All Adjacent Duplicates In String.java
+++ Easy/Remove All Adjacent Duplicates In String.java
@@ -1,20 +1,17 @@
 class Solution {
-  public String removeDuplicates(String s) {
-    Stack<Character> stack = new Stack<>();
-    int idx = 0;
-    while (idx < s.length()) {
-      char c = s.charAt(idx);
-      if (!stack.isEmpty() && stack.peek() == c) {
-        stack.pop();
-      } else {
-        stack.push(c);
-      }
-      idx++;
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
-    StringBuilder sb = new StringBuilder();
-    while (!stack.isEmpty()) {
-      sb.append(stack.pop());
-    }
-    return sb.reverse().toString();
-  }
 }
