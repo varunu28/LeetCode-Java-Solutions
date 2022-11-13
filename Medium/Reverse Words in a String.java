04c87abe28bd81873938b81cdaf3569305d13988
Varun Upadhyay <varun.u28@gmail.com>
2022-11-13 06:54:21 -0800

    Update Reverse Words in a String.java

--- Medium/Reverse Words in a String.java
+++ Medium/Reverse Words in a String.java
@@ -1,49 +1,18 @@
 class Solution {
-  public String reverseWords(String s) {
-    StringBuilder sb = trimSpace(s);
-    reverse(sb, 0, sb.length() - 1);
-    int start = 0;
-    int end = 0;
-    int n = sb.length();
-    while (start < n) {
-      while (end < n && sb.charAt(end) != ' ') {
-        end++;
-      }
-      reverse(sb, start, end - 1);
-      start = end + 1;
-      end++;
-    }
-    return sb.toString();
-  }
-  
-  private void reverse(StringBuilder sb, int start, int end) {
-    while (start <= end) {
-      char temp = sb.charAt(start);
-      sb.setCharAt(start++, sb.charAt(end));
-      sb.setCharAt(end--, temp);
-    }
-  }
-  
-  private StringBuilder trimSpace(String s) {
-    StringBuilder sb = new StringBuilder();
-    int start = 0;
-    int end = s.length() - 1;
-    while (start <= end && s.charAt(start) == ' ') {
-      start++;
-    }
-    while (end >= start && s.charAt(end) == ' ') {
-      end--;
-    }
-    while (start <= end) {
-      if (s.charAt(start) != ' ') {
-        sb.append(s.charAt(start++));
-      } else {
-        if (sb.charAt(sb.length() - 1) != ' ') {
-          sb.append(s.charAt(start));
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        int idx = s.length() - 1;
+        while (idx >= 0) {
+            if (s.charAt(idx) == ' ') {
+                idx--;
+                continue;
+            }
+            StringBuilder sb = new StringBuilder();
+            while (idx >= 0 && Character.isLetterOrDigit(s.charAt(idx))) {
+                sb.append(s.charAt(idx--));
+            }
+            result.append(sb.reverse().toString()).append(" ");
         }
-        start++;
-      }
+        return result.toString().trim();
     }
-    return sb;
-  }
 }
