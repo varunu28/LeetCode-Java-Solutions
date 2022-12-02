2aaa32060b5eed2a459b110c4e1468c2c244c11e
Varun Upadhyay <varun.u28@gmail.com>
2022-12-02 07:12:27 -0800

    Update Minimize Product Sum of Two Arrays.java

--- Medium/Minimize Product Sum of Two Arrays.java
+++ Medium/Minimize Product Sum of Two Arrays.java
@@ -1,29 +1,25 @@
 class Solution {
-  public int minProductSum(int[] nums1, int[] nums2) {
-    int[] frequencyOne = new int[101];
-    int[] frequencyTwo = new int[101];
-    for (int i = 0; i < nums1.length; i++) {
-      frequencyOne[nums1[i]]++;
-      frequencyTwo[nums2[i]]++;
+    public int minProductSum(int[] nums1, int[] nums2) {
+        int[] frequencyOne = new int[101];
+        int[] frequencyTwo = new int[101];
+        for (int i = 0; i < nums1.length; i++) {
+            frequencyOne[nums1[i]]++;
+            frequencyTwo[nums2[i]]++;
+        }
+        int productSum = 0;
+        int idxOne = 1;
+        int idxTwo = 100;
+        while (idxOne < 101 && idxTwo > 0) {
+            if (frequencyOne[idxOne] == 0) {
+                idxOne++;
+            } else if (frequencyTwo[idxTwo] == 0) {
+                idxTwo--;
+            } else {
+                productSum += idxOne * idxTwo;
+                frequencyOne[idxOne]--;
+                frequencyTwo[idxTwo]--;
+            }
+        }
+        return productSum;
     }
-    int idxOne = 0;
-    int idxTwo = 100;
-    int sum = 0;
-    while (idxOne < 101 && idxTwo >= 0) {
-      while (idxOne < 101 && frequencyOne[idxOne] == 0) {
-        idxOne++;
-      }
-      while (idxTwo >= 0 && frequencyTwo[idxTwo] == 0) {
-        idxTwo--;
-      }
-      if (idxOne == 101 || idxTwo == -1) {
-        break;
-      }
-      int counter = Math.min(frequencyOne[idxOne], frequencyTwo[idxTwo]);
-      sum += counter * idxOne * idxTwo;
-      frequencyOne[idxOne] -= counter;
-      frequencyTwo[idxTwo] -= counter;
-    }
-    return sum;
-  }   
 }
