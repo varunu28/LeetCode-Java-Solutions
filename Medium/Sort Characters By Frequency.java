113c6829c318263f68ae8f415c0f93c2bbd74ecb
Varun Upadhyay <varun.u28@gmail.com>
2022-12-02 17:58:28 -0800

    Update Sort Characters By Frequency.java

--- Medium/Sort Characters By Frequency.java
+++ Medium/Sort Characters By Frequency.java
@@ -1,41 +1,32 @@
 class Solution {
-  public String frequencySort(String s) {
-    int[] freqMap = new int[62];
-    int maxFrequency = 0;
-    for (char c : s.toCharArray()) {
-      int charIdx = getCharIndex(c);
-      freqMap[charIdx]++;
-      maxFrequency = Math.max(maxFrequency, freqMap[charIdx]);
-    }
-    List<Character>[] revMap = new List[maxFrequency + 1];
-    for (int i = 0; i < 62; i++) {
-      char c = getDecodedChar(i);
-      int currFreq = freqMap[i];
-      if (currFreq != 0) {
-        if (revMap[currFreq] == null) {
-          revMap[currFreq] = new ArrayList<>();
+    
+    private static final String ALL_LETTERS = 
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxFrequency = 0;
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            maxFrequency = Math.max(maxFrequency, map.get(c));
         }
-        revMap[currFreq].add(c);
-      }
-    }
-    StringBuilder sb = new StringBuilder();
-    for (int i = maxFrequency; i > 0; i--) {
-      if (revMap[i] != null) {
-        List<Character> characterList = revMap[i];
-        for (char c : characterList) {
-          sb.append(String.valueOf(c).repeat(i));
+        List<Character>[] frequencyToChar = new List[maxFrequency + 1];
+        for (char c : map.keySet()) {
+            if (frequencyToChar[map.get(c)] == null) {
+                frequencyToChar[map.get(c)] = new ArrayList<>();
+            }
+            frequencyToChar[map.get(c)].add(c);
         }
-      }
+        StringBuilder sb = new StringBuilder();
+        for (int i = maxFrequency; i > 0; i--) {
+            List<Character> characters = frequencyToChar[i] == null ? 
+                new ArrayList<>() : frequencyToChar[i];
+            for (char c : characters) {
+                for (int j = 0; j < i; j++) {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
     }
-    return sb.toString();
-  }
-
-  private char getDecodedChar(int idx) {
-    return (char) (idx < 26 ? 97 + idx : (idx >= 52 ? 48 + idx - 52 : 65 + idx - 26));
-  }
-
-  private int getCharIndex(char c) {
-    return Character.isDigit(c) ? 52 + c - '0'
-        : (Character.isUpperCase(c) ? c - 'A' + 26 : c - 'a');
-  }
 }
