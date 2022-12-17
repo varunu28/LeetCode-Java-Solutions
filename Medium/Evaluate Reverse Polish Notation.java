9c5f812efa54980ba27f7c1bb0929a6b765a1a19
Varun Upadhyay <varun.u28@gmail.com>
2022-12-17 08:15:32 -0800

    Update Evaluate Reverse Polish Notation.java

--- Medium/Evaluate Reverse Polish Notation.java
+++ Medium/Evaluate Reverse Polish Notation.java
@@ -1,27 +1,26 @@
 class Solution {
-  public int evalRPN(String[] tokens) {
-    Stack<Integer> stack = new Stack<>();
-    String operations = "+-/*";
-    for (String token : tokens) {
-      stack.push(operations.contains(token) ? Objects
-          .requireNonNull(performOperation(stack.pop(), stack.pop(), token))
-          : Integer.parseInt(token));
+    
+    private static final Set<String> OPERATIONS = Set.of("*", "+", "/", "-");
+    
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (OPERATIONS.contains(token)) {
+                stack.push(performOperation(stack.pop(), stack.pop(), token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
     }
-    return stack.pop();
-  }
-
-  private Integer performOperation(Integer secondOperand, Integer firstOperand, String operation) {
-    switch (operation) {
-      case "+":
-        return firstOperand + secondOperand;
-      case "-":
-        return firstOperand - secondOperand;
-      case "*":
-        return firstOperand * secondOperand;
-      case "/":
-        return firstOperand / secondOperand;
-      default:
-        return null;
+    
+    private static int performOperation(int num2, int num1, String operation) throws UnsupportedOperationException {
+        return switch(operation) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> throw new UnsupportedOperationException("Operation not supported");
+        };
     }
-  }
 }
