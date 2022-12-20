a854e63dfc56e32291cd1647c431e08cb4c6dcf8
Varun Upadhyay <varun.u28@gmail.com>
2022-12-19 18:41:16 -0800

    Update Keys and Rooms.java

--- Medium/Keys and Rooms.java
+++ Medium/Keys and Rooms.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        Stack<Integer> stack = new Stack<>();
-        Set<Integer> set = new HashSet<>();
-        set.add(0);
-        stack.add(0);
-        while (!stack.isEmpty()) {
-            int keyPopped = stack.pop();
-            List<Integer> keys = rooms.get(keyPopped);
-            for (Integer key : keys) {
-                if (!set.contains(key)) {
-                    stack.push(key);
-                    set.add(key);
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        visited.add(0);
+        while (!queue.isEmpty()) {
+            int removed = queue.remove();
+            for (Integer nextRoom : rooms.get(removed)) {
+                if (!visited.contains(nextRoom)) {
+                    queue.add(nextRoom);
+                    visited.add(nextRoom);
                 }
             }
         }
-        return set.size() == rooms.size();
+        return visited.size() == rooms.size();
     }
 }
