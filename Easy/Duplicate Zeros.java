90a64d0a37ed431611cbf2ea17d630c4f83f5afb
Varun Upadhyay <varun.u28@gmail.com>
2023-01-20 15:12:58 -0800

    Update Duplicate Zeros.java

--- Easy/Duplicate Zeros.java
+++ Easy/Duplicate Zeros.java
@@ -1,23 +1,23 @@
 class Solution {
-  public void duplicateZeros(int[] arr) {
-    int numOfZeros = 0;
-    for (int num : arr) {
-      numOfZeros += num == 0 ? 1 : 0;
+    public void duplicateZeros(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            count += arr[i] == 0 ? 1 : 0;
+        }
+        int idxOne = arr.length - 1;
+        int idxTwo = arr.length + count - 1;
+        while (idxOne != idxTwo) {
+            insertAtEnd(arr, idxOne, idxTwo--);
+            if (arr[idxOne] == 0) {
+                insertAtEnd(arr, idxOne, idxTwo--);
+            }
+            idxOne--;
+        }
     }
-    int i = arr.length - 1;
-    int j = arr.length + numOfZeros - 1;
-    while (i != j) {
-      insert(arr, i, j--);
-      if (arr[i] == 0) {
-        insert(arr, i, j--);
-      }
-      i--;
+    
+    private void insertAtEnd(int[] arr, int idxOne, int idxTwo) {
+        if (idxTwo < arr.length) {
+            arr[idxTwo] = arr[idxOne];
+        }
     }
-  }
-  
-  private void insert(int[] arr, int i, int j) {
-    if (j < arr.length) {
-      arr[j] = arr[i];
-    }
-  }
 }
