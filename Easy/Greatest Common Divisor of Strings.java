e5b2cf9e87f8d6d9b2af971a8a7b85b6e1d7da8c
Varun Upadhyay <varun.u28@gmail.com>
2023-01-31 19:33:57 -0800

    Update Greatest Common Divisor of Strings.java

--- Easy/Greatest Common Divisor of Strings.java
+++ Easy/Greatest Common Divisor of Strings.java
@@ -1,13 +1,13 @@
 class Solution {
-  public String gcdOfStrings(String str1, String str2) {
-    if (str1.length() < str2.length()) {  
-      return gcdOfStrings(str2, str1); 
-    } else if (!str1.startsWith(str2)) {
-      return "";
-    } else if (str2.isEmpty()) { 
-      return str1;
-    } else { 
-      return gcdOfStrings(str1.substring(str2.length()), str2); 
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
     }
-  }
+    
+    private int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
 }
