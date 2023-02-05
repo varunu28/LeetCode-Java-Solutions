ce91f5858fa1ef983f5d944730d8968120c449fa
Varun Upadhyay <varun.u28@gmail.com>
2023-02-05 09:18:04 -0800

    Update Find All Anagrams in a String.java

--- Medium/Find All Anagrams in a String.java
+++ Medium/Find All Anagrams in a String.java
@@ -1,30 +1,24 @@
 class Solution {
-  public List<Integer> findAnagrams(String s, String p) {
-    List<Integer> list = new ArrayList<>();
-    if (s.length() < p.length()) {
-      return list;
+    public List<Integer> findAnagrams(String s, String p) {
+        if (s.length() < p.length()) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        int[] patternCounter = new int[26];
+        for (char c : p.toCharArray()) {
+            patternCounter[c - 'a']++;
+        }
+        int[] counter = new int[26];
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            if (i >= p.length() - 1) {
+                if (Arrays.equals(patternCounter, counter)) {
+                    result.add(start);
+                }
+                counter[s.charAt(start++) - 'a']--;
+            }
+        }
+        return result;
     }
-    int[] pArr = new int[26];
-    for (char c : p.toCharArray()) {
-      pArr[c - 'a']++;
-    }
-    String pStr = Arrays.toString(pArr);
-    int start = 0;
-    int end = 0;
-    int n = s.length();
-    int[] sArr = new int[26];
-    while (end < (p.length() - 1)) {
-      sArr[s.charAt(end) - 'a']++;
-      end++;
-    }
-    while (end < n) {
-      sArr[s.charAt(end) - 'a']++;
-      end++;
-      if (Arrays.toString(sArr).equals(pStr)) {
-        list.add(start);
-      }
-      sArr[s.charAt(start++) - 'a']--;
-    }
-    return list;
-  }
 }
