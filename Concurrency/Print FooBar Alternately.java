d8c193280da25404d9ac9f4d29cfca00843b7f1b
Varun Upadhyay <varun.u28@gmail.com>
2023-02-07 15:28:38 -0800

    Update Print FooBar Alternately.java

--- Concurrency/Print FooBar Alternately.java
+++ Concurrency/Print FooBar Alternately.java
@@ -1,32 +1,33 @@
 class FooBar {
-  private int n;
+    private int n;
+    
+    private final Semaphore fooMutex;
+    private final Semaphore barMutex;
 
-  private Semaphore fooSemaphore = new Semaphore(1);
-  private Semaphore barSemaphore = new Semaphore(0);
-
-  public FooBar(int n) {
-    this.n = n;
-  }
-
-  public void foo(Runnable printFoo) throws InterruptedException {
-
-    for (int i = 0; i < n; i++) {
-
-      // printFoo.run() outputs "foo". Do not change or remove this line.
-      fooSemaphore.acquire();
-      printFoo.run();
-      barSemaphore.release();
+    public FooBar(int n) {
+        this.n = n;
+        this.fooMutex = new Semaphore(1);
+        this.barMutex = new Semaphore(0);
     }
-  }
 
-  public void bar(Runnable printBar) throws InterruptedException {
-
-    for (int i = 0; i < n; i++) {
-
-      // printBar.run() outputs "bar". Do not change or remove this line.
-      barSemaphore.acquire();
-      printBar.run();
-      fooSemaphore.release();
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+            this.fooMutex.acquire();
+        	printFoo.run();
+            this.barMutex.release();
+        }
     }
-  }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            this.barMutex.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            this.fooMutex.release();
+        }
+    }
 }
