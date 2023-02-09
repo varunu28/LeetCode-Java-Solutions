6e03310edaf59a802d56843c9be946c608a610d1
Varun Upadhyay <varun.u28@gmail.com>
2023-02-09 07:47:26 -0800

    Update Buddy Strings.java

--- Easy/Buddy Strings.java
+++ Easy/Buddy Strings.java
@@ -1,36 +1,33 @@
 class Solution {
-  public boolean buddyStrings(String s, String goal) {
-    if (s.length() != goal.length()) {
-      return false;
-    }
-    if (s.equals(goal)) {
-      Set<Character> set = new HashSet<>();
-      for (char c : s.toCharArray()) {
-        if (set.contains(c)) {
-          return true;
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
         }
-        set.add(c);
-      }
-      return false;
-    }
-    char[] mismatch = {'-', '-'};
-    for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) != goal.charAt(i)) {
-        if (mismatch[0] == '|') {
-          return false;
+        int mismatchIdx = -1;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                if (mismatchIdx == Integer.MAX_VALUE) {
+                    return false;
+                }
+                if (mismatchIdx == -1) {
+                    mismatchIdx = i;
+                } else {
+                    if (!(s.charAt(mismatchIdx) == goal.charAt(i) && s.charAt(i) == goal.charAt(mismatchIdx))) {
+                        return false;
+                    }
+                    mismatchIdx = Integer.MAX_VALUE;
+                }
+            } else {
+                set.add(s.charAt(i));
+            }
         }
-        if (mismatch[0] == '-') {
-          mismatch[0] = s.charAt(i);
-          mismatch[1] = goal.charAt(i);
-        } else {
-          if (goal.charAt(i) == mismatch[0] && s.charAt(i) == mismatch[1]) {
-            mismatch[0] = '|';
-            continue;
-          }
-          return false;
+        if (mismatchIdx != -1 && mismatchIdx != Integer.MAX_VALUE) {
+            return false;
         }
-      }
+        if (mismatchIdx == -1) {
+            return s.length() > set.size();
+        }
+        return true;
     }
-    return mismatch[0] == '|';
-  }
 }
