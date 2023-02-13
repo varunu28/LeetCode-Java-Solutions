87be42c91377f21ea55b34ec2417a668b5efdd2b
Varun Upadhyay <varun.u28@gmail.com>
2023-02-13 14:19:47 -0800

    Update Fizz Buzz Multithreaded.java

--- Concurrency/Fizz Buzz Multithreaded.java
+++ Concurrency/Fizz Buzz Multithreaded.java
@@ -1,77 +1,71 @@
 class FizzBuzz {
-  private int n;
-  private int count;
-  private Semaphore fizz;
-  private Semaphore buzz;
-  private Semaphore fizzBuzz;
-  private Semaphore number;
+    private int n;
+    private final Semaphore fizzSemaphore;
+    private final Semaphore buzzSemaphore;
+    private final Semaphore fizzBuzzSemaphore;
+    private final Semaphore numSemaphore;
 
-  public FizzBuzz(int n) {
-    this.n = n;
-    this.count = 1;
-    this.fizz = new Semaphore(0);
-    this.buzz = new Semaphore(0);
-    this.fizzBuzz = new Semaphore(0);
-    this.number = new Semaphore(1);
-  }
+    public FizzBuzz(int n) {
+        this.n = n;
+        this.fizzSemaphore = new Semaphore(0);
+        this.buzzSemaphore = new Semaphore(0);
+        this.fizzBuzzSemaphore = new Semaphore(0);
+        this.numSemaphore = new Semaphore(1);
+    }
 
-  // printFizz.run() outputs "fizz".
-  public void fizz(Runnable printFizz) throws InterruptedException {
-    while (this.count <= this.n) {
-      if (this.count % 3 == 0 && this.count % 5 != 0) {
-        this.fizz.acquire();
-        printFizz.run();
-        this.count++;
-        releaseLock(this.count);
-      }
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                fizzSemaphore.acquire();
+                printFizz.run();
+                releaseLock(i + 1);
+            }
+        }
     }
-  }
 
-  // printBuzz.run() outputs "buzz".
-  public void buzz(Runnable printBuzz) throws InterruptedException {
-    while (this.count <= this.n) {
-      if (this.count % 3 != 0 && this.count % 5 == 0) {
-        this.buzz.acquire();
-        printBuzz.run();
-        this.count++;
-        releaseLock(this.count);
-      }
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 == 0) {
+                buzzSemaphore.acquire();
+                printBuzz.run();
+                releaseLock(i + 1);
+            }
+        }
     }
-  }
 
-  // printFizzBuzz.run() outputs "fizzbuzz".
-  public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
-    while (this.count <= this.n) {
-      if (this.count % 3 == 0 && this.count % 5 == 0) {
-        this.fizzBuzz.acquire();
-        printFizzBuzz.run();
-        this.count++;
-        releaseLock(this.count);
-      }
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                fizzBuzzSemaphore.acquire();
+                printFizzBuzz.run();
+                releaseLock(i + 1);
+            }
+        }
     }
-  }
 
-  // printNumber.accept(x) outputs "x", where x is an integer.
-  public void number(IntConsumer printNumber) throws InterruptedException {
-    while (this.count <= this.n) {
-      if (this.count % 3 != 0 && this.count % 5 != 0) {
-        this.number.acquire();
-        printNumber.accept(this.count);
-        this.count++;
-        releaseLock(this.count);
-      }
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                numSemaphore.acquire();
+                printNumber.accept(i);
+                releaseLock(i + 1);
+            }
+        }
     }
-  }
-  
-  private void releaseLock(int n) {
-    if (n % 15 == 0) {
-      this.fizzBuzz.release();
-    } else if (n % 3 == 0) {
-      this.fizz.release();
-    } else if (n % 5 == 0) {
-      this.buzz.release();
-    } else {
-      this.number.release();
+    
+    private void releaseLock(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            fizzBuzzSemaphore.release();
+        } else if (num % 3 == 0 && num % 5 != 0) {
+            fizzSemaphore.release();
+        } else if (num % 3 != 0 && num % 5 == 0) {
+            buzzSemaphore.release();
+        } else {
+            numSemaphore.release();
+        }
     }
-  }
 }
