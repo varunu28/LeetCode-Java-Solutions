941a02b99156b765207db30669a59e0a85fe346e
Varun Upadhyay <varun.u28@gmail.com>
2023-02-13 10:03:21 -0800

    Update Print Zero Even Odd.java

--- Concurrency/Print Zero Even Odd.java
+++ Concurrency/Print Zero Even Odd.java
@@ -1,42 +1,43 @@
 class ZeroEvenOdd {
-  private int n;
-  private int flag = 0;
-
-  public ZeroEvenOdd(int n) {
-    this.n = n;
-  }
-
-  // printNumber.accept(x) outputs "x", where x is an integer.
-  public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
-    for (int i = 0; i < n; i++) {
-      while (flag != 0) {
-        this.wait();
-      }
-      printNumber.accept(0);
-      flag = i % 2 == 0 ? 1 : 2;
-      this.notifyAll();
+    private int n;
+    
+    private final Semaphore zeroMutex;
+    private final Semaphore oddMutex;
+    private final Semaphore evenMutex;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+        this.zeroMutex = new Semaphore(1);
+        this.oddMutex = new Semaphore(0);
+        this.evenMutex = new Semaphore(0);
     }
-  }
 
-  public synchronized void even(IntConsumer printNumber) throws InterruptedException {
-    for (int i = 2; i <= n; i += 2) {
-      while (flag != 2) {
-        this.wait();
-      }
-      printNumber.accept(i);
-      flag = 0;
-      this.notifyAll();
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            this.zeroMutex.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                this.oddMutex.release();
+            } else {
+                this.evenMutex.release();
+            }
+        }
     }
-  }
 
-  public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
-    for (int i = 1; i <= n; i += 2) {
-      while (flag != 1) {
-        this.wait();
-      }
-      printNumber.accept(i);
-      flag = 0;
-      this.notifyAll();
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            this.evenMutex.acquire();
+            printNumber.accept(i);
+            this.zeroMutex.release();
+        }
     }
-  }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            this.oddMutex.acquire();
+            printNumber.accept(i);
+            this.zeroMutex.release();
+        } 
+    }
 }
