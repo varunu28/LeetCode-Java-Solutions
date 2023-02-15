c83324310a19917ce235b528685059aea0d12cbd
Varun Upadhyay <varun.u28@gmail.com>
2023-02-14 16:17:25 -0800

    Update Add to Array-Form of Integer.java

--- Easy/Add to Array-Form of Integer.java
+++ Easy/Add to Array-Form of Integer.java
@@ -1,15 +1,22 @@
 class Solution {
-  public List<Integer> addToArrayForm(int[] num, int k) {
-    int carry = 0;
-    int idx = num.length - 1;
-    List<Integer> result = new ArrayList<>();
-    while (idx >= 0 || carry > 0 || k > 0) {
-      int temp = k % 10 + carry + (idx >= 0 ? num[idx--] : 0);
-      result.add(temp % 10);
-      carry = temp / 10;
-      k /= 10;
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+        int idx = num.length - 1;
+        int carry = 0;
+        while (idx >= 0 || k > 0 || carry > 0) {
+            if (idx >= 0 && k > 0) {
+                carry += num[idx--] + k % 10;
+                k /= 10;
+            } else if (idx >= 0 && k == 0) {
+                carry += num[idx--];
+            } else if (idx < 0 && k > 0) {
+                carry += k % 10;
+                k /= 10;
+            }
+            result.add(carry % 10);
+            carry /= 10;
+        }
+        Collections.reverse(result);
+        return result;
     }
-    Collections.reverse(result);
-    return result;
-  }
 }
