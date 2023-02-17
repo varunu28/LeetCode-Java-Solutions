3e3198b55f0d3166f08c5ab86352a665771d008d
Varun Upadhyay <varun.u28@gmail.com>
2023-02-17 06:36:40 -0800

    Update Minimum Distance Between BST Nodes.java

--- Easy/Minimum Distance Between BST Nodes.java
+++ Easy/Minimum Distance Between BST Nodes.java
@@ -14,25 +14,22 @@
  * }
  */
 class Solution {
-  Integer minDiff;
-  Integer prev;
-  public int minDiffInBST(TreeNode root) {
-    minDiff = Integer.MAX_VALUE;
-    prev = null;
-    helper(root);
-    return minDiff;
-  }
-  
-  private void helper(TreeNode root) {
-    if (root == null) {
-      return;
+    public int minDiffInBST(TreeNode root) {
+        int[] minDiff = {Integer.MAX_VALUE};
+        Integer[] prevVal = {null};
+        inorder(root, minDiff, prevVal);
+        return minDiff[0];
     }
-    helper(root.left);
-    if (prev != null) {
-      minDiff = Math.min(minDiff, root.val - prev);
+    
+    private void inorder(TreeNode node, int[] minDiff, Integer[] prevValue) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, minDiff, prevValue);
+        if (prevValue[0] != null) {
+            minDiff[0] = Math.min(minDiff[0], node.val - prevValue[0]);
+        }
+        prevValue[0] = node.val;
+        inorder(node.right, minDiff, prevValue);
     }
-    prev = root.val;
-    helper(root.right);
-  }
 }
-
