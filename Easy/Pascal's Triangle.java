0f8b1ef4172cc6b271c775ce52470680a2ae71b0
Varun Upadhyay <varun.u28@gmail.com>
2023-02-18 07:51:15 -0800

    Update Pascal's Triangle.java

--- Easy/Pascal's Triangle.java
+++ Easy/Pascal's Triangle.java
@@ -1,15 +1,17 @@
 class Solution {
-  public List<List<Integer>> generate(int numRows) {
-    List<List<Integer>> result = new ArrayList<>();
-    for (int i = 0; i < numRows; i++) {
-      List<Integer> temp = new ArrayList<>();
-      for (int j = 0; j <= i; j++) {
-        temp.add(
-          (j == 0 || j == i) ? 1 : 
-          (result.get(i - 1).get(j - 1) + result.get(i - 1).get(j)));
-      }
-      result.add(temp);
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    temp.add(1);
+                } else {
+                    temp.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(temp);
+        }
+        return result;
     }
-    return result;
-  }
 }
