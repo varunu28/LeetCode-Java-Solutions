325529cc8c40d2a54321708adcf6d75e779f2436
varunu28 <varun.u28@gmail.com>
2023-02-20 17:44:04 -0800

    Updated Single Element in a Sorted Array

--- Medium/Single Element in a Sorted Array.java
+++ Medium/Single Element in a Sorted Array.java
@@ -1,25 +1,25 @@
 class Solution {
-  public int singleNonDuplicate(int[] nums) {
-    if (nums.length == 1) {
-      return nums[0];
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid + 1] == nums[mid]) {
+                if ((end - mid) % 2 == 0) {
+                    start = mid + 2;
+                } else {
+                    end = mid - 1;
+                }               
+            } else if (nums[mid - 1] == nums[mid]) {
+                if ((end - mid) % 2 == 0) {
+                    end = mid - 2;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                return nums[mid];
+            }
+        }
+        return nums[start];
     }
-    return binarySearchHelper(nums, 0, nums.length - 1);
-  }
-  
-  private int binarySearchHelper(int[] nums, int start, int end) {
-    if (start > end) {
-      return Integer.MIN_VALUE;
-    }
-    int mid = (start + end) / 2;
-    if (mid == 0 && mid + 1 < nums.length && nums[mid] != nums[mid + 1]) {
-      return nums[mid];
-    }
-    if (mid == nums.length - 1 && mid - 1 >= 0 && nums[mid] != nums[mid - 1]) {
-      return nums[mid];
-    }
-    if (mid + 1 < nums.length && nums[mid] != nums[mid + 1] && mid - 1 >= 0 && nums[mid] != nums[mid - 1]) {
-      return nums[mid];
-    }
-    return Math.max(binarySearchHelper(nums, start, mid - 1), binarySearchHelper(nums, mid + 1, end));
-  }
 }
