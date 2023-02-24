47e01c0983188538f8c087478b4c123d36be4a32
varunu28 <varun.u28@gmail.com>
2023-02-24 07:27:59 -0800

    Updated Symmetric Tree.java to iterative style

--- Easy/Symmetric Tree.java
+++ Easy/Symmetric Tree.java
@@ -12,19 +12,30 @@
  *         this.right = right;
  *     }
  * }
-*/
+ */
 class Solution {
-  public boolean isSymmetric(TreeNode root) {
-    return helper(root.left, root.right);
-  }
-  
-  private boolean helper(TreeNode leftNode, TreeNode rightNode) {
-    if (leftNode == null && rightNode == null) {
-      return true;
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode[]> queue = new LinkedList<>();
+        queue.add(new TreeNode[]{root.left, root.right});
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode[] removed = queue.remove();
+                TreeNode nodeOne = removed[0];
+                TreeNode nodeTwo = removed[1];
+                if (nodeOne == null && nodeTwo == null) {
+                    continue;
+                }
+                if (nodeOne == null || nodeTwo == null) {
+                    return false;
+                }
+                if (nodeOne.val != nodeTwo.val) {
+                    return false;
+                }
+                queue.add(new TreeNode[]{nodeOne.left, nodeTwo.right});
+                queue.add(new TreeNode[]{nodeOne.right, nodeTwo.left});
+            }
+        }
+        return true;
     }
-    if ((leftNode == null || rightNode == null) || leftNode.val != rightNode.val) {
-      return false;
-    }
-    return helper(leftNode.left, rightNode.right) && helper(leftNode.right, rightNode.left);
-  }
 }
