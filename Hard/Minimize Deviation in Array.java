1d92d5d1e5a0182c540ff6fcef5a7d9190d01fb4
varunu28 <varun.u28@gmail.com>
2023-02-24 06:30:26 -0800

    Update Minimize Deviation in Array.java

--- Hard/Minimize Deviation in Array.java
+++ Hard/Minimize Deviation in Array.java
@@ -1,21 +1,21 @@
 class Solution {
-  public int minimumDeviation(int[] nums) {
-    int currMinimum = Integer.MAX_VALUE;
-    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((a, b) -> b - a);
-    for (int num : nums) {
-      if (num % 2 != 0) {
-        num *= 2;
-      }
-      currMinimum = Math.min(currMinimum, num);
-      priorityQueue.add(num);
+    public int minimumDeviation(int[] nums) {
+        int currMinimum = Integer.MAX_VALUE;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                num *= 2;
+            }
+            currMinimum = Math.min(currMinimum, num);
+            pq.add(num);
+        }
+        int deviation = Integer.MAX_VALUE;
+        while (pq.peek() % 2 == 0) {
+            int removed = pq.poll() / 2;
+            currMinimum = Math.min(currMinimum, removed);
+            pq.add(removed);
+            deviation = Math.min(deviation, pq.peek() - currMinimum);
+        }
+        return deviation;
     }
-    int deviation = Integer.MAX_VALUE;
-    while (priorityQueue.peek() % 2 == 0) {
-      int removed = priorityQueue.poll() / 2;
-      currMinimum = Math.min(currMinimum, removed);
-      priorityQueue.add(removed);
-      deviation = Math.min(deviation, priorityQueue.peek() - currMinimum);
-    }
-    return deviation;
-  }
 }
