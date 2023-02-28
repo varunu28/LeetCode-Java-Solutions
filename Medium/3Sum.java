32a5bef670a17c7a973a8be96f7da57e8edf5ed7
varunu28 <varun.u28@gmail.com>
2023-02-28 09:04:22 -0800

    Updated 3Sum.java

--- Medium/3Sum.java
+++ Medium/3Sum.java
@@ -1,21 +1,25 @@
 class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-    Set<List<Integer>> result = new HashSet<>();
-    Set<Integer> duplicates = new HashSet<>();
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-      if (duplicates.add(nums[i])) {
-        for (int j = i + 1; j < nums.length; j++) {
-          int target = -nums[i] - nums[j];
-          if (map.containsKey(target) && map.get(target) == i) {
-            List<Integer> temp = Arrays.asList(nums[i], nums[j], target);
-            Collections.sort(temp);
-            result.add(temp);
-          }
-          map.put(nums[j], i);
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int low = i + 1;
+            int high = nums.length - 1;
+            while (low < high) {
+                int currSum = nums[low] + nums[high] + nums[i];
+                if (currSum > 0 || (high < nums.length - 1 && nums[high] == nums[high + 1])) {
+                    high--;
+                } else if (currSum < 0 || (low > i + 1 && nums[low] == nums[low - 1])) {
+                    low++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
+                }
+                
+            }
         }
-      }
+        return result;
     }
-    return new ArrayList<>(result);
-  }
 }
