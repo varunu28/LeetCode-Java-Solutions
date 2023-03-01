9e9de5a770cd44269526d201256e9a1ad9e2eca2
Varun Upadhyay <varun.u28@gmail.com>
2023-03-01 14:19:09 -0800

    Update Valid Word Square.java

--- Easy/Valid Word Square.java
+++ Easy/Valid Word Square.java
@@ -1,13 +1,23 @@
 class Solution {
     public boolean validWordSquare(List<String> words) {
-        int n = words.size();
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < words.get(i).length(); j++){
-                if(j >= n || words.get(j).length() <= i || words.get(j).charAt(i) != words.get(i).charAt(j)) {
-                    return false;
-                }
+        int column = 0;
+        for (String rowWord : words) {
+            String columnWord = getColumnWord(words, column++);
+            if (!rowWord.equals(columnWord)) {
+                return false;
             }
         }
         return true;
+    }
+    
+    private String getColumnWord(List<String> words, int column) {
+        StringBuilder sb = new StringBuilder();
+        for (String word : words) {
+            if (column >= word.length()) {
+                break;
+            }
+            sb.append(word.charAt(column));
+        }
+        return sb.toString();
     }
 }
