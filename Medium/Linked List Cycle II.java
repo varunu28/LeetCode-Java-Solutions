139f4792b78f4d940221ab535201178cc92bacec
varunu28 <varun.u28@gmail.com>
2023-03-08 17:47:09 -0800

    Updated Linked List Cycle II

--- Medium/Linked List Cycle II.java
+++ Medium/Linked List Cycle II.java
@@ -10,24 +10,24 @@
  * }
  */
 public class Solution {
-  public ListNode detectCycle(ListNode head) {
-    ListNode fast = head;
-    ListNode slow = head;
-    while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
-      if (slow == fast) {
-        break;
-      }
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        ListNode curr = head;
+        while (curr != slow) {
+            curr = curr.next;
+            slow = slow.next;
+        }
+        return curr;
     }
-    if (fast == null || fast.next == null) {
-      return null;
-    }
-    slow = head;
-    while (slow != fast) {
-      slow = slow.next;
-      fast = fast.next;
-    }
-    return slow;
-  }
 }
