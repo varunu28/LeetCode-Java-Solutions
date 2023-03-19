f3c457c2327a18ae71d84d0f21e14ab2aa2f13b0
varunu28 <varun.u28@gmail.com>
2023-03-18 18:16:35 -0700

    Updated Design Add and Search Words Data Structure

--- Medium/Design Add and Search Words Data Structure.java
+++ Medium/Design Add and Search Words Data Structure.java
@@ -1,54 +1,53 @@
 class WordDictionary {
-  
-  private Node root;
-  
-  public WordDictionary() {
-    this.root = new Node();
-  }
 
-  public void addWord(String word) {
-    Node curr = root;
-    for (char c : word.toCharArray()) {
-      if (curr.children[c - 'a'] == null) {
-        curr.children[c - 'a'] = new Node();
-      }
-      curr = curr.children[c - 'a'];
-    }
-    curr.isWord = true;
-  }
+    private final TrieNode root;
 
-  public boolean search(String word) {
-    Node curr = root;
-    return searchHelper(word, 0, curr);
-  }
-  
-  private boolean searchHelper(String word, int idx, Node curr) {
-    if (idx == word.length()) {
-      return curr.isWord;
+    public WordDictionary() {
+        this.root = new TrieNode();
     }
-    if (word.charAt(idx) != '.' && curr.children[word.charAt(idx) - 'a'] == null) {
-      return false;
-    }
-    if (word.charAt(idx) == '.') {
-      for (Node child : curr.children) {
-        if (child != null && searchHelper(word, idx + 1, child)) {
-          return true;
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
         }
-      }
-      return false;
+        curr.isWord = true;
     }
-    return searchHelper(word, idx + 1, curr.children[word.charAt(idx) - 'a']);
-  }
-  
-  private static class Node {
-    Node[] children;
-    boolean isWord;
-    
-    public Node() {
-      this.children = new Node[26];
-      this.isWord = false;
+
+    public boolean search(String word) {
+        return searchHelper(word, 0, root);
     }
-  }
+
+    private boolean searchHelper(String word, int idx, TrieNode node) {
+        if (idx == word.length()) {
+            return node.isWord;
+        }
+        if (node.children.containsKey(word.charAt(idx))) {
+            return searchHelper(word, idx + 1, node.children.get(word.charAt(idx)));
+        }
+        if (word.charAt(idx) == '.') {
+            for (TrieNode child : node.children.values()) {
+                if (searchHelper(word, idx + 1, child)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    static class TrieNode {
+        private final Map<Character, TrieNode> children;
+        private boolean isWord;
+
+        public TrieNode() {
+            this.children = new HashMap<>();
+            this.isWord = false;
+        }
+    }
 }
 
 /**
