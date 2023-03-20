a06b0d228c3383537c9367b3641f0115fc82e66d
varunu28 <varun.u28@gmail.com>
2023-03-19 17:26:33 -0700

    Updated Can Place Flowers

--- Easy/Can Place Flowers.java
+++ Easy/Can Place Flowers.java
@@ -1,19 +1,19 @@
 class Solution {
-  public boolean canPlaceFlowers(int[] flowerbed, int n) {
-    for (int i = 0; i < flowerbed.length && n > 0; i++) {
-      if (flowerbed[i] == 0) {
-        if (i == 0 && (flowerbed.length == 1 || flowerbed[i + 1] == 0)) {
-          flowerbed[i] = 1;
-          n--;
-        } else if (i == flowerbed.length - 1 && flowerbed[i - 1] == 0) {
-          flowerbed[i] = 1;
-          n--;
-        } else if (i + 1 < flowerbed.length && flowerbed[i + 1] == 0 && i - 1 >= 0 && flowerbed[i - 1] == 0) {
-          flowerbed[i] = 1;
-          n--;
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length && n > 0; i++) {
+            if (canPlaceCheck(i, flowerbed)) {
+                flowerbed[i] = 1;
+                n--;
+            } 
         }
-      }
+        return n == 0;
     }
-    return n == 0;
-  }
+    
+    private boolean canPlaceCheck(int i, int[] flowerbed) {
+        return flowerbed[i] == 0 && (
+            (i == 0 && (i + 1 == flowerbed.length || flowerbed[i + 1] == 0)) || 
+            (i == flowerbed.length - 1 && (i - 1 == -1 || flowerbed[i - 1] == 0)) || 
+            (i != 0 && i != flowerbed.length - 1 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+        );
+    }
 }
