ea605a4299846f3f1929e17a4c219c293cd3bf28
varunu28 <varun.u28@gmail.com>
2023-03-24 19:27:03 -0700

    Updated Count Unreachable Pairs of Nodes in an Undirected Graph

--- Medium/Count Unreachable Pairs of Nodes in an Undirected Graph.java
+++ Medium/Count Unreachable Pairs of Nodes in an Undirected Graph.java
@@ -1,29 +1,36 @@
 class Solution {
-  public long countPairs(int n, int[][] edges) {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-    for (int[] edge : edges) {
-      graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
-      graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+    public long countPairs(int n, int[][] edges) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int[] edge : edges) {
+            graph.computeIfAbsent(edge[0], k -> new HashSet<>()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], k -> new HashSet<>()).add(edge[0]);
+        }
+        boolean[] visited = new boolean[n];
+        long unreachableNodeCount = (((long) n) * (n - 1)) / 2;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                long reachCount = traverseHelper(i, graph, visited);
+                unreachableNodeCount -= (reachCount * (reachCount - 1)) / 2;
+            }
+        }
+        return unreachableNodeCount;
     }
-    long unreachableNodes = (((long) n) * (n - 1)) / 2;
-    boolean[] visited = new boolean[n];
-    for (int i = 0; i < n; i++) {
-      if (!visited[i]) {
-        long[] reachCount = {0};
-        dfs(graph, i, reachCount, visited);
-        unreachableNodes -= (reachCount[0] * (reachCount[0] - 1)) / 2;
-      }      
+
+    private long traverseHelper(int node, Map<Integer, Set<Integer>> graph, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        long count = 0;
+        queue.add(node);
+        visited[node] = true;
+        while (!queue.isEmpty()) {
+            int removed = queue.remove();
+            count++;
+            for (Integer conn : graph.getOrDefault(removed, new HashSet<>())) {
+                if (!visited[conn]) {
+                    visited[conn] = true;
+                    queue.add(conn);
+                }
+            }
+        }
+        return count;
     }
-    return unreachableNodes;
-  }
-  
-  private void dfs(Map<Integer, List<Integer>> graph, int node, long[] reachCount, boolean[] visited) {
-    visited[node] = true;
-    reachCount[0]++;
-    for (Integer conn : graph.getOrDefault(node, new ArrayList<>())) {
-      if (!visited[conn]) {
-        dfs(graph, conn, reachCount, visited);
-      }
-    }
-  }
 }
