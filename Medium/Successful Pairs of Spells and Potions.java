f2c68be5498d5e5723f1e0ff2892b175f3fa382e
varunu28 <varun.u28@gmail.com>
2023-04-01 17:22:58 -0700

    Updated Successful Pairs of Spells and Potions.java

--- Medium/Successful Pairs of Spells and Potions.java
+++ Medium/Successful Pairs of Spells and Potions.java
@@ -1,26 +1,26 @@
 class Solution {
-  public int[] successfulPairs(int[] spells, int[] potions, long success) {
-    int[] result = new int[spells.length];
-    Arrays.sort(potions);
-    for (int i = 0; i < spells.length; i++) {
-      int idx = getSuccessIdx(potions, spells[i], success);
-      result[i] = potions.length - idx;
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int[] result = new int[spells.length];
+        for (int i = 0; i < spells.length; i++) {
+            result[i] = findCountOfSuccessfulPotion(potions, success, (long) spells[i]);
+        }
+        return result;
     }
-    return result;
-  }
-  
-  private int getSuccessIdx(int[] potions, int spell, long success) {
-    int left = 0;
-    int right = potions.length;
-    while (left < right) {
-      int mid = (left + right) / 2;
-      long currSuccess = ((long) potions[mid]) * spell;
-      if (currSuccess >= success) {
-        right = mid;
-      } else {
-        left = mid + 1;
-      }
+    
+    private int findCountOfSuccessfulPotion(int[] potions, long success, long spell) {
+        int left = 0;
+        int right = potions.length - 1;
+        int minIdx = Integer.MAX_VALUE;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (potions[mid] * spell >= success) {
+                minIdx = Math.min(minIdx, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return minIdx == Integer.MAX_VALUE ? 0 : (potions.length - minIdx);
     }
-    return left;
-  }
 }
