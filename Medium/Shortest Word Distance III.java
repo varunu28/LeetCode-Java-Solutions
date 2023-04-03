3492e1be759604c0f3b28209b92a742c81368132
varunu28 <varun.u28@gmail.com>
2023-04-03 06:52:29 -0700

    Updated Shortest Word Distance III.java

--- Medium/Shortest Word Distance III.java
+++ Medium/Shortest Word Distance III.java
@@ -1,15 +1,24 @@
 class Solution {
-  public int shortestWordDistance(String[] wordsDict, String word1, String word2) {
-    int minIdxDiff = wordsDict.length;
-    int prevIdx = -1;
-    for (int i = 0; i < wordsDict.length; i++) {
-      if (wordsDict[i].equals(word1) || wordsDict[i].equals(word2)) {
-        if (prevIdx != -1 && (word1.equals(word2) || !wordsDict[prevIdx].equals(wordsDict[i]))) {
-          minIdxDiff = Math.min(minIdxDiff, Math.abs(prevIdx - i));
+    public int shortestWordDistance(String[] wordsDict, String word1, String word2) {
+        int idxOne = -1;
+        int idxTwo = -1;
+        int minDistance = Integer.MAX_VALUE;
+        boolean sameWord = word1.equals(word2);
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                idxOne = sameWord ? (idxOne == -1 ? i : idxOne) : i; 
+            }
+            if (wordsDict[i].equals(word2)) {
+                idxTwo = sameWord ? (idxOne != i ? i : idxTwo) : i; 
+            }
+            if (idxOne != -1 && idxTwo != -1) {
+                minDistance = Math.min(minDistance, Math.abs(idxOne - idxTwo));
+                if (sameWord) {
+                    idxOne = idxTwo;
+                    idxTwo = -1;
+                }
+            }
         }
-        prevIdx = i;
-      }
+        return minDistance;
     }
-    return minIdxDiff;
-  }
 }
