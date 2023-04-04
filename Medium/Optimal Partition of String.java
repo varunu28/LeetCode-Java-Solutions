004d680823e9846a78ec8afbe2915bb678c6fa35
varunu28 <varun.u28@gmail.com>
2023-04-03 17:22:55 -0700

    Updated Optimal Partition of String.java

--- Medium/Optimal Partition of String.java
+++ Medium/Optimal Partition of String.java
@@ -1,14 +1,14 @@
 class Solution {
-  public int partitionString(String s) {
-    int idx = 0;
-    int count = 0;
-    while (idx < s.length()) {
-      Set<Character> set = new HashSet<>();
-      while (idx < s.length() && set.add(s.charAt(idx))) {
-        idx++;
-      }
-      count++;
+    public int partitionString(String s) {
+        int count = 0;
+        int idx = 0;
+        while (idx < s.length()) {
+            Set<Character> set = new HashSet<>();
+            while (idx < s.length() && set.add(s.charAt(idx))) {
+                idx++;
+            }
+            count++;
+        }
+        return count;
     }
-    return count;
-  }
 }
