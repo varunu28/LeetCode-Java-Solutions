563b41ea825069d095fd01527b23781d69529d08
varunu28 <varun.u28@gmail.com>
2023-04-05 17:31:05 -0700

    Updated Number of Closed Islands.java

--- Medium/Number of Closed Islands.java
+++ Medium/Number of Closed Islands.java
@@ -1,40 +1,38 @@
 class Solution {
-  private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-  
-  public int closedIsland(int[][] grid) {
-    int closedIslandCount = 0;
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[0].length; j++) {
-        if (grid[i][j] == 0 && isSurroundedSuccessfully(grid, i, j)) {
-          closedIslandCount++;
+
+    private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    public int closedIsland(int[][] grid) {
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0 && !visited[i][j] && isClosed(grid, i, j, visited)) {
+                    count++;
+                }
+            }
         }
-      }
+        return count;
     }
-    return closedIslandCount;
-  }
-  
-  private boolean isSurroundedSuccessfully(int[][] grid, int i, int j) {
-    Queue<int[]> queue = new LinkedList<>();
-    queue.add(new int[]{i, j});
-    boolean surroundingCheck = true;
-    while (!queue.isEmpty()) {
-      int[] removed = queue.remove();
-      int x = removed[0];
-      int y = removed[1];
-      if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) {
-        surroundingCheck = false;
-        continue;
-      }
-      if (grid[x][y] == 1) {
-        continue;
-      }
-      grid[x][y] = 1;
-      for (int[] dir : DIRS) {
-        int newX = x + dir[0];
-        int newY = y + dir[1];
-        queue.add(new int[]{newX, newY});
-      }
+
+    private boolean isClosed(int[][] grid, int i, int j, boolean[][] visited) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
+        visited[i][j] = true;
+        boolean closed = true;
+        while (!queue.isEmpty()) {
+            int[] removed = queue.remove();
+            for (int[] dir : DIRS) {
+                int newX = removed[0] + dir[0];
+                int newY = removed[1] + dir[1];
+                if (newX < 0 || newY < 0 || newX >= grid.length || newY >= grid[0].length) {
+                    closed = false;
+                } else if (grid[newX][newY] == 0 && !visited[newX][newY]) {
+                    queue.add(new int[]{newX, newY});
+                    visited[newX][newY] = true;
+                }
+            }
+        }
+        return closed;
     }
-    return surroundingCheck;
-  }
 }
