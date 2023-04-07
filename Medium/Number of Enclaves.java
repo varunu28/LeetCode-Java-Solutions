a51ef04068f7a284bb74b5a41aeffb4c3c5c81a3
varunu28 <varun.u28@gmail.com>
2023-04-06 17:34:58 -0700

    Updated Number of Enclaves.java

--- Medium/Number of Enclaves.java
+++ Medium/Number of Enclaves.java
@@ -1,30 +1,40 @@
 class Solution {
-  private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-  public int numEnclaves(int[][] grid) {
-    Queue<int[]> queue = new LinkedList<>();
-    int result = 0;
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[0].length; j++) {
-        result += grid[i][j];
-        if (i * j == 0 || i == grid.length - 1 || j == grid[i].length - 1) {
-          queue.add(new int[]{i, j});
+    
+    private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
+    public int numEnclaves(int[][] grid) {
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    count += getClosedEnclaveCount(grid, i, j, visited);
+                }
+            }
         }
-      }
+        return count;
     }
-    while (!queue.isEmpty()) {
-      int[] removed = queue.remove();
-      int x = removed[0];
-      int y = removed[1];
-      if (x < 0 || y < 0 || x == grid.length || y == grid[0].length || grid[x][y] != 1) {
-        continue;
-      }
-      grid[x][y] = 0;
-      result--;
-      for (int[] dir : DIRS) {
-        queue.add(new int[]{x + dir[0], y + dir[1]});
-      }
+    
+    private int getClosedEnclaveCount(int[][] grid, int i, int j, boolean[][] visited) {
+        boolean isClosed = true;
+        int enclaveCount = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
+        visited[i][j] = true;
+        while (!queue.isEmpty()) {
+            int[] removed = queue.remove();
+            enclaveCount++;
+            for (int[] dir : DIRS) {
+                int newX = removed[0] + dir[0];
+                int newY = removed[1] + dir[1];
+                if (newX < 0 || newY < 0 || newX >= grid.length || newY >= grid[0].length) {
+                    isClosed = false;
+                } else if (grid[newX][newY] == 1 && !visited[newX][newY]) {
+                    visited[newX][newY] = true;
+                    queue.add(new int[]{newX, newY});
+                }
+            }
+        }
+        return isClosed ? enclaveCount : 0;
     }
-    return result;
-  }
 }
