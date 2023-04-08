dce1f7776febe51afbd62b42e3ceb65683d8aeb4
varunu28 <varun.u28@gmail.com>
2023-04-07 17:05:27 -0700

    Updated Clone Graph.java

--- Medium/Clone Graph.java
+++ Medium/Clone Graph.java
@@ -3,17 +3,14 @@
 class Node {
     public int val;
     public List<Node> neighbors;
-    
     public Node() {
         val = 0;
         neighbors = new ArrayList<Node>();
     }
-    
     public Node(int _val) {
         val = _val;
         neighbors = new ArrayList<Node>();
     }
-    
     public Node(int _val, ArrayList<Node> _neighbors) {
         val = _val;
         neighbors = _neighbors;
@@ -22,24 +19,24 @@ class Node {
 */
 
 class Solution {
-  public Node cloneGraph(Node node) {
-    if (node == null) {
-      return null;
-    }
-    Map<Node, Node> map = new HashMap<>();
-    Queue<Node> queue = new LinkedList<>();
-    queue.add(node);
-    map.put(node, new Node(node.val));
-    while (!queue.isEmpty()) {
-      Node removed = queue.remove();
-      for (Node neighbor : removed.neighbors) {
-        if (!map.containsKey(neighbor)) {
-          map.put(neighbor, new Node(neighbor.val));
-          queue.add(neighbor);
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
         }
-        map.get(removed).neighbors.add(map.get(neighbor));
-      }
+        Map<Node, Node> map = new HashMap();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        map.put(node, new Node(node.val));
+        while (!queue.isEmpty()) {
+            Node removed = queue.remove();
+            for (Node neighbor : removed.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, new Node(neighbor.val));
+                    queue.add(neighbor);
+                }
+                map.get(removed).neighbors.add(map.get(neighbor));
+            }
+        }
+        return map.get(node);
     }
-    return map.get(node);
-  }
 }
