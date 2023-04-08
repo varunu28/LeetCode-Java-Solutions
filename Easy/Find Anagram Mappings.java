cb72a61686b69ac51ca4359093e9f288d07e0305
varunu28 <varun.u28@gmail.com>
2023-04-07 17:09:44 -0700

    Updated Find Anagram Mappings.java

--- Easy/Find Anagram Mappings.java
+++ Easy/Find Anagram Mappings.java
@@ -1,12 +1,13 @@
 class Solution {
-  public int[] anagramMappings(int[] nums1, int[] nums2) {
-    Map<Integer, Integer> indexMapping = new HashMap<>();
-    for (int i = 0; i < nums2.length; i++) {
-      indexMapping.put(nums2[i], i);
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            indexMap.putIfAbsent(nums2[i], i);
+        }
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = indexMap.get(nums1[i]);
+        }
+        return result;
     }
-    for (int i = 0; i < nums1.length; i++) {
-      nums1[i] = indexMapping.get(nums1[i]);
-    }
-    return nums1;
-  }
 }
