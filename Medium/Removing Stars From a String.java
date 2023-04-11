342118382d4ce4c5cca25587e1efce1fa1dca3ed
Varun Upadhyay <varun.u28@gmail.com>
2023-04-10 17:43:08 -0700

    Update Removing Stars From a String.java

--- Medium/Removing Stars From a String.java
+++ Medium/Removing Stars From a String.java
@@ -1,17 +1,17 @@
 class Solution {
-  public String removeStars(String s) {
-    Stack<Character> stack = new Stack<>();
-    for (char c : s.toCharArray()) {
-      if (c == '*') {
-        stack.pop();
-      } else {
-        stack.push(c);
-      }
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '*') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
-    StringBuilder sb = new StringBuilder();
-    while (!stack.isEmpty()) {
-      sb.append(stack.pop());
-    }
-    return sb.reverse().toString();
-  }
 }
