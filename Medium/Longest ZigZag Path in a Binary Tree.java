fbc0111a75d33ae06cebb128620d24e609a778f7
varunu28 <varun.u28@gmail.com>
2023-04-18 17:22:15 -0700

    Updated Longest ZigZag Path in a Binary Tree.java

--- Medium/Longest ZigZag Path in a Binary Tree.java
+++ Medium/Longest ZigZag Path in a Binary Tree.java
@@ -4,33 +4,39 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  int max;
-  public int longestZigZag(TreeNode root) {
-    if (root == null) {
-      return -1;
+    
+    private enum DIRECTION {
+        LEFT, RIGHT;
     }
-    max = 0;
-    helper(root.right, 1, true);
-    helper(root.left, 1, false);
-    return max;
-  } 
-  
-  private void helper(TreeNode root, int step, boolean isRight) {
-    if (root == null) {
-      return;
+    
+    public int longestZigZag(TreeNode root) {
+        int[] pathLength = {0};
+        dfs(root, DIRECTION.LEFT, 0, pathLength);
+        dfs(root, DIRECTION.RIGHT, 0, pathLength);
+        return pathLength[0];
     }
-    max = Math.max(max, step);
-    if (isRight) {
-      helper(root.left, step + 1, false);
-      helper(root.right, 1, true);
+    
+    private void dfs(TreeNode node, DIRECTION direction, int steps, int[] pathLength) {
+        if (node == null) {
+            return;
+        }
+        pathLength[0] = Math.max(pathLength[0], steps);
+        if (direction == DIRECTION.LEFT) {
+            dfs(node.left, DIRECTION.RIGHT, steps + 1, pathLength);
+            dfs(node.right, DIRECTION.LEFT, 1, pathLength);
+        } else {
+            dfs(node.left, DIRECTION.RIGHT, 1, pathLength);
+            dfs(node.right, DIRECTION.LEFT, steps + 1, pathLength);
+        }
     }
-    else {
-      helper(root.right, step + 1, true);
-      helper(root.left, 1, false);
-    }
-  }
 }
