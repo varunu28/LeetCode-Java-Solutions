5b31e8013879b0630a52847fde34409d4b492e6d
Varun Upadhyay <varun.u28@gmail.com>
2023-04-22 07:45:07 -0700

    Create Minimum Insertion Steps to Make a String Palindrome.java

---
+++ Hard/Minimum Insertion Steps to Make a String Palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        return n - lcs(s, new StringBuilder(s).reverse().toString(), n, n);
+    }
+    
+    private int lcs(String s1, String s2, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
