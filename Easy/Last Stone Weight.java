9172a003269d65343caa90f3be7a5d10ba74a8f9
varunu28 <varun.u28@gmail.com>
2023-04-23 17:36:37 -0700

    Updated Last Stone Weight.java

--- Easy/Last Stone Weight.java
+++ Easy/Last Stone Weight.java
@@ -1,16 +1,16 @@
 class Solution {
-  public int lastStoneWeight(int[] stones) {
-    PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
-    for (int stone : stones) {
-      pq.add(stone);
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            pq.add(stone);
+        }
+        while (pq.size() > 1) {
+            int stoneOne = pq.poll();
+            int stoneTwo = pq.poll();
+            if (stoneOne != stoneTwo) {
+                pq.add(stoneOne - stoneTwo);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
     }
-    while (pq.size() > 1) {
-      int firstStone = pq.poll();
-      int secondStone = pq.poll();
-      if (firstStone != secondStone) {
-        pq.add(firstStone - secondStone);
-      }
-    }
-    return pq.isEmpty() ? 0 : pq.poll();
-  }
 }
