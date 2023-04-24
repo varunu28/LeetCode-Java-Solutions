12612219c07351b29034b86190f1eff6c828e424
varunu28 <varun.u28@gmail.com>
2023-04-24 13:28:46 -0700

    Updated Task Scheduler.java

--- Medium/Task Scheduler.java
+++ Medium/Task Scheduler.java
@@ -1,16 +1,30 @@
 class Solution {
-  public int leastInterval(char[] tasks, int n) {
-    int[] frequencies = new int[26];
-    for (char task : tasks) {
-      frequencies[task - 'A']++;
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : tasks) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        pq.addAll(map.values());
+        int cycles = 0;
+        while (!pq.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            // Wait for one cooldown period and process one occurrence of all remaining tasks
+            for (int i = 0; i < n + 1; i++) {
+                if (!pq.isEmpty()) {
+                    temp.add(pq.remove());
+                }
+            }
+            // If task has more occurrences left then add it back to heap
+            for (int task : temp) {
+                if (task > 1) {
+                    pq.add(task - 1);
+                }
+            }
+            // If heap is empty that means we finished all tasks and don't need to wait for 
+            // the whole cooldown to finish else add one complete cooldown period
+            cycles += pq.isEmpty() ? temp.size() : n + 1;
+        }
+        return cycles;
     }
-    Arrays.sort(frequencies);
-    int maxFrequency = frequencies[25];
-    int idleTime = (maxFrequency - 1) * n;
-    for (int i = frequencies.length - 2; i >= 0 && idleTime > 0; i--) {
-      idleTime -= Math.min(maxFrequency - 1, frequencies[i]);
-    }
-    idleTime = Math.max(0, idleTime);
-    return idleTime + tasks.length;
-  }
 }
