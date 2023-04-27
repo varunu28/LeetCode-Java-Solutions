86947527a7e06fe4c34a556fc2d2a3365647cfa3
varunu28 <varun.u28@gmail.com>
2023-04-27 06:49:48 -0700

    Updated Minimum Window Subsequence.java

--- Hard/Minimum Window Subsequence.java
+++ Hard/Minimum Window Subsequence.java
@@ -1,29 +1,39 @@
 class Solution {
-  public String minWindow(String s1, String s2) {
-    String window = "";
-    int j = 0;
-    int minLength = s1.length() + 1;
-    for (int i = 0; i < s1.length(); i++) {
-      if (s1.charAt(i) == s2.charAt(j)) {
-        j++;
-        if (j == s2.length()) {
-          int end = i + 1;
-          j--;
-          while (j >= 0) {
-            if (s1.charAt(i) == s2.charAt(j)) {
-              j--;
+    public String minWindow(String s1, String s2) {
+        int right = 0;
+        int minWindowLength = Integer.MAX_VALUE;
+        String result = "";
+        while (right < s1.length()) {
+            int s2Idx = 0;
+            while (right < s1.length()) {
+                if (s1.charAt(right) == s2.charAt(s2Idx)) {
+                    s2Idx++;
+                }
+                if (s2Idx == s2.length()) {
+                    break;
+                }
+                right++;
             }
-            i--;
-          }
-          j++;
-          i++;
-          if (end - i < minLength) {
-            minLength = end - i;
-            window = s1.substring(i, end);
-          }
+            if (right == s1.length()) {
+                break;
+            }
+            int s1EndIdx = right;
+            int s2EndIdx = s2.length() - 1;
+            while (s1EndIdx >= 0) {
+                if (s1.charAt(s1EndIdx) == s2.charAt(s2EndIdx)) {
+                    s2EndIdx--;
+                }
+                if (s2EndIdx < 0) {
+                    break;
+                }
+                s1EndIdx--;
+            }
+            if (right - s1EndIdx + 1 < minWindowLength) {
+                minWindowLength = right - s1EndIdx + 1;
+                result = s1.substring(s1EndIdx, right + 1);
+            }
+            right = s1EndIdx + 1;
         }
-      }
+        return result;
     }
-    return window;
-  }
 }
