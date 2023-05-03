757ae7369d60f144e74af0eed50057e078ea16fa
varunu28 <varun.u28@gmail.com>
2023-05-03 06:48:54 -0700

    Updated Building H2O.java

--- Concurrency/Building H2O.java
+++ Concurrency/Building H2O.java
@@ -1,26 +1,24 @@
 class H2O {
-  
-  private final Semaphore hydrogenSemaphore;
-  private final Semaphore oxygenSemaphore;
+    
+    private Semaphore hydrogenSemaphore;
+    private Semaphore oxygenSemaphore;
 
-  public H2O() {
-    this.hydrogenSemaphore = new Semaphore(2);
-    this.oxygenSemaphore = new Semaphore(0);
-  }
+    public H2O() {
+        this.hydrogenSemaphore = new Semaphore(2);
+        this.oxygenSemaphore = new Semaphore(0);
+    }
 
-  public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+		hydrogenSemaphore.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        this.oxygenSemaphore.release();
+    }
 
-    this.hydrogenSemaphore.acquire();
-    // releaseHydrogen.run() outputs "H". Do not change or remove this line.
-    releaseHydrogen.run();
-    this.oxygenSemaphore.release();
-  }
-
-  public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-
-    this.oxygenSemaphore.acquire(2);
-    // releaseOxygen.run() outputs "O". Do not change or remove this line.
-    releaseOxygen.run();
-    this.hydrogenSemaphore.release(2);
-  }
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        this.oxygenSemaphore.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+		releaseOxygen.run();
+        this.hydrogenSemaphore.release(2);
+    }
 }
