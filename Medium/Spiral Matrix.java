1bfdf2dbabea029b8826106b024e3b872002d2a6
Varun Upadhyay <varun.u28@gmail.com>
2023-05-08 17:33:46 -0700

    Update Spiral Matrix.java

--- Medium/Spiral Matrix.java
+++ Medium/Spiral Matrix.java
@@ -1,32 +1,32 @@
 class Solution {
-  public List<Integer> spiralOrder(int[][] matrix) {
-    List<Integer> list = new ArrayList<>();
-    int rowStart = 0;
-    int rowEnd = matrix.length - 1;
-    int colStart = 0;
-    int colEnd = matrix[0].length - 1;
-    while (rowStart <= rowEnd && colStart <= colEnd) {
-      for (int i = colStart; i <= colEnd; i++) {
-        list.add(matrix[rowStart][i]);
-      }
-      rowStart++;
-      for (int i = rowStart; i <= rowEnd; i++) {
-        list.add(matrix[i][colEnd]);
-      }
-      colEnd--;
-      if (rowStart <= rowEnd) {
-        for (int i = colEnd; i >= colStart; i--) {
-          list.add(matrix[rowEnd][i]);
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1;
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i++) {
+                result.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result.add(matrix[i][colStart]);
+                }
+                colStart++;
+            }
         }
-        rowEnd--;
-      }
-      if (colStart <= colEnd) {
-        for (int i = rowEnd; i >= rowStart; i--) {
-          list.add(matrix[i][colStart]);
-        }
-        colStart++;
-      }
+        return result;
     }
-    return list;
-  }
 }
