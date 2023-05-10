ad7a72e85ed501b25be5277c7bca459f96c2c083
Varun Upadhyay <varun.u28@gmail.com>
2023-05-09 17:34:01 -0700

    Update Spiral Matrix II.java

--- Medium/Spiral Matrix II.java
+++ Medium/Spiral Matrix II.java
@@ -1,29 +1,33 @@
 class Solution {
-  public int[][] generateMatrix(int n) {
-    int[][] ans = new int[n][n];
-    int left = 0;
-    int top = 0;
-    int right = n -1;
-    int down = n - 1;
-    int count = 1;
-    while (left <= right) {
-      for (int j = left; j <= right; j ++) {
-        ans[top][j] = count++;
-      }
-      top++;
-      for (int i = top; i <= down; i ++) {
-        ans[i][right] = count ++;
-      }
-      right--;
-      for (int j = right; j >= left; j --) {
-        ans[down][j] = count ++;
-      }
-      down--;
-      for (int i = down; i >= top; i --) {
-        ans[i][left] = count ++;
-      }
-      left++;
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int rowStart = 0;
+        int rowEnd = n - 1;
+        int colStart = 0;
+        int colEnd = n - 1;
+        int value = 1;
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i++) {
+                result[rowStart][i] = value++;
+            }
+            rowStart++;
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result[i][colEnd] = value++;
+            }
+            colEnd--;
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result[rowEnd][i] = value++;
+                }
+                rowEnd--;
+            }
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result[i][colStart] = value++;
+                }
+                colStart++;
+            }
+        }
+        return result;
     }
-    return ans;
-  }
 }
