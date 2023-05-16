f9edb33c4e3f06aee3698697b55b6d1a769560ea
Varun Upadhyay <varun.u28@gmail.com>
2023-05-15 17:43:15 -0700

    Update Swap Nodes in Pairs.java

--- Medium/Swap Nodes in Pairs.java
+++ Medium/Swap Nodes in Pairs.java
@@ -9,22 +9,23 @@
  * }
  */
 class Solution {
-  public ListNode swapPairs(ListNode head) {
-    ListNode curr = head;
-    ListNode prev = null;
-    while (curr != null && curr.next != null) {
-      ListNode nextNode = curr.next;
-      if (curr == head) {
-        head = nextNode;
-      }
-      curr.next = nextNode.next;
-      nextNode.next = curr;
-      if (prev != null) {
-        prev.next = nextNode;
-      }
-      prev = curr;
-      curr = curr.next;
+    public ListNode swapPairs(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null && curr.next != null) {
+            ListNode nextNode = curr.next;
+            ListNode nextToNext = curr.next.next;
+            if (prev == null) {
+                head = nextNode;
+            } 
+            if (prev != null) {
+                prev.next = nextNode;
+            }
+            nextNode.next = curr;
+            curr.next = nextToNext;
+            prev = curr;
+            curr = nextToNext;
+        } 
+        return head;
     }
-    return head;
-  }
 }
