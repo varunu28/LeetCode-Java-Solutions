2773659c8c184371b3a38c632b750a9b8a995424
varunu28 <varun.u28@gmail.com>
2023-05-18 18:10:31 -0700

    Updated Is Graph Bipartite.java

--- Medium/Is Graph Bipartite.java
+++ Medium/Is Graph Bipartite.java
@@ -1,39 +1,38 @@
 class Solution {
-  public boolean isBipartite(int[][] graph) {
-    int n = graph.length;
-    int[] color = new int[n];
-    Arrays.fill(color, -1);
-    for (int i = 0; i < n; i++) {
-      if (color[i] == -1 && !bipartiteCheck(graph, i, color)) {
-        return false;
-      }
-    }
-    return true;
-  }
-  
-  private boolean bipartiteCheck(int[][] graph, int node, int[] color) {
-    Queue<Integer> queue = new LinkedList<>();
-    queue.add(node);
-    int currColor = 0;
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      while (size-- > 0) {
-        int removed = queue.remove();
-        if (color[removed] != -1) {
-          if (color[removed] != currColor) {
-            return false;
-          }
-          continue;
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (color[i] == 0 && !isBipartite(graph, i, color)) {
+                return false;
+            }
         }
-        color[removed] = currColor;
-        for (int conn : graph[removed]) {
-          if (color[conn] == -1) {
-            queue.add(conn);
-          }
-        }
-      }
-      currColor = currColor == 1 ? 0 : 1;
+        return true;
     }
-    return true;
-  }
+    
+    private boolean isBipartite(int[][] graph, int node, int[] color) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(node);
+        int currColor = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int removed = queue.remove();
+                if (color[removed] != 0) {
+                    if (color[removed] != currColor) {
+                        return false;
+                    }
+                } else {
+                    color[removed] = currColor;
+                    for (int conn : graph[removed]) {
+                        if (color[conn] == 0) {
+                            queue.add(conn);
+                        }
+                    }
+                }
+            }
+            currColor = currColor == 1 ? 2 : 1;
+        }
+        return true;
+    }
 }
