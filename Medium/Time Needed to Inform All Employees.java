e97550355a8734098472d08253d9a1c15a688452
varunu28 <varun.u28@gmail.com>
2023-06-02 17:27:07 -0700

    Updated Time Needed to Inform All Employees.java

--- Medium/Time Needed to Inform All Employees.java
+++ Medium/Time Needed to Inform All Employees.java
@@ -1,23 +1,24 @@
 class Solution {
-  public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-    Map<Integer, List<Integer>> organization = new HashMap<>();
-    for (int i = 0; i < manager.length; i++) {
-      if (manager[i] != -1) {
-        organization.computeIfAbsent(manager[i], k -> new ArrayList<>()).add(i);
-      }
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Map<Integer, List<Integer>> organization = new HashMap<>();
+        for (int i = 0; i < manager.length; i++) {
+            if (manager[i] == -1) {
+                continue;
+            }
+            organization.computeIfAbsent(manager[i], k -> new ArrayList<>()).add(i);
+        }
+        int maxTime = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{headID, 0});
+        while (!queue.isEmpty()) {
+            int[] removed = queue.remove();
+            int currManager = removed[0];
+            int timeElapsed = removed[1];
+            maxTime = Math.max(maxTime, timeElapsed);
+            for (Integer employee : organization.getOrDefault(currManager, new ArrayList<>())) {
+                queue.add(new int[]{employee, timeElapsed + informTime[currManager]});
+            }
+        }
+        return maxTime;
     }
-    int totalTime = 0;
-    Queue<int[]> queue = new LinkedList<>();
-    queue.add(new int[]{headID, 0});
-    while (!queue.isEmpty()) {
-      int[] removed = queue.remove();
-      int id = removed[0]; 
-      int time = removed[1]; 
-      totalTime = Math.max(totalTime, time);
-      for (int associate : organization.getOrDefault(id, new ArrayList<>())) {
-        queue.add(new int[]{associate, time + informTime[id]});
-      }
-    }
-    return totalTime;
-  }
 }
