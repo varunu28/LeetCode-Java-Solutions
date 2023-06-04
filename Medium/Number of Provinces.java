41c51ea48837dff914deaad98223e924a2d9d81b
varunu28 <varun.u28@gmail.com>
2023-06-04 09:00:55 -0700

    Updated Number of Provinces.java

--- Medium/Number of Provinces.java
+++ Medium/Number of Provinces.java
@@ -1,34 +1,37 @@
 class Solution {
-  public int findCircleNum(int[][] isConnected) {
-    int n = isConnected.length;
-    Map<Integer, Set<Integer>> map = new HashMap<>();
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i != j && isConnected[i][j] == 1) {
-          map.computeIfAbsent(i, k -> new HashSet<>()).add(j);
-          map.computeIfAbsent(j, k -> new HashSet<>()).add(i);
-        }
-      }
-    }
-    Set<Integer> visited = new HashSet<>();
-    int provinceCount = 0;
-    for (int i = 0; i < n; i++) {
-      if (!visited.contains(i)) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(i);
-        visited.add(i);
-        while (!queue.isEmpty()) {
-          int removed = queue.remove();
-          for (Integer connection : map.getOrDefault(removed, new HashSet<>())) {
-            if (!visited.contains(connection)) {
-              queue.add(connection);
-              visited.add(connection);
+    public int findCircleNum(int[][] isConnected) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = 0; j < isConnected[i].length; j++) {
+                if (i != j && isConnected[i][j] == 1) {
+                    graph.computeIfAbsent(i, k -> new HashSet<>()).add(j);
+                    graph.computeIfAbsent(j, k -> new HashSet<>()).add(i);
+                }
             }
-          }
         }
-        provinceCount++;
-      }
+        Set<Integer> visited = new HashSet<>();
+        int provinces = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (!visited.contains(i)) {
+                traverse(graph, i, visited);
+                provinces++;
+            }
+        }
+        return provinces;
     }
-    return provinceCount;
-  }
+    
+    private void traverse(Map<Integer, Set<Integer>> graph, int city, Set<Integer> visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(city);
+        visited.add(city);
+        while (!queue.isEmpty()) {
+            int removed = queue.remove();
+            for (Integer conn : graph.getOrDefault(removed, new HashSet<>())) {
+                if (!visited.contains(conn)) {
+                    queue.add(conn);
+                    visited.add(conn);
+                }
+            }
+        }
+    }
 }
