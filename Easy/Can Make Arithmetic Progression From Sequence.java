7cf26a8577ef68df23c38a949b4b006cb630a76a
varunu28 <varun.u28@gmail.com>
2023-06-05 18:01:50 -0700

    Updated Can Make Arithmetic Progression From Sequence.java

--- Easy/Can Make Arithmetic Progression From Sequence.java
+++ Easy/Can Make Arithmetic Progression From Sequence.java
@@ -1,12 +1,12 @@
 class Solution {
-  public boolean canMakeArithmeticProgression(int[] arr) {
-    Arrays.sort(arr);
-    int diff = arr[1] - arr[0];
-    for (int i = 2; i < arr.length; i++) {
-      if (arr[i] - arr[i - 1] != diff) {
-        return false;
-      }
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-  }
 }
