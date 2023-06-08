92994b4fec8f27c58c77b21afd7272bcba76fa22
varunu28 <varun.u28@gmail.com>
2023-06-08 07:40:52 -0700

    Updated Check If a Number Is Majority Element in a Sorted Array.java

--- Easy/Check If a Number Is Majority Element in a Sorted Array.java
+++ Easy/Check If a Number Is Majority Element in a Sorted Array.java
@@ -1,38 +1,38 @@
 class Solution {
-  
-  private static enum DIRECTION {
-    LEFT, RIGHT;
-  }
-  
-  public boolean isMajorityElement(int[] nums, int target) {
-    int rightIdx = getIndex(nums, target, DIRECTION.RIGHT);
-    int leftIdx = getIndex(nums, target, DIRECTION.LEFT);
-    if (leftIdx == -1 || rightIdx == -1) {
-      return false;
+    
+    private static enum Direction {
+        LEFT, RIGHT;
     }
-    int occurrences = rightIdx - leftIdx + 1;
-    return occurrences > nums.length / 2;
-  }
-  
-  private int getIndex(int[] nums, int target, DIRECTION direction) {
-    int resultIdx = -1;
-    int left = 0;
-    int right = nums.length - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (nums[mid] == target) {
-        resultIdx = mid;
-        if (direction == DIRECTION.LEFT) {
-          right = mid - 1;
-        } else {
-          left = mid + 1;
+    
+    public boolean isMajorityElement(int[] nums, int target) {
+        int firstIndex = findIndex(nums, target, Direction.LEFT);
+        if (firstIndex == -1) {
+            return false;
         }
-      } else if (nums[mid] > target) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
+        int lastIndex = findIndex(nums, target, Direction.RIGHT);
+        int occurrences = lastIndex - firstIndex + 1;
+        return occurrences > nums.length / 2;
     }
-    return resultIdx;
-  }
+
+    private int findIndex(int[] nums, int target, Direction direction) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                idx = mid;
+                if (direction == Direction.LEFT) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return idx;
+    }
 }
