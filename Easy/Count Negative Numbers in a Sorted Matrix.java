c94158c3aa2b810811be68c90d1ea4fd3bce7271
Varun Upadhyay <varun.u28@gmail.com>
2023-06-07 17:30:39 -0700

    Update Count Negative Numbers in a Sorted Matrix.java

--- Easy/Count Negative Numbers in a Sorted Matrix.java
+++ Easy/Count Negative Numbers in a Sorted Matrix.java
@@ -1,28 +1,26 @@
 class Solution {
-  public int countNegatives(int[][] grid) {
-    int total = 0;
-    int n = grid[0].length;
-    for (int[] gr : grid) {
-      int negInd = binarySearchHelper(gr, 0, n - 1);
-      if (negInd != -1) {
-        total += n - negInd;
-      }
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int[] row : grid) {
+            int idx = findFirstNonNegativeIndex(row);
+            count += idx == -1 ? 0 : (row.length - idx);
+        }
+        return count;
     }
-    return total;
-  }
-  
-  private int binarySearchHelper(int[] arr, int start, int end) {
-    int idx = -1;
-    while (start <= end) {
-      int mid = (start + end) / 2;
-      if (arr[mid] < 0) {
-        idx = idx == -1 ? mid : Math.min(idx, mid);
-        end = mid - 1;
-      }
-      if (arr[mid] >= 0) {
-        start = mid + 1;
-      }
+    
+    private int findFirstNonNegativeIndex(int[] row) {
+        int start = 0;
+        int end = row.length - 1;
+        int idx = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (row[mid] < 0) {
+                idx = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return idx;
     }
-    return idx;
-  }
 }
