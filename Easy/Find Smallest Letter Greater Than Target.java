0a29fb631d6454d8a29dbc42443b6f9449fe9eec
varunu28 <varun.u28@gmail.com>
2023-06-08 18:56:07 -0700

    Updated Find Smallest Letter Greater Than Target.java

--- Easy/Find Smallest Letter Greater Than Target.java
+++ Easy/Find Smallest Letter Greater Than Target.java
@@ -1,17 +1,17 @@
 class Solution {
-  public char nextGreatestLetter(char[] letters, char target) {
-    int start = 0;
-    int end = letters.length - 1;
-    int minIdx = Integer.MAX_VALUE;
-    while (start <= end) {
-      int mid = (start + end) / 2;
-      if (letters[mid] > target) {
-        minIdx = mid;
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        int idx = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (letters[mid] > target) {
+                idx = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return idx == -1 ? letters[0] : letters[idx];
     }
-    return minIdx == Integer.MAX_VALUE ? letters[0] : letters[minIdx];
-  }
 }
