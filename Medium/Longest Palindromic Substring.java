e84f8856b088683a88b430fca9a548e927ae1fa2
varunu28 <varun.u28@gmail.com>
2023-06-11 09:48:01 -0700

    Updated Longest Palindromic Substring.java

--- Medium/Longest Palindromic Substring.java
+++ Medium/Longest Palindromic Substring.java
@@ -1,24 +1,28 @@
 class Solution {
-  public String longestPalindrome(String s) {
-    int start = 0;
-    int end = 0;
-    for (int i = 0; i < s.length(); i++) {
-      int lenOne = helper(s, i, i);
-      int lenTwo = helper(s, i, i + 1);
-      int maxLength = Math.max(lenOne, lenTwo);
-      if (maxLength > end - start) {
-        start = i - (maxLength - 1) / 2;
-        end = i + maxLength / 2;
-      }
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int[] result = new int[]{0, 0};
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                result[0] = i;
+                result[1] = i + 1;
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                int k = i + j;
+                if (s.charAt(j) == s.charAt(k) && dp[j + 1][k - 1]) {
+                    dp[j][k] = true;
+                    result[0] = j;
+                    result[1] = k;
+                }
+            }
+        }
+        return s.substring(result[0], result[1] + 1);
     }
-    return s.substring(start, end + 1);
-  }
-  
-  private int helper(String s, int left, int right) {
-    while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-      left--;
-      right++;
-    }
-    return right - left - 1;
-  }
 }
