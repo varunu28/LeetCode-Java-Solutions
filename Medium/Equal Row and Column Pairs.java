c2cd62e5dce249eb8126ff5fc47fd2eef8de1f37
varunu28 <varun.u28@gmail.com>
2023-06-12 17:17:06 -0700

    Updated Equal Row and Column Pairs.java

--- Medium/Equal Row and Column Pairs.java
+++ Medium/Equal Row and Column Pairs.java
@@ -1,25 +1,23 @@
 class Solution {
-  public int equalPairs(int[][] grid) {
-    Map<String, Integer> map = new HashMap<>();
-    int rows = grid.length;
-    int cols = grid[0].length;
-    for (int i = 0; i < rows; i++) {
-      StringBuilder sb = new StringBuilder();
-      for (int j = 0; j < cols; j++) {
-        sb.append(grid[i][j]).append("|");
-      }
-      String key = sb.toString();
-      map.put(key, map.getOrDefault(key, 0) + 1);
+    public int equalPairs(int[][] grid) {
+        Map<String, Integer> rowMap = new HashMap<>();
+        for (int[] value : grid) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < grid[0].length; j++) {
+                sb.append(value[j]).append("|");
+            }
+            String key = sb.toString();
+            rowMap.put(key, rowMap.getOrDefault(key, 0) + 1);
+        }
+        int count = 0;
+        for (int i = 0; i < grid[0].length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int[] ints : grid) {
+                sb.append(ints[i]).append("|");
+            }
+            String key = sb.toString();
+            count += rowMap.getOrDefault(key, 0);
+        }
+        return count;
     }
-    int count = 0;
-    for (int i = 0; i < cols; i++) {
-      StringBuilder sb = new StringBuilder();
-      for (int j = 0; j < rows; j++) {
-        sb.append(grid[j][i]).append("|");
-      }
-      String key = sb.toString();
-      count += map.getOrDefault(key, 0);
-    }
-    return count;
-  }
 }
