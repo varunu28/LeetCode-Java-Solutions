5ff595191ff7e7e91b006010218ea4a4703473f9
varunu28 <varun.u28@gmail.com>
2023-06-14 17:10:20 -0700

    Updated Maximum Level Sum of a Binary Tree.java

--- Medium/Maximum Level Sum of a Binary Tree.java
+++ Medium/Maximum Level Sum of a Binary Tree.java
@@ -14,34 +14,31 @@
  * }
  */
 class Solution {
-  public int maxLevelSum(TreeNode root) {
-    if (root == null) {
-      return 0;
-    }
-    int currLevel = 1;
-    int maxSum = Integer.MIN_VALUE;
-    int maxSumLevel = 0;
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      int currSum = 0;
-      while (size-- > 0) {
-        TreeNode removed = queue.remove();
-        currSum += removed.val;
-        if (removed.left != null) {
-          queue.add(removed.left);
+    public int maxLevelSum(TreeNode root) {
+        int maxSum = Integer.MIN_VALUE;
+        int maxSumLevel = 0;
+        int currLevel = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int levelSum = 0;
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode removed = queue.remove();
+                levelSum += removed.val;
+                if (removed.left != null) {
+                    queue.add(removed.left);
+                }
+                if (removed.right != null) {
+                    queue.add(removed.right);
+                }
+            }
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                maxSumLevel = currLevel;
+            }
+            currLevel++;
         }
-        if (removed.right != null) {
-          queue.add(removed.right);
-        }
-      }
-      if (maxSum < currSum) {
-        maxSum = currSum;
-        maxSumLevel = currLevel;
-      }
-      currLevel++;
+        return maxSumLevel;
     }
-    return maxSumLevel;
-  }
 }
