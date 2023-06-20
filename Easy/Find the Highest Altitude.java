5db3be7839486bddeb3e561ee5a887bac4036476
varunu28 <varun.u28@gmail.com>
2023-06-19 17:00:37 -0700

    Updated Find the Highest Altitude.java

--- Easy/Find the Highest Altitude.java
+++ Easy/Find the Highest Altitude.java
@@ -1,11 +1,11 @@
 class Solution {
-  public int largestAltitude(int[] gain) {
-    int highestAltitude = 0;
-    int currAltitude = 0;
-    for (int i = 0; i < gain.length; i++) {
-      currAltitude += gain[i];
-      highestAltitude = Math.max(highestAltitude, currAltitude);
+    public int largestAltitude(int[] gain) {
+        int highestAltitude = 0;
+        int currAltitude = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currAltitude = currAltitude + gain[i];
+            highestAltitude = Math.max(highestAltitude, currAltitude);
+        }
+        return highestAltitude;
     }
-    return highestAltitude;
-  }
 }
