157f7bfd46771922c330031f695cb639389962fb
varunu28 <varun.u28@gmail.com>
2023-06-23 06:50:39 -0700

    Updated Longest Arithmetic Subsequence.java

--- Medium/Longest Arithmetic Subsequence.java
+++ Medium/Longest Arithmetic Subsequence.java
@@ -1,15 +1,15 @@
 class Solution {
-  public int longestArithSeqLength(int[] nums) {
-    int result = 2;
-    Map<Integer, Integer>[] dp = new HashMap[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      dp[i] = new HashMap<>();
-      for (int j = 0; j < i; j++) {
-        int diff = nums[i] - nums[j];
-        dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
-        result = Math.max(result, dp[i].get(diff));
-      }
+    public int longestArithSeqLength(int[] nums) {
+        int result = 0;
+        Map<Integer, Integer>[] dp = new HashMap[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = new HashMap<>();
+            for (int j = 0; j < i; j++) {
+                int diff = nums[j] - nums[i];
+                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
+                result = Math.max(result, dp[i].get(diff));
+            }
+        }
+        return result;
     }
-    return result;
-  }
 }
