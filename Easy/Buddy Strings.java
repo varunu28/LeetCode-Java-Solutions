ab79856e35201bbb7e6b3d0d09808557f0d597d7
varunu28 <varun.u28@gmail.com>
2023-07-02 17:24:13 -0700

    Updated Buddy Strings.java

--- Easy/Buddy Strings.java
+++ Easy/Buddy Strings.java
@@ -1,33 +1,32 @@
 class Solution {
+    
+    private static final int SWAP_DONE_IDX = Integer.MIN_VALUE;
+    
     public boolean buddyStrings(String s, String goal) {
         if (s.length() != goal.length()) {
             return false;
         }
-        int mismatchIdx = -1;
+        int swapIdx = -1;
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == goal.charAt(i)) {
+                set.add(s.charAt(i));
+                continue;
+            }
             if (s.charAt(i) != goal.charAt(i)) {
-                if (mismatchIdx == Integer.MAX_VALUE) {
+                if (swapIdx == SWAP_DONE_IDX) {
                     return false;
                 }
-                if (mismatchIdx == -1) {
-                    mismatchIdx = i;
-                } else {
-                    if (!(s.charAt(mismatchIdx) == goal.charAt(i) && s.charAt(i) == goal.charAt(mismatchIdx))) {
-                        return false;
-                    }
-                    mismatchIdx = Integer.MAX_VALUE;
+                if (swapIdx == -1) {
+                    swapIdx = i;
+                    continue;
+                } 
+                if (!(s.charAt(swapIdx) == goal.charAt(i) && s.charAt(i) == goal.charAt(swapIdx))) {
+                    return false;
                 }
-            } else {
-                set.add(s.charAt(i));
+                swapIdx = SWAP_DONE_IDX;
             }
         }
-        if (mismatchIdx != -1 && mismatchIdx != Integer.MAX_VALUE) {
-            return false;
-        }
-        if (mismatchIdx == -1) {
-            return s.length() > set.size();
-        }
-        return true;
+        return (swapIdx != -1 && swapIdx == SWAP_DONE_IDX) || (swapIdx == -1 && set.size() < s.length());
     }
 }
