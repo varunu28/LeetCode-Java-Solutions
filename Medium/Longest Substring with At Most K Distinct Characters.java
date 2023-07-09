6cb7d533c288ce708d1e8d210a97ed3a63b5ff43
varunu28 <varun.u28@gmail.com>
2023-07-09 06:51:46 -0700

    Updated Longest Substring with At Most K Distinct Characters.java

--- Medium/Longest Substring with At Most K Distinct Characters.java
+++ Medium/Longest Substring with At Most K Distinct Characters.java
@@ -1,21 +1,21 @@
 class Solution {
-  public int lengthOfLongestSubstringKDistinct(String s, int k) {
-    Map<Character, Integer> map = new HashMap<>();
-    int startIdx = 0;
-    int endIdx = 0;
-    int maxLength = 0;
-    while (endIdx < s.length()) {
-      map.put(s.charAt(endIdx), map.getOrDefault(s.charAt(endIdx), 0) + 1);
-      endIdx++;
-      while (startIdx < endIdx && map.size() > k) {
-        map.put(s.charAt(startIdx), map.getOrDefault(s.charAt(startIdx), 0 ) - 1);
-        if (map.get(s.charAt(startIdx))  == 0) {
-          map.remove(s.charAt(startIdx));
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        int end = 0;
+        int maxLength = 0;
+        while (end < s.length()) {
+            map.put(s.charAt(end), map.getOrDefault(s.charAt(end), 0) + 1);
+            while (start <= end && map.size() > k) {
+                map.put(s.charAt(start), map.get(s.charAt(start)) - 1);
+                if (map.get(s.charAt(start)) == 0) {
+                    map.remove(s.charAt(start));
+                }
+                start++;
+            }
+            end++;
+            maxLength = Math.max(maxLength, end - start);
         }
-        startIdx++;
-      }
-      maxLength = Math.max(maxLength, endIdx - startIdx);
+        return maxLength;
     }
-    return maxLength;
-  }
 }
