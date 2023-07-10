f38831aadfe1c0ce6e3ab6c4aa88f958db2e2cad
varunu28 <varun.u28@gmail.com>
2023-07-09 18:46:15 -0700

    Updated Minimum Depth of a Binary Tree.java

--- Easy/Minimum Depth of a Binary Tree.java
+++ Easy/Minimum Depth of a Binary Tree.java
@@ -14,31 +14,29 @@
  * }
  */
 class Solution {
-  public int minDepth(TreeNode root) {
-    if (root == null) {
-      return 0;
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode removed = queue.remove();
+                if (removed.left == null && removed.right == null) {
+                    return level;
+                }
+                if (removed.left != null) {
+                    queue.add(removed.left);
+                }
+                if (removed.right != null) {
+                    queue.add(removed.right);
+                }
+            }
+            level++;
+        }
+        return level;
     }
-    int steps = 1;
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      // Level iteration
-      while (size-- > 0) {
-        TreeNode removed = queue.remove();
-        // Leaf reached
-        if (removed.left == null && removed.right == null) {
-          return steps;
-        }
-        if (removed.left != null) {
-          queue.add(removed.left);
-        }
-        if (removed.right != null) {
-          queue.add(removed.right);
-        }
-      }
-      steps++;
-    }
-    return steps;
-  }
 }
