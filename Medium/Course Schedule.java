3523744aed753badf83a54e68879ab20ed5655ad
varunu28 <varun.u28@gmail.com>
2023-07-13 09:08:53 -0700

    Updated Course Schedule.java

--- Medium/Course Schedule.java
+++ Medium/Course Schedule.java
@@ -1,29 +1,31 @@
 class Solution {
-  public boolean canFinish(int numCourses, int[][] prerequisites) {
-    Map<Integer, Set<Integer>> map = new HashMap<>();
-    int[] indegree = new int[numCourses];
-    for (int[] prerequisite : prerequisites) {
-      map.computeIfAbsent(prerequisite[1], k -> new HashSet<>()).add(prerequisite[0]);
-      indegree[prerequisite[0]]++;
-    }
-    Queue<Integer> queue = new LinkedList<>();
-    Set<Integer> taken = new HashSet<>();
-    for (int i = 0; i < numCourses; i++) {
-      if (indegree[i] == 0) {
-        queue.add(i);
-        taken.add(i);
-      }
-    }
-    while (!queue.isEmpty()) {
-      int removed = queue.remove();
-      for (Integer dependentCourse : map.getOrDefault(removed, new HashSet<>())) {
-        indegree[dependentCourse]--;
-        if (indegree[dependentCourse] == 0) {
-          taken.add(dependentCourse);
-          queue.add(dependentCourse);
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int prereq = prerequisite[1];
+            indegree[course]++;
+            map.computeIfAbsent(prereq, k -> new HashSet<>()).add(course);
         }
-      }
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+                visited.add(i);
+            }            
+        }
+        while (!queue.isEmpty()) {
+            int removed = queue.remove();
+            for (Integer conn : map.getOrDefault(removed, new HashSet<>())) {
+                indegree[conn]--;
+                if (indegree[conn] == 0) {
+                    queue.add(conn);
+                    visited.add(conn);
+                }
+            }
+        }
+        return visited.size() == numCourses;
     }
-    return taken.size() == numCourses;
-  }
 }
