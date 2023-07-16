433c62a177ec3f9403028295aa32399eac6cb811
varunu28 <varun.u28@gmail.com>
2023-07-16 09:07:18 -0700

    Updated Lowest Common Ancestor of a Binary Tree.java

--- Medium/Lowest Common Ancestor of a Binary Tree.java
+++ Medium/Lowest Common Ancestor of a Binary Tree.java
@@ -8,15 +8,21 @@
  * }
  */
 class Solution {
-  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    if (root == null || root == p || root == q) {
-      return root;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null || right != null) {
+            return left != null ? left : right;
+        }
+        return null;
     }
-    TreeNode leftRoot = lowestCommonAncestor(root.left, p, q);
-    TreeNode rightRoot = lowestCommonAncestor(root.right, p, q);
-    if (leftRoot != null && rightRoot != null) {
-      return root;
-    }
-    return leftRoot != null ? leftRoot : rightRoot;
-  }
 }
