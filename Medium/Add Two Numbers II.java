bed63498e394f69d47e6d733ab530b57db7030f8
varunu28 <varun.u28@gmail.com>
2023-07-16 17:44:51 -0700

    Updated Add Two Numbers II.java

--- Medium/Add Two Numbers II.java
+++ Medium/Add Two Numbers II.java
@@ -9,40 +9,40 @@
  * }
  */
 class Solution {
-  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode revL1 = reverse(l1);
-    ListNode revL2 = reverse(l2);
-    ListNode dummy = new ListNode(0);
-    ListNode curr = dummy;
-    int carry = 0;
-    while (revL1 != null || revL2 != null || carry != 0) {
-      if (revL1 != null && revL2 != null) {
-        carry += revL1.val + revL2.val;
-        revL1 = revL1.next;
-        revL2 = revL2.next;
-      } else if (revL1 != null && revL2 == null) {
-        carry += revL1.val;
-        revL1 = revL1.next;
-      } else if (revL1 == null && revL2 != null) {
-        carry += revL2.val;
-        revL2 = revL2.next;
-      }
-      curr.next = new ListNode(carry % 10);
-      carry /= 10;
-      curr = curr.next;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode rev1 = reverse(l1);
+        ListNode rev2 = reverse(l2);
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        int carry = 0;
+        while (rev1 != null || rev2 != null || carry > 0) {
+            if (rev1 != null && rev2 != null) {
+                carry += rev1.val + rev2.val;
+                rev1 = rev1.next;
+                rev2 = rev2.next;
+            } else if (rev1 == null && rev2 != null) {
+                carry += rev2.val;
+                rev2 = rev2.next;
+            } else if (rev1 != null && rev2 == null) {
+                carry += rev1.val;
+                rev1 = rev1.next;
+            }
+            curr.next = new ListNode(carry % 10);
+            curr = curr.next;
+            carry /= 10;
+        }
+        return reverse(dummy.next);
     }
-    return reverse(dummy.next);
-  }
-  
-  private ListNode reverse(ListNode root) {
-    ListNode curr = root;
-    ListNode prev = null;
-    while (curr != null) {
-      ListNode next = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = next;
+    
+    private ListNode reverse(ListNode node) {
+        ListNode curr = node;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
-    return prev;
-  }
 }
