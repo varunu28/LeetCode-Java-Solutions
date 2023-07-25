9cfef955c90e6afd14d08cc15b492c424b720e76
varunu28 <varun.u28@gmail.com>
2023-07-24 17:58:41 -0700

    Added Peak Index in a Mountain Array.java

---
+++ Medium/Peak Index in a Mountain Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
