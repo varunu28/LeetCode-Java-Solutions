f9008e0defd3aae742e88bd0126eb72ab47d0818
Varun Upadhyay <varun.u28@gmail.com>
2023-08-03 11:24:25 -0700

    Update Letter Combinations of a Phone Number.java

--- Medium/Letter Combinations of a Phone Number.java
+++ Medium/Letter Combinations of a Phone Number.java
@@ -1,25 +1,32 @@
 class Solution {
-  
-  private final List<String> LETTER_MAPPING = Arrays.asList("abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz");
-  
-  public List<String> letterCombinations(String digits) {
-    List<String> result = new ArrayList<>();
-    helper(digits, 0, new StringBuilder(), result);
-    return result;
-  }
-  
-  private void helper(String digits, int idx, StringBuilder sb, List<String> result) {
-    if (idx == digits.length()) {
-      if (sb.length() > 0) {
-        result.add(new String(sb.toString()));
-      }
-      return;
-    } 
-    int mappingIdx = Character.getNumericValue(digits.charAt(idx)) - 2;
-    for (char c : LETTER_MAPPING.get(mappingIdx).toCharArray()) {
-      sb.append(c);
-      helper(digits, idx + 1, sb, result);
-      sb.deleteCharAt(sb.length() - 1);
+    private static final Map<Character, String> DIGIT_TO_CHAR_MAPPING = Map.of(
+            '2', "abc",
+            '3', "def",
+            '4', "ghi",
+            '5', "jkl",
+            '6', "mno",
+            '7', "pqrs",
+            '8', "tuv",
+            '9', "wxyz"
+    );
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        letterCombinations(digits, 0, new StringBuilder(), result);
+        return result;
     }
-  }
+
+    private void letterCombinations(String digits, int idx, StringBuilder sb, List<String> result) {
+        if (idx == digits.length()) {
+            if (!sb.isEmpty()) {
+                result.add(sb.toString());
+            }
+            return;
+        }
+        for (char c : DIGIT_TO_CHAR_MAPPING.get(digits.charAt(idx)).toCharArray()) {
+            sb.append(c);
+            letterCombinations(digits, idx + 1, sb, result);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
 }
