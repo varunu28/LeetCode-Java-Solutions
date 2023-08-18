0568a1ad8acc18f44337c12e043a4bbe103829d8
Varun Upadhyay <varun.u28@gmail.com>
2023-08-18 09:44:20 -0700

    Update Maximal Network Rank.java

--- Medium/Maximal Network Rank.java
+++ Medium/Maximal Network Rank.java
@@ -1,19 +1,21 @@
 class Solution {
-  public int maximalNetworkRank(int n, int[][] roads) {
-    boolean[][] connected = new boolean[n][n];
-    int[] count = new int[n];
-    for (int[] road : roads) {
-      count[road[0]]++;
-      count[road[1]]++;
-      connected[road[0]][road[1]] = true;
-      connected[road[1]][road[0]] = true;
+    public int maximalNetworkRank(int n, int[][] roads) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] road : roads) {
+            map.computeIfAbsent(road[0], k -> new HashSet<>()).add(road[1]);
+            map.computeIfAbsent(road[1], k -> new HashSet<>()).add(road[0]);
+        }
+        int maximumRank = 0;
+        for (int i = 0; i < n; i++) {
+            int iRank = map.getOrDefault(i, new HashSet<>()).size();
+            for (int j = i + 1; j < n; j++) {
+                int combinedRank = iRank + map.getOrDefault(j, new HashSet<>()).size();
+                if (map.getOrDefault(j, new HashSet<>()).contains(i)) {
+                    combinedRank--;
+                }
+                maximumRank = Math.max(maximumRank, combinedRank);
+            }
+        }
+        return maximumRank;
     }
-    int maxCount = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        maxCount = Math.max(maxCount, count[i] + count[j] - (connected[i][j] ? 1 : 0));
-      }
-    }
-    return maxCount;
-  }
 }
