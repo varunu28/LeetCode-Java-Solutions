cc431382291b2cf01b8dce7088698cd7759dce6e
Varun Upadhyay <varun.u28@gmail.com>
2023-08-20 17:34:10 -0700

    Update Repeated Substring Pattern.java

--- Easy/Repeated Substring Pattern.java
+++ Easy/Repeated Substring Pattern.java
@@ -1,19 +1,16 @@
 class Solution {
-  public boolean repeatedSubstringPattern(String s) {
-    int n = s.length();
-    for (int i = n / 2; i >= 1; i--) {
-      if (n % i == 0) {
-        int count = n / i;
-        StringBuilder sb = new StringBuilder();
-        String sub = s.substring(0, i);
-        while (count-- > 0) {
-          sb.append(sub);
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = n / 2; i > 0; i--) {
+            if (n % i == 0) {
+                int count = n / i;
+                String substring = s.substring(0, i);
+                String repeatedString = substring.repeat(count);
+                if (repeatedString.equals(s)) {
+                    return true;
+                }
+            }
         }
-        if (sb.toString().equals(s)) {
-          return true;
-        }
-      }
+        return false;
     }
-    return false;
-  }
 }
