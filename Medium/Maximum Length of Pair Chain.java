8c4f2cfb09055746c83be881ed85de019ee11c03
Varun Upadhyay <varun.u28@gmail.com>
2023-08-26 13:12:46 -0700

    Update Maximum Length of Pair Chain.java

--- Medium/Maximum Length of Pair Chain.java
+++ Medium/Maximum Length of Pair Chain.java
@@ -1,22 +1,14 @@
 class Solution {
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-
-        int[] temp = new int[pairs.length];
-        Arrays.fill(temp, 1);
-        for (int i=1; i<pairs.length; i++) {
-            for (int j=0; j<i; j++) {
-                if (pairs[j][1] < pairs[i][0]) {
-                    temp[i] = Math.max(temp[i], temp[j] + 1);
-                }
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[1]));
+        int curr = Integer.MIN_VALUE;
+        int result = 0;
+        for (int[] pair : pairs) {
+            if (pair[0] > curr) {
+                result++;
+                curr = pair[1];
             }
         }
-
-        return Arrays.stream(temp).max().getAsInt();
+        return result;
     }
 }
