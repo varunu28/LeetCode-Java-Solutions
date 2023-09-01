66a79a031f220ba6f295a9db2bb69321aa76a9f9
Varun Upadhyay <varun.u28@gmail.com>
2023-09-01 09:53:25 -0700

    Update Counting Bits.java

--- Easy/Counting Bits.java
+++ Easy/Counting Bits.java
@@ -1,9 +1,9 @@
 class Solution {
-  public int[] countBits(int n) {
-    int[] numofOnes = new int[n + 1];
-    for (int i = 1; i <= n; i++) {
-      numofOnes[i] = numofOnes[i / 2] + i % 2;
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            result[i] = result[i / 2] + i % 2;
+        }
+        return result;
     }
-    return numofOnes;
-  }
 }
