b234fe82a97a0e0eb0ac48c08e11ff4593befaf2
Varun Upadhyay <varun.u28@gmail.com>
2023-09-06 17:16:17 -0700

    Update Reverse Linked List II.java

--- Medium/Reverse Linked List II.java
+++ Medium/Reverse Linked List II.java
@@ -9,41 +9,43 @@
  * }
  */
 class Solution {
-  public ListNode reverseBetween(ListNode head, int left, int right) {
-    ListNode prev = null;
-    ListNode start = head;
-    for (int i = 1; i < left; i++) {
-      prev = start;
-      start = start.next;
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode curr = head;
+        ListNode prevToLeft = null;
+        for (int i = 1; i < left; i++) {
+            prevToLeft = curr;
+            curr = curr.next;
+        }
+        ListNode nextToLeft = prevToLeft == null ? head : prevToLeft.next;
+        for (int i = left; i < right; i++) {
+            curr = curr.next;
+        }
+        ListNode nextToRight = curr.next;
+        curr.next = null;
+        if (prevToLeft != null) {
+            prevToLeft.next = null;
+        }
+        ListNode reversedStart = reverse(nextToLeft);
+        if (prevToLeft != null) {
+            prevToLeft.next = reversedStart;
+        }
+        curr = reversedStart;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = nextToRight;
+        return prevToLeft == null ? reversedStart : head;
     }
-    ListNode end = start;
-    for (int i = left; i < right; i++) {
-      end = end.next;
+    
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
-    ListNode nextToEnd = end.next;
-    end.next = null;
-    ListNode reverseStart = reverse(start);
-    if (prev != null) {
-      prev.next = reverseStart;
-    }
-    ListNode curr = reverseStart;
-    while (curr.next != null) {
-      curr = curr.next;
-    }
-    curr.next = nextToEnd;
-    return prev == null ? reverseStart : head;
-  }
-  
-  private ListNode reverse(ListNode node) {
-    ListNode prev = null;
-    ListNode next = null;
-    ListNode curr = node;
-    while (curr != null) {
-      next = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = next;
-    }
-    return prev;
-  }
 }
