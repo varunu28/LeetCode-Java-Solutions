64429283f6c25e5d98d69cb7fc0aa0427004dc5c
Varun Upadhyay <varun.u28@gmail.com>
2023-09-11 06:56:14 -0700

    Update Group the People Given the Group Size They Belong To.java

--- Medium/Group the People Given the Group Size They Belong To.java
+++ Medium/Group the People Given the Group Size They Belong To.java
@@ -1,20 +1,18 @@
 class Solution {
-  public List<List<Integer>> groupThePeople(int[] groupSizes) {
-    Map<Integer, Queue<Integer>> groupSizeToId = new HashMap<>();
-    for (int i = 0; i < groupSizes.length; i++) {
-      groupSizeToId.computeIfAbsent(groupSizes[i], k -> new LinkedList<>()).add(i);
-    }
-    List<List<Integer>> result = new ArrayList<>();
-    for (Integer groupSize : groupSizeToId.keySet()) {
-      Queue<Integer> ids = groupSizeToId.get(groupSize);
-      while (!ids.isEmpty()) {
-        List<Integer> group = new ArrayList<>(groupSize);
-        for (int i = 0; i< groupSize; i++) {
-          group.add(ids.remove());
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < groupSizes.length; i++) {
+            map.computeIfAbsent(groupSizes[i], k -> new ArrayList<>()).add(i);
         }
-        result.add(group);
-      }
+        List<List<Integer>> result = new ArrayList<>();
+        for (Integer key : map.keySet()) {
+            List<Integer> candidates = map.get(key);
+            int idx = 0;
+            while (idx < candidates.size()) {
+                result.add(candidates.subList(idx, idx + key));
+                idx += key;
+            }
+        }
+        return result;
     }
-    return result;
-  }
 }
