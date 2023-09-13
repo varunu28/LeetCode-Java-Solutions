9759e5a4ba6ea77fbfc3fb32cc8a0c65eba460ea
Varun Upadhyay <varun.u28@gmail.com>
2023-09-13 07:26:55 -0700

    Update Candy.java

--- Hard/Candy.java
+++ Hard/Candy.java
@@ -1,19 +1,22 @@
 class Solution {
-  public int candy(int[] ratings) {
-    int[] candies = new int[ratings.length];
-    Arrays.fill(candies, 1);
-    for (int i = 1; i < ratings.length; i++) {
-      if (ratings[i] > ratings[i - 1]) {
-        candies[i] = candies[i - 1] + 1;
-      }
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int totalCandies = 0;
+        for (int i = 0; i < n; i++) {
+            totalCandies += candies[i];
+        }
+        return totalCandies;
     }
-    int totalCandies = candies[candies.length - 1];
-    for (int i = ratings.length - 2; i >= 0; i--) {
-      if (ratings[i] > ratings[i + 1]) {
-        candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-      }
-      totalCandies += candies[i];
-    }
-    return totalCandies;
-  }
 }
