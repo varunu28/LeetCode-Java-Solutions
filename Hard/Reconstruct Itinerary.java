49a1a51566eb3eeb63ce779c6ddc869246a24e89
Varun Upadhyay <varun.u28@gmail.com>
2023-09-13 18:02:02 -0700

    Update Reconstruct Itinerary.java

--- Hard/Reconstruct Itinerary.java
+++ Hard/Reconstruct Itinerary.java
@@ -1,22 +1,19 @@
 class Solution {
-  public List<String> findItinerary(List<List<String>> tickets) {
-    Map<String, PriorityQueue<String>> map = new HashMap<>();
-    for (List<String> ticket : tickets) {
-      String from = ticket.get(0);
-      String to = ticket.get(1);
-      map.computeIfAbsent(from, k -> new PriorityQueue<>()).add(to);
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            map.computeIfAbsent(ticket.get(0), k -> new PriorityQueue<>()).add(ticket.get(1));
+        }
+        Stack<String> stack = new Stack<>();
+        stack.push("JFK");
+        List<String> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            while (map.containsKey(stack.peek()) && !map.get(stack.peek()).isEmpty()) {
+                stack.push(map.get(stack.peek()).poll());
+            }
+            result.add(stack.pop());
+        }
+        Collections.reverse(result);
+        return result;
     }
-    Stack<String> stack = new Stack<>();
-    LinkedList<String> result = new LinkedList<>();
-    stack.push("JFK");
-    while (!stack.isEmpty()) {
-      String destination = stack.peek();
-      if (!map.getOrDefault(destination, new PriorityQueue<>()).isEmpty()) {
-        stack.push(map.get(destination).remove());
-      } else {
-        result.addFirst(stack.pop());
-      }
-    }
-    return result;
-  }
 }
