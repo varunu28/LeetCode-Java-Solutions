5502fe64029d14cef414b388c85e8b2ba19f4de9
Varun Upadhyay <varun.u28@gmail.com>
2023-09-18 07:05:13 -0700

    Update The K Weakest Rows in a Matrix.java

--- Easy/The K Weakest Rows in a Matrix.java
+++ Easy/The K Weakest Rows in a Matrix.java
@@ -1,28 +1,28 @@
 class Solution {
-  public int[] kWeakestRows(int[][] mat, int k) {
-    PriorityQueue<int[]> pq = new PriorityQueue<>(
-        Comparator.comparingInt((int[] o) -> o[1]).thenComparingInt(o -> o[0]));
-    for (int i = 0; i < mat.length; i++) {
-      pq.add(new int[]{i, getNumberOfOnes(mat[i])});
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<int[]> pq =
+                new PriorityQueue<>(Comparator.comparingInt((int[] o) -> o[1]).thenComparingInt(o -> o[0]));
+        for (int i = 0; i < mat.length; i++) {
+            pq.add(new int[]{i, findNumberOfSoldiers(mat[i])});
+        }
+        int[] result = new int[k];
+        for (int i = 0; i < k && !pq.isEmpty(); i++){
+            result[i] = pq.poll()[0];
+        }
+        return result;
     }
-    int[] result = new int[k];
-    for (int i = 0; i < result.length && !pq.isEmpty(); i++) {
-      result[i] = pq.poll()[0];
-    }
-    return result;
-  }
 
-  private int getNumberOfOnes(int[] arr) {
-    int start = 0;
-    int end = arr.length - 1;
-    while (start <= end) {
-      int mid = (start + end) / 2;
-      if (arr[mid] == 0) {
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
+    private int findNumberOfSoldiers(int[] row) {
+        int left = 0;
+        int right = row.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (row[mid] == 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right < 0 ? 0 : left;
     }
-    return end < 0 ? 0 : start;
-  }
 }
