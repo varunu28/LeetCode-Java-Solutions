b43550af66a9288fe7487d71cb1a95d81b8344c9
Varun Upadhyay <varun.u28@gmail.com>
2023-09-19 08:19:33 -0700

    Update Find the Duplicate Number.java

--- Medium/Find the Duplicate Number.java
+++ Medium/Find the Duplicate Number.java
@@ -1,16 +1,20 @@
 class Solution {
-  public int findDuplicate(int[] nums) {
-    int slow = nums[0];
-    int fast = nums[0];
-    do {
-      slow = nums[slow];
-      fast = nums[nums[fast]];
-    } while (slow != fast);
-    slow = nums[0];
-    while (slow != fast) {
-      slow = nums[slow];
-      fast = nums[fast];
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+        }
+        int idxOne = nums[0];
+        int idxTwo = slow;
+        while (idxOne != idxTwo) {
+            idxOne = nums[idxOne];
+            idxTwo = nums[idxTwo];
+        }
+        return idxOne;
     }
-    return fast;
-  }
 }
