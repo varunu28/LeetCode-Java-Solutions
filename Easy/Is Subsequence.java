e2c2b2372b952c4ee16f19250a05a4a7346defb4
Varun Upadhyay <varun.u28@gmail.com>
2023-09-21 17:20:10 -0700

    Update Is Subsequence.java

--- Easy/Is Subsequence.java
+++ Easy/Is Subsequence.java
@@ -1,11 +1,13 @@
 class Solution {
-  public boolean isSubsequence(String s, String t) {
-    int j = 0;
-    for (int i = 0; i < t.length() && j < s.length(); i++) {
-      if (s.charAt(j) == t.charAt(i)) {
-        j++;
-      }
+    public boolean isSubsequence(String s, String t) {
+        int sIdx = 0;
+        int tIdx = 0;
+        while (sIdx < s.length() && tIdx < t.length()) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+            }
+            tIdx++;
+        }
+        return sIdx == s.length();
     }
-    return j == s.length();
-  }
 }
