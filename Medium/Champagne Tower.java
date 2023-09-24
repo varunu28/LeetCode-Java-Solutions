5c7f8b7106ab6174d147f94899798faa047ac10f
Varun Upadhyay <varun.u28@gmail.com>
2023-09-23 17:54:15 -0700

    Update Champagne Tower.java

--- Medium/Champagne Tower.java
+++ Medium/Champagne Tower.java
@@ -1,14 +1,13 @@
 class Solution {
-  public double champagneTower(int poured, int query_row, int query_glass) {
-    double[][] glassCapacity = new double[query_row + 1][query_glass + 2];
-    glassCapacity[0][0] = poured;
-    for (int currRow = 0; currRow < query_row; currRow++) {
-      for (int currCol = 0; currCol <= query_glass; currCol++) {
-        double currCapacity = Math.max(glassCapacity[currRow][currCol] - 1.0, 0);
-        glassCapacity[currRow + 1][currCol] += currCapacity / 2.0;
-        glassCapacity[currRow + 1][currCol + 1] += currCapacity / 2.0;
-      }
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[] glass = new double[query_row + 2];
+        glass[0] = poured;
+        for (int row = 1; row <= query_row; row++) {
+            for (int idx = row; idx >= 0; idx--) {
+                glass[idx] = Math.max(0.0, (glass[idx] - 1) / 2);
+                glass[idx + 1] += glass[idx];
+            }
+        }
+        return Math.min(glass[query_glass], 1.0);
     }
-    return Math.min(glassCapacity[query_row][query_glass], 1.0);
-  } 
 }
