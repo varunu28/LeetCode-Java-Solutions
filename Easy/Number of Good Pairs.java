64077658a5f27876f8118439012ae020f4a4532c
Varun Upadhyay <varun.u28@gmail.com>
2023-10-02 17:15:32 -0700

    Update Number of Good Pairs.java

--- Easy/Number of Good Pairs.java
+++ Easy/Number of Good Pairs.java
@@ -1,11 +1,11 @@
 class Solution {
-  public int numIdenticalPairs(int[] nums) {
-    Map<Integer, Integer> map = new HashMap<>();
-    int count = 0;
-    for (int num : nums) {
-      count += map.getOrDefault(num, 0);
-      map.put(num, map.getOrDefault(num, 0) + 1);
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int num : nums) {
+            count += map.getOrDefault(num, 0);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return count;
     }
-    return count;
-  }
 }
