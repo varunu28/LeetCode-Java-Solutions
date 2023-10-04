13ffcb896d6fe6ffa120ce3d89070d629ca6e41c
Varun Upadhyay <varun.u28@gmail.com>
2023-10-04 05:30:41 -0700

    Update Design HashMap.java

--- Easy/Design HashMap.java
+++ Easy/Design HashMap.java
@@ -1,42 +1,40 @@
 class MyHashMap {
 
-  /** Initialize your data structure here. */
-  Integer[][] map;
-  int NUM_OF_BUCKETS = 1000;
-  int BUCKET_SIZE = 1000;
-  public MyHashMap() {
-    map = new Integer[NUM_OF_BUCKETS][BUCKET_SIZE];
-  }
+    private static final int BUCKET_COUNT = 1001;
 
-  /** value will always be non-negative. */
-  public void put(int key, int value) {
-    Integer[] bucket = getBucket(key);
-    int keyHash = getKeyHash(key);
-    bucket[keyHash] = value;
-  }
+    private static final int BUCKET_SIZE = 1001;
 
-  /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-  public int get(int key) {
-    Integer[] bucket = getBucket(key);
-    int keyHash = getKeyHash(key);
-    return bucket[keyHash] == null ? -1 : bucket[keyHash];
-  }
+    private final Integer[][] map;
 
-  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-  public void remove(int key) {
-    Integer[] bucket = getBucket(key);
-    int keyHash = getKeyHash(key);
-    bucket[keyHash] = null;
-  }
-  
-  private Integer[] getBucket(int key) {
-    int bucketIdx = key / BUCKET_SIZE;
-    return map[bucketIdx];
-  }
-  
-  private Integer getKeyHash(int key) {
-    return key % BUCKET_SIZE;
-  }
+    public MyHashMap() {    
+        this.map = new Integer[BUCKET_COUNT][BUCKET_SIZE];
+    }
+    
+    public void put(int key, int value) {
+        Position position = getPosition(key);
+        map[position.bucket()][position.index()] = value;
+    }
+    
+    public int get(int key) {
+        Position position = getPosition(key);
+        Integer value = map[position.bucket()][position.index()];
+        return value == null ? -1 : value;
+    }
+    
+    public void remove(int key) {
+        Position position = getPosition(key);
+        map[position.bucket()][position.index()] = null;
+    }
+
+    private static Position getPosition(int key) {
+        int bucket = key / BUCKET_COUNT;
+        int index = key % BUCKET_SIZE;
+        return new Position(bucket, index);
+    }
+
+    private static record Position(int bucket, int index) {
+      
+    }
 }
 
 /**
