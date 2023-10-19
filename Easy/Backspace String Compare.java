e7be3158b5dde10e199433f5730620621c5a7c56
Varun Upadhyay <varun.u28@gmail.com>
2023-10-19 06:53:50 -0700

    Update Backspace String Compare.java

--- Easy/Backspace String Compare.java
+++ Easy/Backspace String Compare.java
@@ -1,23 +1,23 @@
 class Solution {
-  public boolean backspaceCompare(String s, String t) {
-    return formBackSpaceString(s).equals(formBackSpaceString(t));
-  }
-  
-  private String formBackSpaceString(String s) {
-    Stack<Character> stack = new Stack<>();
-    for (char c : s.toCharArray()) {
-      if (c == '#') {
-        if (!stack.isEmpty()) {
-          stack.pop();
+    public boolean backspaceCompare(String s, String t) {
+        return buildBackspaceString(s).equals(buildBackspaceString(t));
+    }
+
+    private static String buildBackspaceString(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '#') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);
+            }
         }
-      } else {
-        stack.push(c);
-      }
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
     }
-    StringBuilder sb = new StringBuilder();
-    while (!stack.isEmpty()) {
-      sb.append(stack.pop());
-    }
-    return sb.toString();
-  }
 }
