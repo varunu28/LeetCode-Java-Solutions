de3fdc9fd167ea78df037aff9d4a914cecb3000d
Varun Upadhyay <varun.u28@gmail.com>
2023-10-23 08:52:06 -0700

    Update Power of Four.java

--- Easy/Power of Four.java
+++ Easy/Power of Four.java
@@ -1,19 +1,5 @@
 class Solution {
-  public boolean isPowerOfFour(int n) {
-    long left = 0;
-    long right = n / 2 + 1;
-    while (left <= right) {
-      long mid = (left + right) / 2;
-      long pow = (long) (Math.pow(4, mid));
-      if (pow == ((long) n)) {
-        return true;
-      }
-      if (pow > ((long) n)) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && Math.log(n) / Math.log(2) % 2 == 0;
     }
-    return false;
-  }
 }
