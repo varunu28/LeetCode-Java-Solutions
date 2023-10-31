7b6bf88a2086e01e8c047c87b8e92f43a3bd8c80
Varun Upadhyay <varun.u28@gmail.com>
2023-10-31 13:20:10 +0530

    Update Find The Original Array of Prefix Xor.java

--- Medium/Find The Original Array of Prefix Xor.java
+++ Medium/Find The Original Array of Prefix Xor.java
@@ -1,11 +1,11 @@
 class Solution {
-  public int[] findArray(int[] pref) {
-    int n = pref.length;
-    int[] result = new int[n];
-    result[0] = pref[0];
-    for (int i = 1; i < n; i++) {
-      result[i] = pref[i] ^ pref[i - 1];
+    public int[] findArray(int[] pref) {
+        int n = pref.length;
+        int[] result = new int[n];
+        result[0] = pref[0];
+        for (int i = 1; i < n; i++) {
+            result[i] = pref[i - 1] ^ pref[i];
+        }
+        return result;
     }
-    return result;
-  }
 }
