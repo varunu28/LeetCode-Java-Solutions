08d5d21f043018b65b0d55daac6349464b930e2e
Varun Upadhyay <varun.u28@gmail.com>
2023-11-05 03:19:02 +0530

    Update Last Moment Before All Ants Fall Out of a Plank.java

--- Medium/Last Moment Before All Ants Fall Out of a Plank.java
+++ Medium/Last Moment Before All Ants Fall Out of a Plank.java
@@ -1,12 +1,12 @@
 class Solution {
-  public int getLastMoment(int n, int[] left, int[] right) {
-    int res = 0;
-    for (int ant : left) {
-      res = Math.max(res, ant);
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int result = 0;
+        for (int num : left) {
+            result = Math.max(result, num);
+        }
+        for (int num : right) {
+            result = Math.max(result, n - num);
+        }
+        return result;
     }
-    for (int ant : right) {
-      res = Math.max(res, n - ant);
-    }
-    return res;
-  }
 }
