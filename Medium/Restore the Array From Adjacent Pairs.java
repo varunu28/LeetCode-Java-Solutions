0878cdcb6c23c7231b892eec6c24ba8b51186ccc
Varun Upadhyay <varun.u28@gmail.com>
2023-11-10 06:36:35 +0530

    Update Restore the Array From Adjacent Pairs.java

--- Medium/Restore the Array From Adjacent Pairs.java
+++ Medium/Restore the Array From Adjacent Pairs.java
@@ -1,25 +1,31 @@
 class Solution {
-  public int[] restoreArray(int[][] adjacentPairs) {
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    for (int[] pair : adjacentPairs) {
-      map.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
-      map.computeIfAbsent(pair[1], k -> new ArrayList<>()).add(pair[0]);
+    public int[] restoreArray(int[][] adjacentPairs) {
+        int n = adjacentPairs.length + 1;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] pair : adjacentPairs) {
+            map.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
+            map.computeIfAbsent(pair[1], k -> new ArrayList<>()).add(pair[0]);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        int firstElement = map.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(-1);
+        queue.add(firstElement);
+        Set<Integer> set = new HashSet<>();
+        int[] result = new int[n];
+        int idx = 0;
+        while (!queue.isEmpty()) {
+            int removed = queue.remove();
+            if (set.contains(removed)) {
+                continue;
+            }
+            result[idx++] = removed;
+            set.add(removed);
+            queue.addAll(map.get(removed));
+        }
+        return result;
     }
-    int head = map.keySet().stream().filter(k -> map.get(k).size() == 1).findFirst().orElse(-1);
-    Set<Integer> visited = new HashSet<>();
-    int[] result = new int[map.size()];
-    int idx = 0;
-    Queue<Integer> queue = new LinkedList<>();
-    queue.add(head);
-    while (!queue.isEmpty()) {
-      int removed = queue.remove();
-      if (visited.contains(removed)) {
-        continue;
-      }
-      result[idx++] = removed;
-      visited.add(removed);
-      queue.addAll(map.get(removed));
-    }
-    return result;
-  }
 }
