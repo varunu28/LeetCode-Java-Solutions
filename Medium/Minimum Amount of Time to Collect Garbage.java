9e4fc67c04fb3ccdced7fd70f8666fdce7b2ef2b
Varun Upadhyay <varun.u28@gmail.com>
2023-11-20 15:19:10 +0530

    Update Minimum Amount of Time to Collect Garbage.java

--- Medium/Minimum Amount of Time to Collect Garbage.java
+++ Medium/Minimum Amount of Time to Collect Garbage.java
@@ -1,34 +1,25 @@
 class Solution {
-  public int garbageCollection(String[] garbage, int[] travel) {
-    int[] lastIdx = {-1, -1, -1};
-    Map<Integer, int[]> garbageCount = new HashMap<>();
-    for (int i = 0; i < garbage.length; i++) {
-      garbageCount.put(i, new int[3]);
-      for (char c : garbage[i].toCharArray()) {
-        if (c == 'M') {
-          lastIdx[0] = i;
-          garbageCount.get(i)[0]++;
+    public int garbageCollection(String[] garbage, int[] travel) {
+        Map<Character, Integer> lastIdx = new HashMap<>();
+        for (int i = garbage.length - 1; i >= 0; i--) {
+            if (garbage[i].indexOf('M') != -1) {
+                lastIdx.putIfAbsent('M', i);
+            }
+            if (garbage[i].indexOf('G') != -1) {
+                lastIdx.putIfAbsent('G', i);
+            }
+            if (garbage[i].indexOf('P') != -1) {
+                lastIdx.putIfAbsent('P', i);
+            }
         }
-        if (c == 'P') {
-          lastIdx[1] = i;
-          garbageCount.get(i)[1]++;
+        int totalTime = 0;
+        for (int i = 0; i < garbage.length; i++) {
+            int travelTime = i == 0 ? 0 : travel[i - 1];
+            totalTime += garbage[i].length();
+            totalTime += lastIdx.getOrDefault('P', -1) >= i ? travelTime : 0;
+            totalTime += lastIdx.getOrDefault('M', -1) >= i ? travelTime : 0;
+            totalTime += lastIdx.getOrDefault('G', -1) >= i ? travelTime : 0;
         }
-        if (c == 'G') {
-          lastIdx[2] = i;
-          garbageCount.get(i)[2]++;
-        }
-      }
+        return totalTime;
     }
-    int time = 0;
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < garbage.length; j++) {
-        if (lastIdx[i] < j) {
-          break;
-        }
-        time += j == 0 ? 0 : travel[j - 1];
-        time += garbageCount.get(j)[i];
-      }
-    }
-    return time;
-  }
 }
