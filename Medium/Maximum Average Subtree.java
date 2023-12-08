ee241fbceafe5625316b337b7b5ec6dc7e72f5bb
Varun Upadhyay <varun.u28@gmail.com>
2023-12-08 10:18:30 -0800

    Update Maximum Average Subtree.java

--- Medium/Maximum Average Subtree.java
+++ Medium/Maximum Average Subtree.java
@@ -4,28 +4,32 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  double maxAvg;
-  public double maximumAverageSubtree(TreeNode root) {
-    maxAvg = 0.0;
-    helper(root);
-    return maxAvg;
-  }
-  
-  private int[] helper(TreeNode root) {
-    int[] data = new int[2];
-    if (root == null) {
-      return data;
+    public double maximumAverageSubtree(TreeNode root) {
+        double[] result = {0.0};
+        traverse(root, result);
+        return result[0];
     }
-    int[] dataLeft = helper(root.left);
-    int[] dataRight = helper(root.right);
-    data[0] += root.val + dataLeft[0] + dataRight[0];
-    data[1] += 1 + dataLeft[1] + dataRight[1];
-    double avg = ((double) data[0]) / data[1];
-    maxAvg = Math.max(maxAvg, avg);
-    return data;
-  }
+
+    private double[] traverse(TreeNode root, double[] result) {
+        if (root == null) {
+            return new double[]{0.0, 0.0};
+        }
+        double[] leftValues = traverse(root.left, result);
+        double[] rightValues = traverse(root.right, result);
+        double sum = root.val + leftValues[0] + rightValues[0];
+        double count = 1 + leftValues[1] + rightValues[1];
+        double avg = sum / count;
+        result[0] = Math.max(result[0], avg);
+        return new double[]{sum, count};
+    }
 }
