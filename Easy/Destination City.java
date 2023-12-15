847953331f0961ef8e7ba965f170bf78443f66c8
Varun Upadhyay <varun.u28@gmail.com>
2023-12-14 17:53:24 -0800

    Update Destination City.java

--- Easy/Destination City.java
+++ Easy/Destination City.java
@@ -1,17 +1,12 @@
 class Solution {
-  public String destCity(List<List<String>> paths) {
-    Set<String> outgoingPathCities = new HashSet<>();
-    Set<String> cities = new HashSet<>();
-    for (List<String> path : paths) {
-      outgoingPathCities.add(path.get(0));
-      cities.add(path.get(0));
-      cities.add(path.get(1));
+    public String destCity(List<List<String>> paths) {
+        Set<String> cities = new HashSet<>();
+        Set<String> outgoingCities = new HashSet<>();
+        for (List<String> path : paths) {
+            cities.addAll(path);
+            outgoingCities.add(path.get(0));
+        }
+        cities.removeAll(outgoingCities);
+        return cities.iterator().next();
     }
-    for (String city : cities) {
-      if (!outgoingPathCities.contains(city)) {
-        return city;
-      }
-    }
-    return "";
-  }
 }
