85f5352083dcee5950f3ad52194876b7a7464c23
Varun Upadhyay <varun.u28@gmail.com>
2023-12-24 07:20:23 -0800

    Update Minimum Changes To Make Alternating Binary String.java

--- Easy/Minimum Changes To Make Alternating Binary String.java
+++ Easy/Minimum Changes To Make Alternating Binary String.java
@@ -1,11 +1,16 @@
 class Solution {
-  public int minOperations(String s) {
-    int numOfChanges = 0;
-    for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) - '0' != i % 2) {
-        numOfChanges++;
-      }
+    public int minOperations(String s) {
+        int startZero = 0;
+        int startOne = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                startOne += s.charAt(i) == '0' ? 1 : 0;
+                startZero += s.charAt(i) == '1' ? 1 : 0;
+            } else {
+                startOne += s.charAt(i) == '1' ? 1 : 0;
+                startZero += s.charAt(i) == '0' ? 1 : 0;
+            }
+        }
+        return Math.min(startZero, startOne);
     }
-    return Math.min(numOfChanges, s.length() - numOfChanges);
-  }
 }
