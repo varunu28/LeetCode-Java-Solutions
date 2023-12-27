c4505bfb84748231912291819dd8eead17dfb16b
Varun Upadhyay <varun.u28@gmail.com>
2023-12-26 17:28:19 -0800

    Update Minimum Time to Make Rope Colorful.java

--- Medium/Minimum Time to Make Rope Colorful.java
+++ Medium/Minimum Time to Make Rope Colorful.java
@@ -1,19 +1,19 @@
 class Solution {
-  public int minCost(String colors, int[] neededTime) {
-    int totalTime = 0;
-    int left = 0;
-    int right = 0;
-    while (right < colors.length()) {
-      int currTotal = 0;
-      int currMax = 0;
-      while (left < neededTime.length && colors.charAt(right) == colors.charAt(left)) {
-        currTotal += neededTime[left];
-        currMax = Math.max(currMax, neededTime[left]);
-        left++;
-      }
-      totalTime += currTotal - currMax;
-      right = left;
+    public int minCost(String colors, int[] neededTime) {
+        Stack<Integer> stack = new Stack<>();
+        int cost = 0;
+        for (int i = 0; i < colors.length(); i++) {
+            if (stack.isEmpty() || colors.charAt(i) != colors.charAt(stack.peek())) {
+                stack.push(i);
+                continue;
+            }
+            if (neededTime[i] > neededTime[stack.peek()]) {
+                cost += neededTime[stack.pop()];
+                stack.push(i);
+            } else {
+                cost += neededTime[i]; 
+            }
+        }
+        return cost;
     }
-    return totalTime;
-  }
 }
