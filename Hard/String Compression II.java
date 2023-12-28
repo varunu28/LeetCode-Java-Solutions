ddcb14efbe1a54349dca61da5587fdcf283d2186
Varun Upadhyay <varun.u28@gmail.com>
2023-12-28 10:18:05 -0800

    Update String Compression II.java

--- Hard/String Compression II.java
+++ Hard/String Compression II.java
@@ -1,69 +1,58 @@
 class Solution {
-  
-  /**
-  * First dimension : Current index
-  * Second dimension : Amount of available deletions
-  * Third dimension : Index of previous character
-  * Fourth dimension : Consecutive count of previous character
-  */
-  private static int[][][][] dp = new int[100][101][100][26];
-  
-  public int getLengthOfOptimalCompression(String s, int k) {
-    int n = s.length();
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j <= k; j++) {
-        for(int z = 0; z < n; z++) {
-          for(int l = 0; l < 26; l++) {
-            dp[i][j][z][l]=-1;
-          }
+
+    private static int[][][][] dp = new int[100][101][100][26];
+
+    public int getLengthOfOptimalCompression(String s, int k) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                for (int x = 0; x < n; x++) {
+                    for (int y = 0; y < 26; y++) {
+                        dp[i][j][x][y] = -1;
+                    }
+                }
+            }
         }
-      }
+        return solver(s.toCharArray(), 0, k, 0, s.charAt(0));
     }
-    return best(s.toCharArray(), /* idx= */ 0, k, /* count= */ 0, /* prevChar= */ s.charAt(0));
-  }
-  
-  private int best(char[] letters, int idx, int k, int count, char prevChar) {
-    if (idx == letters.length) {
-      return countNumberOfLetters(count);
+
+    private int solver(char[] letters, int idx, int k, int currFrequency, char prevChar) {
+        if (idx == letters.length) {
+            return getLetterCount(currFrequency);
+        }
+        if (dp[idx][k][currFrequency][prevChar - 'a'] != -1) {
+            return dp[idx][k][currFrequency][prevChar - 'a'];
+        }
+        int result = 0;
+        if (letters[idx] == prevChar) {
+            result = k == 0 ? 
+                solver(letters, idx + 1, k, currFrequency + 1, prevChar) : 
+                Math.min(
+                    solver(letters, idx + 1, k , currFrequency + 1, prevChar), // don't delete
+                    solver(letters, idx + 1, k - 1 , currFrequency, prevChar) // delete
+                );
+        } else {
+            result = k == 0 ?
+                getLetterCount(currFrequency) + solver(letters, idx + 1, k, 1, letters[idx]) :
+                Math.min(
+                    getLetterCount(currFrequency) + solver(letters, idx + 1, k, 1, letters[idx]), // don't delete
+                    solver(letters, idx + 1, k - 1, currFrequency, prevChar) // delete
+                );
+        }
+        return dp[idx][k][currFrequency][prevChar - 'a'] = result;
     }
-    if (dp[idx][k][count][prevChar - 'a'] != -1) {
-      return dp[idx][k][count][prevChar - 'a'];
+
+    private static int getLetterCount(int frequency) {
+        if (frequency == 0) {
+            return 0;
+        }
+        int digits = 1;
+        if (frequency > 9) {
+            digits = 2;
+        }
+        if (frequency > 99) {
+            digits = 3;
+        }
+        return frequency == 1 ? 1 : (digits + 1);
     }
-    if (k == 0) {
-      int result = 0;
-      if (letters[idx] == prevChar) {
-        result = best(letters, idx + 1, k, count + 1, prevChar);
-      } else {
-        result = countNumberOfLetters(count) + best(letters, idx + 1, k, 1, letters[idx]);
-      }
-      dp[idx][k][count][prevChar - 'a'] = result;
-      return result;
-    }
-    int result = 0;
-    if (letters[idx] == prevChar) {
-      result = Math.min(
-        best(letters, idx + 1, k, count + 1, prevChar), // Delete and recurse forward
-        best(letters, idx + 1, k - 1, count, prevChar)); // Don't delete and recurse forward
-    } else {
-      result = Math.min(
-        countNumberOfLetters(count) + best(letters, idx + 1, k, /* count= */ 1, /* prevChar= */ letters[idx]), // Don't delete current char and recurse forward with new count
-        best(letters, idx + 1, k - 1, count, prevChar)); // Delete current character and recurse forward
-    }
-    dp[idx][k][count][prevChar - 'a'] = result;
-    return result;
-  }
-  
-  private static int countNumberOfLetters(int count) {
-    if (count == 0) {
-      return count;
-    }
-    int numberOfDigits = 1;
-    if (count > 9) {
-      numberOfDigits = 2;
-    } 
-    if (count > 99) {
-      numberOfDigits = 3;
-    }
-    return count == 1 ? 1 : (numberOfDigits + 1);
-  }
 }
