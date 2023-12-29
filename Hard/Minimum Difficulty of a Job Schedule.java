bd407202de2446c8dfa576516865b4b2034e140d
Varun Upadhyay <varun.u28@gmail.com>
2023-12-29 10:44:34 -0800

    Update Minimum Difficulty of a Job Schedule.java

--- Hard/Minimum Difficulty of a Job Schedule.java
+++ Hard/Minimum Difficulty of a Job Schedule.java
@@ -1,30 +1,30 @@
 class Solution {
-  public int minDifficulty(int[] jobDifficulty, int d) {
-    int n = jobDifficulty.length;
-    if (n < d) {
-      return -1;
+    
+    private static final int INVALID_VALUE = 300_001;
+
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        if (jobDifficulty.length < d) {
+            return -1;
+        }
+        Integer[][][] dp = new Integer[jobDifficulty.length][d + 1][1001];
+        return dfs(jobDifficulty, 0, d, 0, dp);
     }
-    Integer[][] dp = new Integer[n][d + 1];
-    return minDifficulty(/* index= */0, d, jobDifficulty, dp);
-  }
-  
-  private int minDifficulty(int index, int dLeft, int[] jobDifficulty, Integer[][] dp)  {
-    if (dp[index][dLeft] != null) {
-      return dp[index][dLeft];
+
+    private int dfs(int[] jobDifficulty, int idx, int d, int currMax, Integer[][][] dp) {
+        if (idx == jobDifficulty.length) {
+            return d == 0 ? 0 : INVALID_VALUE;
+        }
+        if (d == 0) {
+            return INVALID_VALUE;
+        }
+        if (dp[idx][d][currMax] != null) {
+            return dp[idx][d][currMax];
+        }
+        currMax = Math.max(currMax, jobDifficulty[idx]);
+        int result = Math.min(dfs(
+                jobDifficulty, idx + 1, d, currMax, dp), // continue same sequence
+                currMax + dfs(jobDifficulty, idx + 1, d - 1, 0, dp) // break sequence
+        );
+        return dp[idx][d][currMax] = result;
     }
-    if (dLeft == 1) {
-      int result = 0;
-      for (int j = index; j < jobDifficulty.length; j++) {
-        result = Math.max(result, jobDifficulty[j]);
-      }
-      return result;
-    }
-    int result = Integer.MAX_VALUE;
-    int dailyMaxJobDiff = 0;
-    for (int j = index; j < jobDifficulty.length - dLeft + 1; j++) {
-      dailyMaxJobDiff = Math.max(dailyMaxJobDiff, jobDifficulty[j]);
-      result = Math.min(result, dailyMaxJobDiff + minDifficulty(j + 1, dLeft - 1, jobDifficulty, dp));
-    }
-    return dp[index][dLeft] = result;
-  } 
 }
