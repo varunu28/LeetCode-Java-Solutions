5fff8fb179026d7a7bafb646479e42345f7330df
Varun Upadhyay <varun.u28@gmail.com>
2023-12-30 17:55:12 -0800

    Update Largest Substring Between Two Equal Characters.java

--- Easy/Largest Substring Between Two Equal Characters.java
+++ Easy/Largest Substring Between Two Equal Characters.java
@@ -1,12 +1,11 @@
 class Solution {
-  public int maxLengthBetweenEqualCharacters(String s) {
-    Map<Character, Integer> startIndex = new HashMap<>();
-    int largestSubstringLength = -1;
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-      startIndex.putIfAbsent(c, i);
-      largestSubstringLength = Math.max(largestSubstringLength, i - startIndex.get(c) - 1);
+    public int maxLengthBetweenEqualCharacters(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLength = -1;
+        for (int i = 0; i < s.length(); i++) {
+            map.putIfAbsent(s.charAt(i), i);
+            maxLength = Math.max(maxLength, i - map.get(s.charAt(i)) - 1);
+        }
+        return maxLength;
     }
-    return largestSubstringLength;
-  }
 }
