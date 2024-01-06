d376fe1e6c95798c21db6276c2b111909916079a
Varun Upadhyay <varun.u28@gmail.com>
2024-01-06 12:39:41 -0800

    Update Maximum Profit in Job Scheduling.java

--- Hard/Maximum Profit in Job Scheduling.java
+++ Hard/Maximum Profit in Job Scheduling.java
@@ -1,28 +1,46 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
+        int n = profit.length;
         int[][] jobs = new int[n][3];
         for (int i = 0; i < n; i++) {
             jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-        Integer[] cache = new Integer[n];
-        return dfs(0, jobs, cache);
+        for (int i = 0; i < n; i++) {
+            startTime[i] = jobs[i][0];
+        }
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return findMaxProfit(jobs, startTime, 0, n, dp);
     }
-    
-    private int dfs(int idx, int[][] jobs, Integer[] cache) {
-        if (idx == jobs.length) {
+
+    private int findMaxProfit(int[][] jobs, int[] startTime, int position, int n, int[] dp) {
+        if (position == n) {
             return 0;
         }
-        if (cache[idx] != null) {
-            return cache[idx];
+        if (dp[position] != -1) {
+            return dp[position];
         }
-        int nextIdx = idx + 1;
-        while (nextIdx < jobs.length && jobs[idx][1] > jobs[nextIdx][0]) {
-            nextIdx++;
+        int nextPosition = binarySearch(startTime, jobs[position][1]);
+        return dp[position] = Math.max(
+            findMaxProfit(jobs, startTime, position + 1, n, dp), // skip executing current job
+            jobs[position][2] + findMaxProfit(jobs, startTime, nextPosition, n, dp) // execute current job & move to next position
+        );
+    }
+
+    private static int binarySearch(int[] startTime, int endTime) {
+        int left = 0;
+        int right = startTime.length - 1;
+        int nextPosition = startTime.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (startTime[mid] >= endTime) {
+                nextPosition = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        int jobScheduled = jobs[idx][2] + dfs(nextIdx, jobs, cache);
-        int jobNotScheduled = dfs(idx + 1, jobs, cache);
-        return cache[idx] = Math.max(jobScheduled, jobNotScheduled);
+        return nextPosition;
     }
 }
