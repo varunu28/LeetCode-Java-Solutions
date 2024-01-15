290f75fa9e8d3840aeafc27141b7dd7000225c12
Varun Upadhyay <varun.u28@gmail.com>
2024-01-15 13:13:06 -0800

    Update Single-Row Keyboard.java

--- Easy/Single-Row Keyboard.java
+++ Easy/Single-Row Keyboard.java
@@ -1,16 +1,15 @@
 class Solution {
-  public int calculateTime(String keyboard, String word) {
-    Map<Character, Integer> map = new HashMap<>();
-    int pos = 0;
-    for (char c : keyboard.toCharArray()) {
-      map.put(c, pos++);
+    public int calculateTime(String keyboard, String word) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            map.put(keyboard.charAt(i), i);
+        }
+        int position = 0;
+        int time = 0;
+        for (char c : word.toCharArray()) {
+            time += Math.abs(position - map.get(c));
+            position = map.get(c);
+        }
+        return time;
     }
-    int total = 0;
-    int currPos = 0;
-    for (char c : word.toCharArray()) {
-      total += Math.abs(currPos - map.get(c));
-      currPos = map.get(c);
-    }
-    return total;
-  }
 }
