cb64d7eb820a18aabb0569c53cf9bfa56e9d81a7
Varun Upadhyay <varun.u28@gmail.com>
2024-01-18 07:55:19 -0800

    Update Climbing Stairs.java

--- Easy/Climbing Stairs.java
+++ Easy/Climbing Stairs.java
@@ -1,14 +1,19 @@
 class Solution {
-  public int climbStairs(int n) {
-    if (n == 1) {
-      return 1;
+    public int climbStairs(int n) {
+        Integer[] dp = new Integer[n + 1];
+        return climbStairs(dp, n);
     }
-    int[] dp = new int[n + 1];
-    dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; i++) {
-      dp[i] = dp[i - 1] + dp[i - 2];
+
+    private int climbStairs(Integer[] dp, int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (dp[n] != null) {
+            return dp[n];
+        }
+        return dp[n] = climbStairs(dp, n - 1) + climbStairs(dp, n - 2);
     }
-    return dp[n];
-  }
 }
