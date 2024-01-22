c674418fc8b72aa72b59557cb3541841d2b70b72
Varun Upadhyay <varun.u28@gmail.com>
2024-01-21 18:07:48 -0800

    Update Set Mismatch.java

--- Easy/Set Mismatch.java
+++ Easy/Set Mismatch.java
@@ -1,18 +1,19 @@
 class Solution {
-  public int[] findErrorNums(int[] nums) {
-    int[] result = new int[2];
-    Set<Integer> set = new HashSet<>();
-    int n = nums.length;
-    for (int num : nums) {
-      if (!set.add(num)) {
-        result[0] = num;
-      }
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2];
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            if (!set.add(nums[i - 1])) {
+                result[0] = nums[i - 1];
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!set.contains(i)) {
+                result[1] = i;
+                break;
+            }
+        }
+        return result;
     }
-    for (int i = 1; i <= n; i++) {
-      if (!set.contains(i)) {
-        result[1] = i;
-      }
-    }
-    return result;
-  }
 }
