6b0aba9576f5f097cd78ef11144be0b9617c875b
Varun Upadhyay <varun.u28@gmail.com>
2024-01-24 07:01:26 -0800

    Update Pseudo-Palindromic Paths in a Binary Tree.java

--- Medium/Pseudo-Palindromic Paths in a Binary Tree.java
+++ Medium/Pseudo-Palindromic Paths in a Binary Tree.java
@@ -14,43 +14,38 @@
  * }
  */
 class Solution {
-  public int pseudoPalindromicPaths (TreeNode root) {
-    Queue<NodePathPair> queue = new LinkedList<>();
-    NodePathPair nodePathPair = new NodePathPair(root, 0);
-    queue.add(nodePathPair);
-    int count = 0;
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      while (size-- > 0) {
-        NodePathPair removed = queue.remove();
-        TreeNode node = removed.node;
-        int path = removed.path;
-        // XOR operation ensures that the bit corresponding to the number only appears in the
-        // path if it has occurred odd number of times. 
-        // 1 << node.val decides the bit of the value
-        path = path ^ (1 << node.val);
-        if (node.left == null && node.right == null) {
-          // Subtracting 1 means setting rightmost 1 bit to 0 and setting all lower bits to 1
-          count += (path & (path - 1)) == 0 ? 1 : 0;
-        } 
-        if (node.left != null) {
-          queue.add(new NodePathPair(node.left, path));
-        }
-        if (node.right != null) {
-          queue.add(new NodePathPair(node.right, path));
-        }
-      }
+    public int pseudoPalindromicPaths (TreeNode root) {
+        int[] result = {0};
+        Map<Integer, Integer> map = new HashMap<>();
+        helper(root, map, result);
+        return result[0];
     }
-    return count;
-  }
-  
-  private static class NodePathPair {
-    TreeNode node;
-    Integer path;
-    
-    public NodePathPair(TreeNode node, int path) {
-      this.node = node;
-      this.path = path;
+
+    private void helper(TreeNode root, Map<Integer, Integer> map, int[] result) {
+        if (root == null) {
+            return;
+        }
+        map.put(root.val, map.getOrDefault(root.val, 0) + 1);
+        if (root.left == null && root.right == null) {
+            if (isPseudoPalindromic(map)) {
+                result[0]++;
+            }
+        }
+        helper(root.left, map, result);
+        helper(root.right, map, result);
+        map.put(root.val, map.getOrDefault(root.val, 0) - 1);
     }
-  }
+
+    private static boolean isPseudoPalindromic(Map<Integer, Integer> map) {
+        boolean oddFound = false;
+        for (Integer key : map.keySet()) {
+            if (map.get(key) % 2 != 0) {
+                if (oddFound) {
+                    return false;
+                }
+                oddFound = true;
+            }
+        }
+        return true;
+    }
 }
