0d3ab9b8f111e098b98e89b8c2ea28ec69a71f14
Varun Upadhyay <varun.u28@gmail.com>
2024-01-25 07:25:30 -0800

    Create Longest Common Subsequence.java

---
+++ Medium/Longest Common Subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i + 1][j + 1] = 1 + dp[i][j];
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
