a55f997ae2efba4c176623b6664f35c69a17f3dd
Varun Upadhyay <varun.u28@gmail.com>
2024-01-30 17:33:11 -0800

    Update Daily Temperatures.java

--- Medium/Daily Temperatures.java
+++ Medium/Daily Temperatures.java
@@ -1,14 +1,15 @@
 class Solution {
-  public int[] dailyTemperatures(int[] T) {
-    Stack<Integer> stack = new Stack<>();
-    int[] ans = new int[T.length];
-    for (int i = T.length - 1; i >= 0; i--) {
-      while (!stack.isEmpty() && T[stack.peek()] <= T[i]) {
-        stack.pop();
-      }
-      ans[i] = stack.isEmpty() ? 0 : (stack.peek() - i);
-      stack.push(i);
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        return result;
     }
-    return ans;
-  }
 }
