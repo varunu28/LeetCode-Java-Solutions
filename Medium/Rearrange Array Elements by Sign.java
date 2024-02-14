e9713200867ee54d79dcd980c35754a893f4bde7
Varun Upadhyay <varun.u28@gmail.com>
2024-02-13 18:44:00 -0800

    Update Rearrange Array Elements by Sign.java

--- Medium/Rearrange Array Elements by Sign.java
+++ Medium/Rearrange Array Elements by Sign.java
@@ -1,21 +1,18 @@
 class Solution {
-  public int[] rearrangeArray(int[] nums) {
-    List<Integer> positive = new ArrayList<>();
-    List<Integer> negative = new ArrayList<>();
-    for (int num : nums) {
-      if (num > 0) {
-        positive.add(num);
-      } else {
-        negative.add(num);
-      }
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int posIndex = 0;
+        int negIndex = 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result[posIndex] = nums[i];
+                posIndex += 2;
+            } else {
+                result[negIndex] = nums[i];
+                negIndex += 2;
+            }
+        }
+        return result;
     }
-    int positiveIdx = 0;
-    int negativeIdx = 0;
-    boolean positiveFlag = true;
-    for (int i = 0; i < nums.length; i++) {
-      nums[i] = positiveFlag ? positive.get(positiveIdx++) : negative.get(negativeIdx++);
-      positiveFlag = !positiveFlag;
-    }
-    return nums;
-  }
 }
