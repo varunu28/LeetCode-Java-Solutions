d39e2e130ed7d4bc456ccfce11ba7a41a5f410be
Varun Upadhyay <varun.u28@gmail.com>
2024-02-17 08:33:11 -0800

    Update Furthest Building You Can Reach.java

--- Medium/Furthest Building You Can Reach.java
+++ Medium/Furthest Building You Can Reach.java
@@ -1,18 +1,20 @@
 class Solution {
-  public int furthestBuilding(int[] heights, int bricks, int ladders) {
-    PriorityQueue<Integer> pq = new PriorityQueue<>();
-    for (int i = 1; i < heights.length; i++) {
-      int diff = heights[i] - heights[i - 1];
-      if (diff > 0) {
-        pq.add(diff);
-        if (pq.size() > ladders) {
-          bricks -= pq.poll();
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 1; i < heights.length; i++) {
+            int diff = heights[i] - heights[i - 1];
+            if (diff > 0) {
+                pq.add(diff);
+                bricks -= diff;
+                if (bricks < 0) {
+                    if (ladders == 0) {
+                        return i - 1;
+                    }
+                    ladders--;
+                    bricks += pq.poll();
+                }
+            }
         }
-        if (bricks < 0) {
-          return i - 1;
-        }
-      }
+        return heights.length - 1;
     }
-    return heights.length - 1;
-  }
 }
