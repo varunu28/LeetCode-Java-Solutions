4ecc1187000ae9cd7d8eb20cd84265081c11ba5c
Varun Upadhyay <varun.u28@gmail.com>
2024-03-09 16:21:59 -0800

    Update Intersection of Two Arrays.java

--- Easy/Intersection of Two Arrays.java
+++ Easy/Intersection of Two Arrays.java
@@ -1,25 +1,8 @@
 class Solution {
-  public int[] intersection(int[] nums1, int[] nums2) {
-    boolean[][] found = new boolean[1001][2];
-    for (int num : nums1) {
-      found[num][0] = true;
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> set2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        set1.retainAll(set2);
+        return set1.stream().mapToInt(Integer::intValue).toArray();
     }
-    int count = 0;
-    for (int num : nums2) {
-      if (!found[num][1]) {
-        if (found[num][0]) {
-          count++;
-        }
-      }
-      found[num][1] = true;
-    }
-    int[] result = new int[count];
-    int idx = 0;
-    for (int i = 1; i <= 1000; i++) {
-      if (found[i][0] && found[i][1]) {
-        result[idx++] = i;
-      }
-    }
-    return result;
-  }
 }
