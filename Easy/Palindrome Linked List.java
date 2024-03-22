d7bef6088335dd3c0ab9ee007605c45acba13137
Varun Upadhyay <varun.u28@gmail.com>
2024-03-22 07:34:22 -0700

    Update Palindrome Linked List.java

--- Easy/Palindrome Linked List.java
+++ Easy/Palindrome Linked List.java
@@ -9,39 +9,35 @@
  * }
  */
 class Solution {
-  public boolean isPalindrome(ListNode head) {
-    if (head.next == null) {
-      return true;
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalf = slow;
+        secondHalf = reverse(secondHalf);
+        ListNode firstHalf = head;
+        while (secondHalf != null && firstHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
     }
-    ListNode slow = head;
-    ListNode fast = head;
-    ListNode prev = null;
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      fast = fast.next.next;
-      slow = slow.next;
+
+    private ListNode reverse(ListNode node) {
+        ListNode curr = node;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
-    ListNode revNode = reverse(slow);
-    while (revNode != null) {
-      if (revNode.val != head.val) {
-        return false;
-      }
-      revNode = revNode.next;
-      head = head.next;
-    }
-    return true;
-  }
-  
-  private ListNode reverse(ListNode node) {
-    ListNode prev = null;
-    ListNode curr = node;
-    ListNode next = node;
-    while (curr != null) {
-      next = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = next;
-    }
-    return prev;
-  }
 }
