1e074c2fc9157c916f40e745ce9bd6d9f9b73245
Varun Upadhyay <varun.u28@gmail.com>
2024-04-02 06:15:30 -0700

    Update Isomorphic Strings.java

--- Easy/Isomorphic Strings.java
+++ Easy/Isomorphic Strings.java
@@ -1,12 +1,18 @@
 class Solution {
-  public boolean isIsomorphic(String s, String t) {
-    int[] position = new int[512];
-    for (int i = 0; i < s.length(); i++) {
-      if (position[s.charAt(i)] != position[t.charAt(i) + 256]) {
-        return false;
-      }
-      position[s.charAt(i)] = position[t.charAt(i) + 256] = i + 1;
+    public boolean isIsomorphic(String s, String t) {
+        return mapString(s).equals(mapString(t));
     }
-    return true;
-  }
+
+    private static String mapString(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c)) {
+                map.put(c, count++);
+            }
+            sb.append(map.get(c)).append("|");
+        }
+        return sb.toString();
+    }
 }
