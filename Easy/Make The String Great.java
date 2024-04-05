0b9bea2ac20b3e7f0ac3debde30d3885926ac8ba
Varun Upadhyay <varun.u28@gmail.com>
2024-04-05 06:56:58 -0700

    Update Make The String Great.java

--- Easy/Make The String Great.java
+++ Easy/Make The String Great.java
@@ -1,18 +1,24 @@
 class Solution {
-  public String makeGood(String s) {
-    Stack<Character> stack = new Stack<>();
-    for (char c : s.toCharArray()) {
-      if (!stack.isEmpty() && Math.abs(stack.peek() - c) == 32) {
-        stack.pop();
-      }
-      else {
-        stack.push(c);
-      }
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && isBad(c, stack.peek())) {
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
-    StringBuilder sb = new StringBuilder();
-    while (!stack.isEmpty()) {
-      sb.append(stack.pop());
+
+    private static boolean isBad(char c1, char c2) {
+        if (c1 == c2) {
+            return false;
+        }
+        return Character.toLowerCase(c1) == Character.toLowerCase(c2);
     }
-    return sb.reverse().toString();
-  }
 }
