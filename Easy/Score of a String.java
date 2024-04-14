adeed10feaa5927ac9055ca0912690250abe5942
Varun Upadhyay <varun.u28@gmail.com>
2024-04-14 15:56:31 -0700

    Create Score of a String.java

---
+++ Easy/Score of a String.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 1; i < s.length(); i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i - 1));
+        }
+        return score;
+    }
+}
