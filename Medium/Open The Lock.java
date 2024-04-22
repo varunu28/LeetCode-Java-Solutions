e1fcc313d62a9e158251ff7c0a2c932e96116917
Varun Upadhyay <varun.u28@gmail.com>
2024-04-22 08:07:39 -0700

    Update Open The Lock.java

--- Medium/Open The Lock.java
+++ Medium/Open The Lock.java
@@ -1,40 +1,38 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
         Set<String> deadEndSet = new HashSet<>(Arrays.asList(deadends));
-        if (deadEndSet.contains(target) || deadEndSet.contains("0000")) {
+        String startingPoint = "0000";
+        if (deadEndSet.contains(startingPoint)) {
             return -1;
         }
         Queue<String> queue = new LinkedList<>();
-        Set<String> seen = new HashSet<>();
-        queue.add("0000");
-        seen.add("0000");
-        int steps = 0;
+        Set<String> visited = new HashSet<>();
+        queue.add(startingPoint);
+        visited.add(startingPoint);
+        int attempts = 0;
         int[] rotations = {-1, 1};
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 String removed = queue.remove();
                 if (removed.equals(target)) {
-                    return steps;
+                    return attempts;
                 }
                 if (deadEndSet.contains(removed)) {
                     continue;
                 }
                 for (int j = 0; j < 4; j++) {
                     for (int rotation : rotations) {
-                        int changedVal = ((removed.charAt(j) - '0') + rotation + 10) % 10;
-                        String newRotation = new StringBuilder()
-                                                    .append(removed.substring(0,j))                                                                                         .append(changedVal)
-                                                    .append(removed.substring(j + 1))
-                                                    .toString();
-                        if (!seen.contains(newRotation)) {
-                            seen.add(newRotation);
+                        int newValue = ((removed.charAt(j) - '0') + rotation + 10) % 10;
+                        String newRotation = removed.substring(0, j) + newValue + removed.substring(j + 1);
+                        if (!visited.contains(newRotation)) {
+                            visited.add(newRotation);
                             queue.add(newRotation);
                         }
                     }
                 }
             }
-            steps++;
+            attempts++;
         }
         return -1;
     }
