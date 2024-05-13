f93fb78f11158ccf99e8af4c60c0867d57dde076
Varun Upadhyay <varun.u28@gmail.com>
2024-05-13 07:14:12 -0700

    Update Score After Flipping Matrix.java

--- Medium/Score After Flipping Matrix.java
+++ Medium/Score After Flipping Matrix.java
@@ -1,59 +1,33 @@
 class Solution {
-    public int matrixScore(int[][] A) {
-        for (int i=0; i<A.length; i++) {
-            int actualSum = getDecimalValue(A[i], false);
-            int toggledSum = getDecimalValue(A[i], true);
-            
-            if (toggledSum > actualSum) {
-                for (int j=0; j<A[i].length; j++) {
-                    A[i][j] ^= 1;
+    public int matrixScore(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            if (grid[i][0] == 0) {
+                for (int j = 0; j < cols; j++) {
+                    grid[i][j] = 1 - grid[i][j];
                 }
             }
         }
-        
-        for (int i=0; i<A[0].length; i++) {
-            int numZeroes = 0;
-            int numOnes = 0;
-            
-            for (int j=0; j<A.length; j++) {
-                if (A[j][i] == 1 ) {
-                    numOnes++;
-                }
-                else {
-                    numZeroes++;
+        for (int j = 1; j < cols; j++) {
+            int countZero = 0;
+            for (int[] row : grid) {
+                if (row[j] == 0) {
+                    countZero++;
                 }
             }
-            
-            if (numZeroes > numOnes) {
-                for (int j=0; j<A.length; j++) {
-                    A[j][i] ^= 1;
+            if(countZero > rows-countZero) {
+                for(int i = 0; i < rows; i++) {
+                    grid[i][j] = 1 - grid[i][j];
                 }
             }
         }
-        
-        int maxSum = 0;
-        for (int[] arr : A) {
-            maxSum += getDecimalValue(arr, false);
-        }
-        
-        return maxSum;
-    }
-    
-    private int getDecimalValue(int[] arr, boolean isToggled) {
-        int sum = 0;
-        int pow = 0;
-        
-        for (int i=arr.length-1; i>=0; i--) {
-            if (isToggled) {
-                sum += Math.pow(2, pow) * (arr[i] ^ 1);
+        int score = 0;
+        for (int[] row : grid) {
+            for (int j = 0; j < cols; j++) {
+                score += row[j] << (cols - j - 1);
             }
-            else {
-                sum += Math.pow(2, pow) * arr[i];
-            }
-            
-            pow++;
         }
-        
-        return sum;
+        return score;
     }
 }
