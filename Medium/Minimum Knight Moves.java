fc9d892f1be05e0b508276ebfa9529de36b4dbdb
Varun Upadhyay <varun.u28@gmail.com>
2024-05-21 08:14:13 -0700

    Update Minimum Knight Moves.java

--- Medium/Minimum Knight Moves.java
+++ Medium/Minimum Knight Moves.java
@@ -1,30 +1,30 @@
 class Solution {
-  private final int[][] DIRS = {{1, 2}, {2, 1}, {1, -2}, {-2, 1}, {2, -1}, {-2, -1}, {-1, -2}, {-1, 2}};
-  
-  public int minKnightMoves(int x, int y) {
-    int numOfSteps = 0;
-    boolean[][] visited = new boolean[607][607];
-    Queue<int[]> queue = new LinkedList<>();
-    queue.add(new int[]{0, 0});
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      while (size-- > 0) {
-        int[] removed = queue.remove();
-        if (removed[0] == x && removed[1] == y) {
-          return numOfSteps;
+
+    private static final int[][] MOVES = {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+
+    public int minKnightMoves(int x, int y) {
+        boolean[][] visited = new boolean[607][607];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] removed = queue.remove();
+                if (removed[0] == x && removed[1] == y) {
+                    return steps;
+                }
+                for (int[] move : MOVES) {
+                    int newX = removed[0] + move[0];
+                    int newY = removed[1] + move[1];
+                    if (!visited[newX + 302][newY + 302]) {
+                        visited[newX + 302][newY + 302] = true;
+                        queue.add(new int[]{newX, newY});
+                    }
+                }
+            }
+            steps++;
         }
-        for (int[] dir : DIRS) {
-          int newX = removed[0] + dir[0];
-          int newY = removed[1] + dir[1];
-          if (visited[newX + 302][newY + 302]) {
-            continue;
-          }
-          visited[newX + 302][newY + 302] = true;
-          queue.add(new int[]{newX, newY});
-        }
-      }
-      numOfSteps++;
+        return steps;
     }
-    return numOfSteps;
-  }
 }
