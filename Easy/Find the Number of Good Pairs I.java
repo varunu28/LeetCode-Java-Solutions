f967067bddf85a35522c0246e0a85386d0514cb8
Varun Upadhyay <varun.u28@gmail.com>
2024-05-29 07:20:42 -0700

    Create Find the Number of Good Pairs I.java

---
+++ Easy/Find the Number of Good Pairs I.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int count = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] % (nums2[j] * k) == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
