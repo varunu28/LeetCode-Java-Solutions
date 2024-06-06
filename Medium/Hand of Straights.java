4b1a03b1fdf1cb4b387ee4ff9b60d649971e3ab2
Varun Upadhyay <varun.u28@gmail.com>
2024-06-06 07:16:35 -0700

    Update Hand of Straights.java

--- Medium/Hand of Straights.java
+++ Medium/Hand of Straights.java
@@ -1,29 +1,32 @@
 class Solution {
-  public boolean isNStraightHand(int[] hand, int W) {
-    if (hand.length % W != 0) {
-      return false;
-    }
-    Map<Integer, Integer> counterMap = new HashMap<>();
-    Set<Integer> set = new TreeSet<>();
-    for (int num : hand) {
-      counterMap.put(num, counterMap.getOrDefault(num, 0) + 1);
-      set.add(num);
-    }
-    Iterator<Integer> iter = set.iterator();
-    Integer curr = iter.next();
-    while (counterMap.get(curr) > 0) {
-      int temp = curr;
-      for (int i = 0; i < W; i++) {
-        if (counterMap.getOrDefault(temp, 0) == 0) {
-          return false;
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) {
+            return false;
         }
-        counterMap.put(temp, counterMap.get(temp) - 1);
-        temp++;
-      }
-      while (iter.hasNext() && counterMap.get(curr) == 0) {
-        curr = iter.next();
-      }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : hand) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(map.keySet());
+        while (!pq.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int prev = -1;
+            for (int i = 0; i < groupSize; i++) {
+                if (pq.isEmpty()) {
+                    return false;
+                }
+                int removed = pq.remove();
+                map.put(removed, map.get(removed) - 1);
+                if (map.get(removed) > 0) {
+                    temp.add(removed);
+                }
+                if (prev != -1 && removed != prev + 1) {
+                    return false;
+                }
+                prev = removed;
+            }
+            pq.addAll(temp);
+        }
+        return true;
     }
-    return true;
-  }
 }
