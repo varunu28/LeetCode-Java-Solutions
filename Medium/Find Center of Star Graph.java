f981b3283fbd6a9bd483a813cd1c502c3e27de67
Varun Upadhyay <varun.u28@gmail.com>
2024-06-27 06:32:40 -0700

    Update Find Center of Star Graph.java

--- Medium/Find Center of Star Graph.java
+++ Medium/Find Center of Star Graph.java
@@ -1,13 +1,16 @@
-import java.util.Map.Entry;
-
 class Solution {
-  public int findCenter(int[][] edges) {
-    Map<Integer, Set<Integer>> map = new HashMap<>();
-    for (int[] edge : edges) {
-      map.computeIfAbsent(edge[0], k -> new HashSet<>()).add(edge[1]);
-      map.computeIfAbsent(edge[1], k -> new HashSet<>()).add(edge[0]);
+    public int findCenter(int[][] edges) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = edges.length;
+        for (int[] edge : edges) {
+            map.put(edge[0], map.getOrDefault(edge[0], 0) + 1);
+            map.put(edge[1], map.getOrDefault(edge[1], 0) + 1);
+        }
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == n) {
+                return key;
+            }
+        }
+        return -1;
     }
-    return map.entrySet().stream().filter(entry -> entry.getValue().size() == edges.length).map(
-        Entry::getKey).findFirst().orElse(-1);
-  }
 }
