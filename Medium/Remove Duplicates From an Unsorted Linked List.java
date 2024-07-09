be7bd21bae156c80b5d304f095e4c1c7c3c1a9ba
Varun Upadhyay <varun.u28@gmail.com>
2024-07-09 16:31:31 -0700

    Update Remove Duplicates From an Unsorted Linked List.java

--- Medium/Remove Duplicates From an Unsorted Linked List.java
+++ Medium/Remove Duplicates From an Unsorted Linked List.java
@@ -9,31 +9,29 @@
  * }
  */
 class Solution {
-  public ListNode deleteDuplicatesUnsorted(ListNode head) {
-    Map<Integer, Integer> map = new HashMap<>();
-    ListNode curr = head;
-    while (curr != null) {
-      map.put(curr.val, map.getOrDefault(curr.val, 0) + 1);
-      curr = curr.next;
-    }
-    curr = head;
-    ListNode newHead = null;
-    ListNode prev = null;
-    while (curr != null) {
-      if (map.get(curr.val) > 1) {
-        ListNode nextNode = curr.next;
-        if (prev != null) {
-          prev.next = nextNode;
+    public ListNode deleteDuplicatesUnsorted(ListNode head) {
+        Map<Integer, Integer> map = new HashMap<>();
+        ListNode curr = head;
+        while (curr != null) {
+            map.put(curr.val, map.getOrDefault(curr.val, 0) + 1);
+            curr = curr.next;
         }
-        curr = nextNode;
-      } else {
-        if (newHead == null) {
-          newHead = curr;
+        curr = head;
+        ListNode prev = null;
+        ListNode newHead = null;
+        while (curr != null) {
+            if (map.get(curr.val) > 1) {
+                if (prev != null) {
+                    prev.next = curr.next;
+                }
+            } else {
+                prev = curr;
+                if (newHead == null) {
+                    newHead = curr;
+                }
+            }
+            curr = curr.next;
         }
-        prev = curr;
-        curr = curr.next;
-      }
+        return newHead;
     }
-    return newHead;
-  }
 }
