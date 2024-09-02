abc7fd46d0bd7d89fb9534dc4487f107a4293a21
Varun Upadhyay <varun.u28@gmail.com>
2024-09-01 17:39:57 -0700

    Update Find the Student that Will Replace the Chalk.java

--- Medium/Find the Student that Will Replace the Chalk.java
+++ Medium/Find the Student that Will Replace the Chalk.java
@@ -1,17 +1,23 @@
 class Solution {
-  public int chalkReplacer(int[] chalk, int k) {
-    int initialResult = chalkReplacerHelper(chalk, k);
-    return initialResult != -1 ? initialResult
-        : chalkReplacerHelper(chalk, k % Arrays.stream(chalk).sum());
-  }
-
-  private int chalkReplacerHelper(int[] chalk, int k) {
-    for (int i = 0; i < chalk.length; i++) {
-      if (k - chalk[i] < 0) {
-        return i;
-      }
-      k -= chalk[i];
+    public int chalkReplacer(int[] chalk, int k) {
+        int n = chalk.length;
+        long[] prefixSum = new long[n];
+        prefixSum[0] = chalk[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = chalk[i] + prefixSum[i - 1]; 
+        }
+        long total = prefixSum[n - 1];
+        long remaining = k % total;
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (prefixSum[mid] <= remaining) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
     }
-    return -1;
-  }
 }
