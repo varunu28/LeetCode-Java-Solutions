4137cce7391bef40a4fff4400dcf863c0115596f
Varun Upadhyay <varun.u28@gmail.com>
2024-09-04 17:30:29 -0700

    Update Buildings With an Ocean View.java

--- Medium/Buildings With an Ocean View.java
+++ Medium/Buildings With an Ocean View.java
@@ -1,13 +1,18 @@
 class Solution {
-  public int[] findBuildings(int[] heights) {
-    List<Integer> oceanViewBuildings = new ArrayList<>();
-    int maxHeight = 0;
-    for (int i = heights.length - 1; i >= 0; i--) {
-      if (heights[i] > maxHeight) {
-        oceanViewBuildings.add(i);
-      }
-      maxHeight = Math.max(maxHeight, heights[i]);
+    public int[] findBuildings(int[] heights) {
+        List<Integer> result = new ArrayList<>();
+        int maxHeight = -1;
+        for (int i = heights.length - 1; i >= 0; i--) {
+            if (heights[i] > maxHeight) {
+                result.add(i);
+            }
+            maxHeight = Math.max(maxHeight, heights[i]);
+        }
+        int[] ans = new int[result.size()];
+        Collections.reverse(result);
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
     }
-    return oceanViewBuildings.stream().sorted().mapToInt(Integer::valueOf).toArray();
-  }
 }
