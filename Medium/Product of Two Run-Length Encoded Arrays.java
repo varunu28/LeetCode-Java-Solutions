6f1feb4a469506b8555f0b0ef8f0e367535874ee
Varun Upadhyay <varun.u28@gmail.com>
2024-09-07 16:08:59 -0700

    Update Product of Two Run-Length Encoded Arrays.java

--- Medium/Product of Two Run-Length Encoded Arrays.java
+++ Medium/Product of Two Run-Length Encoded Arrays.java
@@ -1,39 +1,31 @@
 class Solution {
-  public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
-    int idxOne = 0;
-    int idxTwo = 0;
-    List<List<Integer>> result = new ArrayList<>();
-    while (idxOne < encoded1.length && idxTwo < encoded2.length) {
-      int valueOne = encoded1[idxOne][0];
-      int frequencyOne = 0;
-      while (idxOne < encoded1.length && encoded1[idxOne][0] == valueOne) {
-        frequencyOne += encoded1[idxOne++][1];
-      }
-      idxOne--;
-      encoded1[idxOne][1] = frequencyOne;
-      int valueTwo = encoded2[idxTwo][0];
-      int frequencyTwo = 0;
-      while (idxTwo < encoded2.length && encoded2[idxTwo][0] == valueTwo) {
-        frequencyTwo += encoded2[idxTwo++][1];
-      }
-      idxTwo--;
-      encoded2[idxTwo][1] = frequencyTwo;
-      int prod = encoded1[idxOne][0] * encoded2[idxTwo][0];
-      int frequency = Math.min(encoded1[idxOne][1], encoded2[idxTwo][1]);
-      if (result.size() > 0 && result.get(result.size() - 1).get(0) == prod) {
-        result.get(result.size() - 1).set(1, result.get(result.size() - 1).get(1) + frequency);
-      } else {
-        result.add(Arrays.asList(prod, frequency));
-      }
-      encoded1[idxOne][1] -= frequency;
-      encoded2[idxTwo][1] -= frequency;
-      if (encoded1[idxOne][1] == 0) {
-        idxOne++;
-      }
-      if (encoded2[idxTwo][1] == 0) {
-        idxTwo++;
-      }
+    public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
+        int idxOne = 0;
+        int idxTwo = 0;
+        List<List<Integer>> result = new ArrayList<>();
+        while (idxOne < encoded1.length && idxTwo < encoded2.length) {
+            int valOne = encoded1[idxOne][0];
+            int valTwo = encoded2[idxTwo][0];
+            int countOne = encoded1[idxOne][1];
+            int countTwo = encoded2[idxTwo][1];
+            int value = valOne * valTwo;
+            int count = Math.min(countOne, countTwo);
+            encoded1[idxOne][1] -= count;
+            encoded2[idxTwo][1] -= count;
+            if (encoded1[idxOne][1] == 0) {
+                idxOne++;
+            }
+            if (encoded2[idxTwo][1] == 0) {
+                idxTwo++;
+            }
+            if (!result.isEmpty() && result.get(result.size() - 1).get(0) == value) {
+                int existingCount = result.get(result.size() - 1).get(1);
+                int newCount = existingCount + count;
+                result.get(result.size() - 1).set(1, newCount);
+            } else {
+                result.add(Arrays.asList(value, count));
+            }
+        }
+        return result;
     }
-    return result;
-  }
 }
