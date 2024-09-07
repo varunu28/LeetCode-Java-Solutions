6a4ac34e8808f4eff237a7d4d4a95eb79d8e3a47
Varun Upadhyay <varun.u28@gmail.com>
2024-09-07 14:32:30 -0700

    Update Exclusive Time of Functions.java

--- Medium/Exclusive Time of Functions.java
+++ Medium/Exclusive Time of Functions.java
@@ -1,33 +1,25 @@
 class Solution {
-  public int[] exclusiveTime(int n, List<String> logs) {
-    int[] result = new int[n];
-    Stack<Log> stack = new Stack<>();
-    for (String log : logs) {
-      Log currentLog = new Log(log);
-      if (currentLog.isStart) {
-        stack.push(currentLog);
-      } else {
-        Log topLog = stack.pop();
-        result[topLog.id] += currentLog.time - topLog.time + 1 - topLog.overLappingTime;
-        if (!stack.isEmpty()) {
-          stack.peek().overLappingTime += currentLog.time - topLog.time + 1;
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[n];
+        int lastTimestamp = 0;
+        for (String log : logs) {
+            String[] split = log.split(":");
+            int id = Integer.parseInt(split[0]);
+            int timestamp = Integer.parseInt(split[2]);
+            if (split[1].equals("start")) {
+                // We consider timestamp as part of current computation 
+                if (!stack.isEmpty()) {
+                    result[stack.peek()] += timestamp - lastTimestamp;
+                }
+                stack.push(id);
+                lastTimestamp = timestamp;
+            } else {
+                // We consider timestamp as part of previous computation
+                result[stack.pop()] += timestamp - lastTimestamp + 1;
+                lastTimestamp = timestamp + 1;
+            }
         }
-      }
+        return result;
     }
-    return result;
-  }
-
-  private static class Log {
-    public int id;
-    public boolean isStart;
-    public int time;
-    public int overLappingTime;
-
-    public Log(String log) {
-      String[] split = log.split(":");
-      this.id = Integer.parseInt(split[0]);
-      this.isStart = split[1].equals("start");
-      this.time = Integer.parseInt(split[2]);
-    }
-  }
 }
