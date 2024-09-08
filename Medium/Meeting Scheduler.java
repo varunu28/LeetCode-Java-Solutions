56554ae1b5bbc84477ce970a0e70578697429c5b
Varun Upadhyay <varun.u28@gmail.com>
2024-09-08 12:08:47 -0700

    Update Meeting Scheduler.java

--- Medium/Meeting Scheduler.java
+++ Medium/Meeting Scheduler.java
@@ -1,21 +1,22 @@
 class Solution {
-  public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
-    Arrays.sort(slots1, Comparator.comparingInt(o -> o[0]));
-    Arrays.sort(slots2, Comparator.comparingInt(o -> o[0]));
-    int idxOne = 0;
-    int idxTwo = 0;
-    while (idxOne < slots1.length && idxTwo < slots2.length) {
-      int maxStart = Math.max(slots1[idxOne][0], slots2[idxTwo][0]);
-      int minEnd = Math.min(slots1[idxOne][1], slots2[idxTwo][1]);
-      if (minEnd - maxStart >= duration) {
-        return Arrays.asList(maxStart, maxStart + duration);
-      }
-      if (slots1[idxOne][1] > slots2[idxTwo][1]) {
-        idxTwo++;
-      } else {
-        idxOne++;
-      }
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        Arrays.sort(slots1, Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(slots2, Comparator.comparingInt(o -> o[0]));
+        int idxOne = 0;
+        int idxTwo = 0;
+        while (idxOne < slots1.length && idxTwo < slots2.length) {
+            int maxStart = Math.max(slots1[idxOne][0], slots2[idxTwo][0]);
+            int minEnd = Math.min(slots1[idxOne][1], slots2[idxTwo][1]);
+            if (minEnd - maxStart >= duration) {
+                return List.of(maxStart, maxStart + duration);
+            }
+            if (minEnd == slots1[idxOne][1]) {
+                idxOne++;
+            }
+            if (minEnd == slots2[idxTwo][1]) {
+                idxTwo++;
+            }
+        }
+        return Collections.emptyList();
     }
-    return new ArrayList<>();
-  }
 }
