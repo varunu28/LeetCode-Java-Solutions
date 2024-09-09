76879286a3f180f54da092d0425dd8377e9d3eac
Varun Upadhyay <varun.u28@gmail.com>
2024-09-09 08:48:50 -0700

    Update Largest BST Subtree.java

--- Medium/Largest BST Subtree.java
+++ Medium/Largest BST Subtree.java
@@ -4,43 +4,41 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public int largestBSTSubtree(TreeNode root) {
-        return largestBSTSubtreeHelper(root);
-    }
-    
-    private int largestBSTSubtreeHelper(TreeNode root) {
-        if (isBST(root)) {
-            return getNodeCount(root);
-        }
-        
-        return Math.max(-1, Math.max(largestBSTSubtreeHelper(root.left), largestBSTSubtreeHelper(root.right)));
-    }
-    
-    private boolean isBST(TreeNode root) {
-        return isBSTHelper(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
-    }
-    
-    private boolean isBSTHelper(TreeNode root, int max, int min) {
-        if (root == null) {
-            return true;
-        }
-        
-        if (root.val >= max || root.val <= min) {
-            return false;
-        }
-        
-        return isBSTHelper(root.left, root.val, min) && isBSTHelper(root.right, max, root.val);
-    }
-
-    private int getNodeCount(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        
-        return 1 + getNodeCount(root.left) + getNodeCount(root.right);
+        int[] isBstResult = isBst(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        if (isBstResult[0] == 1) {
+            return isBstResult[1];
+        }
+        return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
+    }
+
+    private int[] isBst(TreeNode root, int minValue, int maxValue) {
+        if (root == null) {
+            return new int[]{1, 0};
+        }
+        int[] result = new int[2];
+        if (!(root.val > minValue && root.val < maxValue)) {
+            return result;
+        }
+        int[] leftIsBst = isBst(root.left, minValue, root.val);
+        int[] rightIsBst = isBst(root.right, root.val, maxValue);
+        if (leftIsBst[0] == 1 && rightIsBst[0] == 1) {
+            result[0] = 1;
+            result[1] = 1 + leftIsBst[1] + rightIsBst[1];
+        }
+        return result;
     }
 }
