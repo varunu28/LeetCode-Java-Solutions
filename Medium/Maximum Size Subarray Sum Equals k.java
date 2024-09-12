f029595755aa34a81caae72c31ad1a629cfd9fda
Varun Upadhyay <varun.u28@gmail.com>
2024-09-12 08:30:42 -0700

    Update Maximum Size Subarray Sum Equals k.java

--- Medium/Maximum Size Subarray Sum Equals k.java
+++ Medium/Maximum Size Subarray Sum Equals k.java
@@ -1,18 +1,17 @@
 class Solution {
-  public int maxSubArrayLen(int[] nums, int k) {
-    Map<Integer, Integer> map = new HashMap<>();
-    int res = 0;
-    int sum = 0;
-    for (int i = 0; i < nums.length; i++) {
-      sum += nums[i];
-      if (sum == k) {
-        res = i + 1;
-      }
-      else if (map.containsKey(sum - k)) {
-        res = Math.max(res, i - map.get(sum - k));
-      }
-      map.putIfAbsent(sum, i);
+    public int maxSubArrayLen(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            if (prefixSum == k) {
+                max = i + 1;
+            } else if (map.containsKey(prefixSum - k)) {
+                max = Math.max(max, i - map.get(prefixSum - k));
+            }
+            map.putIfAbsent(prefixSum, i);
+        }
+        return max;
     }
-    return res;
-  }
 }
