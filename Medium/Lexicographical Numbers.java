c97bb9f4059847b67a7f53ca9fb9ee11ff385ab1
Varun Upadhyay <varun.u28@gmail.com>
2024-09-20 17:44:54 -0700

    Update Lexicographical Numbers.java

--- Medium/Lexicographical Numbers.java
+++ Medium/Lexicographical Numbers.java
@@ -1,19 +1,18 @@
 class Solution {
-  public List<Integer> lexicalOrder(int n) {
-    List<Integer> result = new ArrayList<>();
-    for (int i = 1; i < 10; i++) {
-      dfs(i, n, result);
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        int curr = 1;
+        for (int i = 0; i < n; i++) {
+            result.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else {
+                while (curr % 10 == 9 || curr == n) {
+                    curr /= 10;
+                }
+                curr++;
+            }
+        }
+        return result;
     }
-    return result;
-  }
-  
-  private void dfs(int curr, int n, List<Integer> result) {
-    if (curr > n) {
-      return;
-    }
-    result.add(curr);
-    for (int i = 0; i < 10; i++) {
-      dfs(10 * curr + i, n, result);
-    }
-  }
 }
