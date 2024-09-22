049b2483f54ff74ed04e872c54e272529c99ca8d
Varun Upadhyay <varun.u28@gmail.com>
2024-09-22 08:29:37 -0700

    Update Longest Word With All Prefixes.java

--- Medium/Longest Word With All Prefixes.java
+++ Medium/Longest Word With All Prefixes.java
@@ -1,53 +1,59 @@
 class Solution {
-  public String longestWord(String[] words) {
-    TrieNode root = new TrieNode();
-    root.isWord = true;
-    Map<Integer, PriorityQueue<String>> map = new TreeMap<>((o1, o2) -> o2 - o1);
-    for (String word : words) {
-      addToTrie(word, root);
-      map.computeIfAbsent(word.length(), k -> new PriorityQueue<>(String::compareTo)).add(word);
-    }
-    for (Integer key : map.keySet()) {
-      PriorityQueue<String> priorityQueue = map.get(key);
-      while (!priorityQueue.isEmpty()) {
-        String candidateWord = priorityQueue.poll();
-        if (wordContainAllPrefixes(candidateWord, root)) {
-          return candidateWord;
+    public String longestWord(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            root.insert(word);
         }
-      }
+        String longestValidWord = "";
+        for (String word : words) {
+            if (root.hasAllPrefixes(word)) {
+                if (word.length() > longestValidWord.length() || 
+                    (word.length() == longestValidWord.length() && word.compareTo(longestValidWord) < 0)) {
+                    longestValidWord = word;
+                }
+            }
+        }
+        return longestValidWord;
     }
-    return "";
-  }
 
-  private boolean wordContainAllPrefixes(String word, TrieNode root) {
-    TrieNode curr = root;
-    for (int i = 0; i < word.length(); i++) {
-      if (!curr.isWord) {
-        return false;
-      }
-      curr = curr.children.get(word.charAt(i));
+    static class TrieNode {
+        private final Map<Character, TrieNode> children;
+        private boolean isWord;
+
+        public TrieNode() {
+            this.children = new HashMap<>();
+        }
+
+        public void insert(String word) {
+            TrieNode curr = this;
+            for (char c : word.toCharArray()) {
+                curr = curr.getOrCreateChild(c);
+            }
+            curr.isWord = true;
+        }
+
+        public boolean hasAllPrefixes(String word) {
+            TrieNode curr = this;
+            for (char c : word.toCharArray()) {
+                TrieNode child = curr.children.getOrDefault(c, null);
+                if (child == null) {
+                    return false;
+                }       
+                if (!child.isWord) {
+                    return false;
+                }
+                curr = child;
+            }
+            return true;
+        }
+
+        private TrieNode getOrCreateChild(char c) {
+            if (children.containsKey(c)) {
+                return children.get(c);
+            }
+            TrieNode node = new TrieNode();
+            children.put(c, node);
+            return node;
+        }
     }
-    return curr.isWord;
-  }
-
-  private void addToTrie(String word, TrieNode root) {
-    TrieNode curr = root;
-    for (int i = 0; i < word.length(); i++) {
-      if (!curr.children.containsKey(word.charAt(i))) {
-        curr.children.put(word.charAt(i), new TrieNode());
-      }
-      curr = curr.children.get(word.charAt(i));
-    }
-    curr.isWord = true;
-  }
-
-  private static class TrieNode {
-    Map<Character, TrieNode> children;
-    boolean isWord;
-
-    public TrieNode() {
-      this.children = new HashMap<>();
-      this.isWord = false;
-    }
-  }
 }
