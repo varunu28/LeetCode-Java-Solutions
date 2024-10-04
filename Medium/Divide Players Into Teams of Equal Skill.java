650d084e89e1fe0d4ac6452d502fd280787e4f14
Varun Upadhyay <varun.u28@gmail.com>
2024-10-03 17:51:53 -0700

    Update Divide Players Into Teams of Equal Skill.java

--- Medium/Divide Players Into Teams of Equal Skill.java
+++ Medium/Divide Players Into Teams of Equal Skill.java
@@ -1,19 +1,25 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        Arrays.sort(skill);
+        int n = skill.length;
+        int totalSkill = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int s : skill) {
+            totalSkill += s;
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        if (totalSkill % (n / 2) != 0) {
+            return -1;
+        }
+        int targetSkill = totalSkill / (n / 2);
         long totalChemistry = 0;
-        int start = 0;
-        int end = skill.length - 1;
-        int totalSkill = skill[start] + skill[end];
-        while (start < end) {
-            int currTotal = skill[start] + skill[end];
-            if (currTotal != totalSkill) {
+        for (int currSkill : map.keySet()) {
+            int partnerSkill = targetSkill - currSkill;
+            int currSkillFreq = map.get(currSkill);
+            if (!map.containsKey(partnerSkill) || currSkillFreq != map.get(partnerSkill)) {
                 return -1;
             }
-            totalChemistry += skill[start] * skill[end];
-            start++;
-            end--;
+            totalChemistry += (long) currSkill * (long) partnerSkill * (long) currSkillFreq;
         }
-        return totalChemistry;
+        return totalChemistry / 2;
     }
 }
