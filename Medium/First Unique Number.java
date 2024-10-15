d69d6fa23d5303c23a6bb1e7882f71cc5a3faccf
Varun Upadhyay <varun.u28@gmail.com>
2024-10-15 07:54:33 -0700

    Update First Unique Number.java

--- Medium/First Unique Number.java
+++ Medium/First Unique Number.java
@@ -1,68 +1,27 @@
 class FirstUnique {
-  
-  private Node head;
-  private Node tail;
-  private Map<Integer, Node> map;
-  
-  public FirstUnique(int[] nums) {
-    this.head = new Node(-1);
-    this.tail = new Node(-1);
-    this.head.next = this.tail;
-    this.tail.prev = this.head;
-    this.map = new HashMap<>();
-    for (int num : nums) {
-      if (map.containsKey(num)) {
-        deleteNode(map.get(num));
-      } else {
-        Node node = new Node(num);
-        map.put(num, node);
-        addToTail(node);
-      }
-    }
-  }
 
-  public int showFirstUnique() {
-    return this.head.next.val;
-  }
+    private Map<Integer, Integer> map;
+    private Queue<Integer> queue;
 
-  public void add(int value) {
-    if (map.containsKey(value)) {
-      deleteNode(map.get(value));
-      return;
+    public FirstUnique(int[] nums) {
+        map = new HashMap<>();
+        queue = new LinkedList<>();
+        for (int num : nums) {
+            add(num);
+        }
     }
-    Node node = new Node(value);
-    map.put(value, node);
-    addToTail(node);
-  }
-  
-  private void addToTail(Node node) {
-    Node prevToTail = this.tail.prev;
-    prevToTail.next = node;
-    node.next = this.tail;
-    node.prev = prevToTail;
-    this.tail.prev = node;
-  }
-  
-  private void deleteNode(Node node) {
-    if (node == null) {
-      return;
-    }
-    map.put(node.val, null);
-    Node prevToNode = node.prev;
-    Node nextToNode = node.next;
-    prevToNode.next = nextToNode;
-    nextToNode.prev = prevToNode;
-  }
-  
-  private static class Node {
-    int val;
-    Node next;
-    Node prev;
     
-    public Node(int val) {
-      this.val = val;
+    public int showFirstUnique() {
+        while (!queue.isEmpty() && map.get(queue.peek()) > 1) {
+            queue.remove();
+        }
+        return queue.isEmpty() ? -1 : queue.peek();
     }
-  }
+    
+    public void add(int value) {
+        queue.add(value);
+        map.put(value, map.getOrDefault(value, 0) + 1);
+    }
 }
 
 /**
