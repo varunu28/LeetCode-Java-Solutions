45105d882a6f2b03e88fc2198a54990a1b91e620
Varun Upadhyay <varun.u28@gmail.com>
2024-10-17 07:04:29 -0700

    Update Maximum Swap.java

--- Medium/Maximum Swap.java
+++ Medium/Maximum Swap.java
@@ -1,29 +1,20 @@
 class Solution {
-  public int maximumSwap(int num) {
-    String stringValue = Integer.toString(num);
-    Map<Integer, Integer> valToIndexMap = new HashMap<>();
-    int[] digits = new int[String.valueOf(num).length()];
-    for (int i = digits.length - 1; i >= 0; i--) {
-      int digit = num % 10;
-      num /= 10;
-      digits[i] = digit;
-      valToIndexMap.putIfAbsent(digit, i);
-    }
-    for (int i = 0; i < digits.length; i++) {
-      for (int k = 9; k > digits[i]; k--) {
-        if (valToIndexMap.getOrDefault(k, -1) > i) {
-          int swapIndex = valToIndexMap.get(k);
-          return Integer.parseInt(
-              stringValue.substring(0, i) // Digits before swap index
-                  + k // Swapped value
-                  + stringValue.substring(i + 1, swapIndex) // Digits after original index(i) and before swappedIndex
-                  + digits[i] // Digit at original index(i)
-                  + ((swapIndex + 1) != stringValue.length() // Check if swapIndex is last digit of num
-                      ? stringValue.substring(swapIndex + 1) // If not then add digits that come after the swapIndex
-                      : "")); // Else add an empty string
+    public int maximumSwap(int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        int n = digits.length;
+        int[] maxRightIdx = new int[n];
+        maxRightIdx[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            maxRightIdx[i] = (digits[i] > digits[maxRightIdx[i + 1]]) ? i : maxRightIdx[i + 1];
         }
-      }
+        for (int i = 0; i < n; i++) {
+            if (digits[i] < digits[maxRightIdx[i]]) {
+                char temp = digits[i];
+                digits[i] = digits[maxRightIdx[i]];
+                digits[maxRightIdx[i]] = temp;
+                return Integer.parseInt(String.valueOf(digits));
+            }
+        }
+        return num;
     }
-    return Integer.parseInt(stringValue);
-  }
 }
