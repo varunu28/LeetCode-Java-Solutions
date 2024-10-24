2dea3e54c58e5a45993f084c75aca141c5ca978f
Varun Upadhyay <varun.u28@gmail.com>
2024-10-24 07:18:31 -0700

    Update Flip Equivalent Binary Trees.java

--- Medium/Flip Equivalent Binary Trees.java
+++ Medium/Flip Equivalent Binary Trees.java
@@ -1,31 +1,31 @@
-i/**
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-    if (root1 == null && root2 == null) {
-      return true;
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        boolean noSwap = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        boolean swap = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+        return noSwap || swap;
     }
-    if (root1 == null || root2 == null) {
-      return false;
-    }
-    if (root1.val != root2.val) {
-      return false;
-    }
-    return (
-        (
-          flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
-        ) || 
-        (
-          flipEquiv(root1.left, root2.right) && 
-          flipEquiv(root1.right, root2.left)
-        )
-      );
-  }
 }
