0298b447e75aa7ceedb837d55c3790ac0f7073b8
Varun Upadhyay <varun.u28@gmail.com>
2025-01-14 06:41:46 -0800

    Update Find the Prefix Common Array of Two Arrays.java

--- Medium/Find the Prefix Common Array of Two Arrays.java
+++ Medium/Find the Prefix Common Array of Two Arrays.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
-        int[] result = new int[A.length];
-        for (int i = 0; i < A.length; i++) {
-            map.put(A[i], map.getOrDefault(A[i], 0) + 1);
-            map.put(B[i], map.getOrDefault(B[i], 0) + 1);
-            count += map.get(A[i]) == 2 ? 1 : 0;
-            if (A[i] != B[i]) {
-                count += map.get(B[i]) == 2 ? 1 : 0;
+        int n = A.length;
+        int[] result = new int[n];
+        int[] frequency = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            frequency[A[i]]++;
+            if (frequency[A[i]] == 2) {
+                count++;
+            }
+            frequency[B[i]]++;
+            if (frequency[B[i]] == 2) {
+                count++;
             }
             result[i] = count;
         }
