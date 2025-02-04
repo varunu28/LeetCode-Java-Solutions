c259c7ab846d08dcc0fc9d3cd4d07ac1c901a2e6
Varun Upadhyay <varun.u28@gmail.com>
2025-02-03 17:35:11 -0800

    Update Maximum Ascending Subarray Sum.java

--- Easy/Maximum Ascending Subarray Sum.java
+++ Easy/Maximum Ascending Subarray Sum.java
@@ -1,14 +1,15 @@
 class Solution {
-  public int maxAscendingSum(int[] nums) {
-    int maximumSum = 0;
-    int idx = 0;
-    while (idx < nums.length) {
-      int currSum = nums[idx++];
-      while (idx < nums.length && nums[idx] > nums[idx - 1]) {
-        currSum += nums[idx++];
-      }
-      maximumSum = Math.max(maximumSum, currSum);
+    public int maxAscendingSum(int[] nums) {
+        int maxSum = 0;
+        int idx = 0;
+        int n = nums.length;
+        while (idx < n) {
+            int currSum = nums[idx++];
+            while (idx < n && nums[idx] > nums[idx - 1]) {
+                currSum += nums[idx++];
+            }
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
     }
-    return maximumSum;
-  }
 }
