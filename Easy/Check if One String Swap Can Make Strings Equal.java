d9a05d959355c1e27adb0d404872b4d3bafe5763
Varun Upadhyay <varun.u28@gmail.com>
2025-02-05 07:54:25 -0800

    Update Check if One String Swap Can Make Strings Equal.java

--- Easy/Check if One String Swap Can Make Strings Equal.java
+++ Easy/Check if One String Swap Can Make Strings Equal.java
@@ -1,16 +1,21 @@
 class Solution {
-  public boolean areAlmostEqual(String s1, String s2) {
-    int mismatchIdx = -1;
-    for (int i = 0; i < s1.length(); i++) {
-      if (s1.charAt(i) != s2.charAt(i)) {
-        if (mismatchIdx != -1) {
-          return s1.charAt(mismatchIdx) == s2.charAt(i) && 
-            s1.charAt(i) == s2.charAt(mismatchIdx) && 
-            s1.substring(i + 1).equals(s2.substring(i + 1));
+    public boolean areAlmostEqual(String s1, String s2) {
+        int swapIdx = -1;
+        boolean swapped = false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (swapped) {
+                    return false;
+                }
+                if (swapIdx != -1) {
+                    if (!(s1.charAt(swapIdx) == s2.charAt(i) && s1.charAt(i) == s2.charAt(swapIdx))) {
+                        return false;
+                    }
+                    swapped = true;
+                } 
+                swapIdx = i;
+            }
         }
-        mismatchIdx = i;
-      }
+        return swapIdx == -1 || (swapIdx != -1 && swapped);
     }
-    return mismatchIdx == -1;
-  }
 }
