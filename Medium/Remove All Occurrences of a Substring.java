583c210c39e1ecb62cde8460bcd55f23f2123f66
Varun Upadhyay <varun.u28@gmail.com>
2025-02-11 07:07:35 -0800

    Update Remove All Occurrences of a Substring.java

--- Medium/Remove All Occurrences of a Substring.java
+++ Medium/Remove All Occurrences of a Substring.java
@@ -1,12 +1,32 @@
 class Solution {
-  public String removeOccurrences(String s, String part) {
-    while (true) {
-      int idx = s.indexOf(part);
-      if (idx == -1) {
-        break;
-      }
-      s = s.substring(0, idx) + s.substring(idx + part.length());
+    public String removeOccurrences(String s, String part) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        int partLength = part.length();
+        for (int i = 0; i < n; i++) {
+            stack.push(s.charAt(i));
+            if (stack.size() >= partLength && check(stack, part, partLength)) {
+                for (int j = 0; j < partLength; j++) {
+                    stack.pop();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
-    return s;
-  }
+
+    private boolean check(Stack<Character> stack, String part, int partLength) {
+        Stack<Character> temp = new Stack<>();
+        temp.addAll(stack);
+        for (int i = partLength - 1; i >= 0; i--) {
+            if (temp.isEmpty() || temp.peek() != part.charAt(i)) {
+                return false;
+            }
+            temp.pop();
+        }
+        return true;
+    }
 }
