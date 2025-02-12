2daac9d705fcc126adceb388263b9c526b9961c3
Varun Upadhyay <varun.u28@gmail.com>
2025-02-11 17:29:01 -0800

    Update Max Sum of a Pair With Equal Sum of Digits.java

--- Medium/Max Sum of a Pair With Equal Sum of Digits.java
+++ Medium/Max Sum of a Pair With Equal Sum of Digits.java
@@ -1,25 +1,25 @@
 class Solution {
-  public int maximumSum(int[] nums) {
-    int maxSum = -1;
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int num : nums) {
-      int digitSum = getDigitSum(num);
-      if (map.containsKey(digitSum)) {
-        maxSum = Math.max(maxSum, map.get(digitSum) + num);
-        map.put(digitSum, Math.max(map.get(digitSum), num));
-      } else {
-        map.put(digitSum, num);
-      }
+    public int maximumSum(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxSum = -1;
+        for (int num : nums) {
+            int digitSum = calculateDigitSum(num);
+            if (map.containsKey(digitSum)) {
+                maxSum = Math.max(maxSum, map.get(digitSum) + num);
+                map.put(digitSum, Math.max(map.get(digitSum), num));
+            } else {
+                map.put(digitSum, num);
+            }
+        }
+        return maxSum;
     }
-    return maxSum;
-  }
-  
-  private int getDigitSum(int num) {
-    int sum = 0;
-    while (num > 0) {
-      sum += num % 10;
-      num /= 10;
+
+    private int calculateDigitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
-    return sum;
-  }
 }
