04175b684ba89a52fedb9b8a5ccd9989c2a21c95
Varun Upadhyay <varun.u28@gmail.com>
2025-02-19 07:32:27 -0800

    Create Construct Smallest Number From DI String.java

---
+++ Medium/Construct Smallest Number From DI String.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String smallestNumber(String pattern) {
+        StringBuilder result = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <= pattern.length(); i++) {
+            stack.push(i + 1);
+            if (i == pattern.length() || pattern.charAt(i) == 'I') {
+                while (!stack.isEmpty()) {
+                    result.append(stack.pop());
+                }
+            }
+        }
+        return result.toString();
+    }
+}
