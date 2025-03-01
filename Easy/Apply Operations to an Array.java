88ecb654b06441c1414888c2b0994fa092cd693e
Varun Upadhyay <varun.u28@gmail.com>
2025-03-01 09:10:57 -0800

    Update Apply Operations to an Array.java

--- Easy/Apply Operations to an Array.java
+++ Easy/Apply Operations to an Array.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
             if (nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
         }
-        int startIdx = 0;
-        int endIdx = 0;
-        while (endIdx < nums.length) {
-            if (nums[endIdx] != 0) {
-                nums[startIdx++] = nums[endIdx];
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[start++] = nums[i];
             }
-            endIdx++;
         }
-        while (startIdx < nums.length) {
-            nums[startIdx++] = 0;
+        while (start < n) {
+            nums[start++] = 0;
         }
         return nums;
     }
