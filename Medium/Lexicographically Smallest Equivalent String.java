b962419d92bbd2d824f9c3a624416d0ff4ed6ff6
Varun Upadhyay <varun.u28@gmail.com>
2025-06-05 06:20:55 -0700

    Update Lexicographically Smallest Equivalent String.java

--- Medium/Lexicographically Smallest Equivalent String.java
+++ Medium/Lexicographically Smallest Equivalent String.java
@@ -1,31 +1,46 @@
 class Solution {
-  public String smallestEquivalentString(String s1, String s2, String baseStr) {
-    int[] graph = new int[26];
-    for (int i = 0; i < 26; i++) {
-      graph[i] = i;
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        UnionFind unionFind = new UnionFind(26);
+        for (int i = 0; i < s1.length(); i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            unionFind.union(c1 - 'a', c2 - 'a');
+        }
+        StringBuilder result = new StringBuilder();
+        for (char c : baseStr.toCharArray()) {
+            result.append((char) (unionFind.find(c - 'a') + 'a'));
+        }
+        return result.toString();
     }
-    for (int i = 0; i < s1.length(); i++) {
-      int idxOne = s1.charAt(i) - 'a';
-      int idxTwo = s2.charAt(i) - 'a';
-      int parentOne = find(graph, idxOne);
-      int parentTwo = find(graph, idxTwo);
-      if(parentOne < parentTwo) {
-        graph[parentTwo] = parentOne;
-      } else {
-        graph[parentOne] = parentTwo;
-      }
+
+    class UnionFind {
+        private int[] parent;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int node) {
+            if (parent[node] == node) {
+                return node;
+            }
+            parent[node] = find(parent[node]);
+            return parent[node];
+        }
+
+        public void union(int nodeOne, int nodeTwo) {
+            int parentOne = find(nodeOne);
+            int parentTwo = find(nodeTwo);
+            if (parentOne != parentTwo) {
+                if (parentOne < parentTwo) {
+                    parent[parentTwo] = parentOne;
+                } else {
+                    parent[parentOne] = parentTwo;
+                }
+            }
+        }
     }
-    StringBuilder sb = new StringBuilder();
-    for (char c : baseStr.toCharArray()) {
-      sb.append((char) ('a' + find(graph, c - 'a')));
-    }
-    return sb.toString();
-  }
-  
-  private int find(int[] graph, int idx) {
-    while(graph[idx] != idx) {
-      idx = graph[idx];
-    }
-    return idx;
-  }
 }
