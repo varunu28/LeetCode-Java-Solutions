af947dedc140a0d23999f4117795d8c533eb84be
Varun Upadhyay <varun.u28@gmail.com>
2025-07-23 07:54:19 -0700

    Update Maximum Score From Removing Substrings.java

--- Medium/Maximum Score From Removing Substrings.java
+++ Medium/Maximum Score From Removing Substrings.java
@@ -1,29 +1,35 @@
 class Solution {
     public int maximumGain(String s, int x, int y) {
-        int score = 0;
-        String firstPass = removeSubstring(s, x > y ? "ab" : "ba");
-        int removeCount = (s.length() - firstPass.length()) / 2;
-        score += removeCount * Math.max(x, y);
-        String secondPass = removeSubstring(firstPass, x > y ? "ba" : "ab");
-        removeCount = (firstPass.length() - secondPass.length()) / 2;
-        score += removeCount * Math.min(x, y);
-        return score;
-    }
-
-    private String removeSubstring(String s, String target) {
-        Stack<Character> stack = new Stack<>();
+        // Swap if x < y so we can follow a single iteration
+        if (x < y) {
+            int temp = x;
+            x = y;
+            y = temp;
+            s = new StringBuilder(s).reverse().toString();
+        }
+        int countA = 0;
+        int countB = 0;
+        int total = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == target.charAt(1) && !stack.isEmpty() && stack.peek() == target.charAt(0)) {
-                stack.pop();
+            if (c == 'a') {
+                countA++;
+            } else if (c == 'b') {
+                // Make a pair to consume the excess 'a' count
+                if (countA > 0) {
+                    countA--;
+                    total += x;
+                } else {
+                    countB++;
+                }
             } else {
-                stack.push(c);
+                // Consume any possible number of pairs & reset the counter
+                total += Math.min(countA, countB) * y;
+                countA = 0;
+                countB = 0;
             }
         }
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        return sb.reverse().toString();
+        total += Math.min(countA, countB) * y;
+        return total;
     }
 }
