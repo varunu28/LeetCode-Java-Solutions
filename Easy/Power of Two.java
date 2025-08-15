67f26f9de6e94c56da713e37719603cd9fadf7ca
Varun Upadhyay <varun.u28@gmail.com>
2025-08-15 07:08:01 -0700

    Update Power of Two.java

--- Easy/Power of Two.java
+++ Easy/Power of Two.java
@@ -1,21 +1,9 @@
 class Solution {
-  public boolean isPowerOfTwo(int n) {
-    if (n > 1 && n % 2 != 0) {
-      return false;
+    public boolean isPowerOfTwo(int n) {
+        // Check 1: n is positive
+        // Check 2: If n is a power of 2 then only one bit will be set for n. Eg 4 -> 100, 16 -> 10000
+        // Now if we calculate bit representation of n - 1, all the bits will be flipped. Eg 3 -> 001, 15 -> 0111
+        // Performing & operation on n & n - 1 should result in 0 as there won't be any index in both bit representations which will be set
+        return n > 0 && (n & (n - 1)) == 0;
     }
-    int start = 0;
-    int end = n / 2;
-    while (start <= end) {
-      int mid = (start + end) / 2;
-      int pow = (int) Math.pow(2, mid);
-      if (pow == n) {
-        return true;
-      } else if (pow > n) {
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
-    }
-    return false;
-  }
 }
