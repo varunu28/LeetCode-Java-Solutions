a7078ca921fbd9f5e42e7eb2926da2a72a0282b3
Varun Upadhyay <varun.u28@gmail.com>
2025-12-09 06:51:40 -0800

    Update Insertion Sort List.java

--- Medium/Insertion Sort List.java
+++ Medium/Insertion Sort List.java
@@ -9,21 +9,28 @@
  * }
  */
 class Solution {
-  public ListNode insertionSortList(ListNode head) {
-    ListNode dummy = new ListNode(0);
-    ListNode curr = head;
-    ListNode prev = dummy;
-    ListNode next = null;
-    while (curr != null) {
-      next = curr.next;
-      while (prev.next != null && prev.next.val < curr.val) {
-        prev = prev.next;
-      }
-      curr.next = prev.next;
-      prev.next = curr;
-      prev = dummy;
-      curr = next;
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = head;
+        // Used for inserting in the right position in sorted list
+        ListNode prev = dummy;
+        ListNode next = null;
+        while (curr != null) {
+            // Traversal #1
+            next = curr.next;
+            // This iteration finds the correct insertion order in new sorted list
+            while (prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+            // Update the pointers to set the right position
+            curr.next = prev.next;
+            prev.next = curr;
+            // Reset back to dummy so it can be used in next iteration
+            prev = dummy;
+            // Traversal #2
+            curr = next;
+        }
+        // Return the node for sorted list
+        return dummy.next;
     }
-    return dummy.next; 
-  }
 }
