608da4294828c80c722940d774162d06594ce886
Varun Upadhyay <varun.u28@gmail.com>
2026-01-29 14:33:16 -0800

    Update Word Break.java

--- Medium/Word Break.java
+++ Medium/Word Break.java
@@ -1,23 +1,42 @@
 class Solution {
-  public boolean wordBreak(String s, List<String> wordDict) {
-    Set<String> set = new HashSet<>(wordDict);
-    Queue<Integer> queue = new LinkedList<>();
-    int[] visited = new int[s.length()];
-    queue.add(0);
-    while (!queue.isEmpty()) {
-      int start = queue.remove();
-      if (visited[start] == 0) {
-        for (int end = start + 1; end <= s.length(); end++) {
-          if (set.contains(s.substring(start, end))) {
-            queue.add(end);
-            if (end == s.length()) {
-              return true;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        TrieNode root = new TrieNode();
+        for (String word : wordDict) {
+            TrieNode curr = root;
+            for (char c : word.toCharArray()) {
+                if (!curr.children.containsKey(c)) {
+                    curr.children.put(c, new TrieNode());
+                }
+                curr = curr.children.get(c);
             }
-          }
+            curr.isWord = true;
         }
-        visited[start] = 1;
-      }
+        boolean[] isWord = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            // If it is either first character or the previous character was ending of a word
+            if (i == 0 || isWord[i - 1]) {
+                TrieNode curr = root;
+                for (int j = i; j < s.length(); j++) {
+                    char c = s.charAt(j);
+                    if (!curr.children.containsKey(c)) {
+                        break;
+                    }
+                    curr = curr.children.get(c);
+                    if (curr.isWord) {
+                        isWord[j] = true;
+                    }
+                }
+            }
+        }
+        return isWord[s.length() - 1];
     }
-    return false;
-  }   
+
+    class TrieNode {
+        boolean isWord;
+        Map<Character, TrieNode> children;
+
+        public TrieNode() {
+            this.children = new HashMap<>();
+        }
+    }
 }
