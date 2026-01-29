0ccf387b20169b5e4c96b1c22252732a49646a55
Varun Upadhyay <varun.u28@gmail.com>
2026-01-28 19:31:13 -0800

    Update Minimum Absolute Difference.java

--- Easy/Minimum Absolute Difference.java
+++ Easy/Minimum Absolute Difference.java
@@ -1,16 +1,33 @@
 class Solution {
-  public List<List<Integer>> minimumAbsDifference(int[] arr) {
-    int minDiff = Integer.MAX_VALUE;
-    Arrays.sort(arr);
-    for (int i = 0; i < arr.length - 1; i++) {
-      minDiff = Math.min(minDiff, arr[i + 1] - arr[i]);
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        int minValue = arr[0];
+        int maxValue = arr[0];
+        for (int num : arr) {
+            minValue = Math.min(minValue, num);
+            maxValue = Math.max(maxValue, num);
+        }
+        // This is done so that smallest element in the array points to index 0
+        int shift = -minValue;
+        int[] line = new int[maxValue - minValue + 1];
+        for (int num : arr) {
+            line[num + shift] = 1;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        int minPairDiff = maxValue - minValue;
+        int prev = 0;
+        for (int curr = 1; curr <= maxValue + shift; curr++) {
+            if (line[curr] == 0) {
+                continue;
+            }
+            if (curr - prev == minPairDiff) {
+                result.add(List.of(prev - shift, curr - shift));
+            } else if (curr - prev < minPairDiff) {
+                minPairDiff = curr - prev;
+                result.clear();
+                result.add(List.of(prev - shift, curr - shift));
+            }
+            prev = curr;
+        }
+        return result;
     }
-    List<List<Integer>> result = new ArrayList<>();
-    for (int i = 0; i < arr.length - 1; i++) {
-      if (arr[i + 1] - arr[i] == minDiff) {
-        result.add(Arrays.asList(arr[i], arr[i + 1]));        
-      }
-    }
-    return result;
-  }
 }
