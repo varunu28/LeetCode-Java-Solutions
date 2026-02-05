90ef33a626dc68c6194723be1625a3de7c8871e9
Varun Upadhyay <varun.u28@gmail.com>
2026-02-05 08:20:35 -0800

    Update Search in Rotated Sorted Array.java

--- Medium/Search in Rotated Sorted Array.java
+++ Medium/Search in Rotated Sorted Array.java
@@ -1,29 +1,30 @@
 class Solution {
-  public int search(int[] nums, int target) {
-    int start = 0;
-    int end = nums.length - 1;
-    while (start <= end) {
-      int mid = (start + end) / 2;
-      if (nums[mid] == target) {
-        return mid;
-      }
-      if (nums[mid] >= nums[start]) {
-        if (nums[start] <= target && nums[mid] > target) {
-            end = mid - 1;
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            // Is left half sorted?
+            if (nums[start] <= nums[mid]) {
+                // Is target present in left half?
+                if (nums[start] <= target && nums[mid] >= target) {
+                    end = mid - 1; // Eliminate right half
+                } else {
+                    start = mid + 1; // Eliminate left half
+                }
+            } else {
+                // Is target present in right half?
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid + 1; // Eliminate left half
+                } else {
+                    end = mid - 1; // Eliminate right half
+                }
+            }
         }
-        else {
-            start = mid + 1;
-        }
-      }
-      else {
-        if (nums[end] >= target && nums[mid] < target) {
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
-      }
+        return -1;
     }
-    return -1;
-  }
 }
