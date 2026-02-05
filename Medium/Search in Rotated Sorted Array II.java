80ac6fa00ae52ea2731ca3418648a8a1b3ecefe2
Varun Upadhyay <varun.u28@gmail.com>
2026-02-05 08:29:28 -0800

    Update Search in Rotated Sorted Array II.java

--- Medium/Search in Rotated Sorted Array II.java
+++ Medium/Search in Rotated Sorted Array II.java
@@ -1,26 +1,34 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        int n = nums.length;
         int start = 0;
-        int end = nums.length - 1;
+        int end = n - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (nums[mid] == target) {
                 return true;
+            } 
+            // If we cannot detect sorted side then shrink the search space
+            if (nums[start] == nums[end] && nums[start] == nums[mid]) {
+                start++;
+                end--;
+                continue;
             }
-            if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
+            // Is left half sorted?
+            if (nums[start] <= nums[mid]) {
+                // Is target present in left half?
+                if (nums[start] <= target && nums[mid] >= target) {
+                    end = mid - 1; // Eliminate right half
                 } else {
-                    end = mid - 1;
-                }
-            } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
-                if (target < nums[mid] && target >= nums[start]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
+                    start = mid + 1; // Eliminate left half
                 }
             } else {
-                end--;
+                // Is target present in right half?
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid + 1; // Eliminate left half
+                } else {
+                    end = mid - 1; // Eliminate right half
+                }
             }
         }
         return false;
