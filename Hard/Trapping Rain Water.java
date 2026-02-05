95c95bb2c64499e32d815e4306fb09ffd36a4e84
Varun Upadhyay <varun.u28@gmail.com>
2026-02-05 15:49:05 -0800

    Update Trapping Rain Water.java

--- Hard/Trapping Rain Water.java
+++ Hard/Trapping Rain Water.java
@@ -1,22 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        if (height.length == 0) {
-            return 0;
-        }
         int n = height.length;
-        int[] leftMax = new int[n];
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
-        }
-        int[] rightMax = new int[n];
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-        }
         int result = 0;
-        for (int i = 1; i < n - 1; i++) {
-            result += Math.min(leftMax[i], rightMax[i]) - height[i];
+        int leftIdx = 0;
+        int rightIdx = n - 1;
+        int leftMax = height[leftIdx];
+        int rightMax = height[rightIdx];
+        while (leftIdx < rightIdx) {
+            if (leftMax <= rightMax) {
+                leftIdx++;
+                leftMax = Math.max(leftMax, height[leftIdx]);
+                result += leftMax - height[leftIdx];
+            } else {
+                rightIdx--;
+                rightMax = Math.max(rightMax, height[rightIdx]);
+                result += rightMax - height[rightIdx];
+            }
         }
         return result;
     }
