c4e1be364a7d836bab06b3e84e7332b0a9833629
Varun Upadhyay <varun.u28@gmail.com>
2026-02-08 10:27:34 -0800

    Update Brace Expansion.java

--- Medium/Brace Expansion.java
+++ Medium/Brace Expansion.java
@@ -1,47 +1,40 @@
 class Solution {
-  public String[] expand(String s) {
-    List<List<String>> splits = parseInput(s);
-    List<String> result = new ArrayList<>();
-    StringBuilder sb = new StringBuilder();
-    helper(splits, result, sb, 0);
-    Collections.sort(result);
-    String[] answer = new String[result.size()];
-    for (int i = 0; i < result.size(); i++) {
-      answer[i] = result.get(i);
-    }
-    return answer;
-  }
-
-  private void helper(List<List<String>> splits, List<String> result, StringBuilder sb, int idx) {
-    if (idx >= splits.size()) {
-      if (sb.length() == splits.size()) {
-        result.add(new StringBuilder(sb.toString()).toString());
-      }
-    } else {
-      for (int i = idx; i < splits.size(); i++) {
-        List<String> currentSplit = splits.get(i);
-        for (String section : currentSplit) {
-          sb.append(section);
-          helper(splits, result, sb, i + 1);
-          sb.deleteCharAt(sb.length() - 1);
+    public String[] expand(String s) {
+        List<String> list = new ArrayList<>();
+        int idx = 0;
+        int n = s.length();
+        while (idx < n) {
+            if (s.charAt(idx) == '{') {
+                idx++;
+                StringBuilder sb = new StringBuilder();
+                while (idx < n && s.charAt(idx) != '}') {
+                    if (s.charAt(idx) == ',') {
+                        idx++;
+                        continue;
+                    }
+                    sb.append(s.charAt(idx++));
+                }
+                list.add(sb.toString());
+                idx++;
+            } else {
+                list.add(s.charAt(idx++) + "");
+            }
         }
-      }
+        List<String> result = new ArrayList<>();
+        recurse(result, list, new StringBuilder(), 0);
+        Collections.sort(result);
+        return result.toArray(new String[]{});
     }
-  }
 
-  private List<List<String>> parseInput(String s) {
-    List<List<String>> splits = new ArrayList<>();
-    int idx = 0;
-    while (idx < s.length()) {
-      if (s.charAt(idx) == '{') {
-        int endIdx = s.indexOf('}', idx);
-        String[] segements = s.substring(idx + 1, endIdx).split(",");
-        splits.add(Arrays.asList(segements));
-        idx = endIdx + 1;
-      } else {
-        splits.add(List.of(String.valueOf(s.charAt(idx++))));
-      }
+    private void recurse(List<String> result, List<String> list, StringBuilder sb, int idx) {
+        if (sb.length() == list.size()) {
+            result.add(new String(sb.toString()));
+            return;
+        }
+        for (char c : list.get(idx).toCharArray()) {
+            sb.append(c);
+            recurse(result, list, sb, idx + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
-    return splits;
-  }
 }
