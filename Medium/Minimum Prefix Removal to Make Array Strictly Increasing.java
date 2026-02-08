265e845a9b00065686b93904d43c9c515fa65522
Varun Upadhyay <varun.u28@gmail.com>
2026-02-08 12:32:19 -0800

    Create Minimum Prefix Removal to Make Array Strictly Increasing.java

---
+++ Medium/Minimum Prefix Removal to Make Array Strictly Increasing.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumPrefixLength(int[] nums) {
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] >= nums[i + 1]) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+}
