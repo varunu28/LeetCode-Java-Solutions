dda903053ec6b5a2e19a6fddd7df0c3c9de3b9cc
Varun Upadhyay <varun.u28@gmail.com>
2026-02-08 09:35:21 -0800

    Update Check if a Parentheses String Can Be Valid.java

--- Medium/Check if a Parentheses String Can Be Valid.java
+++ Medium/Check if a Parentheses String Can Be Valid.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
-        if (s.length() % 2 != 0) {
-            return false;
-        }
-        Stack<Integer> openBrackets = new Stack<>();
+        Stack<Integer> lockedIndex = new Stack<>();
         Stack<Integer> unlocked = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (locked.charAt(i) == '0') {
                 unlocked.push(i);
-            } else if (c == '(') {
-                openBrackets.push(i);
-            } else if (c == ')') {
-                if (!openBrackets.isEmpty()) {
-                    openBrackets.pop();
-                } else if (!unlocked.isEmpty()) {
-                    unlocked.pop();
+            } else {
+                if (c == '(') {
+                    lockedIndex.push(i);
                 } else {
-                    return false;
+                    if (!lockedIndex.isEmpty()) {
+                        lockedIndex.pop();
+                    } else if (!unlocked.isEmpty()) {
+                        unlocked.pop();
+                    } else {
+                        return false;
+                    }
                 }
             }
+            
         }
-        while (!openBrackets.isEmpty() && !unlocked.isEmpty() && openBrackets.peek() < unlocked.peek()) {
-            openBrackets.pop();
+        while (!lockedIndex.isEmpty()) {
+            int removed = lockedIndex.pop();
+            if (unlocked.isEmpty() || unlocked.peek() < removed) {
+                return false;
+            }
             unlocked.pop();
         }
-        return openBrackets.isEmpty();
+        return unlocked.size() % 2 == 0;
     }
 }
