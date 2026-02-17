d972752da77d356463711cc91eb2b810c381a2cb
Varun Upadhyay <varun.u28@gmail.com>
2026-02-17 09:40:36 -0800

    Update Partition Equal Subset Sum.java

--- Medium/Partition Equal Subset Sum.java
+++ Medium/Partition Equal Subset Sum.java
@@ -1,23 +1,27 @@
 class Solution {
-  public boolean canPartition(int[] nums) {
-    int sum = 0;
-    for (int num : nums) {
-      sum += num;
-    }
-    if (sum % 2 == 1) {
-      return false;
-    }
-    sum /= 2;
-    int n = nums.length;
-    boolean[] dp = new boolean[sum + 1];
-    dp[0] = true;
-    for (int num : nums) {
-      for (int i = sum; i > 0; i--) {
-        if (i >= num) {
-          dp[i] = dp[i] || dp[i - num];
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
-      }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum /= 2;
+        Boolean[][] memo = new Boolean[nums.length + 1][sum + 1];
+        return recurse(nums, 0, sum, memo);
     }
-    return dp[sum];
-  }
+
+    private boolean recurse(int[] nums, int idx, int sum, Boolean[][] memo) {
+        if (sum == 0) {
+            return true;
+        }
+        if (sum < 0 || idx == nums.length) {
+            return false;
+        }
+        if (memo[idx][sum] != null) {
+            return memo[idx][sum];
+        }
+        return memo[idx][sum] = (recurse(nums, idx + 1, sum - nums[idx], memo) || recurse(nums, idx + 1, sum, memo));
+    }
 }
