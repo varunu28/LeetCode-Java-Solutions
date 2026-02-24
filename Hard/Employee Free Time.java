6fd25efd16b61c20ba0d64b4f8a08f12320842ae
Varun Upadhyay <varun.u28@gmail.com>
2026-02-24 09:41:27 -0800

    Update Employee Free Time.java

--- Hard/Employee Free Time.java
+++ Hard/Employee Free Time.java
@@ -1,32 +1,37 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 /*
+// Definition for an Interval.
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
 */
+
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        List<Interval> ans = new ArrayList<>();
-        PriorityQueue<Interval> pq = new PriorityQueue<>((a,b) -> a.start - b.start);
-        schedule.forEach(e -> pq.addAll(e));    
-    
-        Interval temp = pq.poll();
+        int lastIntervalEnd = -1;
+        List<Interval> result = new ArrayList<>();
+        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
+        for (List<Interval> intervals : schedule) {
+            pq.addAll(intervals);
+        } 
         while (!pq.isEmpty()) {
-            if (temp.end < pq.peek().start) {
-                ans.add(new Interval(temp.end, pq.peek().start));
-                temp = pq.poll();
+            Interval removed = pq.remove();
+            if (lastIntervalEnd != -1 && removed.start - lastIntervalEnd > 0) {
+                result.add(new Interval(lastIntervalEnd, removed.start));
             }
-            else {
-                temp = temp.end < pq.peek().end ? pq.peek() : temp;
-                pq.poll();
+            int maxEnd = removed.end;
+            while (!pq.isEmpty() && pq.peek().start <= maxEnd) {
+                maxEnd = Math.max(maxEnd, pq.remove().end);
             }
+            lastIntervalEnd = maxEnd;
         }
-        
-        return ans;
-    }   
+        return result;
+    }
 }
