986fc505cde8ec89e4dc7aa7101c22c2f5c0f4b5
Varun Upadhyay <varun.u28@gmail.com>
2026-02-27 13:23:50 -0800

    Update Traffic Light Controlled Intersection.java

--- Concurrency/Traffic Light Controlled Intersection.java
+++ Concurrency/Traffic Light Controlled Intersection.java
@@ -1,26 +1,30 @@
 class TrafficLight {
 
-  private boolean greenOnRoadA;
-  private ReentrantLock lock;
-  
-  public TrafficLight() {
-    this.greenOnRoadA = true;
-    this.lock = new ReentrantLock();
-  }
+    private ReentrantLock lock;
+    private boolean isGreen;
 
-  public void carArrived(
-      int carId,           // ID of the car
-      int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
-      int direction,       // Direction of the car
-      Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
-      Runnable crossCar    // Use crossCar.run() to make car cross the intersection 
-  ) {
-    this.lock.lock();
-    if(greenOnRoadA && roadId==2 || !greenOnRoadA && roadId==1){
-      greenOnRoadA = !greenOnRoadA;
-      turnGreen.run();
+    public TrafficLight() {
+        lock = new ReentrantLock();
+        isGreen = true;
     }
-    crossCar.run();
-    this.lock.unlock();     
-  }
+    
+    public void carArrived(
+        int carId,           // ID of the car
+        int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        int direction,       // Direction of the car
+        Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
+        Runnable crossCar    // Use crossCar.run() to make car cross the intersection 
+    ) {
+        lock.lock();
+        try {
+            if (!isGreen && roadId == 1 || isGreen && roadId == 2) {
+                isGreen = !isGreen;
+                turnGreen.run();
+            }
+            crossCar.run();
+        } finally {
+            lock.unlock();
+        }
+
+    }
 }
