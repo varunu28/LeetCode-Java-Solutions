dfef5fa9cbc1907b2552e830eee1d6253abe3479
Varun Upadhyay <varun.u28@gmail.com>
2026-03-10 11:25:10 -0700

    Update Maximum Profit in Job Scheduling.java

--- Hard/Maximum Profit in Job Scheduling.java
+++ Hard/Maximum Profit in Job Scheduling.java
@@ -6,35 +6,35 @@ class Solution {
             jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-        for (int i = 0; i < n; i++) {
-            startTime[i] = jobs[i][0];
-        }
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        return findMaxProfit(jobs, startTime, 0, n, dp);
+        return findMaxProfit(jobs, 0, n, dp);
     }
 
-    private int findMaxProfit(int[][] jobs, int[] startTime, int position, int n, int[] dp) {
+    private int findMaxProfit(int[][] jobs, int position, int n, int[] dp) {
         if (position == n) {
             return 0;
         }
         if (dp[position] != -1) {
             return dp[position];
         }
-        int nextPosition = binarySearch(startTime, jobs[position][1]);
+        int nextPosition = binarySearch(jobs, jobs[position][1]);
         return dp[position] = Math.max(
-            findMaxProfit(jobs, startTime, position + 1, n, dp), // skip executing current job
-            jobs[position][2] + findMaxProfit(jobs, startTime, nextPosition, n, dp) // execute current job & move to next position
+            findMaxProfit(jobs, position + 1, n, dp), // skip executing current job
+            jobs[position][2] + findMaxProfit(jobs, nextPosition, n, dp) // execute current job & move to next position
         );
     }
 
-    private static int binarySearch(int[] startTime, int endTime) {
+    /**
+    Binary search to figure out which job to start after the current job finishes
+    */
+    private static int binarySearch(int[][] jobs, int endTime) {
         int left = 0;
-        int right = startTime.length - 1;
-        int nextPosition = startTime.length;
+        int right = jobs.length - 1;
+        int nextPosition = jobs.length;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (startTime[mid] >= endTime) {
+            if (jobs[mid][0] >= endTime) {
                 nextPosition = mid;
                 right = mid - 1;
             } else {
