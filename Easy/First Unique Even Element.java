daa55c4a24548d5dcfe08fab26651a4b49206bd2
Varun Upadhyay <varun.u28@gmail.com>
2026-03-14 16:05:45 -0700

    Create First Unique Even Element.java

---
+++ Easy/First Unique Even Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
