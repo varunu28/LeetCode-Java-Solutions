16d3e02f61d160c9c7f5a409009db83f0cfd2dbf
Varun Upadhyay <varun.u28@gmail.com>
2026-03-21 13:58:00 -0700

    Update Optimal Account Balancing.java

--- Hard/Optimal Account Balancing.java
+++ Hard/Optimal Account Balancing.java
@@ -1,73 +1,38 @@
 class Solution {
-    int min = Integer.MAX_VALUE;
     public int minTransfers(int[][] transactions) {
-        Map<Integer, Integer> account = new HashMap<>();
+        Map<Integer, Integer> creditMap = new HashMap<>();
         for (int[] transaction : transactions) {
+            int from = transaction[0];
+            int to = transaction[1];
             int amount = transaction[2];
-            account.put(transaction[0], account.getOrDefault(transaction[0], 0) + amount);
-            account.put(transaction[1], account.getOrDefault(transaction[1], 0) - amount);
+            creditMap.put(from, creditMap.getOrDefault(from, 0) - amount);
+            creditMap.put(to, creditMap.getOrDefault(to, 0) + amount);
         }
-        
-        LinkedList<Integer> posBalance = new LinkedList<>();
-        LinkedList<Integer> negBalance = new LinkedList<>();
-        
-        for (Integer key : account.keySet()) {
-            if (account.get(key) < 0) {
-                negBalance.add(account.get(key));
-            }
-            else if (account.get(key) > 0) {
-                posBalance.add(account.get(key));
+        List<Integer> creditList = new ArrayList<>();
+        for (Integer credit : creditMap.values()) {
+            if (credit != 0) {
+                creditList.add(credit);
             }
         }
-        
-        dfs(posBalance, negBalance, 0);
-        return min;
+        int n = creditList.size();
+        return dfs(0, n, creditList);
     }
-    
-    private void dfs(List<Integer> posBalance, List<Integer> negBalance, int count) {
-        if (posBalance.size() == 0 || negBalance.size() == 0) {
-            min = Math.min(count, min);
-            return;
-        }
-        
-        if (count >= min) {
-            return;
-        }
-        
-        int posAmount = posBalance.get(0);
-        
-        for(int j = 0; j < negBalance.size(); j++) {
-            int negAmount = negBalance.get(j);
-            int newPositiveVal = Math.max(posAmount + negAmount, 0);
-            int newNegativeVal = Math.min(0, posAmount + negAmount);
-            if (newPositiveVal == 0){
-                posBalance.remove(0);
-            } 
-            else {
-                posBalance.set(0, newPositiveVal);
-            }
-            if (newNegativeVal == 0){
-                negBalance.remove(j);
-            }
-            else{
-                negBalance.set(j, newNegativeVal);
-            }
 
-            dfs(posBalance, negBalance, count + 1);
-            
-            if(newPositiveVal == 0){
-                posBalance.add(0, posAmount);
-            }
-            else {
-                posBalance.set(0, posAmount);
-            }
-            
-            if(newNegativeVal == 0){
-                negBalance.add(j, negAmount);
-            }
-            else{
-                negBalance.set(j, negAmount);
+    private int dfs(int curr, int n, List<Integer> creditList) {
+        while (curr < n && creditList.get(curr) == 0) {
+            curr++;
+        }
+        if (curr == n) {
+            return 0;
+        }
+        int transactions = Integer.MAX_VALUE;
+        for (int next = curr + 1; next < n; next++) {
+            if (creditList.get(next) * creditList.get(curr) < 0) {
+                creditList.set(next, creditList.get(next) + creditList.get(curr));
+                transactions = Math.min(transactions, 1 + dfs(curr + 1, n, creditList));
+                creditList.set(next, creditList.get(next) - creditList.get(curr));
             }
         }
+        return transactions;
     }
 }
