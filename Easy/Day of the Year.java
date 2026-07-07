0e8245c4936c2f18d980e7bd474cdb4afde13047
Varun Upadhyay <varun.u28@gmail.com>
2026-07-07 07:03:26 +0100

    Update Day of the Year.java

--- Easy/Day of the Year.java
+++ Easy/Day of the Year.java
@@ -1,37 +1,24 @@
 class Solution {
-    List<Integer> daysInMonth = Arrays.asList(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+    private static final int[] DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     public int dayOfYear(String date) {
-        int year = Integer.parseInt(date.split("-")[0]);
-        int month = Integer.parseInt(date.split("-")[1]);
-        int day = Integer.parseInt(date.split("-")[2]);
-        
-        int numOfDays = getNumOfDays(month, isLeapYear(year));
-        
-        return numOfDays + day;
-    }
-    
-    private boolean isLeapYear(int year) {
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                return year % 400 == 0;
-            }
-            else {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    private int getNumOfDays(int month, boolean isLeapYear) {
-        int numOfDays = 0;
+        String[] splits = date.split("-");
+        int year = Integer.parseInt(splits[0]);
+        int month = Integer.parseInt(splits[1]);
+        int day = Integer.parseInt(splits[2]);
+        int days = 0;
         for (int i = 1; i < month; i++) {
-            if (i == 2) {
-                numOfDays += isLeapYear ? daysInMonth.get(i - 1) + 1 : daysInMonth.get(i - 1);            
-            } 
-            else {
-                numOfDays += daysInMonth.get(i - 1);
+            days += DAYS_IN_MONTH[i - 1];
+            if (i == 2 && isLeapYear(year)) {
+                days++;
             }
         }
-        return numOfDays;
+        days += day;
+        return days;
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
