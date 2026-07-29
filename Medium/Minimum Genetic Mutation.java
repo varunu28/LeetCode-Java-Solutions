aa2d23f302ab4fcdf5e66618dd790ce7b6e935f8
Varun Upadhyay <varun.u28@gmail.com>
2026-07-29 07:21:52 +0100

    Update Minimum Genetic Mutation.java

--- Medium/Minimum Genetic Mutation.java
+++ Medium/Minimum Genetic Mutation.java
@@ -1,45 +1,36 @@
 class Solution {
-    public int minMutation(String start, String end, String[] bank) {
-        if (start.equals(end)) {
-            return 0;
+
+    private static final char[] CHOICES = {'A', 'C', 'G', 'T'};
+
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Set<String> validMutations = new HashSet<>();
+        for (String mutation : bank) {
+            validMutations.add(mutation);
         }
-        
-        Set<String> bankSet = new HashSet<>(Arrays.asList(bank));
-        
-        char[] charSet = {'A', 'C', 'G', 'T'};
-        int currLevel = 0;
-        Set<String> visited = new HashSet<>();
         Queue<String> queue = new LinkedList<>();
-        
-        queue.add(start);
-        visited.add(start);
-        
+        Set<String> visited = new HashSet<>();
+        queue.add(startGene);
+        visited.add(startGene);
+        int count = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             while (size-- > 0) {
-                String curr = queue.remove();
-                if (curr.equals(end)) {
-                    return currLevel;
+                String node = queue.remove();
+                if (node.equals(endGene)) {
+                    return count;
                 }
-                
-                char[] currCharArr = curr.toCharArray();
-                for (int i = 0; i < currCharArr.length; i++) {
-                    char old = currCharArr[i];
-                    for (char c : charSet) {
-                        currCharArr[i] = c;
-                        String newGene = String.valueOf(currCharArr);
-                        if (!visited.contains(newGene) && bankSet.contains(newGene)) {
-                            queue.add(newGene);
-                            visited.add(newGene);
+                for (char choice : CHOICES) {
+                    for (int i = 0; i < node.length(); i++) {
+                        String nextNode = node.substring(0, i) + choice + node.substring(i + 1);
+                        if (validMutations.contains(nextNode) && !visited.contains(nextNode)) {
+                            queue.add(nextNode);
+                            visited.add(nextNode);
                         }
                     }
-                    currCharArr[i] = old;
                 }
             }
-            
-            currLevel++;
+            count++;
         }
-        
         return -1;
     }
 }
