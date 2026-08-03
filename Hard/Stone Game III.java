17cce5eb934167fc1dd67801d7dc4993c89f7580
Varun Upadhyay <varun.u28@gmail.com>
2026-08-03 06:58:25 +0100

    Update Stone Game III.java

--- Hard/Stone Game III.java
+++ Hard/Stone Game III.java
@@ -1,16 +1,28 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
-        int[] dp = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = stoneValue[i] - dp[i + 1];
-            if (i + 2 <= n) {
-                dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
-            }
-            if (i + 3 <= n) {
-                dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3]);
-            }
+        Integer[] dp = new Integer[n];
+        int finalScore = score(stoneValue, 0, dp);
+        return finalScore == 0 ? "Tie" : (finalScore > 0 ? "Alice" : "Bob");
+    }
+
+    private int score(int[] stoneValue, int idx, Integer[] dp) {
+        if (idx >= stoneValue.length) {
+            return 0;
         }
-        return dp[0] == 0 ? "Tie" : (dp[0] > 0 ? "Alice" : "Bob");
+        if (dp[idx] != null) {
+            return dp[idx];
+        }
+        int takeOne = Integer.MIN_VALUE;
+        int takeTwo = Integer.MIN_VALUE;
+        int takeThree = Integer.MIN_VALUE;
+        takeOne = stoneValue[idx] - score(stoneValue, idx + 1, dp);
+        if (idx + 1 < stoneValue.length) {
+            takeTwo = stoneValue[idx] + stoneValue[idx + 1] - score(stoneValue, idx + 2, dp);
+        }
+        if (idx + 2 < stoneValue.length) {
+            takeThree = stoneValue[idx] + stoneValue[idx + 1] + stoneValue[idx + 2] - score(stoneValue, idx + 3, dp);
+        }
+        return dp[idx] = Math.max(takeOne, Math.max(takeTwo, takeThree));
     }
 }
